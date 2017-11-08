ab9e6f3f1b384bfa8b3a76629eb5b27067053665
seaofocean <xianyan.jia@intel.com>
2017-11-08 13:16:49 +0800

    add read write

--- transform/vision/src/main/scala/com/intel/analytics/zoo/transform/vision/image/Types.scala
+++ transform/vision/src/main/scala/com/intel/analytics/zoo/transform/vision/image/Types.scala
@@ -16,18 +16,27 @@
 
 package com.intel.analytics.zoo.transform.vision.image
 
+import java.io.File
+import java.nio.file.Paths
+
+import com.google.common.io.Files
 import com.intel.analytics.bigdl.dataset.{ChainedTransformer, Transformer}
 import com.intel.analytics.bigdl.tensor.{Storage, Tensor}
 import com.intel.analytics.bigdl.utils.RandomGenerator._
-import com.intel.analytics.bigdl.utils.T
+import com.intel.analytics.bigdl.utils.{Engine, T}
 import com.intel.analytics.zoo.transform.vision.image.opencv.OpenCVMat
+import org.apache.commons.io.FileUtils
 import org.apache.log4j.Logger
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 import scala.collection.{Iterator, Set, mutable}
 import scala.reflect.ClassTag
 
 class ImageFeature extends Serializable {
+
   import ImageFeature.logger
+
   def this(bytes: Array[Byte], label: Any = null, uri: String = null) {
     this
     state(ImageFeature.bytes) = bytes
@@ -90,7 +99,7 @@ class ImageFeature extends Serializable {
 
 
   def copyTo(storage: Array[Float], offset: Int, floatKey: String = ImageFeature.floats,
-             toRGB: Boolean = true): Unit = {
+    toRGB: Boolean = true): Unit = {
     require(contains(floatKey), s"there should be ${floatKey} in ImageFeature")
     val data = getFloats(floatKey)
     require(data.length >= getWidth() * getHeight() * 3,
@@ -117,6 +126,7 @@ class ImageFeature extends Serializable {
 
   /**
    * Convert ImageFeature to image tensor
+   *
    * @param floatKey key that maps the float array
    * @param toChw transpose the image from hwc to chw
    * @return tensor that represents an image
@@ -156,16 +166,25 @@ object ImageFeature {
   val cropBbox = "cropBbox"
   val expandBbox = "expandBbox"
 
-  def apply(bytes: Array[Byte], label: Any = null, path: String = null)
-  : ImageFeature = new ImageFeature(bytes, label, path)
+  def apply(bytes: Array[Byte], label: Any = null, uri: String = null)
+  : ImageFeature = new ImageFeature(bytes, label, uri)
 
   def apply(): ImageFeature = new ImageFeature()
+
+  // todo: convert label
+  def fromRow(row: Row): ImageFeature = {
+    val uri = row.getAs[String](ImageFeature.uri)
+    val image = row.getAs[Array[Byte]](ImageFeature.bytes)
+    ImageFeature(image, uri = uri)
+  }
 
   val logger = Logger.getLogger(getClass)
 }
 
 abstract class FeatureTransformer() extends Transformer[ImageFeature, ImageFeature] {
+
   import FeatureTransformer.logger
+
   private var outKey: Option[String] = None
 
   def setOutKey(key: String): this.type = {
@@ -205,13 +224,13 @@ abstract class FeatureTransformer() extends Transformer[ImageFeature, ImageFeatu
 
   // scalastyle:off methodName
   // scalastyle:off noSpaceBeforeLeftBracket
-  def -> (other: FeatureTransformer): FeatureTransformer = {
+  def ->(other: FeatureTransformer): FeatureTransformer = {
     new ChainedFeatureTransformer(this, other)
   }
 
   // scalastyle:off methodName
   // scalastyle:off noSpaceBeforeLeftBracket
-  override def -> [C](other: Transformer[ImageFeature, C]): Transformer[ImageFeature, C] = {
+  override def ->[C](other: Transformer[ImageFeature, C]): Transformer[ImageFeature, C] = {
     new ChainedTransformer(this, other)
   }
 }
@@ -249,7 +268,40 @@ object RandomTransformer {
 }
 
 object Image {
-//  def read(): ImageFrame = {
-//
-//  }
+  def read(path: String, sc: SparkContext): DistributedImageFrame = {
+    val images = sc.binaryFiles(path).map { case (p, stream) =>
+      ImageFeature(stream.toArray(), uri = p)
+    }
+    ImageFrame.rdd(images)
+  }
+
+  def read(path: String): LocalImageFrame = {
+    val dir = new File(path)
+    require(dir.exists(), s"$path not exists!")
+    val images = dir.listFiles().map { p =>
+      ImageFeature(FileUtils.readFileToByteArray(p), uri = p.getAbsolutePath)
+    }
+    ImageFrame.array(images)
+  }
+
+  def readSequenceFile(path: String, sc: SparkSession): DistributedImageFrame = {
+    val df = sc.sqlContext.read.parquet(path)
+    val images = df.rdd.map(row => {
+      ImageFeature.fromRow(row)
+    })
+    ImageFrame.rdd(images)
+  }
+
+  def writeSequenceFile(path: String, output: String, spark: SparkSession): Unit = {
+    import spark.implicits._
+    spark.sparkContext.binaryFiles(path).toDF.write.parquet(output)
+  }
+
+
+  def main(args: Array[String]) {
+    val conf = Engine.createSparkConf().setAppName("BigDL SSD Demo")
+    val sc = new SparkContext(conf)
+    Engine.init
+    read("/home/jxy/data/testdata/test")
+  }
 }
