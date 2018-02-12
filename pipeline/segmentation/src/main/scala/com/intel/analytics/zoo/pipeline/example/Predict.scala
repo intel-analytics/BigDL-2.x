78bd54aef53c5df52567a266a8396e3515e869bd
seaofocean <xianyan.jia@intel.com>
2018-02-12 14:30:39 +0800

    Add header

--- pipeline/segmentation/src/main/scala/com/intel/analytics/zoo/pipeline/example/Predict.scala
+++ pipeline/segmentation/src/main/scala/com/intel/analytics/zoo/pipeline/example/Predict.scala
@@ -1,22 +1,32 @@
+/*
+ * Copyright 2016 The BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.intel.analytics.zoo.pipeline.example
 
 import java.nio.file.Paths
 
-import com.intel.analytics.bigdl.dataset.PaddingParam
 import com.intel.analytics.bigdl.nn.Module
 import com.intel.analytics.bigdl.transform.vision.image.{ImageFeature, ImageFrame, ImageFrameToSample, MatToTensor}
 import com.intel.analytics.bigdl.utils.{Engine, File}
 import com.intel.analytics.bigdl.numeric.NumericFloat
-import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.bigdl.transform.vision.image.augmentation.{AspectScale, ChannelNormalize, FixExpand}
-import com.intel.analytics.bigdl.transform.vision.image.opencv.OpenCVMat
 import com.intel.analytics.zoo.pipeline.utils._
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkContext
-import org.opencv.imgcodecs.Imgcodecs
 import scopt.OptionParser
-
-import scala.io.Source
 
 object Predict {
   Logger.getLogger("org").setLevel(Level.ERROR)
--- pipeline/segmentation/src/main/scala/com/intel/analytics/zoo/pipeline/pythonapi/PythonSegmentation.scala
+++ pipeline/segmentation/src/main/scala/com/intel/analytics/zoo/pipeline/pythonapi/PythonSegmentation.scala
@@ -1,8 +1,23 @@
+/*
+ * Copyright 2016 The BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.intel.analytics.zoo.pipeline.pythonapi
 
 import java.util.{Map => JMap}
 
-import com.intel.analytics.bigdl.Module
 import com.intel.analytics.bigdl.python.api.PythonBigDL
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
 import com.intel.analytics.bigdl.transform.vision.image.{BytesToMat, FeatureTransformer, MatToFloats}
@@ -38,9 +53,5 @@ class PythonSegmentation[T: ClassTag](implicit ev: TensorNumeric[T]) extends Pyt
 
   def readCocoLabelMap(): JMap[Int, String] = {
     LabelReader.readCocoLabelMap().asJava
-  }
-
-  def shareMemory(model: Module[T]): Unit = {
-    ModuleUtil.shareMemory(model.asInstanceOf[Module[Float]])
   }
 }
