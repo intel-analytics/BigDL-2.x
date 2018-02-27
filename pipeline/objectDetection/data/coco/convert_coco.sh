f0f72d7e869b4d82940fdcb5030ab5dcffe0c439
Xianyan <SeaOfOcean@users.noreply.github.com>
2018-02-27 15:29:42 +0800

    fix convert doc (#111)

--- pipeline/objectDetection/data/coco/convert_coco.sh
+++ pipeline/objectDetection/data/coco/convert_coco.sh
@@ -9,21 +9,21 @@ if [ ! -d "${data_root}/seq/train" ]; then
   mkdir -p ${data_root}/seq/test
 fi
 
-java -cp ${ssd_root}/target/pipeline-0.1-SNAPSHOT-jar-with-dependencies-and-spark.jar \
+java -cp ${ssd_root}/dist/target/object-detection-0.1-SNAPSHOT-jar-with-dependencies-and-spark.jar \
  com.intel.analytics.zoo.pipeline.common.dataset.RoiImageSeqGenerator \
  -i coco_minival -f ${data_root} -o ${data_root}/seq/coco-minival -p 28
 
 rm ${data_root}/seq/coco-minival/.*crc
 echo 'convert minival data done'
 
-java -cp ${ssd_root}/target/pipeline-0.1-SNAPSHOT-jar-with-dependencies-and-spark.jar \
+java -cp ${ssd_root}/dist/target/object-detection-0.1-SNAPSHOT-jar-with-dependencies-and-spark.jar \
  com.intel.analytics.zoo.pipeline.common.dataset.RoiImageSeqGenerator \
  -i coco-testdev -f ${data_root} -o ${data_root}/seq/coco-testdev -p 28
 
 rm ${data_root}/seq/coco-testdev/.*crc
 echo 'convert testdev data done'
 
-java -cp ${ssd_root}/target/pipeline-0.1-SNAPSHOT-jar-with-dependencies-and-spark.jar \
+java -cp ${ssd_root}/dist/target/object-detection-0.1-SNAPSHOT-jar-with-dependencies-and-spark.jar \
          com.intel.analytics.zoo.pipeline.common.dataset.RoiImageSeqGenerator \
       -i voc_0712_trainval -f ${data_root} -o ${data_root}/seq/train -p 28
 
--- pipeline/objectDetection/data/pascal/convert_pascal.sh
+++ pipeline/objectDetection/data/pascal/convert_pascal.sh
@@ -9,14 +9,14 @@ if [ ! -d "${data_root}/seq/train" ]; then
   mkdir -p ${data_root}/seq/test
 fi
 
-java -cp ${ssd_root}/target/pipeline-0.1-SNAPSHOT-jar-with-dependencies-and-spark.jar \
+java -cp ${ssd_root}/dist/target/object-detection-0.1-SNAPSHOT-jar-with-dependencies-and-spark.jar \
          com.intel.analytics.zoo.pipeline.common.dataset.RoiImageSeqGenerator \
      -f ${data_root}/VOCdevkit -o ${data_root}/seq/test -i voc_2007_test -p 28
 
 rm ${data_root}/seq/test/.*crc
 echo 'convert test data done'
 
-java -cp ${ssd_root}/target/pipeline-0.1-SNAPSHOT-jar-with-dependencies-and-spark.jar \
+java -cp ${ssd_root}/dist/target/object-detection-0.1-SNAPSHOT-jar-with-dependencies-and-spark.jar \
          com.intel.analytics.zoo.pipeline.common.dataset.RoiImageSeqGenerator \
      -f ${data_root}/VOCdevkit -o ${data_root}/seq/train -i voc_0712_trainval -p 28
 
