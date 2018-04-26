462926df84ab0fa8c4c1602d66f70c9086b8df38
Yuhao Yang <yuhao.yang@intel.com>
2018-04-25 18:05:43 -0700

    scala for estimator

---
+++ zoo/src/main/scala/com/intel/analytics/zoo/pipeline/nnframes/transformers/FeatureLabelTransformer.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2018 Analytics Zoo Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.analytics.zoo.pipeline.nnframes.transformers
+
+import com.intel.analytics.bigdl.dataset.{Sample, Transformer}
+import com.intel.analytics.bigdl.tensor.Tensor
+import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
+
+import scala.reflect.ClassTag
+
+private[nnframes] class FeatureLabelTransformer[F, L, T: ClassTag] (
+    featureTransfomer: Transformer[F, Tensor[T]],
+    labelTransformer: Transformer[L, Tensor[T]])(implicit ev: TensorNumeric[T])
+  extends Transformer[(F, L), Sample[T]] {
+
+  override def apply(prev: Iterator[(F, L)]): Iterator[Sample[T]] = {
+    val outputFeatures = featureTransfomer.apply(prev.map(_._1))
+    val outputLabels = labelTransformer.apply(prev.map(_._2))
+    val samples = outputFeatures.zip(outputLabels).map { case (featureTensor, labelTensor) =>
+      Sample[T](featureTensor, labelTensor)
+    }
+    samples
+  }
+}
