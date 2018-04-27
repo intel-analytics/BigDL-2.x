7d2077513ea6f16024453b1a8b8302f9dccf132f
Yuhao Yang <yuhao.yang@intel.com>
2018-04-26 18:11:22 -0700

    python support and image support

---
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/nnframes/transformers/RowToImageFeature.scala
@@ -0,0 +1,24 @@
+package com.intel.analytics.zoo.pipeline.nnframes.transformers
+
+import com.intel.analytics.bigdl.dataset.Transformer
+import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
+import com.intel.analytics.bigdl.transform.vision.image.ImageFeature
+import com.intel.analytics.zoo.pipeline.nnframes.NNImageSchema
+import org.apache.spark.sql.Row
+
+import scala.reflect.ClassTag
+
+
+class RowToImageFeature [T: ClassTag]()(implicit ev: TensorNumeric[T])
+  extends Transformer[Row, ImageFeature] {
+
+  override def apply(prev: Iterator[Row]): Iterator[ImageFeature] = {
+    prev.map { row =>
+      NNImageSchema.row2IMF(row)
+    }
+  }
+}
+
+object RowToImageFeature {
+  def apply[T: ClassTag]()(implicit ev: TensorNumeric[T]) = new RowToImageFeature[T]()
+}
