9e6e3b41a923aa5585a32728c4a5d6ce482ab224
Yuhao Yang <yuhao.yang@intel.com>
2018-04-30 11:56:40 -0700

    comments and example

--- nn_transformers.py
+++ nn_transformers.py
@@ -36,38 +36,68 @@ class ChainedTransformer(NNTransformer):
     """
     Pipeline of FeatureTransformer
     """
-
     def __init__(self, transformers, bigdl_type="float"):
         for transfomer in transformers:
-            assert transfomer.__class__.__bases__[0].__name__ == "NNTransformer", \
-                str(transfomer) + " should be subclass of NNTransformer"
+            assert transfomer.__class__.__bases__[0].__name__ in [
+                "Transformer", "NNTransformer", "FeatureTransformer"], \
+                str(transfomer) + " should be subclass of Transformer or FeatureTransformer"
         super(ChainedTransformer, self).__init__(bigdl_type, transformers)
 
+
 class NumToTensor(NNTransformer):
+    """
+    a Transformer that converts a number to a Tensor.
+    """
     def __init__(self, bigdl_type="float"):
         super(NumToTensor, self).__init__(bigdl_type)
 
 class SeqToTensor(NNTransformer):
+    """
+    a Transformer that converts an Array[_] or Seq[_] to a Tensor.
+    :param size dimensions of target Tensor.
+    """
     def __init__(self, size, bigdl_type="float"):
         super(SeqToTensor, self).__init__(bigdl_type, size)
 
+
 class MLlibVectorToTensor(NNTransformer):
+    """
+    a Transformer that converts MLlib Vector to a Tensor.
+    :param size dimensions of target Tensor.
+    """
     def __init__(self, size, bigdl_type="float"):
         super(MLlibVectorToTensor, self).__init__(bigdl_type, size)
 
+
 class ImageFeatureToTensor(NNTransformer):
+    """
+    a Transformer that convert ImageFeature to a Tensor.
+    """
     def __init__(self, bigdl_type="float"):
         super(ImageFeatureToTensor, self).__init__(bigdl_type)
 
 class RowToImageFeature(NNTransformer):
+    """
+    a Transformer that converts a Spark Row to a BigDL ImageFeature.
+    """
     def __init__(self, bigdl_type="float"):
         super(RowToImageFeature, self).__init__(bigdl_type)
 
 class FeatureLabelTransformer(NNTransformer):
+    """
+    construct a Transformer that convert (Feature, Label) tuple to a Sample.
+    The returned Transformer is robust for the case label = null, in which the
+    Sample is derived from Feature only.
+    :param feature_transformer transformer for feature, transform F to Tensor[T]
+    :param label_transformer transformer for label, transform L to Tensor[T]
+    """
     def __init__(self, feature_transformer, label_transformer, bigdl_type="float"):
         super(FeatureLabelTransformer, self).__init__(bigdl_type, feature_transformer, label_transformer)
 
 class TensorToSample(NNTransformer):
+    """
+     a Transformer that converts Tensor to Sample.
+    """
     def __init__(self, bigdl_type="float"):
         super(TensorToSample, self).__init__(bigdl_type)
         
