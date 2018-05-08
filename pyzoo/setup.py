5d5a540829d2e9d3a099b1af32d8723bb4eeb29f
Calculuser <alvin_zuo@sjtu.edu.cn>
2018-05-08 10:58:32 +0800

    Add release scripts.

--- pyzoo/setup.py
+++ pyzoo/setup.py
@@ -15,3 +15,131 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+import os
+import sys
+from shutil import copyfile, copytree, rmtree
+
+from setuptools import setup
+
+TEMP_PATH = "zoo/share"
+zoo_home = os.path.abspath(__file__ + "/../../")
+
+try:
+    exec(open('zoo/version.py').read())
+except IOError:
+    print("Failed to load zoo version file for packaging. You must be in zoo's pyzoo dir.")
+    sys.exit(-1)
+
+VERSION = __version__
+
+building_error_msg = """
+If you are packing python API from zoo source, you must build zoo first
+and run sdist.
+    To build zoo with maven you can run:
+      cd $ZOO_HOME
+      ./make-dist.sh
+    Building the source dist is done in the Python directory:
+      cd pyzoo
+      python setup.py sdist
+      pip install dist/*.tar.gz"""
+
+def build_from_source():
+    code_path = zoo_home + "/pyzoo/zoo/common/nncontext.py"
+    print("Checking: %s to see if build from source" % code_path)
+    if os.path.exists(code_path):
+        return True
+    return False
+
+
+def init_env():
+    if build_from_source():
+        print("Start to build distributed package")
+        print("HOME OF ZOO: " + zoo_home)
+        dist_source = zoo_home + "/dist"
+        if not os.path.exists(dist_source):
+            print(building_error_msg)
+            sys.exit(-1)
+        if os.path.exists(TEMP_PATH):
+            rmtree(TEMP_PATH)
+        copytree(dist_source, TEMP_PATH)
+        copyfile(zoo_home + "/pyzoo/zoo/models/__init__.py", TEMP_PATH + "/__init__.py")
+    else:
+        print("Do nothing for release installation")
+
+
+def setup_package():
+    metadata = dict(
+        name='zoo',
+        version=VERSION,
+        description='A distributed analytics library for Apache Spark',
+        author='zoo Authors',
+        author_email='zoo-user-group@googlegroups.com',
+        license='Apache License, Version 2.0',
+        url='https://github.com/intel-analytics/zoo',
+        packages=['zoo',
+                  'zoo.common',
+                  'zoo.examples',
+                  'zoo.examples.objectdetection',
+                  'zoo.examples.textclassification',
+                  'zoo.feature',
+                  'zoo.feature.image',
+                  'zoo.models',
+                  'zoo.models.common',
+                  'zoo.models.image',
+                  'zoo.models.image.common',
+                  'zoo.models.image.objectdetection',
+                  'zoo.models.recommendation',
+                  'zoo.models.textclassification',
+                  'zoo.pipeline',
+                  'zoo.pipeline.api',
+                  'zoo.pipeline.api.keras',
+                  'zoo.pipeline.api.keras.engine',
+                  'zoo.pipeline.api.keras.layers',
+                  'zoo.pipeline.api.keras.metrics',
+                  'zoo.pipeline.nnframes',
+                  'zoo.share',
+                  'bigdl',
+                  'bigdl.dataset',
+                  'bigdl.nn',
+                  'bigdl.nn.keras',
+                  'bigdl.transform',
+                  'bigdl.transform.vision',
+                  'bigdl.keras',
+                  'bigdl.examples',
+                  'bigdl.examples.keras',
+                  'bigdl.models',
+                  'bigdl.models.lenet',
+                  'bigdl.models.local_lenet',
+                  'bigdl.models.ml_pipeline',
+                  'bigdl.models.rnn',
+                  'bigdl.models.textclassifier',
+                  'bigdl.optim',
+                  'bigdl.util'],
+        install_requires=['numpy>=1.7', 'six>=1.10.0'],
+        dependency_links=['https://d3kbcqa49mib13.cloudfront.net/spark-2.0.0-bin-hadoop2.7.tgz'],
+        include_package_data=True,
+        package_dir={"bigdl": '../backend/bigdl/pyspark/bigdl' "zoo.share": TEMP_PATH},
+        package_data={"zoo.share": ['lib/*', 'conf/*', 'bin/*']},
+        classifiers=[
+            'License :: OSI Approved :: Apache Software License',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: Implementation :: CPython'],
+        platforms=['mac', 'linux']
+    )
+
+    setup(**metadata)
+
+
+if __name__ == '__main__':
+    try:
+        init_env()
+        setup_package()
+    except Exception as e:
+        raise e
+    finally:
+        if build_from_source() and os.path.exists(TEMP_PATH):
+             rmtree(TEMP_PATH)
---
+++ pyzoo/test/dev/release/release.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+#
+# Copyright 2018 Analytics Zoo Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+RUN_SCRIPT_DIR=$(cd $(dirname $0) ; pwd)
+echo $RUN_SCRIPT_DIR
+ZOO_DIR="$(cd ${RUN_SCRIPT_DIR}/../../../../; pwd)"
+echo $ZOO_DIR
+ZOO_PYTHON_DIR="$(cd ${RUN_SCRIPT_DIR}/../../../../pyzoo; pwd)"
+echo $ZOO_PYTHON_DIR
+
+if (( $# < 2)); then
+  echo "Bad parameters. Usage: release.sh mac spark_2.x"
+  exit -1
+fi
+
+platform=$1
+spark_profile=$2
+quick=$3
+input_version=$4
+zoo_version=$(python -c "exec(open('$ZOO_DIR/pyzoo/zoo/version.py').read()); print(__version__)")
+
+if [ "$input_version" != "$zoo_version" ]; then
+   echo "Not the proposed version"
+   exit -1
+fi
+
+cd ${ZOO_DIR}
+if [ "$platform" ==  "mac" ]; then
+    echo "Building zoo for mac system"
+    dist_profile="-P mac -P $spark_profile"
+    verbose_pname="macosx_10_11_x86_64"
+elif [ "$platform" == "linux" ]; then
+    echo "Building zoo for linux system"
+    dist_profile="-P $spark_profile"
+    verbose_pname="manylinux1_x86_64"
+else
+    echo "unsupport platform"
+fi
+
+zoo_build_command="${ZOO_DIR}/make-dist.sh ${dist_profile}"
+if [ "$quick" == "true" ]; then
+    echo "Skip disting ZOO"
+else
+    echo "Dist ZOO: $zoo_build_command"
+    $zoo_build_command
+fi
+
+cd $ZOO_PYTHON_DIR
+sdist_command="python setup.py sdist"
+echo "packing source code: ${sdist_command}"
+$sdist_command
+
+if [ -d "${ZOO_DIR}/pyzoo/build" ]; then
+   rm -r ${ZOO_DIR}/pyzoo/build
+fi
+
+if [ -d "${ZOO_DIR}/pyzoo/dist" ]; then
+   rm -r ${ZOO_DIR}/pyzoo/dist
+fi
+wheel_command="python setup.py bdist_wheel --plat-name ${verbose_pname}"
+echo "Packing python distribution:   $wheel_command"
+${wheel_command}
+
+upload_command="twine upload dist/zoo-${zoo_version}-py2.py3-none-${verbose_pname}.whl"
+echo "Please manually upload with this command:  $upload_command"
+
+$upload_command
