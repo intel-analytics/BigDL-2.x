a95f80a8d432ee218ce8991bed0be7330636d412
jenniew <jenniewang123@gmail.com>
2018-05-15 00:35:40 -0700

    update tag check

--- zoo/src/main/scala/com/intel/analytics/zoo/models/image/common/ImageConfigure.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/models/image/common/ImageConfigure.scala
@@ -57,14 +57,14 @@ object ImageConfigure {
    */
   def parse[T: ClassTag](tag: String)(implicit ev: TensorNumeric[T]): ImageConfigure[T] = {
     val splits = tag.split(splitter)
-    require(splits.length >= 4, s"tag ${tag}" +
-      s" needs at least 4 elements, publisher, model, dataset, version")
     if (!splits(0).equalsIgnoreCase("analytics-zoo")) {
       logger.warn("Only support to create default image configuration for models published by " +
         "analytics zoo. Third-party models need to pass its own image configuration during " +
         "prediction")
       null
     } else {
+      require(splits.length >= 4, s"tag ${tag}" +
+        s" needs at least 4 elements, publisher, model, dataset, version")
       val model = splits(1)
       val dataset = splits(2)
       val version = splits(3)
