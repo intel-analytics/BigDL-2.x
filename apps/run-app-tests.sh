77f5e3617a36438778c282c096b24dd1fa810444
changlinzhang <changlin.zhang@intel.com>
2018-05-15 14:14:49 +0800

    modify test script

--- apps/run-app-tests.sh
+++ apps/run-app-tests.sh
@@ -11,6 +11,25 @@ export ANALYTICS_ZOO_CONF=${ANALYTICS_ZOO_HOME_DIST}/conf/spark-bigdl.conf
 export PYTHONPATH=${ANALYTICS_ZOO_PYZIP}:$PYTHONPATH
 
 chmod +x ./apps/ipynb2py.sh
+
+./apps/ipynb2py.sh ./apps/image-augmentation/image-augmentation
+echo "#3 start app test for image-augmentation"
+FILENAME="$ANALYTICS_ZOO_HOME/apps/image-augmentation/image-augmentation"
+${SPARK_HOME}/bin/spark-submit \
+        --master ${MASTER} \
+        --driver-cores 2  \
+        --driver-memory 1g  \
+        --total-executor-cores 2  \
+        --executor-cores 2  \
+        --executor-memory 1g \
+        --conf spark.akka.frameSize=64 \
+        --py-files ${ANALYTICS_ZOO_PYZIP},${ANALYTICS_ZOO_HOME}/apps/image-augmentation/image-augmentation.py  \
+        --properties-file ${ANALYTICS_ZOO_CONF} \
+        --jars ${ANALYTICS_ZOO_JAR} \
+        --conf spark.driver.extraClassPath=${ANALYTICS_ZOO_JAR} \
+        --conf spark.executor.extraClassPath=${ANALYTICS_ZOO_JAR} \
+        ${ANALYTICS_ZOO_HOME}/apps/image-augmentation/image-augmentation.py
+
 ./apps/ipynb2py.sh ./apps/anomaly-detection/anomaly-detection-nyc-taxi
 
 echo "#1 start app test for anomaly-detection-nyc-taxi"
@@ -75,19 +94,3 @@ ${SPARK_HOME}/bin/spark-submit \
         --conf spark.executor.extraClassPath=${ANALYTICS_ZOO_JAR} \
         ${ANALYTICS_ZOO_HOME}/apps/object-detection/object-detection.py
 
-echo "#3 start app test for image-augmentation"
-FILENAME="$ANALYTICS_ZOO_HOME/apps/image-augmentation/image-augmentation"
-${SPARK_HOME}/bin/spark-submit \
-        --master ${MASTER} \
-        --driver-cores 2  \
-        --driver-memory 1g  \
-        --total-executor-cores 2  \
-        --executor-cores 2  \
-        --executor-memory 1g \
-        --conf spark.akka.frameSize=64 \
-        --py-files ${ANALYTICS_ZOO_PYZIP},${ANALYTICS_ZOO_HOME}/apps/image-augmentation/image-augmentation.py  \
-        --properties-file ${ANALYTICS_ZOO_CONF} \
-        --jars ${ANALYTICS_ZOO_JAR} \
-        --conf spark.driver.extraClassPath=${ANALYTICS_ZOO_JAR} \
-        --conf spark.executor.extraClassPath=${ANALYTICS_ZOO_JAR} \
-        ${ANALYTICS_ZOO_HOME}/apps/image-augmentation/image-augmentation.py
