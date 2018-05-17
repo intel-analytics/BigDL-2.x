003888f76113fc47e61e918c551c4e9184ecd9b6
wngzhiqi <zhiqiwang97@gmail.com>
2018-05-17 09:50:30 +0800

    Add run vae test

--- apps/run-app-tests.sh
+++ apps/run-app-tests.sh
@@ -10,7 +10,112 @@ export ANALYTICS_ZOO_PYZIP=`find ${ANALYTICS_ZOO_HOME_DIST}/lib -type f -name "a
 export ANALYTICS_ZOO_CONF=${ANALYTICS_ZOO_HOME_DIST}/conf/spark-bigdl.conf
 export PYTHONPATH=${ANALYTICS_ZOO_PYZIP}:$PYTHONPATH
 
-echo "#3 start app test for using_variational_autoencoder_and_deep_feature_loss_to_generate_faces"
+chmod +x ./apps/ipynb2py.sh
+./apps/ipynb2py.sh ./apps/anomaly-detection/anomaly-detection-nyc-taxi
+
+echo "#1 start app test for anomaly-detection-nyc-taxi"
+chmod +x $ANALYTICS_ZOO_HOME/data/NAB/nyc_taxi/get_nyc_taxi.sh
+$ANALYTICS_ZOO_HOME/data/NAB/nyc_taxi/get_nyc_taxi.sh
+${SPARK_HOME}/bin/spark-submit \
+        --master ${MASTER} \
+        --driver-cores 2  \
+        --driver-memory 12g  \
+        --total-executor-cores 2  \
+        --executor-cores 2  \
+        --executor-memory 12g \
+        --conf spark.akka.frameSize=64 \
+        --py-files ${ANALYTICS_ZOO_PYZIP},${ANALYTICS_ZOO_HOME}/apps/anomaly-detection/anomaly-detection-nyc-taxi.py  \
+        --properties-file ${ANALYTICS_ZOO_CONF} \
+        --jars ${ANALYTICS_ZOO_JAR} \
+        --conf spark.driver.extraClassPath=${ANALYTICS_ZOO_JAR} \
+        --conf spark.executor.extraClassPath=${ANALYTICS_ZOO_JAR} \
+        ${ANALYTICS_ZOO_HOME}/apps/anomaly-detection/anomaly-detection-nyc-taxi.py
+
+./apps/ipynb2py.sh ./apps/object-detection/object-detection
+
+echo "#2 start app test for object-detection"
+FILENAME="$ANALYTICS_ZOO_HOME/apps/object-detection/analytics-zoo_ssd-mobilenet-300x300_PASCAL_0.1.0.model"
+if [ -f "$FILENAME" ]
+then
+    echo "$FILENAME already exists" 
+else
+    wget $FTP_URI/analytics-zoo-models-new/analytics-zoo_ssd-mobilenet-300x300_PASCAL_0.1.0.model -P $ANALYTICS_ZOO_HOME/apps/object-detection/
+fi 
+if [ -f "$FILENAME" ]
+then
+    echo "$FILENAME already exists" 
+else
+    wget https://s3-ap-southeast-1.amazonaws.com/analytics-zoo-models/object-detection/analytics-zoo_ssd-mobilenet-300x300_PASCAL_0.1.0.model -P $ANALYTICS_ZOO_HOME/apps/object-detection/
+fi 
+FILENAME="$ANALYTICS_ZOO_HOME/apps/object-detection/train_dog.mp4"
+if [ -f "$FILENAME" ]
+then
+    echo "$FILENAME already exists" 
+else
+    wget $FTP_URI/analytics-zoo-data/data/apps/object-detection/train_dog.mp4 -P $ANALYTICS_ZOO_HOME/apps/object-detection/
+fi 
+if [ -f "$FILENAME" ]
+then
+    echo "$FILENAME already exists" 
+else
+    wget https://s3.amazonaws.com/analytics-zoo-data/train_dog.mp4 -P $ANALYTICS_ZOO_HOME/apps/object-detection/
+fi 
+${SPARK_HOME}/bin/spark-submit \
+        --master ${MASTER} \
+        --driver-cores 2  \
+        --driver-memory 12g  \
+        --total-executor-cores 2  \
+        --executor-cores 2  \
+        --executor-memory 12g \
+        --conf spark.akka.frameSize=64 \
+        --py-files ${ANALYTICS_ZOO_PYZIP},${ANALYTICS_ZOO_HOME}/apps/object-detection/object-detection.py  \
+        --properties-file ${ANALYTICS_ZOO_CONF} \
+        --jars ${ANALYTICS_ZOO_JAR} \
+        --conf spark.driver.extraClassPath=${ANALYTICS_ZOO_JAR} \
+        --conf spark.executor.extraClassPath=${ANALYTICS_ZOO_JAR} \
+        ${ANALYTICS_ZOO_HOME}/apps/object-detection/object-detection.py
+
+./apps/ipynb2py.sh ./apps/recommendation/ncf-explicit-feedback
+
+echo "#3 start app test for ncf-explicit-feedback"
+${SPARK_HOME}/bin/spark-submit \
+        --master ${MASTER} \
+        --driver-cores 2  \
+        --driver-memory 12g  \
+        --total-executor-cores 2  \
+        --executor-cores 2  \
+        --executor-memory 12g \
+        --conf spark.akka.frameSize=64 \
+        --py-files ${ANALYTICS_ZOO_PYZIP},${ANALYTICS_ZOO_HOME}/apps/recommendation/ncf-explicit-feedback.py  \
+        --properties-file ${ANALYTICS_ZOO_CONF} \
+        --jars ${ANALYTICS_ZOO_JAR} \
+        --conf spark.driver.extraClassPath=${ANALYTICS_ZOO_JAR} \
+        --conf spark.executor.extraClassPath=${ANALYTICS_ZOO_JAR} \
+        ${ANALYTICS_ZOO_HOME}/apps/recommendation/ncf-explicit-feedback.py
+
+./apps/ipynb2py.sh ./apps/recommendation/wide_n_deep
+
+echo "#4 start app test for wide_n_deep"
+${SPARK_HOME}/bin/spark-submit \
+        --master ${MASTER} \
+        --driver-cores 2  \
+        --driver-memory 12g  \
+        --total-executor-cores 2  \
+        --executor-cores 2  \
+        --executor-memory 12g \
+        --conf spark.akka.frameSize=64 \
+        --py-files ${ANALYTICS_ZOO_PYZIP},${ANALYTICS_ZOO_HOME}/apps/recommendation/wide_n_deep.py  \
+        --properties-file ${ANALYTICS_ZOO_CONF} \
+        --jars ${ANALYTICS_ZOO_JAR} \
+        --conf spark.driver.extraClassPath=${ANALYTICS_ZOO_JAR} \
+        --conf spark.executor.extraClassPath=${ANALYTICS_ZOO_JAR} \
+        ${ANALYTICS_ZOO_HOME}/apps/recommendation/wide_n_deep.py
+
+
+
+
+
+echo "#5 start app test for using_variational_autoencoder_and_deep_feature_loss_to_generate_faces"
 chmod +x ./apps/ipynb2py.sh
 ./apps/ipynb2py.sh ./apps/variational_autoencoder/using_variational_autoencoder_and_deep_feature_loss_to_generate_faces
 sed -i "s/data_files\[\:100000\]/data_files\[\:5000\]/g" ./apps/variational_autoencoder/using_variational_autoencoder_and_deep_feature_loss_to_generate_faces.py
@@ -53,7 +158,7 @@ ${SPARK_HOME}/bin/spark-submit \
         
                                   
 
-echo "#4 start app test for using_variational_autoencoder_to_generate_faces"
+echo "#6 start app test for using_variational_autoencoder_to_generate_faces"
 chmod +x ./apps/ipynb2py.sh
 ./apps/ipynb2py.sh ./apps/variational_autoencoder/using_variational_autoencoder_to_generate_faces
 sed -i "s/data_files\[\:100000\]/data_files\[\:5000\]/g" ./apps/variational_autoencoder/using_variational_autoencoder_to_generate_faces.py
@@ -87,7 +192,7 @@ ${SPARK_HOME}/bin/spark-submit \
                           
         
         
-echo "#5 start app test for using_variational_autoencoder_to_generate_digital_numbers"        
+echo "#7 start app test for using_variational_autoencoder_to_generate_digital_numbers"        
 chmod +x ./apps/ipynb2py.sh
 ./apps/ipynb2py.sh ./apps/variational_autoencoder/using_variational_autoencoder_to_generate_digital_numbers
 
