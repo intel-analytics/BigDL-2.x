89b9577446aaf6e97fdb361463ef927913ba6f6d
simanw304 <simanw304@gmail.com>
2018-05-21 15:31:07 +0800

    fix styling

--- pyzoo/docs/bigdl_pytext.py
+++ pyzoo/docs/bigdl_pytext.py
@@ -16,6 +16,7 @@
 
 import re
 
+
 def _process_docstring(app, what, name, obj, options, lines):
     liter_re = re.compile(r'\s*```\s*$')
 
@@ -28,20 +29,21 @@ def _process_docstring(app, what, name, obj, options, lines):
         # first literal block line
         if not liter_flag and liter_re.match(line):
             liter_flag = True
-            lines.insert(i+1,'')
-            offset += 1
-            lines[i]='::'
+            lines.insert(i+1, '')
+            offset + = 1
+            lines[i] = '::'
         # last literal block line
         elif liter_flag and liter_re.match(line):
             liter_flag = False
-            lines[i]=''
+            lines[i] = ''
         # regular line within literal block
         elif liter_flag:
-            line = ' '+line
-            lines[i]=line
+            line = ' ' + line
+            lines[i] = line
         # regualr line
         else:
-            lines[i]=line.lstrip()
+            lines[i] = line.lstrip()
+
 
 def setup(app):
     app.connect("autodoc-process-docstring", _process_docstring)
--- pyzoo/docs/doc-web.py
+++ pyzoo/docs/doc-web.py
@@ -20,12 +20,15 @@ urls = (
     '/(.*)', 'router'
 )
 
-app = web.application(urls,globals())
+
+app = web.application(urls, globals())
+
 
 class router:
     def GET(self, path):
-        if path == '': path = 'index.html'
-        f = open('_build/html/'+path) 
+        if path == '':
+            path = 'index.html'
+        f = open('_build/html/'+path)
         return f.read()
 
 if __name__ == "__main__":
