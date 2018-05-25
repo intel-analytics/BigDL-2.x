d21576816aa38c73223df4dde4c0f4106a0b1a09
simanw <simanw304@gmail.com>
2018-05-25 16:07:59 +0800

    fix styling

--- pyzoo/docs/doc-web.py
+++ pyzoo/docs/doc-web.py
@@ -22,12 +22,14 @@ urls = (
     '/(.*)', 'router'
 )
 
-app = web.application(urls,globals())
+app = web.application(urls, globals())
+
 
 class router:
     def GET(self, path):
-        if path == '': path = 'index.html'
-        f = open('_build/html/'+path) 
+        if path == '': 
+		path = 'index.html'
+        f = open('_build/html/'+path)
         return f.read()
 
 if __name__ == "__main__":
