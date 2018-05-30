c4af6fc28f96c3b0b7e99333e20afbaa0ef4a44b
simanw304 <30874441+simanw304@users.noreply.github.com>
2018-05-30 13:10:32 +0800

    Update math.scala

--- zoo/src/main/scala/com/intel/analytics/zoo/pipeline/api/autograd/math.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/pipeline/api/autograd/math.scala
@@ -128,21 +128,6 @@ object AutoGrad {
       implicit ev: TensorNumeric[T]): Variable[T] = {
     Variable(Power[T](a).inputs(x.node))
   }
-
-  def softplus[T: ClassTag](x: Variable[T])(
-    implicit ev: TensorNumeric[T]): Variable[T] = {
-    val o: KerasLayer[Activity, Activity, T] =
-      new KerasLayerWrapper[T](
-        bnn.SoftPlus[T]().asInstanceOf[AbstractModule[Activity, Activity, T]])
-    Variable(o.inputs(x.node))
-  }
-
-  def softsign[T: ClassTag](a: Variable[T])(
-    implicit ev: TensorNumeric[T]): Variable[T] = {
-    val o: KerasLayer[Activity, Activity, T] =
-      new KerasLayerWrapper(bnn.SoftSign[T]().asInstanceOf[AbstractModule[Activity, Activity, T]])
-    Variable(o.inputs(a.node))
-  }
 }
 
 object Variable extends {
