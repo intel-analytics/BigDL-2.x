a94aadf5650aca0a4d62dc557fa0f6fc955aa1c3
root <root@Gondolin-Node-067.Gondolin-Node-067>
2018-05-30 14:09:52 +0800

    adjust code format

--- zoo/src/main/scala/com/intel/analytics/zoo/pipeline/inference/FloatInferenceModel.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/pipeline/inference/FloatInferenceModel.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Analytics Zoo Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.intel.analytics.zoo.pipeline.inference
 
 import com.intel.analytics.bigdl.optim.LocalPredictor
@@ -8,7 +24,7 @@ import com.intel.analytics.bigdl.tensor.Tensor
 import scala.collection.JavaConverters._
 
 case class FloatInferenceModel(
-  model:     AbstractModule[Activity, Activity, Float],
+  model: AbstractModule[Activity, Activity, Float],
   predictor: LocalPredictor[Float]) extends InferenceSupportive {
 
   def forward(data: Tensor[Float])(implicit om: Manifest[Float]): Array[Float] = {
@@ -19,7 +35,8 @@ case class FloatInferenceModel(
     predictor.predictClass(samples)
   }
 
-  def predict(input: java.util.List[java.util.List[java.lang.Float]]): java.util.List[java.lang.Float] = {
+  def predict(input: java.util.List[java.util.List[java.lang.Float]]):
+    java.util.List[java.lang.Float] = {
     timing(s"predict for $input") {
       val tensor = transferInferenceInputToTensor(input)
       val result = model.forward(tensor).asInstanceOf[Tensor[Float]].storage.array.toList
--- zoo/src/main/scala/com/intel/analytics/zoo/pipeline/inference/InferenceSupportive.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/pipeline/inference/InferenceSupportive.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Analytics Zoo Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.intel.analytics.zoo.pipeline.inference
 
 import org.slf4j.LoggerFactory
@@ -17,17 +33,16 @@ trait InferenceSupportive {
     result
   }
 
-  def transferInferenceInputToTensor(input: java.util.List[java.util.List[java.lang.Float]]): Tensor[Float] = {
+  def transferInferenceInputToTensor(input: java.util.List[java.util.List[java.lang.Float]]):
+    Tensor[Float] = {
     val arrays = input.asScala.map(_.asScala.toArray.map(_.asInstanceOf[Float]))
     val _buffer = Tensor[Float]()
     arrays.length match {
-      case 0 => {}
-      case 1 => {
-        val size = arrays.head.length
+      case 0 => ;
+      case 1 => val size = arrays.head.length
         _buffer.resize(size)
         System.arraycopy(arrays.head, 0, _buffer.storage().array(), 0, size)
-      }
-      case _ => {
+      case _ =>
         val size = arrays.head.length
         arrays.map(arr => require(size == arr.length, "input array have different lengths"))
         _buffer.resize(Array(arrays.length, size))
@@ -36,7 +51,6 @@ trait InferenceSupportive {
           System.arraycopy(arrays(d), 0, _buffer.storage().array(), d * size, size)
           d += 1
         }
-      }
     }
     _buffer
   }
