5bffb3681094beb6a26197844ff070c29fa44848
root <root@Gondolin-Node-067.Gondolin-Node-067>
2018-05-30 15:01:54 +0800

    addjust format

--- zoo/src/main/scala/com/intel/analytics/zoo/pipeline/inference/FloatInferenceModel.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/pipeline/inference/FloatInferenceModel.scala
@@ -27,14 +27,6 @@ case class FloatInferenceModel(
   model: AbstractModule[Activity, Activity, Float],
   predictor: LocalPredictor[Float]) extends InferenceSupportive {
 
-  def forward(data: Tensor[Float])(implicit om: Manifest[Float]): Array[Float] = {
-    model.forward(data).asInstanceOf[Tensor[Float]].storage().array()
-  }
-
-  def predict(samples: Array[Sample[Float]]): Array[Int] = {
-    predictor.predictClass(samples)
-  }
-
   def predict(input: java.util.List[java.util.List[java.lang.Float]]):
     java.util.List[java.lang.Float] = {
     timing(s"predict for $input") {
