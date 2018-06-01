01252fe32886274b95dcf33112ffcba23646a1a2
Dongjie Shi <glorysdj@gmail.com>
2018-06-01 12:32:26 +0800

    fix inferncemodel missing input shape (#310)
    * fix inferncemodel missing input shape
    
    * change the return of the predict
    
    * refactor predict api
    
    * refactor predict api
    
    * refactor predict api

--- zoo/src/main/scala/com/intel/analytics/zoo/pipeline/inference/FloatInferenceModel.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/pipeline/inference/FloatInferenceModel.scala
@@ -19,21 +19,21 @@ package com.intel.analytics.zoo.pipeline.inference
 import com.intel.analytics.bigdl.optim.LocalPredictor
 import com.intel.analytics.bigdl.nn.abstractnn.AbstractModule
 import com.intel.analytics.bigdl.nn.abstractnn.Activity
-import com.intel.analytics.bigdl.dataset.Sample
 import com.intel.analytics.bigdl.tensor.Tensor
+
 import scala.collection.JavaConverters._
+import java.util.{List => JList}
+import java.lang.{Float => JFloat}
+import java.lang.{Integer => JInt}
 
 case class FloatInferenceModel(
   model: AbstractModule[Activity, Activity, Float],
   predictor: LocalPredictor[Float]) extends InferenceSupportive {
 
-  def predict(input: java.util.List[java.util.List[java.lang.Float]]):
-    java.util.List[java.lang.Float] = {
-    timing(s"predict for input") {
-      val tensor = transferInferenceInputToTensor(input)
-      val result = model.forward(tensor).asInstanceOf[Tensor[Float]].storage.array.toList
-        .asJava.asInstanceOf[java.util.List[java.lang.Float]]
-      result
-    }
+  def predict(input: JList[JFloat], shape: JList[JInt]): JList[JFloat] = {
+    val sample = transferInputToSample(input, shape.asScala.toArray.map(_.asInstanceOf[Int]))
+    val result = predictor.predict(Array(sample))
+    require(result.length == 1, "only one input, should get only one prediction")
+    result(0).asInstanceOf[Tensor[Float]].toArray().toList.asJava.asInstanceOf[JList[JFloat]]
   }
 }
