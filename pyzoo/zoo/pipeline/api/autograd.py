16262a22fb8074fcc33062f728745da9e441a7d2
simanw304 <simanw304@gmail.com>
2018-06-01 16:33:44 +0800

    update

--- pyzoo/zoo/pipeline/api/autograd.py
+++ pyzoo/zoo/pipeline/api/autograd.py
@@ -216,17 +216,31 @@ class Variable(ZooKerasCreator):
     def __sub__(self, other):
         return self.sub(other)
 
+    def __rsub__(self, other):
+        return Variable.from_jvalue(callBigDlFunc("float", "sub", other, self))
+
     def __add__(self, other):
         return self.add(other)
+
+    __radd__ = __add__
 
     def __mul__(self, other):
         return Variable.from_jvalue(callBigDlFunc("float", "mul", self, other))
 
+    __rmul__ = __mul__
+
     def __div__(self, other):
         return Variable.from_jvalue(callBigDlFunc("float", "div", self, other))
 
-    def __truediv__(self, other):
-        return Variable.from_jvalue(callBigDlFunc("float", "div", self, other))
+    __truediv__ = __div__
+
+    def __rdiv__(self, other):
+        return Variable.from_jvalue(callBigDlFunc("float", "div", other, self))
+
+    __rtruediv__ = __div__
+
+    def __neg__(self):
+        return neg(self)
 
 
 class Lambda(ZooKerasCreator):
