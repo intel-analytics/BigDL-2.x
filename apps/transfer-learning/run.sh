485df1680f78a46417b777e083d2cbecd39da82e
xiaxue <xiaxue_2014@foxmail.com>
2018-06-06 12:10:55 +0800

    update transfer-learning

--- apps/transfer-learning/run.sh
+++ apps/transfer-learning/run.sh
@@ -1,11 +1,11 @@
-export SPARK_HOME=/home/xiaxue/spark/spark-2.1.1-bin-hadoop2.6
-export ANALYTICS_ZOO_HOME=/home/xiaxue/529/analytics-zoo/dist/
-export MASTER=local[1]
+export SPARK_HOME=/home/xiaxue/spark/spark-2.2.0-bin-hadoop2.6
+export ANALYTICS_ZOO_HOME=/home/xiaxue/529/analytics-zoo/dist
+export MASTER=local[4]
 
 ${ANALYTICS_ZOO_HOME}/bin/jupyter-with-zoo.sh \
     --master ${MASTER} \
     --driver-cores 1  \
-    --driver-memory 8g  \
+    --driver-memory 20g  \
     --total-executor-cores 1  \
     --executor-cores 1  \
-    --executor-memory 8g
+    --executor-memory 20g
