7357391c6d9b1ce83008c5985183f93521231b73
xiaxue <xiaxue_2014@foxmail.com>
2018-06-06 12:18:11 +0800

    delete some file

--- apps/transfer-learning/run.sh
+++ apps/transfer-learning/run.sh
@@ -1,5 +1,5 @@
-export SPARK_HOME=/home/xiaxue/spark/spark-2.2.0-bin-hadoop2.6
-export ANALYTICS_ZOO_HOME=/home/xiaxue/529/analytics-zoo/dist
+export SPARK_HOME=/home/arda/chentao/apache/spark-2.2.0-bin-hadoop2.6
+export ANALYTICS_ZOO_HOME=/home/arda/chentao/work/zoo/dist
 export MASTER=local[4]
 
 ${ANALYTICS_ZOO_HOME}/bin/jupyter-with-zoo.sh \
