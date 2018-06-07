d469bb7ddcf16f5ebc23f5a140c42f5ac36db2dc
Xue Xia <30511345+xuex2017@users.noreply.github.com>
2018-06-07 15:23:45 +0800

    Update run.sh
    add --memory

--- apps/fraud-detection/run.sh
+++ apps/fraud-detection/run.sh
@@ -1,1 +1,1 @@
-SPARK_OPTS='--master=local[1] --jars ${ANALYTICS_ZOO_HOME}/lib/analytics-zoo-VERSION-jar-with-dependencies.jar,./fraud-1.0.1-SNAPSHOT.jar' TOREE_OPTS='--nosparkcontext' jupyter notebook
+SPARK_OPTS='--master=local[1] --jars ${ANALYTICS_ZOO_HOME}/lib/analytics-zoo-VERSION-jar-with-dependencies.jar,./fraud-1.0.1-SNAPSHOT.jar' --driver-memory 10g --executor-memory 10g TOREE_OPTS='--nosparkcontext' jupyter notebook
