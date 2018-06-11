d30ac41cf778096eb99d1db65799a1629d180d99
simanw304 <simanw304@gmail.com>
2018-06-11 17:13:22 +0800

    add Maximum, Minimum, MaxPooling1D

--- pyzoo/zoo/pipeline/api/keras2/layers/merge.py
+++ pyzoo/zoo/pipeline/api/keras2/layers/merge.py
@@ -14,7 +14,48 @@
 # limitations under the License.
 #
 import sys
+from zoo.pipeline.api.keras2.base import ZooKeras2Layer
 
 if sys.version >= '3':
     long = int
     unicode = str
+
+
+class Maximum(ZooKeras2Layer):
+    """
+    Layer that computes the maximum (element-wise) a list of inputs.
+
+    It takes as input a list of tensors,
+    all of the same shape, and returns
+    a single tensor (also of the same shape).
+    >>> from zoo.pipeline.api.keras.layers import Input
+    >>> max = Maximum()([Input(shape=(4, 5)), Input(shape=(4, 5))])
+    creating: createZooKeras2Maximum
+    creating: createZooKerasInput
+    creating: createZooKerasInput
+    """
+    def __init__(self,
+                 input_shape=None, **kwargs):
+        super(Maximum, self).__init__(None,
+                                      list(input_shape) if input_shape else None,
+                                      **kwargs)
+
+
+class Minimum(ZooKeras2Layer):
+    """
+    Layer that computes the minimum (element-wise) a list of inputs.
+
+    It takes as input a list of tensors,
+    all of the same shape, and returns
+    a single tensor (also of the same shape).
+    >>> from zoo.pipeline.api.keras.layers import Input
+    >>> max = Minimum()([Input(shape=(4, 5)), Input(shape=(4, 5))])
+    creating: createZooKeras2Minimum
+    creating: createZooKerasInput
+    creating: createZooKerasInput
+    """
+    def __init__(self,
+                 input_shape=None, **kwargs):
+        super(Minimum, self).__init__(None,
+                                      list(input_shape) if input_shape else None,
+                                      **kwargs)
