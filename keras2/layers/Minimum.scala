520e3d37d81ba7be7e9b1a61efdeaabff3d2047b
simanw304 <30874441+simanw304@users.noreply.github.com>
2018-06-12 17:50:30 +0800

    add Maximum, Minimum, MaxPooling1D keras2 layers (#389)
    * add keras2 Conv1D layer and test
    
    * style
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * add python Conv1D
    
    * update
    
    * add Maximum, Minimum, MaxPooling1D
    
    * add Maximum, Minimum, MaxPooling1D
    
    * add
    
    * add maximum and minimum methods
    
    * update
    
    * update
    
    * style

---
+++ keras2/layers/Minimum.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2018 Analytics Zoo Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.analytics.zoo.pipeline.api.keras2.layers
+
+import com.intel.analytics.bigdl.nn.Graph.ModuleNode
+import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
+import com.intel.analytics.bigdl.utils.Shape
+import com.intel.analytics.zoo.pipeline.api.Net
+import com.intel.analytics.zoo.pipeline.api.keras.layers.Merge
+
+import scala.reflect.ClassTag
+
+/**
+ * Layer that computes the minimum (element-wise) a list of inputs.
+ *
+ * It takes as input a list of nodes,
+ * all of the same shape, and returns
+ * a single node (also of the same shape).
+ */
+class Minimum[T: ClassTag](
+    override val inputShape: Shape = null)(implicit ev: TensorNumeric[T])
+  extends Merge[T](layers = null, mode = "min", inputShape = inputShape)
+    with Net {
+}
+
+object Minimum {
+  def apply[@specialized(Float, Double) T: ClassTag](inputShape: Shape = null)
+    (implicit ev: TensorNumeric[T]): Minimum[T] = {
+    new Minimum[T](inputShape)
+  }
+  def minimum[@specialized(Float, Double) T: ClassTag](inputs: List[ModuleNode[T]])
+    (implicit ev: TensorNumeric[T]): ModuleNode[T] = {
+    val layer = new Minimum[T]()
+    layer.inputs(inputs.toArray)
+  }
+}
