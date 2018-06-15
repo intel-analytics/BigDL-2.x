954db41027251677d6c4db4c1307695da11eba01
xuex2017 <xuex2017@gmail.com>
2018-06-15 15:41:36 +0800

    update jupyter-with-zoo-scala.sh

--- scripts/jupyter-with-zoo-scala.sh
+++ scripts/jupyter-with-zoo-scala.sh
@@ -13,14 +13,8 @@ fi
 
 # setup paths
 export ANALYTICS_ZOO_JAR=`find ${ANALYTICS_ZOO_HOME}/lib -type f -name "analytics-zoo*jar-with-dependencies.jar"`
-export ANALYTICS_ZOO_CONF=${ANALYTICS_ZOO_HOME}/conf/spark-analytics-zoo.conf
 
 # Check files
-if [ ! -f ${ANALYTICS_ZOO_CONF} ]; then
-    echo "Cannot find ${ANALYTICS_ZOO_CONF}"
-    exit 1
-fi
-
 
 if [ ! -f ${ANALYTICS_ZOO_JAR} ]; then
     echo "Cannot find ${ANALYTICS_ZOO_JAR}"
