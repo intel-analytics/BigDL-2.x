196c3bb91fc559bc54b856f0b42760347793141c
dding3 <ding.ding@intel.com>
2018-06-15 15:02:21 -0700

    support resize image when reading image (#313)
    * support resize when reading image

--- scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/feature/FeatureSpec.scala
+++ scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/feature/FeatureSpec.scala
@@ -15,15 +15,35 @@
  */
 package com.intel.analytics.zoo.feature
 
+import com.intel.analytics.zoo.common.NNContext
 import com.intel.analytics.zoo.feature.common.{BigDLAdapter, Preprocessing}
-import com.intel.analytics.zoo.feature.image.ImageResize
+import com.intel.analytics.zoo.feature.image.{ImageResize, ImageSet}
+import org.apache.spark.SparkConf
 import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
 
-class CommonSpec extends FlatSpec with Matchers with BeforeAndAfter {
+class FeatureSpec extends FlatSpec with Matchers with BeforeAndAfter {
+  val resource = getClass.getClassLoader.getResource("imagenet/n04370456/")
 
   "BigDLAdapter" should "adapt BigDL Transformer" in {
     val newResize = BigDLAdapter(ImageResize(1, 1))
     assert(newResize.isInstanceOf[Preprocessing[_, _]])
+  }
+
+  "Local ImageSet" should "work with resize" in {
+    val image = ImageSet.read(resource.getFile, resizeH = 200, resizeW = 200)
+    val imf = image.toLocal().array.head
+    require(imf.getHeight() == 200)
+    require(imf.getWidth() == 200)
+  }
+
+  "Distribute ImageSet" should "work with resize" in {
+    val conf = new SparkConf().setAppName("Feature Test").setMaster("local[1]")
+    val sc = NNContext.initNNContext(conf)
+    val image = ImageSet.read(resource.getFile, sc, resizeH = 200, resizeW = 200)
+    val imf = image.toDistributed().rdd.collect().head
+    require(imf.getHeight() == 200)
+    require(imf.getWidth() == 200)
+    sc.stop()
   }
 }
