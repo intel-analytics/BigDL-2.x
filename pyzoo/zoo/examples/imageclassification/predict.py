75d68fb2d2dd31edc36de2c64ef63dd734e6c8bb
jenniew <jenniewang123@gmail.com>
2018-06-20 17:09:32 -0700

    update image classification predict according to predict api change

--- pyzoo/zoo/examples/imageclassification/predict.py
+++ pyzoo/zoo/examples/imageclassification/predict.py
@@ -31,7 +31,8 @@ def predict(model_path, img_path, topN):
     predicts = output.get_predict().collect()
     for predict in predicts:
         (uri, probs) = predict
-        sortedProbs = [(prob, index) for index, prob in enumerate(probs)]
+        print(probs[0].shape)
+        sortedProbs = [(prob, index) for index, prob in enumerate(probs[0])]
         sortedProbs.sort()
         print("Image : %s, top %d prediction result" % (uri, topN))
         for i in range(topN):
