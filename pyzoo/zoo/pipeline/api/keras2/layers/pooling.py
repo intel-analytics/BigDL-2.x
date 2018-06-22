a2dad8211323d2ce853dadb5637accdd2bf4ee1f
yjjiang11 <yjjiang11@gmail.com>
2018-06-22 14:54:55 +0800

    add

--- pyzoo/zoo/pipeline/api/keras2/layers/pooling.py
+++ pyzoo/zoo/pipeline/api/keras2/layers/pooling.py
@@ -57,3 +57,43 @@ class MaxPooling1D(ZooKeras2Layer):
                                            padding,
                                            list(input_shape) if input_shape else None,
                                            **kwargs)
+
+
+class GlobalAveragePooling1D(ZooKeras2Layer):
+
+    """Global average pooling operation for spatial data.
+
+    # Arguments
+        data_format: A string,
+            one of `channels_last` (default) or `channels_first`.
+            The ordering of the dimensions in the inputs.
+            `channels_last` corresponds to inputs with shape
+            `(batch, height, width, channels)` while `channels_first`
+            corresponds to inputs with shape
+            `(batch, channels, height, width)`.
+            It defaults to the `image_data_format` value found in your
+            Keras config file at `~/.keras/keras.json`.
+            If you never set it, then it will be "channels_last".
+
+    # Input shape
+        - If `data_format='channels_last'`:
+            4D tensor with shape:
+            `(batch_size, rows, cols, channels)`
+        - If `data_format='channels_first'`:
+            4D tensor with shape:
+            `(batch_size, channels, rows, cols)`
+
+    # Output shape
+        2D tensor with shape:
+        `(batch_size, channels)`
+
+    >>> globalaveragepooling1d = GlobalAveragePooling1D(input_shape=(12, 12))
+    creating: createZooKeras2GlobalAveragePooling1D
+    """
+    def __init__(self,
+                 input_shape=None,
+                 **kwargs):
+
+        super(GlobalAveragePooling1D, self).__init__(None,
+                                                     list(input_shape) if input_shape else None,
+                                                     **kwargs)
