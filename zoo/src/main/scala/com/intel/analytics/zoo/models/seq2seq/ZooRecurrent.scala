56501ef616e4f06400ed5675006a94b26f810f19
dding3 <ding.ding@intel.com>
2018-06-26 03:41:09 -0700

    support mask in ZooRecurrent

--- zoo/src/main/scala/com/intel/analytics/zoo/models/seq2seq/ZooRecurrent.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/models/seq2seq/ZooRecurrent.scala
@@ -16,20 +16,30 @@
 
 package com.intel.analytics.zoo.models.seq2seq
 
-import com.intel.analytics.bigdl.nn.{Recurrent, RecurrentDecoder}
+import com.intel.analytics.bigdl.nn.{BatchNormParams, Recurrent, RecurrentDecoder, Utils}
 import com.intel.analytics.bigdl.nn.abstractnn.Activity
 import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
-import com.intel.analytics.bigdl.utils.T
+import com.intel.analytics.bigdl.utils.{T, Table}
 import com.intel.analytics.bigdl.wrapper.BigDLWrapper
 
+import scala.collection.mutable.ArrayBuffer
 import scala.reflect.ClassTag
 
 /**
  * [[ZooRecurrent]] module is a container of rnn cells
  * Different types of rnn cells can be added using add() function
  */
-class ZooRecurrent[T : ClassTag]()(implicit ev: TensorNumeric[T]) extends Recurrent[T] {
+class ZooRecurrent[T : ClassTag](batchNormParams: BatchNormParams[T] = null,
+                                 maskZero: Boolean = false)(implicit ev: TensorNumeric[T])
+  extends Recurrent[T](batchNormParams, maskZero) {
+  protected var maskBuffer: Tensor[T] = Tensor()
+  protected var gradOutputBuff: Table = T()
+  protected var indexBuffer: Tensor[T] = Tensor()
+  protected var inputBuffer: Tensor[T] = Tensor()
+  protected var outputBuffers: ArrayBuffer[Tensor[T]] = ArrayBuffer(Tensor())
+  protected var minLength: Int = 0
+
   // get gradient hidden state at the first time step
   def getGradHiddenState(): Activity = {
     require(cells != null && cells(0).gradInput != null,
@@ -41,6 +51,77 @@ class ZooRecurrent[T : ClassTag]()(implicit ev: TensorNumeric[T]) extends Recurr
   // set gradient hiddent state at the last time step
   def setGradHiddenState(gradHiddenState: Activity): Unit = {
     initGradHiddenState = gradHiddenState
+  }
+
+  override def updateOutput(input: Tensor[T]): Tensor[T] = {
+    require(input.dim == 3 || input.dim == 5 || input.dim == 6,
+      "Recurrent: input should be a 3D/5D/6D Tensor, e.g [batch, times, nDim], " +
+        s"current input.dim = ${input.dim}")
+
+    batchSize = input.size(batchDim)
+    times = input.size(timeDim)
+
+    input2Cell = if (preTopology != null) {
+      preTopology.forward(input).toTensor[T]
+    } else {
+      input
+    }
+
+    val hiddenSize = topology.hiddensShape(0)
+    val outputSize = input.size()
+    outputSize(2) = hiddenSize
+    output.resize(outputSize)
+
+    /**
+     * currentInput forms a T() type. It contains two elements, hidden and input.
+     * Each time it will feed the cell with T(hidden, input) (or T(input, hidden) depends on
+     * your hidDim and inputDim), and the cell will give a table output containing two
+     * identical elements T(output, output). One of the elements from the cell output is
+     * the updated hidden. Thus the currentInput will update its hidden element with this output.
+     */
+    var i = 1
+    // Clone N modules along the sequence dimension.
+    initHidden(outputSize.drop(2))
+    cloneCells()
+    if (maskZero) {
+      require(input.dim == 3,
+        "If maskZero set to true, input should be a 3D Tensor, e.g [batch, times, nDim]")
+      inputBuffer.resizeAs(input).abs(input).max(maskBuffer, indexBuffer, 3)
+      minLength = ev.toType[Int](maskBuffer.sign().sum(2).min(1)._1(Array(1, 1, 1)))
+    }
+
+    currentInput(hidDim) = if (initHiddenState != null) initHiddenState
+    else hidden
+
+    while (i <= times) {
+      currentInput(inputDim) = BigDLWrapper.copy(input2Cell, i, stepInput2CellBuf)
+      cells(i - 1).forward(currentInput)
+      val curOutput = cells(i - 1).output
+      if (maskZero && i > minLength) {
+        val curMask = maskBuffer.select(2, i)
+        val curOut = curOutput[Table](hidDim)[Tensor[T]](1)
+        // Copy output to a new new tensor as output, because for some cells
+        // such as LSTM the hidden h and ouput o refer to the same tensor.
+        // But in this case, we want h and o have difference values.
+        curOutput.update(inputDim, outputBuffers(i - 1).resizeAs(curOut).copy(curOut))
+        for (b <- 1 to curMask.size(1)) {
+          if (curMask(Array(b, 1)) == ev.zero) {
+            val newState = curOutput[Table](hidDim)
+            val originState = currentInput[Table](hidDim)
+            for (j <- 1 to newState.length()) {
+              newState[Tensor[T]](j).select(1, b).copy(originState[Tensor[T]](j).select(1, b))
+            }
+            curOutput[Tensor[T]](inputDim).select(1, b).zero()
+          }
+        }
+      }
+      currentInput(hidDim) = curOutput[Table](hidDim)
+      i += 1
+    }
+
+    BigDLWrapper.copy(cells.map(x => x.output.toTable[Tensor[T]](inputDim)),
+      output)
+    output
   }
 
   override def backward(input: Tensor[T], gradOutput: Tensor[T]): Tensor[T] = {
@@ -62,15 +143,44 @@ class ZooRecurrent[T : ClassTag]()(implicit ev: TensorNumeric[T]) extends Recurr
       } else {
         cells(i - 1).regluarized(false)
       }
-      cells(i - 1).backward(_input, currentGradOutput)
+
+      if (maskZero && i > minLength) {
+        val curMask = maskBuffer.select(2, i)
+        if (gradOutputBuff.length() == 0) {
+          Utils.recursiveResizeAs(gradOutputBuff, currentGradOutput)
+        }
+        Utils.recursiveCopy(gradOutputBuff, currentGradOutput)
+        for (b <- 1 to curMask.size(1)) {
+          if (curMask(Array(b, 1)) == ev.zero) {
+            val originState = gradOutputBuff[Table](Recurrent.hidDim)
+            for (j <- 1 to originState.length()) {
+              originState[Tensor[T]](j).select(1, b).zero()
+            }
+          }
+        }
+
+        cells(i - 1).backward(_input, gradOutputBuff).toTable
+
+        for (b <- 1 to curMask.size(1)) {
+          if (curMask(Array(b, 1)) == ev.zero) {
+            val newState = cells(i - 1).gradInput[Table](hidDim)
+            val originState = currentGradOutput[Table](hidDim)
+            for (j <- 1 to newState.length()) {
+              newState[Tensor[T]](j).select(1, b).copy(originState[Tensor[T]](j).select(1, b))
+            }
+          }
+        }
+      } else {
+        cells(i - 1).backward(_input, currentGradOutput)
+      }
       i -= 1
     }
 
     gradInput = if (preTopology != null) {
       /**
-       * if preTopology is Sequential, it has not created gradInput.
-       * Thus, it needs to create a new Tensor.
-       */
+        * if preTopology is Sequential, it has not created gradInput.
+        * Thus, it needs to create a new Tensor.
+        */
       if (preTopology.gradInput == null) {
         preTopology.gradInput = Tensor[T]()
       }
