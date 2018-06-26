6f25617460dd18656ea1d1a796a7f9ab4c0beb7b
simanw304 <30874441+simanw304@users.noreply.github.com>
2018-06-26 13:47:50 +0800

    add Threshold and AveragePooling1D (#426)
    * add Threshold and AveragePooling1D
    
    * update

---
+++ api/keras2/layers/AveragePooling1D.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2018 Analytics Zoo Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.analytics.zoo.pipeline.api.keras2.layers
+
+import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
+import com.intel.analytics.bigdl.utils.Shape
+import com.intel.analytics.zoo.pipeline.api.Net
+import com.intel.analytics.zoo.pipeline.api.keras.{layers => klayers1}
+
+import scala.reflect.ClassTag
+
+/**
+ * Average pooling operation for temporal data.
+ *
+ * Input shape
+ * 3D tensor with shape: `(batch_size, steps, features)`.
+ *
+ * Output shape
+ * 3D tensor with shape: `(batch_size, downsampled_steps, features)`.
+ *
+ * @param poolSize Size of the region to which average pooling is applied. Integer. Default is 2.
+ * @param strides Factor by which to downscale. Integer, or -1. 2 will halve the input.
+ *               If -1, it will default to poolSize. Default is -1.
+ * @param padding One of `"valid"` or `"same"` (case-insensitive). Default is 'valid'.
+ * @tparam T Numeric type of parameter(e.g. weight, bias). Only support float/double now
+ */
+class AveragePooling1D[T: ClassTag](
+   val poolSize: Int = 2,
+   val strides: Int = -1,
+   val padding: String = "valid",
+   override val inputShape: Shape = null)(implicit ev: TensorNumeric[T])
+  extends klayers1.AveragePooling1D[T](poolLength = poolSize,
+    stride = strides, borderMode = padding, inputShape) with Net {
+
+}
+
+object AveragePooling1D {
+  def apply[@specialized(Float, Double) T: ClassTag](
+    poolSize: Int = 2,
+    strides: Int = -1,
+    padding: String = "valid",
+    inputShape: Shape = null)(implicit ev: TensorNumeric[T]): AveragePooling1D[T] = {
+    new AveragePooling1D[T](poolSize, strides, padding, inputShape)
+  }
+}
