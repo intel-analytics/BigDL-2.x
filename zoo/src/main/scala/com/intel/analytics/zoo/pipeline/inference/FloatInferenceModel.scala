9cc3a81b88dc9f570fd88053baaadb8009491af3
yangw <yang3.wang@intel.com>
2018-07-05 15:12:54 +0800

    remove ut

--- zoo/src/main/scala/com/intel/analytics/zoo/pipeline/inference/FloatInferenceModel.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/pipeline/inference/FloatInferenceModel.scala
@@ -61,24 +61,22 @@ class FloatInferenceModel(
         i += 1
       }
 
-      val results: Array[Activity] = timing("predictor predict time") {
-        predictor.predict(samples)
-      }
+      val results: Array[Activity] = predictor.predict(samples)
       val outputResults: Array[JList[JTensor]] = results.map(result => {
-        val outputs: List[JTensor] = result.isTensor match {
+        val outputs: Seq[JTensor] = result.isTensor match {
           case true =>
             val outputTensor = result.asInstanceOf[Tensor[Float]]
-            List(transferTensorToJTensor(outputTensor))
+            Seq(transferTensorToJTensor(outputTensor))
           case false =>
             val outputTable = result.toTable
 
             outputTable.toSeq[Tensor[Float]].map(t =>
               transferTensorToJTensor(t)
-            ).toList
+            )
         }
-        outputs.asJava.asInstanceOf[JList[JTensor]]
+        outputs.asJava
       })
-      outputResults.toList.asJava.asInstanceOf[JList[JList[JTensor]]]
+      outputResults.toList.asJava
     }
   }
 
