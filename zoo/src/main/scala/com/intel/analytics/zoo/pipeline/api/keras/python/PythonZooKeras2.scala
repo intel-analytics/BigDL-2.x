9a0e100e6a8a7d918283904c14cfa837937c5465
yjjiang11 <yjjiang11@gmail.com>
2018-07-05 14:35:48 +0800

    add

--- zoo/src/main/scala/com/intel/analytics/zoo/pipeline/api/keras/python/PythonZooKeras2.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/pipeline/api/keras/python/PythonZooKeras2.scala
@@ -24,10 +24,9 @@ import com.intel.analytics.bigdl.python.api.PythonBigDLKeras
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
 import com.intel.analytics.zoo.pipeline.api.Net
 import com.intel.analytics.zoo.pipeline.api.autograd.Variable
-import com.intel.analytics.zoo.pipeline.api.keras2.layers.{Conv1D, Dense, MaxPooling1D, AveragePooling1D, Maximum, Minimum}
-import scala.collection.JavaConverters._
-import com.intel.analytics.zoo.pipeline.api.keras2.layers._
+import com.intel.analytics.zoo.pipeline.api.keras2.layers.{AveragePooling1D, Conv1D, Dense, GlobalAveragePooling1D, MaxPooling1D, Maximum, Minimum, _}
 
+import scala.collection.JavaConverters._
 import scala.reflect.ClassTag
 
 object PythonZooKeras2 {
@@ -118,6 +117,12 @@ class PythonZooKeras2[T: ClassTag](implicit ev: TensorNumeric[T]) extends Python
   def createZooKeras2Minimum(
       inputShape: JList[Int] = null): Minimum[T] = {
     Minimum(
+      toScalaShape(inputShape))
+  }
+
+  def createZooKeras2GlobalAveragePooling1D(
+      inputShape: JList[Int] = null): GlobalAveragePooling1D[T] = {
+    GlobalAveragePooling1D(
       toScalaShape(inputShape))
   }
 
