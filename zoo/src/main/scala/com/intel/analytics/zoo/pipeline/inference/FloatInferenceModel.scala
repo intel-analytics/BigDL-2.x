7d6cc948fad8563ffb559e32da8563049527953c
Dongjie Shi <glorysdj@gmail.com>
2018-07-05 22:06:36 +0800

    Revert "fix inference output tensor order mess up" (#476)
    * Revert "fix bug of InferenceSupportive"
    
    This reverts commit 93dea41d8742c27a177178c9919626282421ffc2.
    
    * Revert "fix bug of transient logger (#475)"
    
    This reverts commit 1dd915f551e496680c757a0b91bc3f82fdbfcd52.
    
    * Revert "fix inference output tensor order mess up (#470)"
    
    This reverts commit bb49b9401f18d9e2a5f968b3cdc77c5cb606f80c.

--- zoo/src/main/scala/com/intel/analytics/zoo/pipeline/inference/FloatInferenceModel.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/pipeline/inference/FloatInferenceModel.scala
@@ -61,22 +61,24 @@ class FloatInferenceModel(
         i += 1
       }
 
-      val results: Array[Activity] = predictor.predict(samples)
+      val results: Array[Activity] = timing("predictor predict time") {
+        predictor.predict(samples)
+      }
       val outputResults: Array[JList[JTensor]] = results.map(result => {
-        val outputs: Seq[JTensor] = result.isTensor match {
+        val outputs: List[JTensor] = result.isTensor match {
           case true =>
             val outputTensor = result.asInstanceOf[Tensor[Float]]
-            Seq(transferTensorToJTensor(outputTensor))
+            List(transferTensorToJTensor(outputTensor))
           case false =>
             val outputTable = result.toTable
-
-            outputTable.toSeq[Tensor[Float]].map(t =>
-              transferTensorToJTensor(t)
-            )
+            outputTable.keySet.map(key => {
+              val outputTensor = outputTable.get(key).get.asInstanceOf[Tensor[Float]]
+              transferTensorToJTensor(outputTensor)
+            }).toList
         }
-        outputs.asJava
+        outputs.asJava.asInstanceOf[JList[JTensor]]
       })
-      outputResults.toList.asJava
+      outputResults.toList.asJava.asInstanceOf[JList[JList[JTensor]]]
     }
   }
 
