96f51aa7fb1b7618c9c6a89fd4ab3bcf7185129f
yjjiang11 <yjjiang11@gmail.com>
2018-07-06 09:01:05 +0800

    add

--- zoo/src/main/scala/com/intel/analytics/zoo/pipeline/api/keras/python/PythonZooKeras2.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/pipeline/api/keras/python/PythonZooKeras2.scala
@@ -22,10 +22,10 @@ import com.intel.analytics.bigdl.nn.abstractnn.{AbstractModule, Activity}
 import com.intel.analytics.bigdl.optim.Regularizer
 import com.intel.analytics.bigdl.python.api.PythonBigDLKeras
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
-import com.intel.analytics.zoo.pipeline.api.Net
-import com.intel.analytics.zoo.pipeline.api.autograd.Variable
+
+
 import com.intel.analytics.zoo.pipeline.api.keras2.layers.{Conv1D, Dense, MaxPooling1D, AveragePooling1D, Maximum, Minimum}
-import scala.collection.JavaConverters._
+_
 import com.intel.analytics.zoo.pipeline.api.keras2.layers._
 
 import scala.reflect.ClassTag
