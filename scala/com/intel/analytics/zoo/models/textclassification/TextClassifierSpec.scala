16c6f0f42e37d94129f08b6990e8ec9003d4cbdc
Kai Huang <huangkaivision@gmail.com>
2018-07-06 13:29:30 +0800

    Keras Objectives and 0-based label (#458)
    * keras objectives
    
    * style
    
    * alias
    
    * accuracy
    
    * meet review
    
    * doc
    
    * more doc
    
    * python
    
    * fix
    
    * style
    
    * fix scala ut
    
    * fix python
    
    * fix
    
    * customloss
    
    * predict classes
    
    * python predict classes
    
    * update
    
    * update doc
    
    * fix
    
    * update
    
    * update mapping
    
    * meet review; add accuracy ut
    
    * fix ut
    
    * update doc

--- scala/com/intel/analytics/zoo/models/textclassification/TextClassifierSpec.scala
+++ scala/com/intel/analytics/zoo/models/textclassification/TextClassifierSpec.scala
@@ -16,12 +16,29 @@
 
 package com.intel.analytics.zoo.models.textclassification
 
+import com.intel.analytics.bigdl.dataset.Sample
 import com.intel.analytics.bigdl.tensor.Tensor
+import com.intel.analytics.zoo.common.NNContext
 import com.intel.analytics.zoo.models.common.ZooModel
 import com.intel.analytics.zoo.pipeline.api.keras.ZooSpecHelper
 import com.intel.analytics.zoo.pipeline.api.keras.serializer.ModuleSerializationTest
+import org.apache.spark.{SparkConf, SparkContext}
 
 class TextClassifierSpec extends ZooSpecHelper {
+
+  private var sc: SparkContext = _
+
+  override def doBefore(): Unit = {
+    val conf = new SparkConf()
+      .setMaster("local[4]")
+    sc = NNContext.initNNContext(conf, appName = "TrainingSpec")
+  }
+
+  override def doAfter(): Unit = {
+    if (sc != null) {
+      sc.stop()
+    }
+  }
 
   "TextClassifier model" should "compute the correct output shape" in {
     val model = TextClassifier[Float](classNum = 20, tokenLength = 200).buildModel()
@@ -43,6 +60,27 @@ class TextClassifierSpec extends ZooSpecHelper {
     val input = Tensor[Float](Array(1, 40, 15)).rand()
     val output = model.forward(input)
     val gradInput = model.backward(input, output)
+  }
+
+  "TextClassifier predictClasses giving zero-based label" should "work properly" in {
+    val model = TextClassifier[Float](classNum = 10, tokenLength = 20)
+    val data = new Array[Sample[Float]](10)
+    var i = 0
+    while (i < data.length) {
+      val input = Tensor[Float](500, 20).rand()
+      val label = Tensor[Float](1).fill(0.0f)
+      data(i) = Sample(input, label)
+      i += 1
+    }
+    val dataSet = sc.parallelize(data, 2)
+    val result = model.predictClasses(dataSet)
+
+    val prob = result.collect()
+    prob.zip(data).foreach(item => {
+      val res = model.forward(item._2.feature.reshape(Array(1, 500, 20)))
+        .toTensor[Float].squeeze().max(1)._2.valueAt(1).toInt
+      (res-1) should be (item._1)
+    })
   }
 
 }
