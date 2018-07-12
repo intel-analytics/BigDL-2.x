2dd14dc399502976b480a209a1e8ccdfb63cb352
xuex2017 <xuex2017@gmail.com>
2018-07-12 17:24:15 +0800

    bug fix

---
+++ zoo/src/main/java/com/intel/analytics/zoo/pipeline/inference/TextClassificationModel.java
@@ -0,0 +1,7 @@
+package com.intel.analytics.zoo.pipeline.inference;
+
+/**
+ * Created by xiaxue on 7/11/18.
+ */
+public class TextClassificationModel {
+}
---
+++ zoo/src/main/java/com/intel/analytics/zoo/pipeline/inference/inferenceTest.java
@@ -0,0 +1,7 @@
+package com.intel.analytics.zoo.pipeline.inference;
+
+/**
+ * Created by xiaxue on 7/10/18.
+ */
+public class inferenceTest {
+}
