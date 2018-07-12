5fd9272366410d06fcb6fc799544fa1a7fb960f2
edieyoung <370676740@qq.com>
2018-07-12 15:51:39 +0800

    update

--- apps/web-service-sample/analytics-zoo-preprocess/src/main/scala/com/intel/analytics/zoo/preprocess/TextProcessing.scala
+++ apps/web-service-sample/analytics-zoo-preprocess/src/main/scala/com/intel/analytics/zoo/preprocess/TextProcessing.scala
@@ -6,8 +6,15 @@ import java.util.{List => JList, Map => JMap}
 import scala.collection.immutable.ListMap
 import scala.collection.mutable.{ArrayBuffer, Map => MMap}
 import scala.io.Source
+import com.intel.analytics.zoo.pipeline.inference.InferenceSupportive
+import org.slf4j.LoggerFactory
+import scala.collection.JavaConverters._
+import collection.JavaConversions._
 
 trait TextProcessing {
+
+  val logger = LoggerFactory.getLogger(getClass)
+
   def doTokenize(text: String): List[String] = {
     text.replaceAll("[^a-zA-Z]", " ").toLowerCase().split("\\s+").filter(_.size > 2).toList
   }
@@ -76,17 +83,26 @@ trait TextProcessing {
     }
     val coefSize = tokensToCoefs(0).size()
     if (tokensToCoefs.size < tokens.size) {
-      val coefZero = List.fill[JFloat](coefSize)(float2Float(0)).asInstanceOf[JList[JFloat]]
-      val newArray = Array.fill(tokens.size - tokensToCoefs.size)(coefZero)
+      val coefZero = List.fill[JFloat](coefSize)(float2Float(0))
+      val newArray = Array.fill(tokens.size - tokensToCoefs.size)(coefZero.asJava)
       tokensToCoefs ++= newArray
     }
     tokensToCoefs.toList
   }
 
   def doPreprocessWithEmbMap(text: String, stopWordsCount: Int, sequenceLength: Int, embMap: JMap[String, JList[JFloat]]): List[JList[JFloat]] = {
+    var begin = System.currentTimeMillis
     val tokens = doTokenize(text)
+    var end = System.currentTimeMillis
+    InferenceSupportive.logger.info(s"tokenize time elapsed ${end-begin} ms")
+    begin = System.currentTimeMillis
     val shapedTokens = doShaping(doStopWords(tokens, stopWordsCount), sequenceLength)
+    end = System.currentTimeMillis
+    InferenceSupportive.logger.info(s"stopwords and shaping time elapsed ${end-begin} ms")
+    begin == System.currentTimeMillis
     val vectorizedTokens = doVectorizeWithJMap(shapedTokens, embMap)
+    end == System.currentTimeMillis
+    InferenceSupportive.logger.info(s"vectorized time elapsed ${end-begin} ms")
     vectorizedTokens
   }
 }
