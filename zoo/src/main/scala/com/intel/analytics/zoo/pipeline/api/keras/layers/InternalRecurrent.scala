6628d1a9e73524287c749b5640f6c15c2744b40c
zhichao-li <zhichao.li@intel.com>
2018-07-17 09:38:56 +0800

    simplify

--- zoo/src/main/scala/com/intel/analytics/zoo/pipeline/api/keras/layers/InternalRecurrent.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/pipeline/api/keras/layers/InternalRecurrent.scala
@@ -27,55 +27,14 @@ private[zoo]class InternalRecurrent[T: ClassTag](
     maskZero: Boolean = false
 )(implicit ev: TensorNumeric[T]) extends Recurrent[T](batchNormParams, maskZero) {
 
+
   override def add(module: AbstractModule[_ <: Activity, _ <: Activity, T]): this.type = {
-    require(module.isInstanceOf[Cell[T]],
-      "Recurrent: added module should be Cell type!")
-    require(!module.isInstanceOf[MultiRNNCell[T]],
-      "Recurrent: added module cannot be MultiRNNCell," +
-        "use Sequential().add(Recurrent(cell)).add(Recurrent(cell))... instead!")
-
-    topology = module.asInstanceOf[Cell[T]]
-    if (topology.preTopology != null) {
-      val tmp = topology.preTopology.cloneModule()
-      topology.preTopology = null
-      preTopology = TimeDistributed(tmp,
-        maskZero = maskZero).asInstanceOf[AbstractModule[Activity, Activity, T]]
+    super.add(module)
+    if (this.preTopology != null) {
+      this.preTopology = this.preTopology.cloneModule()
+      modules(0) = preTopology
+      module.asInstanceOf[Cell[T]].preTopology = null
     }
-
-    if (batchNormParams != null && preTopology == null) {
-      throw new IllegalArgumentException(
-        s"${topology.getName} does not support BatchNormalization." +
-          s" Please add preTopology for it. You can simply using: " +
-          s"override def preTopology: AbstractModule[Activity, Activity, T] = Identity()")
-    }
-
-
-    if (batchNormParams != null) {
-      val clz =  classOf[Recurrent[T]]
-      val methods = clz.getDeclaredMethods.filter(_.getName() == "batchNormalization")
-      require(methods.length == 1)
-      val method = methods(0)
-      method.setAccessible(true)
-      val batchNorm= method.invoke(this, batchNormParams).asInstanceOf[TimeDistributed[T]]
-
-      val field = clz.getDeclaredField("layer")
-      field.setAccessible(true)
-      field.set(this, batchNorm)
-      val layer = field.get(this.asInstanceOf[Recurrent[T]]).asInstanceOf[TensorModule[T]]
-
-      preTopology = Sequential[T]().add(preTopology).add(layer)
-    }
-
-    if (preTopology != null) {
-      modules += preTopology
-    }
-
-    modules += topology
-
-    require((preTopology == null && modules.length == 1) ||
-      (topology != null && preTopology != null && modules.length == 2),
-      "Recurrent extend: should contain only one cell or plus a pre-topology" +
-        " to process input")
     this
   }
 }
