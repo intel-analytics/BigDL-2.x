6ebd6d447413cd59f5be74af261fe571c260804c
Kai Huang <huangkaivision@gmail.com>
2018-07-18 09:41:49 +0800

    Fix creating sc when import bigdl.nn.keras and check version (#543)
    * fix
    
    * fix
    
    * fix check version
    
    * fix ut
    
    * fix example

--- pyzoo/zoo/__init__.py
+++ pyzoo/zoo/__init__.py
@@ -16,11 +16,8 @@
 
 from zoo.common.nncontext import *
 from zoo.util.engine import prepare_env
-from pyspark.files import SparkFiles
 
 prepare_env()
-if not SparkFiles._is_running_on_worker:
-    check_version()
 JavaCreator.add_creator_class("com.intel.analytics.zoo.pipeline.nnframes.python.PythonNNFrames")
 JavaCreator.add_creator_class("com.intel.analytics.zoo.feature.python.PythonImageFeature")
 JavaCreator.add_creator_class("com.intel.analytics.zoo.pipeline.api.keras.python.PythonAutoGrad")
