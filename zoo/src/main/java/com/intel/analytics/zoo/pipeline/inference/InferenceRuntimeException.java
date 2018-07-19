b61905e9064d25012049594a0cd3ee0d8239c268
xuex2017 <xuex2017@gmail.com>
2018-07-19 11:55:14 +0800

    add inference runtime exception

--- zoo/src/main/java/com/intel/analytics/zoo/pipeline/inference/InferenceRuntimeException.java
+++ zoo/src/main/java/com/intel/analytics/zoo/pipeline/inference/InferenceRuntimeException.java
@@ -1,5 +1,8 @@
 package com.intel.analytics.zoo.pipeline.inference;
 
+/**
+ * Created by xiaxue on 7/19/18.
+ */
 public class InferenceRuntimeException extends RuntimeException {
 	public InferenceRuntimeException(String msg) {
 		super(msg);
