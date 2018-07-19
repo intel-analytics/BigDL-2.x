6f0cb4effb3237abd0e143f1ffbc42d21ecce3c6
Xue Xia <30511345+xuex2017@users.noreply.github.com>
2018-07-19 14:46:17 +0800

    Delete run.sh

--- apps/fraud-detection/run.sh
+++
@@ -1,9 +0,0 @@
-# Check environment variables
-if [ -z "${ANALYTICS_ZOO_HOME}" ]; then
-    echo "Please set ANALYTICS_ZOO_HOME environment variable"
-    exit 1
-fi
-
-export ANALYTICS_ZOO_JAR=`find ${ANALYTICS_ZOO_HOME}/lib -type f -name "analytics-zoo*jar-with-dependencies.jar"`
-
-SPARK_OPTS='--master=local[*] --jars ${ANALYTICS_ZOO_JAR},./fraud-1.0.1-SNAPSHOT.jar --driver-memory 10g --executor-memory 10g' TOREE_OPTS='--nosparkcontext' jupyter notebook
