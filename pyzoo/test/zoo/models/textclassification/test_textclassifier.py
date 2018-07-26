84bfbbf88745948bf3014b319256069b01b76f1b
hkvision <huangkaivision@gmail.com>
2018-07-26 10:00:50 +0800

    add wordembedding

--- pyzoo/test/zoo/models/textclassification/test_textclassifier.py
+++ pyzoo/test/zoo/models/textclassification/test_textclassifier.py
@@ -15,6 +15,7 @@
 #
 
 import pytest
+import os
 
 import numpy as np
 from zoo.models.textclassification import TextClassifier
@@ -26,15 +27,12 @@ np.random.seed(1337)  # for reproducibility
 class TestTextClassifier(ZooTestCase):
 
     def test_forward_backward(self):
-        model = TextClassifier(10, 30, 100)
+        resource_path = os.path.join(os.path.split(__file__)[0], "../../resources")
+        glove_path = os.path.join(resource_path, "glove.6B/glove.6B.50d.txt")
+        model = TextClassifier(10, glove_path)
         model.summary()
-        input_data = np.random.random([3, 100, 30])
+        input_data = np.random.randint(20, size=(4, 500))
         self.assert_forward_backward(model, input_data)
-
-    def test_save_load(self):
-        model = TextClassifier(20, 200)
-        input_data = np.random.random([2, 500, 200])
-        self.assert_zoo_model_save_load(model, input_data)
 
 
 if __name__ == "__main__":
