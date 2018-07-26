bd580a22d61e1d8d6707c555dd1801fba2f328ea
xuex2017 <xuex2017@gmail.com>
2018-07-26 14:36:06 +0800

    reformat

--- zoo/src/main/scala/com/intel/analytics/zoo/pipeline/inference/FloatInferenceModel.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/pipeline/inference/FloatInferenceModel.scala
@@ -34,7 +34,8 @@ import com.intel.analytics.bigdl.utils.Engine
 
 class FloatInferenceModel(
                            var model: AbstractModule[Activity, Activity, Float],
-                           @transient var predictor: LocalPredictor[Float]) extends InferenceSupportive with Serializable {
+                           @transient var predictor: LocalPredictor[Float])
+  extends InferenceSupportive with Serializable {
 
   @deprecated
   def predict(input: JList[JFloat], shape: JList[JInt]): JList[JFloat] = {
