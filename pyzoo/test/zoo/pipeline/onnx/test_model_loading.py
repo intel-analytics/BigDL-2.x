622957ae27120ffd9f2391dc9894e61cb69e021f
xiaxin1998 <1124687301@qq.com>
2018-08-31 17:09:33 +0800

    add back

--- pyzoo/test/zoo/pipeline/onnx/test_model_loading.py
+++ pyzoo/test/zoo/pipeline/onnx/test_model_loading.py
@@ -30,6 +30,24 @@ class TestModelLoading(OnnxTestCase):
         input_shape_with_batch = (1, 3, 224, 224)
         self.compare_with_pytorch(pytorch_model, input_shape_with_batch)
 
+    def test_onnx_gemm(self):
+        # TODO: Linear(bias = Flase) is mapped to Transpose + MatMul, not GEMM
+        pytorch_model = torch.nn.Sequential(
+            torch.nn.Linear(in_features=3, out_features=4, bias=True)
+        )
+        input_shape_with_batch = (1, 3)
+        self.compare_with_pytorch(pytorch_model, input_shape_with_batch)
+
+    def test_onnx_add(self):
+
+        class Add(torch.nn.Module):
+            def forward(self, x):
+                return x[0] + x[1]
+
+        pytorch_model = Add()
+        input_shape_with_batch = [(1, 3), (1, 3)]
+        self.compare_with_pytorch(pytorch_model, input_shape_with_batch)
+
     def test_onnx_conv2d_2(self):
         pytorch_model = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3),
