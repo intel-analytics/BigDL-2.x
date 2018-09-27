97421a815bd346d452b3e298948e1dfa3f48f391
dding3 <ding.ding@intel.com>
2018-09-27 15:35:26 -0700

    fix jekins failure

--- pyzoo/zoo/feature/image/imagePreprocessing.py
+++ pyzoo/zoo/feature/image/imagePreprocessing.py
@@ -88,8 +88,10 @@ class ImageChannelNormalize(ImagePreprocessing):
 class PerImageNormalize(ImagePreprocessing):
     """
     Normalizes the norm or value range per image, similar to opencv::normalize
-    https://docs.opencv.org/ref/master/d2/de8/group__core__array.html#ga87eef7ee3970f86906d69a92cbf064bd
-    ImageNormalize normalizes scale and shift the input features. Various normalize methods are supported,
+    https://docs.opencv.org/ref/master/d2/de8/group__core__array.html
+    #ga87eef7ee3970f86906d69a92cbf064bd
+    ImageNormalize normalizes scale and shift the input features. Various normalize
+    methods are supported,
     Eg. NORM_INF, NORM_L1, NORM_L2 or NORM_MINMAX
     Pleas notice it's a per image normalization.
     :param min lower range boundary in case of the range normalization or
@@ -97,7 +99,8 @@ class PerImageNormalize(ImagePreprocessing):
     :param max upper range boundary in case of the range normalization.
     It is not used for the norm normalization.
     :param norm_type normalization type, see opencv:NormTypes.
-    https://docs.opencv.org/ref/master/d2/de8/group__core__array.html#gad12cefbcb5291cf958a85b4b67b6149f
+    https://docs.opencv.org/ref/master/d2/de8/group__core__array.html
+    #gad12cefbcb5291cf958a85b4b67b6149f
     Default Core.NORM_MINMAX
     """
     def __init__(self, min, max, norm_type=32, bigdl_type="float"):
