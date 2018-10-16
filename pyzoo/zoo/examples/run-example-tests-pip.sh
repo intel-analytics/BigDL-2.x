6fefba9e40a7a8bbbcdf75e9c713eb3eaae84ee9
hkvision <huangkaivision@gmail.com>
2018-10-16 16:41:52 +0800

    update

--- pyzoo/zoo/examples/run-example-tests-pip.sh
+++ pyzoo/zoo/examples/run-example-tests-pip.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 echo "#1 start example test for textclassification"
-#timer
 start=$(date "+%s")
+
+# Data preparation
 if [ -f analytics-zoo-data/data/glove.6B.zip ]
 then
     echo "analytics-zoo-data/data/glove.6B.zip already exists"
@@ -18,9 +19,14 @@ else
     tar zxf analytics-zoo-data/data/20news-18828.tar.gz -C analytics-zoo-data/data/
 fi
 
+# Run the example
 export SPARK_DRIVER_MEMORY=2g
 python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/textclassification/text_classification.py \
     --nb_epoch 2 \
     --data_path analytics-zoo-data/data/20news-18828 \
     --embedding_path analytics-zoo-data/data/glove.6B
 unset SPARK_DRIVER_MEMORY
+
+now=$(date "+%s")
+time1=$((now-start))
+echo "#1 textclassification time used:$time1 seconds"
