b8c05357d604edefb10802991d86fed6961e567e
hkvision <huangkaivision@gmail.com>
2018-10-16 16:38:04 +0800

    update

--- pyzoo/zoo/examples/run-example-tests-pip.sh
+++ pyzoo/zoo/examples/run-example-tests-pip.sh
@@ -18,7 +18,9 @@ else
     tar zxf analytics-zoo-data/data/20news-18828.tar.gz -C analytics-zoo-data/data/
 fi
 
+export SPARK_DRIVER_MEMORY=2g
 python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/textclassification/text_classification.py \
     --nb_epoch 2 \
     --data_path analytics-zoo-data/data/20news-18828 \
     --embedding_path analytics-zoo-data/data/glove.6B
+unset SPARK_DRIVER_MEMORY
