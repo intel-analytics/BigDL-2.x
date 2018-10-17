5588118f1659c89b6d7d5d4519c79a2497640b21
hkvision <huangkaivision@gmail.com>
2018-10-17 20:03:16 +0800

    fix

--- apps/run-app-tests-pip.sh
+++ apps/run-app-tests-pip.sh
@@ -7,20 +7,20 @@ clear_up () {
     pip uninstall -y pyspark
 }
 
-chmod +x ${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh
+chmod +x ${ANALYTICS_ZOO_ROOT}/apps/ipynb2py.sh
 
 echo "#1 start app test for anomaly-detection-nyc-taxi"
 start=$(date "+%s")
 
 # Conversion to py file and data preparation
-${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/apps/anomaly-detection/anomaly-detection-nyc-taxi
-chmod +x ${ANALYTICS_ZOO_HOME}/bin/data/NAB/nyc_taxi/get_nyc_taxi.sh
-${ANALYTICS_ZOO_HOME}/bin/data/NAB/nyc_taxi/get_nyc_taxi.sh
-sed "s/nb_epoch=30/nb_epoch=15/g" ${ANALYTICS_ZOO_HOME}/apps/anomaly-detection/anomaly-detection-nyc-taxi.py >${ANALYTICS_ZOO_HOME}/apps/anomaly-detection/tmp_test.py
+${ANALYTICS_ZOO_ROOT}/apps/ipynb2py.sh ${ANALYTICS_ZOO_ROOT}/apps/anomaly-detection/anomaly-detection-nyc-taxi
+chmod +x ${ANALYTICS_ZOO_ROOT}/bin/data/NAB/nyc_taxi/get_nyc_taxi.sh
+${ANALYTICS_ZOO_ROOT}/bin/data/NAB/nyc_taxi/get_nyc_taxi.sh
+sed "s/nb_epoch=30/nb_epoch=15/g" ${ANALYTICS_ZOO_ROOT}/apps/anomaly-detection/anomaly-detection-nyc-taxi.py >${ANALYTICS_ZOO_ROOT}/apps/anomaly-detection/tmp_test.py
 
 # Run the example
 export SPARK_DRIVER_MEMORY=2g
-python ${ANALYTICS_ZOO_HOME}/apps/anomaly-detection/tmp_test.py
+python ${ANALYTICS_ZOO_ROOT}/apps/anomaly-detection/tmp_test.py
 
 exit_status=$?
 if [ $exit_status -ne 0 ];
