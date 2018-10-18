565787207c24cbae349d3364574d8ca58374907c
hkvision <huangkaivision@gmail.com>
2018-10-18 09:40:35 +0800

    fix path

--- apps/run-app-tests-pip.sh
+++ apps/run-app-tests-pip.sh
@@ -9,14 +9,14 @@ clear_up () {
 
 chmod +x ${ANALYTICS_ZOO_ROOT}/apps/ipynb2py.sh
 
-echo "#1 start app test for anomaly-detection-nyc-taxi"
+echo "#1 start app test for anomaly-detection"
 start=$(date "+%s")
 
 # Conversion to py file and data preparation
 ${ANALYTICS_ZOO_ROOT}/apps/ipynb2py.sh ${ANALYTICS_ZOO_ROOT}/apps/anomaly-detection/anomaly-detection-nyc-taxi
-chmod +x ${ANALYTICS_ZOO_ROOT}/bin/data/NAB/nyc_taxi/get_nyc_taxi.sh
-${ANALYTICS_ZOO_ROOT}/bin/data/NAB/nyc_taxi/get_nyc_taxi.sh
-sed "s/nb_epoch=30/nb_epoch=15/g" ${ANALYTICS_ZOO_ROOT}/apps/anomaly-detection/anomaly-detection-nyc-taxi.py >${ANALYTICS_ZOO_ROOT}/apps/anomaly-detection/tmp_test.py
+chmod +x ${ANALYTICS_ZOO_ROOT}/scripts/data/NAB/nyc_taxi/get_nyc_taxi.sh
+${ANALYTICS_ZOO_ROOT}/scripts/data/NAB/nyc_taxi/get_nyc_taxi.sh
+sed "s/nb_epoch=30/nb_epoch=15/g" ${ANALYTICS_ZOO_ROOT}/apps/anomaly-detection/anomaly-detection-nyc-taxi.py > ${ANALYTICS_ZOO_ROOT}/apps/anomaly-detection/tmp_test.py
 
 # Run the example
 export SPARK_DRIVER_MEMORY=2g
@@ -26,7 +26,7 @@ exit_status=$?
 if [ $exit_status -ne 0 ];
 then
     clear_up
-    echo "anomaly-detection-nyc-taxi failed"
+    echo "anomaly-detection failed"
     exit $exit_status
 fi
 
