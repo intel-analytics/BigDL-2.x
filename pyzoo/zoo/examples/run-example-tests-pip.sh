fd25218453c035c9b2b71c8a2fdc8f2e9a063eff
Kai Huang <huangkaivision@gmail.com>
2018-10-19 13:18:41 +0800

    Add script for pip example integration test (#783)
    * add script
    
    * update
    
    * update
    
    * update batchsize
    
    * minor
    
    * update
    
    * add exit status
    
    * update
    
    * minor

---
+++ pyzoo/zoo/examples/run-example-tests-pip.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+clear_up () {
+    echo "Clearing up environment. Uninstalling analytics-zoo"
+    pip uninstall -y analytics-zoo
+    pip uninstall -y bigdl
+    pip uninstall -y pyspark
+}
+
+echo "start example test for textclassification"
+start=$(date "+%s")
+
+# Data preparation
+if [ -f analytics-zoo-data/data/glove.6B.zip ]
+then
+    echo "analytics-zoo-data/data/glove.6B.zip already exists"
+else
+    wget $FTP_URI/analytics-zoo-data/data/glove/glove.6B.zip -P analytics-zoo-data/data
+    unzip -q analytics-zoo-data/data/glove.6B.zip -d analytics-zoo-data/data/glove.6B
+fi
+if [ -f analytics-zoo-data/data/20news-18828.tar.gz ]
+then
+    echo "analytics-zoo-data/data/20news-18828.tar.gz already exists"
+else
+    wget $FTP_URI/analytics-zoo-data/data/news20/20news-18828.tar.gz -P analytics-zoo-data/data
+    tar zxf analytics-zoo-data/data/20news-18828.tar.gz -C analytics-zoo-data/data/
+fi
+
+# Run the example
+export SPARK_DRIVER_MEMORY=2g
+python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/textclassification/text_classification.py \
+    --nb_epoch 2 \
+    --batch_size 112 \
+    --data_path analytics-zoo-data/data/20news-18828 \
+    --embedding_path analytics-zoo-data/data/glove.6B
+exit_status=$?
+if [ $exit_status -ne 0 ];
+then
+    clear_up
+    echo "textclassification failed"
+    exit $exit_status
+fi
+
+unset SPARK_DRIVER_MEMORY
+now=$(date "+%s")
+time1=$((now-start))
+echo "textclassification time used:$time1 seconds"
+
+# This should be done at the very end after all tests finish.
+clear_up
