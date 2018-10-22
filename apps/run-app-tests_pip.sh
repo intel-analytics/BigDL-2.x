3d41ba83cdaf275b39814a0bc05b7fab42d6f0a1
cynthia-liu <xinqi.liu@intel.com>
2018-10-23 06:51:30 +0800

    new

--- apps/run-app-tests_pip.sh
+++ apps/run-app-tests_pip.sh
@@ -24,19 +24,10 @@ sed "s/nb_epoch=20/nb_epoch=2/g; s/batch_size=1024/batch_size=1008/g" ${ANALYTIC
 export SPARK_DRIVER_MEMORY=2g
 python ${ANALYTICS_ZOO_HOME}/apps/anomaly-detection/tmp_test.py
 
-exit_status=$?
-if [ $exit_status -ne 0 ];
-then
-    clear_up
-    echo "anomaly-detection failed"
-    exit $exit_status
-fi
-
 unset SPARK_DRIVER_MEMORY
 now=$(date "+%s")
 time1=$((now-start))
 echo "anomaly-detection-nyc-taxi time used:$time1 seconds"
-
 
 echo "#2 start app test for object-detection"
 start=$(date "+%s")
@@ -79,19 +70,13 @@ fi
 export SPARK_DRIVER_MEMORY=12g
 python ${ANALYTICS_ZOO_HOME}/apps/object-detection/object-detection.py
 
-exit_status=$?
-if [ $exit_status -ne 0 ];
-then
-    clear_up
-    echo "object-detection failed"
-    exit $exit_status
-fi
-
 unset SPARK_DRIVER_MEMORY
 now=$(date "+%s")
 time2=$((now-start))
 echo "object-detection time used:$time2 seconds"
 
+echo "anomaly-detection-nyc-taxi time used:$time1 seconds"
+echo "object-detection time used:$time2 seconds"
 
 # This should be done at the very end after all tests finish.
 clear_up
