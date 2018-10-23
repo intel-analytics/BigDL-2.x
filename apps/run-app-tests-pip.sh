cda4dcd456993cd087c43adc9875a88810e8a0d2
xiaxin1998 <1124687301@qq.com>
2018-10-23 13:20:20 +0800

    sentiment

--- apps/run-app-tests-pip.sh
+++ apps/run-app-tests-pip.sh
@@ -11,28 +11,27 @@ clear_up () {
 
 chmod +x ${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh
 
-echo "#1 start app test for anomaly-detection"
+echo "#1 start app test for sentiment-analysis"
 start=$(date "+%s")
 
 # Conversion to py file and data preparation
-${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/apps/recommendation-wide-n-deep/wide_n_deep
-sed "s/end_trigger=MaxEpoch(10)/end_trigger=MaxEpoch(5)/g" ${ANALYTICS_ZOO_HOME}/apps/recommendation-wide-n-deep/wide_n_deep.py >${ANALYTICS_ZOO_HOME}/apps/recommendation-wide-n-deep/tmp_test.py
+${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/apps/sentiment-analysis/sentiment
 # Run the example
 export SPARK_DRIVER_MEMORY=2g
-python ${ANALYTICS_ZOO_HOME}/apps/recommendation-wide-n-deep/tmp_test.py
+python ${ANALYTICS_ZOO_HOME}/apps/sentiment-analysis/sentiment.py
 
 exit_status=$?
 if [ $exit_status -ne 0 ];
 then
     clear_up
-    echo "anomaly-detection failed"
+    echo "sentiment analysis failed"
     exit $exit_status
 fi
 
 unset SPARK_DRIVER_MEMORY
 now=$(date "+%s")
 time1=$((now-start))
-echo "anomaly-detection-nyc-taxi time used:$time1 seconds"
+echo "sentiment-analysis time used:$time1 seconds"
 
 # This should be done at the very end after all tests finish.
 clear_up
