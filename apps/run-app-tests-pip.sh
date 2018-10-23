1635af34b2c2e1e6e01b629c586ebf5a085d120d
cynthia-liu <40779025+cynthia-liu@users.noreply.github.com>
2018-10-23 09:53:10 +0800

    Update run-app-tests-pip.sh

--- apps/run-app-tests-pip.sh
+++ apps/run-app-tests-pip.sh
@@ -37,5 +37,62 @@ now=$(date "+%s")
 time1=$((now-start))
 echo "anomaly-detection-nyc-taxi time used:$time1 seconds"
 
+echo "#2 start app test for object-detection"
+start=$(date "+%s")
+
+${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/apps/object-detection/object-detection
+FILENAME="${ANALYTICS_ZOO_HOME}/apps/object-detection/analytics-zoo_ssd-mobilenet-300x300_PASCAL_0.1.0.model"
+if [ -f "$FILENAME" ]
+then
+    echo "$FILENAME already exists"
+else
+    wget $FTP_URI/analytics-zoo-models/object-detection/analytics-zoo_ssd-mobilenet-300x300_PASCAL_0.1.0.model -P ${ANALYTICS_ZOO_HOME}/apps/object-detection/
+fi
+if [ -f "$FILENAME" ]
+then
+    echo "$FILENAME already exists"
+else
+    wget https://s3-ap-southeast-1.amazonaws.com/analytics-zoo-models/object-detection/analytics-zoo_ssd-mobilenet-300x300_PASCAL_0.1.0.model -P ${ANALYTICS_ZOO_HOME}/apps/object-detection/
+fi
+FILENAME="${ANALYTICS_ZOO_HOME}/apps/object-detection/train_dog.mp4"
+if [ -f "$FILENAME" ]
+then
+    echo "$FILENAME already exists"
+else
+    wget $FTP_URI/analytics-zoo-data/apps/object-detection/train_dog.mp4 -P ${ANALYTICS_ZOO_HOME}/apps/object-detection/
+fi
+if [ -f "$FILENAME" ]
+then
+    echo "$FILENAME already exists"
+else
+    wget https://s3.amazonaws.com/analytics-zoo-data/train_dog.mp4 -P ${ANALYTICS_ZOO_HOME}/apps/object-detection/
+fi
+FILENAME="/root/.imageio/ffmpeg/ffmpeg-linux64-v3.3.1"
+if [ -f "$FILENAME" ]
+then
+    echo "$FILENAME already exists"
+else
+    wget $FTP_URI/analytics-zoo-data/apps/object-detection/ffmpeg-linux64-v3.3.1 -P /root/.imageio/ffmpeg/
+fi
+
+export SPARK_DRIVER_MEMORY=12g
+python ${ANALYTICS_ZOO_HOME}/apps/object-detection/object-detection.py
+
+exit_status=$?
+if [ $exit_status -ne 0 ];
+then
+    clear_up
+    echo "anomaly-detection failed"
+    exit $exit_status
+fi
+
+unset SPARK_DRIVER_MEMORY
+now=$(date "+%s")
+time2=$((now-start))
+echo "object-detection time used:$time2 seconds"
+
+echo "anomaly-detection-nyc-taxi time used:$time1 seconds"
+echo "object-detection time used:$time2 seconds"
+
 # This should be done at the very end after all tests finish.
 clear_up
