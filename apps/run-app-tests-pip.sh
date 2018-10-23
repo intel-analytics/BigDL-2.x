638d9a0719fc424ce3e7c50a6a58fffdc875da14
LudviqIntel <manfei.bai@intel.com>
2018-10-23 09:02:21 +0800

    recommentation-ncf_test

--- apps/run-app-tests-pip.sh
+++ apps/run-app-tests-pip.sh
@@ -11,31 +11,33 @@ clear_up () {
 
 chmod +x ${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh
 
-echo "#1 start app test for anomaly-detection"
+echo "#10 start app test for recommendation-ncf"
 start=$(date "+%s")
 
 # Conversion to py file and data preparation
-${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/apps/anomaly-detection/anomaly-detection-nyc-taxi
-chmod +x ${ANALYTICS_ZOO_HOME}/bin/data/NAB/nyc_taxi/get_nyc_taxi.sh
-${ANALYTICS_ZOO_HOME}/bin/data/NAB/nyc_taxi/get_nyc_taxi.sh
-sed "s/nb_epoch=20/nb_epoch=2/g; s/batch_size=1024/batch_size=1008/g" ${ANALYTICS_ZOO_HOME}/apps/anomaly-detection/anomaly-detection-nyc-taxi.py > ${ANALYTICS_ZOO_HOME}/apps/anomaly-detection/tmp_test.py
 
+ ###################################################################################################
+
+${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/apps/recommendation-ncf/ncf-explicit-feedback
+sed "s/end_trigger=MaxEpoch(10)/end_trigger=MaxEpoch(5)/g" ${ANALYTICS_ZOO_HOME}/apps/recommendation-ncf/ncf-explicit-feedback.py >${ANALYTICS_ZOO_HOME}/apps/recommendation-ncf/tmp.py
+
+ ###################################################################################################
 # Run the example
-export SPARK_DRIVER_MEMORY=2g
-python ${ANALYTICS_ZOO_HOME}/apps/anomaly-detection/tmp_test.py
+export SPARK_DRIVER_MEMORY=12g
+python ${ANALYTICS_ZOO_HOME}/apps/recommendation-ncf/tmp_test.py
 
 exit_status=$?
 if [ $exit_status -ne 0 ];
 then
     clear_up
-    echo "anomaly-detection failed"
+    echo "recommendation-ncf failed"
     exit $exit_status
 fi
 
 unset SPARK_DRIVER_MEMORY
 now=$(date "+%s")
 time1=$((now-start))
-echo "anomaly-detection-nyc-taxi time used:$time1 seconds"
+echo "recommendation-ncf time used:$time1 seconds"
 
 echo "#10 start app test for dogs-vs-cats"
 start=$(date "+%s")
