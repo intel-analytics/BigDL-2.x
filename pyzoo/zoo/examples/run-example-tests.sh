996c563f8b67518f7ccc7156830526a5405a313f
Deliadd <38576895+Deliadd@users.noreply.github.com>
2018-10-25 13:16:12 +0800

    Update run-example-tests.sh

--- pyzoo/zoo/examples/run-example-tests.sh
+++ pyzoo/zoo/examples/run-example-tests.sh
@@ -187,50 +187,8 @@ ${SPARK_HOME}/bin/spark-submit \
 now=$(date "+%s")
 time5=$((now-start))
 
-echo "#6 start example test for tensorflow"
-#timer
-start=$(date "+%s")
-echo "start example test for tensorflow tfnet"
-if [ -f analytics-zoo-models/ssd_mobilenet_v1_coco_2017_11_17.tar.gz ]
-then
-   echo "analytics-zoo-models/bigdl_inception-v1_imagenet_0.4.0.model already exists."
-else
-   wget http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_coco_2017_11_17.tar.gz \
-    -P analytics-zoo-models
-   tar zxf analytics-zoo-models/ssd_mobilenet_v1_coco_2017_11_17.tar.gz -C analytics-zoo-models/
-fi
-${SPARK_HOME}/bin/spark-submit \
-    --master ${MASTER} \
-    --driver-memory 200g \
-    --executor-memory 200g \
-    --properties-file ${ANALYTICS_ZOO_CONF} \
-    --py-files ${ANALYTICS_ZOO_PYZIP},${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/tensorflow/tfnet/predict.py \
-    --jars ${ANALYTICS_ZOO_JAR} \
-    --conf spark.driver.extraClassPath=${ANALYTICS_ZOO_JAR} \
-    --conf spark.executor.extraClassPath=${ANALYTICS_ZOO_JAR} \
-    ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/tensorflow/tfnet/predict.py \
-    --image hdfs://172.168.2.181:9000/kaggle/train_100 \
-    --model analytics-zoo-models/ssd_mobilenet_v1_coco_2017_11_17/frozen_inference_graph.pb
-    
-echo "start example test for tensorflow distributed_training"
-if [ -d analytics-zoo-models/model ]
-then
-    echo "analytics-zoo-models/bigdl_inception-v1_imagenet_0.4.0.model already exists."
-else
-    rm -rf analytics-zoo-models
-    git clone https://github.com/tensorflow/models/ analytics-zoo-models
-    export PYTHONPATH=$PYTHONPATH:`pwd`/analytics-zoo-models/model/research:`pwd`/analytics-zoo-models/model/research/slim
- fi
-${SPARK_HOME}/bin/spark-submit \
-    --master local[4] \
-   ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/tensorflow/distributed_training/train_lenet.py
-   ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/tensorflow/distributed_training/evaluate_lenet.py
-now=$(date "+%s")
-time6=$((now-start))
-
 echo "#1 textclassification time used:$time1 seconds"
 echo "#2 customized loss and layer time used:$time2 seconds"
 echo "#3 image-classification time used:$time3 seconds"
 echo "#4 object-detection loss and layer time used:$time4 seconds"
 echo "#5 nnframes time used:$time5 seconds"
-echo "#6 Tensorflow time used:$time6 seconds"
