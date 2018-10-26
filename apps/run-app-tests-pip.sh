8b98879d7aa399ad61dabd6757a31db7126583f0
insularZJ <1600292041@qq.com>
2018-10-26 11:09:11 +0800

    fix bug

--- apps/run-app-tests-pip.sh
+++ apps/run-app-tests-pip.sh
@@ -11,18 +11,17 @@ clear_up () {
 
 chmod +x ${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh
 
-echo "#1 start app test for anomaly-detection"
+
+echo "#9 start app test for image-augmentation"
+# timer
 start=$(date "+%s")
 
 # Conversion to py file and data preparation
-${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/apps/anomaly-detection/anomaly-detection-nyc-taxi
-chmod +x ${ANALYTICS_ZOO_HOME}/bin/data/NAB/nyc_taxi/get_nyc_taxi.sh
-${ANALYTICS_ZOO_HOME}/bin/data/NAB/nyc_taxi/get_nyc_taxi.sh
-sed "s/nb_epoch=20/nb_epoch=2/g; s/batch_size=1024/batch_size=1008/g" ${ANALYTICS_ZOO_HOME}/apps/anomaly-detection/anomaly-detection-nyc-taxi.py > ${ANALYTICS_ZOO_HOME}/apps/anomaly-detection/tmp_test.py
+${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/apps/image-augmentation/image-augmentation
 
 # Run the example
-export SPARK_DRIVER_MEMORY=2g
-python ${ANALYTICS_ZOO_HOME}/apps/anomaly-detection/tmp_test.py
+export SPARK_DRIVER_MEMORY=1g
+python ${ANALYTICS_ZOO_HOME}/apps/image-augmentation/image-augmentation.py
 
 exit_status=$?
 if [ $exit_status -ne 0 ];
@@ -34,128 +33,31 @@ fi
 
 unset SPARK_DRIVER_MEMORY
 now=$(date "+%s")
-time1=$((now-start))
-echo "#1 anomaly-detection-nyc-taxi time used:$time1 seconds"
-
-echo "#9 start app test for image-augmentation"
-#timer
-start=$(date "+%s")
-${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/apps/image-augmentation/image-augmentation
-
-${SPARK_HOME}/bin/spark-submit \
-        --master ${MASTER} \
-        --driver-memory 1g  \
-        --executor-memory 1g \
-        --conf spark.akka.frameSize=64 \
-        --py-files ${ANALYTICS_ZOO_PYZIP},${ANALYTICS_ZOO_HOME}/apps/image-augmentation/image-augmentation.py  \
-        --properties-file ${ANALYTICS_ZOO_CONF} \
-        --jars ${ANALYTICS_ZOO_JAR} \
-        --conf spark.driver.extraClassPath=${ANALYTICS_ZOO_JAR} \
-        --conf spark.executor.extraClassPath=${ANALYTICS_ZOO_JAR} \
-        ${ANALYTICS_ZOO_HOME}/apps/image-augmentation/image-augmentation.py
-now=$(date "+%s")
 time9=$((now-start))
 echo "#9 image-augmentation time used:$time9 seconds"
 
-echo "#10 start app test for dogs-vs-cats"
+echo "#11 start app test for image-augementation-3d"
+# timer
 start=$(date "+%s")
 
 # Conversion to py file and data preparation
-
-${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/apps/dogs-vs-cats/transfer-learning
-
-sed "s/setBatchSize(40)/setBatchSize(56)/g; s/file:\/\/path\/to\/data\/dogs-vs-cats\/demo/demo/g;s/path\/to\/model\/bigdl_inception-v1_imagenet_0.4.0.model/demo\/bigdl_inception-v1_imagenet_0.4.0.model/g" ${ANALYTICS_ZOO_HOME}/apps/dogs-vs-cats/transfer-learning.py >${ANALYTICS_ZOO_HOME}/apps/dogs-vs-cats/tmp.py
-
-FILENAME="${ANALYTICS_ZOO_HOME}/apps/dogs-vs-cats/bigdl_inception-v1_imagenet_0.4.0.model"
-if [ -f "$FILENAME" ]
-then
-   echo "$FILENAME already exists."
-else
-   echo "Downloading model"
-
-   wget $FTP_URI/analytics-zoo-models/image-classification/bigdl_inception-v1_imagenet_0.4.0.model -P demo
-
-   echo "Finished downloading model"
-fi
-
-FILENAME="${ANALYTICS_ZOO_HOME}/apps/dogs-vs-cats/train.zip"
-if [ -f "$FILENAME" ]
-then
-   echo "$FILENAME already exists."
-else
-   echo "Downloading dogs and cats images"
-   wget  $FTP_URI/analytics-zoo-data/data/dogs-vs-cats/train.zip  -P ${ANALYTICS_ZOO_HOME}/apps/dogs-vs-cats
-   unzip -d ${ANALYTICS_ZOO_HOME}/apps/dogs-vs-cats/ ${ANALYTICS_ZOO_HOME}/apps/dogs-vs-cats/train.zip
-   mkdir -p demo/dogs
-   mkdir -p demo/cats
-   cp ${ANALYTICS_ZOO_HOME}/apps/dogs-vs-cats/train/cat.7* demo/cats
-   cp ${ANALYTICS_ZOO_HOME}/apps/dogs-vs-cats/train/dog.7* demo/dogs
-   echo "Finished downloading images"
-fi
+${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/apps/image-augmentation-3d/image-augementation-3d
 
 # Run the example
-export SPARK_DRIVER_MEMORY=2g
-python ${ANALYTICS_ZOO_HOME}/apps/dogs-vs-cats/tmp.py
+export SPARK_DRIVER_MEMORY=1g
+python ${ANALYTICS_ZOO_HOME}/apps/image-augmentation-3d/image-augmentation-3d.py
 
 exit_status=$?
 if [ $exit_status -ne 0 ];
 then
     clear_up
-    echo "dogs-vs-cats failed"
+    echo "anomaly-detection failed"
     exit $exit_status
 fi
 
 unset SPARK_DRIVER_MEMORY
-now=$(date "+%s")
-time1=$((now-start))
-echo "#10 dogs-vs-cats time used:$time1 seconds"
-
-echo "#11 start app test for image-augementation-3d"
-#timer
-start=$(date "+%s")
-${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/apps/image-augmentation-3d/image-augementation-3d
-${SPARK_HOME}/bin/spark-submit \
-        --master ${MASTER} \
-        --driver-cores 2  \
-        --total-executor-cores 2  \
-        --executor-cores 2  \
-        --driver-memory 1g  \
-        --executor-memory 1g \
-        --conf spark.akka.frameSize=64 \
-        --py-files ${ANALYTICS_ZOO_PYZIP},${ANALYTICS_ZOO_HOME}/apps/image-augmentation-3d/image-augementation-3d.py \
-        --properties-file ${ANALYTICS_ZOO_CONF} \
-        --jars ${ANALYTICS_ZOO_JAR} \
-        --conf spark.driver.extraClassPath=${ANALYTICS_ZOO_JAR} \
-        --conf spark.executor.extraClassPath=${ANALYTICS_ZOO_JAR} \
-        ${ANALYTICS_ZOO_HOME}/apps/image-augmentation-3d/image-augementation-3d.py
 now=$(date "+%s")
 time11=$((now-start))
 echo "#11 image-augementation-3d time used:$time11 seconds"
 
-echo "#5 start app test for using_variational_autoencoder_to_generate_digital_numbers"
-#timer
-start=$(date "+%s")
 
-${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/apps/variational-autoencoder/using_variational_autoencoder_to_generate_digital_numbers
-
-sed "s/nb_epoch = 6/nb_epoch=2/g; s/batch_size=batch_size/batch_size=1008/g" ${ANALYTICS_ZOO_HOME}/apps/variational-autoencoder/using_variational_autoencoder_to_generate_digital_numbers.py > ${ANALYTICS_ZOO_HOME}/apps/variational-autoencoder/tmp_test.py
-
-export SPARK_DRIVER_MEMORY=12g
-python ${ANALYTICS_ZOO_HOME}/apps/variational-autoencoder/tmp_test.py
-
-exit_status=$?
-if [ $exit_status -ne 0 ];
-then
-    clear_up
-    echo "using_variational_autoencoder_to_generate_digital_numbers failed"
-    exit $exit_status
-fi
-
-unset SPARK_DRIVER_MEMORY
-now=$(date "+%s")
-time5=$((now-start))
-echo "#5 using_variational_autoencoder_to_generate_digital_numbers time used:$time5 seconds"
-
-
-# This should be done at the very end after all tests finish.
-clear_up
