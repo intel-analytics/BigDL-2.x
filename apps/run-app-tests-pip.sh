54883f68b5617c3d06f5ab01d917e3f3c512e304
xiaxin1998 <1124687301@qq.com>
2018-10-29 12:21:54 +0800

    echo 4 11

--- apps/run-app-tests-pip.sh
+++ apps/run-app-tests-pip.sh
@@ -427,5 +427,47 @@ fi
 time7=$((now-start))
 echo "#7 using_variational_autoencoder_and_deep_feature_loss_to_generate_faces time used:$time7 seconds"
 
+echo "#4 start app test for wide_n_deep"
+start=$(date "+%s")
+
+# Conversion to py file and data preparation
+${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/apps/recommendation-wide-n-deep/wide_n_deep
+sed "s/end_trigger=MaxEpoch(10)/end_trigger=MaxEpoch(5)/g" ${ANALYTICS_ZOO_HOME}/apps/recommendation-wide-n-deep/wide_n_deep.py >${ANALYTICS_ZOO_HOME}/apps/recommendation-wide-n-deep/tmp_test.py
+
+# Run the example
+export SPARK_DRIVER_MEMORY=22g
+python ${ANALYTICS_ZOO_HOME}/apps/recommendation-wide-n-deep/wide_n_deep.py
+
+unset SPARK_DRIVER_MEMORY
+now=$(date "+%s")
+time4=$((now-start))
+echo "recommendation-wide-n-deep time used:$time4 seconds"
+rm ${ANALYTICS_ZOO_HOME}/apps/recommendation-wide-n-deep/tmp_test.py
+
+echo "#11 start app test for sentiment-analysis"
+start=$(date "+%s")
+
+# Conversion to py file and data preparation
+${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/apps/sentiment-analysis/sentiment
+FILENAME="/tmp/.bigdl/dataset/glove.6B.zip"
+if [ -f "$FILENAME" ]
+then
+   echo "$FILENAME already exists."
+else
+   echo "Downloading glove6B"
+   wget -P /tmp/.bigdl/dataset/ $FTP_URI/analytics-zoo-data/data/glove/glove.6B.zip
+   echo "Finished"
+fi
+
+# Run the example
+export SPARK_DRIVER_MEMORY=12g
+python ${ANALYTICS_ZOO_HOME}/apps/sentiment-analysis/sentiment.py
+
+unset SPARK_DRIVER_MEMORY
+now=$(date "+%s")
+time11=$((now-start))
+echo "sentiment-analysis time used:$time11 seconds"
+rm ${ANALYTICS_ZOO_HOME}/apps/sentiment-analysis/sentiment.py
+
 # This should be done at the very end after all tests finish.
 clear_up
