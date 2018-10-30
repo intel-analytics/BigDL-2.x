1cfa6ab9af867b08388eb7db46617d971d3dc721
yangw <yang3.wang@intel.com>
2018-10-30 18:03:20 +0800

    fix test

--- pyzoo/test/zoo/pipeline/api/keras/test_simple_integration.py
+++ pyzoo/test/zoo/pipeline/api/keras/test_simple_integration.py
@@ -103,7 +103,7 @@ class TestSimpleIntegration(ZooTestCase):
         model = Sequential()
         model.add(Dense(4, activation="relu", input_shape=(10, )))
         x = np.random.random([300, 10])
-        y = np.random.random([300, ])
+        y = np.random.random([300, 4])
         model.compile(optimizer="sgd", loss="mae")
         model.fit(x, y, batch_size=112, nb_epoch=2)
         model.predict(x)
