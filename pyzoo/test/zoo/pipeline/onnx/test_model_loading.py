7e620545c63f07a20849c5afe73562c6e7301523
xiaxin1998 <38903481+xiaxin1998@users.noreply.github.com>
2018-11-08 14:48:30 +0800

    Update test_model_loading.py

--- pyzoo/test/zoo/pipeline/onnx/test_model_loading.py
+++ pyzoo/test/zoo/pipeline/onnx/test_model_loading.py
@@ -488,7 +488,7 @@ class TestModelLoading(OnnxTestCase):
         class MnistNet(nn.Module):
             def __init__(self):
                 super(MnistNet, self).__init__()
-                self.conv1 = torch.nn.Conv2d(1, 10, kernel_size=5)
+                self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
                 self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
                 self.conv2_drop = nn.Dropout2d()
                 self.fc1 = nn.Linear(320, 50)
