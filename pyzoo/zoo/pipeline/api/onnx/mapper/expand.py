2d9e01610a783b082b493962ad66f8871d4e8ec3
xiaxin1998 <1124687301@qq.com>
2018-11-21 14:54:34 +0800

    update

--- pyzoo/zoo/pipeline/api/onnx/mapper/expand.py
+++ pyzoo/zoo/pipeline/api/onnx/mapper/expand.py
@@ -16,14 +16,23 @@
 from zoo.pipeline.api.onnx.mapper.operator_mapper import OperatorMapper
 from zoo.pipeline.api.onnx.onnx_helper import OnnxHelper
 import zoo.pipeline.api.keras.layers as zlayers
+import numpy as np
 
 
 class ExpandMapper(OperatorMapper):
     def __init__(self, node, _params, _all_tensors):
         super(ExpandMapper, self).__init__(node, _params, _all_tensors)
 
+    def _extract_model_inputs(self):
+        """
+        :return: list of OnnxInput
+        """
+        return [self._to_zoo_input(self._input_list[0])]
+
     def _to_tensor(self):
-        assert len(self.model_inputs) == 1, "AveragePool accept single input only"
-        shape = [int(i) for i in self.onnx_attr['shape']]
-        expand = zlayers.autograd.expand_dims()
+        assert len(self.model_inputs) == 1, "Expand accept single input only"
+        data = [int(i) for i in self._input_list[0].data]
+        shape = np.shape(data)
+        new_shape = [int(i) for i in self._input_list[1].zvalue]
+        expand = zlayers.autograd.expand_dims(self.model_inputs, 1)
         return expand(self.model_inputs[0].zvalue)
