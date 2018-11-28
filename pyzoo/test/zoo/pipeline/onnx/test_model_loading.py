480f97d284dc53082649e4020ad2f75742215d33
cynthia-liu <40779025+cynthia-liu@users.noreply.github.com>
2018-11-28 13:30:55 +0800

    Update test_model_loading.py

--- pyzoo/test/zoo/pipeline/onnx/test_model_loading.py
+++ pyzoo/test/zoo/pipeline/onnx/test_model_loading.py
@@ -874,15 +874,9 @@ class TestModelLoading(OnnxTestCase):
         self.compare_with_pytorch(pytorch_model, input_shape_with_batch)
 
     def test_onnx_embedding(self):
-        class Embedding(torch.nn.Module):
-            def __init__(self, *parameter):
-                super(Embedding, self).__init__()
-                self.weight = torch.randn(parameter)
-
-            def forward(self, x):
-                return torch.embedding(self.weight, x)
-
-        pytorch_model = Embedding(10, 3)
+        pytorch_model = torch.nn.Sequential(
+            torch.nn.Embedding(num_embeddings=10, embedding_dim=3)
+        )
         input_shape_with_batch = (2, 4)
         input_data_with_batch = [[1,2,4,5],[4,3,2,9]]
         self.compare_with_pytorch(pytorch_model, input_shape_with_batch, input_data_with_batch)
