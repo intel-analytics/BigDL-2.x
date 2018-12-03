e105d2095111113cd47c28e9ebe76118e8ae877b
Deliadd <chinadingdi@gmail.com>
2018-12-03 16:45:52 +0800

    update

--- pyzoo/zoo/pipeline/api/onnx/mapper/concat.py
+++ pyzoo/zoo/pipeline/api/onnx/mapper/concat.py
@@ -23,8 +23,7 @@ class ConcatMapper(OperatorMapper):
         super(ConcatMapper, self).__init__(node, _params, _all_tensors)
 
     def _to_tensor(self):
-        axis = -1
-        if "axis" in self.onnx_attr.keys():
-            axis = int(self.onnx_attr['axis'])
+        assert 'axis' in self.onnx_attr, "axis is a required attribute"
+        axis = int(self.onnx_attr['axis'])
         data = [i.zvalue for i in self.model_inputs]
         return zlayers.Merge(mode="concat", concat_axis=axis)(data)
