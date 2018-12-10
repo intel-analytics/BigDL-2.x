6a132611cd9a2c777fdb8bb87fe307adeb0f15cc
yangw <yang3.wang@intel.com>
2018-12-10 18:36:51 +0800

    fix test

--- pyzoo/test/zoo/pipeline/api/test_tf.py
+++ pyzoo/test/zoo/pipeline/api/test_tf.py
@@ -43,12 +43,12 @@ class TestTF(ZooTestCase):
 
     def test_for_scalar(self):
         import tensorflow as tf
-        tf.reset_default_graph()
-        input1 = tf.placeholder(dtype=tf.float32, shape=())
-        output = input1 + 1
-        sess = tf.Session()
-        net = TFNet.from_session(sess, [input1], [output])
-        sess.close()
+        with tf.Graph().as_default():
+            input1 = tf.placeholder(dtype=tf.float32, shape=())
+            output = input1 + 1
+            sess = tf.Session()
+            net = TFNet.from_session(sess, [input1], [output])
+            sess.close()
         out_value = net.forward(np.array(1.0))
         assert len(out_value.shape) == 0
 
@@ -60,22 +60,23 @@ class TestTF(ZooTestCase):
 
     def test_init_tfnet_from_session(self):
         import tensorflow as tf
-        tf.reset_default_graph()
-        input1 = tf.placeholder(dtype=tf.float32, shape=(None, 2))
-        label1 = tf.placeholder(dtype=tf.float32, shape=(None, 1))
-        hidden = tf.layers.dense(input1, 4)
-        output = tf.layers.dense(hidden, 1)
-        loss = tf.reduce_mean(tf.square(output - label1))
-        grad_inputs = tf.gradients(loss, input1)
-        with tf.Session() as sess:
-            sess.run(tf.global_variables_initializer())
-            data = np.random.rand(2, 2)
-            output_value_ref = sess.run(output, feed_dict={input1: data})
-            label_value = output_value_ref - 1.0
-            grad_input_value_ref = sess.run(grad_inputs[0],
-                                            feed_dict={input1: data,
-                                                       label1: label_value})
-            net = TFNet.from_session(sess, [input1], [output], generate_backward=True)
+        with tf.Graph().as_default():
+            input1 = tf.placeholder(dtype=tf.float32, shape=(None, 2))
+            label1 = tf.placeholder(dtype=tf.float32, shape=(None, 1))
+            hidden = tf.layers.dense(input1, 4)
+            output = tf.layers.dense(hidden, 1)
+            loss = tf.reduce_mean(tf.square(output - label1))
+            grad_inputs = tf.gradients(loss, input1)
+            with tf.Session() as sess:
+                sess.run(tf.global_variables_initializer())
+                data = np.random.rand(2, 2)
+                output_value_ref = sess.run(output, feed_dict={input1: data})
+                label_value = output_value_ref - 1.0
+                grad_input_value_ref = sess.run(grad_inputs[0],
+                                                feed_dict={input1: data,
+                                                           label1: label_value})
+                net = TFNet.from_session(sess, [input1], [output], generate_backward=True)
+
         output_value = net.forward(data)
 
         grad_input_value = net.backward(data, np.ones(shape=(2, 1)))
@@ -85,29 +86,30 @@ class TestTF(ZooTestCase):
 
     def test_tf_optimizer_with_sparse_gradient(self):
         import tensorflow as tf
-        tf.reset_default_graph()
+
         ids = np.random.randint(0, 10, size=[40])
         labels = np.random.randint(0, 5, size=[40])
         id_rdd = self.sc.parallelize(ids)
         label_rdd = self.sc.parallelize(labels)
         training_rdd = id_rdd.zip(label_rdd).map(lambda x: [x[0], x[1]])
-        dataset = TFDataset.from_rdd(training_rdd,
-                                     names=["ids", "labels"],
-                                     shapes=[[], []],
-                                     types=[tf.int32, tf.int32],
-                                     batch_size=8)
-        id_tensor, label_tensor = dataset.tensors
-        embedding_table = tf.get_variable(
-            name="word_embedding",
-            shape=[10, 5])
+        with tf.Graph().as_default():
+            dataset = TFDataset.from_rdd(training_rdd,
+                                         names=["ids", "labels"],
+                                         shapes=[[], []],
+                                         types=[tf.int32, tf.int32],
+                                         batch_size=8)
+            id_tensor, label_tensor = dataset.tensors
+            embedding_table = tf.get_variable(
+                name="word_embedding",
+                shape=[10, 5])
 
-        embedding = tf.nn.embedding_lookup(embedding_table, id_tensor)
-        loss = tf.reduce_mean(tf.losses.
-                              sparse_softmax_cross_entropy(logits=embedding,
-                                                           labels=label_tensor))
-
-        optimizer = TFOptimizer(loss, Adam(1e-3))
-        optimizer.optimize(end_trigger=MaxEpoch(1))
+            embedding = tf.nn.embedding_lookup(embedding_table, id_tensor)
+            loss = tf.reduce_mean(tf.losses.
+                                  sparse_softmax_cross_entropy(logits=embedding,
+                                                               labels=label_tensor))
+            optimizer = TFOptimizer(loss, Adam(1e-3))
+            optimizer.optimize(end_trigger=MaxEpoch(1))
+            optimizer.sess.close()
 
 
 if __name__ == "__main__":
