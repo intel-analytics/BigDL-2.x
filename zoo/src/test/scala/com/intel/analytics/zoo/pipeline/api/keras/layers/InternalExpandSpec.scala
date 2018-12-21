e00bd2e92b1d76289933d3e4f6e5fa4c48e832a6
dding3 <ding.ding@intel.com>
2018-12-20 16:08:43 -0800

    remove expandBatchDim

--- zoo/src/test/scala/com/intel/analytics/zoo/pipeline/api/keras/layers/InternalExpandSpec.scala
+++ zoo/src/test/scala/com/intel/analytics/zoo/pipeline/api/keras/layers/InternalExpandSpec.scala
@@ -1,4 +1,3 @@
-
 /*
  * Copyright 2018 Analytics Zoo Authors.
  *
@@ -18,13 +17,14 @@
 package com.intel.analytics.zoo.pipeline.api.keras.layers
 
 import com.intel.analytics.bigdl.tensor.Tensor
-import com.intel.analytics.bigdl.utils.Shape
+import com.intel.analytics.bigdl.utils.{RandomGenerator, Shape}
 import com.intel.analytics.zoo.pipeline.api.keras.layers.internal.InternalExpand
 import com.intel.analytics.zoo.pipeline.api.keras.serializer.ModuleSerializationTest
 
 
 class InternalExpandSpec extends KerasBaseSpec {
   "InternalExpand" should "generate correct output" in {
+    RandomGenerator.RNG.setSeed(100)
     val tgtSizes = Array(5, 4, 3)
     val layer = InternalExpand[Float](tgtSizes)
     val input = Tensor[Float](5, 4, 1).rand()
@@ -34,20 +34,30 @@ class InternalExpandSpec extends KerasBaseSpec {
       require(output.narrow(3, i, 1).almostEqual(input, 1e-8) == true)
     }
     val gradInput = layer.backward(input, gradOutput)
-    require(gradInput.nElement() == input.nElement())
+
+    val expectGradInput = Tensor[Float](Array[Float](1.724836f, 1.2709723f, 0.33389157f,
+      2.2362313f, 1.1102785f, 1.2065659f, 1.3381615f, 2.2813537f, 2.073785f, 1.0430324f,
+      0.5571449f, 1.4316915f, 0.9529829f, 1.6594222f, 1.0953714f, 1.2271657f, 1.3691416f,
+      1.7870495f, 1.5014464f, 1.4872407f), Array(5, 4, 1))
+    require(gradInput.almostEqual(expectGradInput, 1e-8) == true)
   }
 
-  "InternalExpand with expand batch dim" should "generate correct output" in {
-    val tgtSizes = Array(5, 4, 3)
-    val layer = InternalExpand[Float](tgtSizes, true)
-    val input = Tensor[Float](1, 4, 3).rand()
+  "InternalExpand with -1 batch dim" should "generate correct output" in {
+    RandomGenerator.RNG.setSeed(100)
+    val tgtSizes = Array(-1, 4, 3)
+    val layer = InternalExpand[Float](tgtSizes)
+    val input = Tensor[Float](5, 4, 1).rand()
     val gradOutput = Tensor[Float](5, 4, 3).rand()
     val output = layer.forward(input)
-    for (i <- 1 to 5) {
-      require(output.narrow(1, i, 1).almostEqual(input, 1e-8) == true)
+    for (i <- 1 to 3) {
+      require(output.narrow(3, i, 1).almostEqual(input, 1e-8) == true)
     }
     val gradInput = layer.backward(input, gradOutput)
-    require(gradInput.nElement() == input.nElement())
+    val expectGradInput = Tensor[Float](Array[Float](1.724836f, 1.2709723f, 0.33389157f,
+      2.2362313f, 1.1102785f, 1.2065659f, 1.3381615f, 2.2813537f, 2.073785f, 1.0430324f,
+      0.5571449f, 1.4316915f, 0.9529829f, 1.6594222f, 1.0953714f, 1.2271657f, 1.3691416f,
+      1.7870495f, 1.5014464f, 1.4872407f), Array(5, 4, 1))
+    require(gradInput.almostEqual(expectGradInput, 1e-8) == true)
   }
 }
 
