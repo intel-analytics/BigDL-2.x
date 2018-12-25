a432bbfa4c54bfdf4942cd5ddbb1d39631e0ff3f
Deliadd <chinadingdi@gmail.com>
2018-12-25 11:26:42 +0800

    update

--- pyzoo/zoo/pipeline/api/onnx/mapper/batchnormalization.py
+++ pyzoo/zoo/pipeline/api/onnx/mapper/batchnormalization.py
@@ -31,7 +31,7 @@ class BatchNormalizationMapper(OperatorMapper):
 
     def _extract_trainable_values(self):
         if len(self._input_list) == 5:
-            return [self._input_list[1].zvalue, self._input_list[2].zvalue]
+            return [self._input_list[1].zvalue.get_weight(), self._input_list[2].zvalue.get_weight()]
         else:
             return None
 
