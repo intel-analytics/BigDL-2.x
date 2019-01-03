Fix sparse gradient bug (#1036)
* keras model support sparse_gradient

* refactor

pyzoo/zoo/pipeline/api/net.py
@@ -386,6 +386,9 @@ class TFOptimizer:
         self.inputs = inputs
         self.graph = graph
 
+        from zoo.util.tf import process_grad
+        grads = [process_grad(grad) for grad in grads]
+
         if self.dataset.batch_size <= 0:
             raise ValueError("You should set batch_size instead of batch_per_thread for training")
 
@@ -478,10 +481,8 @@ class TFOptimizer:
         grads_vars = tf.train.GradientDescentOptimizer(0).compute_gradients(loss)
         variables = []
         grads = []
-        from zoo.util.tf import process_grad
         for (grad, var) in grads_vars:
             variables.append(var)
-            grad = process_grad(grad)
             grads.append(grad)
 
         all_required_inputs = _find_placeholders([loss])