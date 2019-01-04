5e77cac5939cd985fbdac47647c1ad285f823885
hkvision <huangkaivision@gmail.com>
2019-01-04 19:36:28 +0800

    fix style

--- pyzoo/zoo/feature/text/text_set.py
+++ pyzoo/zoo/feature/text/text_set.py
@@ -191,10 +191,12 @@ class TextSet(JavaValue):
                             Default is 0, namely remove nothing.
         :param max_words_num: Int. The maximum number of words to be taken into consideration.
                               Default is -1, namely all words will be considered.
-        :param min_freq: Positive int. Only those words with frequency >= min_freq will be taken into
-                         consideration. Default is 1, namely all words that occur will be considered.
-        :param existing_map: Existing map of word index if any. Default is None and in this case a new
-                             map with index starting from 1 will be generated.
+        :param min_freq: Positive int. Only those words with frequency >= min_freq will be taken
+                         into consideration.
+                         Default is 1, namely all words that occur will be considered.
+        :param existing_map: Existing map of word index if any.
+                             Default is None and in this case a new map with index starting
+                             from 1 will be generated.
                              If not None, then the generated map will preserve the word index in
                              existing_map and assign subsequent indices to new words.
 
