a69dd6f3336212f8e34e0460f354cd91df3a5718
cynthia-liu <xinqi.liu@intel.com>
2019-01-15 10:57:58 +0800

    update

--- pyzoo/test/zoo/pipeline/onnx/test_model_loading.py
+++ pyzoo/test/zoo/pipeline/onnx/test_model_loading.py
@@ -1260,6 +1260,21 @@ class TestModelLoading(OnnxTestCase):
         output = OnnxLoader.run_node(node, [x])
         np.testing.assert_almost_equal(output["y"], y, decimal=5)
 
+    def test_unsqueeze_list(self):
+        node = onnx.helper.make_node(
+            'Unsqueeze',
+            inputs=['x'],
+            outputs=['y'],
+            axes=[2, 3, 5],
+        )
+        x = np.random.randn(3, 4, 5, 6, 7, 8).astype(np.float32)
+        y = np.expand_dims(x, axis=2)
+        y = np.expand_dims(y, axis=4)
+        y = np.expand_dims(y, axis=7)
+
+        output = OnnxLoader.run_node(node, [x])
+        np.testing.assert_almost_equal(output["y"], y, decimal=5)
+
     def test_onnx_transpose(self):
         pytorch_model = Transpose(2, 3)
         input_shape_with_batch = (3, 7, 8, 9)
