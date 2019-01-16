f40f33f271a99408ffef2f8782f1aca51246d3be
dding3 <ding.ding@intel.com>
2019-01-16 10:11:45 -0800

    seq2seq and InternalTimeDistributed bug fix (#1067)

--- zoo/src/main/scala/com/intel/analytics/zoo/models/seq2seq/Seq2seq.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/models/seq2seq/Seq2seq.scala
@@ -103,8 +103,9 @@ model.asInstanceOf[KerasNet[T]].compile(optimizer, loss, metrics)
             stopSign: Tensor[T] = null,
             buildOutput: KerasLayer[Tensor[T], Tensor[T], T] = null): Tensor[T] = {
     val sent1 = input
-    val sent2 = startSign
-    sent2.resize(Array(1, 1) ++ startSign.size())
+    val sent2 = Tensor[T](startSign.size())
+    sent2.copy(startSign)
+    sent2.resize(Array(1) ++ startSign.size())
 
     var curInput = sent2
     val sizes = curInput.size()
@@ -112,8 +113,12 @@ model.asInstanceOf[KerasNet[T]].compile(optimizer, loss, metrics)
     concat.narrow(Seq2seq.timeDim, 1, 1).copy(sent2)
     var break = false
 
-    if (!buildOutput.isBuilt()) {
-      buildOutput.build(generator.getOutputShape())
+    if (buildOutput != null && !buildOutput.isBuilt()) {
+      if (generator != null) {
+        buildOutput.build(generator.getOutputShape())
+      } else {
+        buildOutput.build(decoder.getOutputShape())
+      }
     }
     var j = 1
     // Iteratively output predicted words
