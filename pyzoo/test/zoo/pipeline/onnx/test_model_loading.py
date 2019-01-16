e87d7861efcd544924f7b9a906f2c54ee11952e4
cynthia-liu <xinqi.liu@intel.com>
2019-01-16 10:22:06 +0800

    update

--- pyzoo/test/zoo/pipeline/onnx/test_model_loading.py
+++ pyzoo/test/zoo/pipeline/onnx/test_model_loading.py
@@ -1374,6 +1374,58 @@ class TestModelLoading(OnnxTestCase):
         output = OnnxLoader.run_node(node, [data_0, data_1])
         np.testing.assert_almost_equal(output["result"], result, decimal=5)
 
+    def test_lrn_default(self):
+        import math
+        alpha = 0.0001
+        beta = 0.75
+        bias = 1.0
+        nsize = 3
+        node = onnx.helper.make_node(
+            'LRN',
+            inputs=['x'],
+            outputs=['y'],
+            size=3
+        )
+        x = np.random.randn(5, 5, 5, 5).astype(np.float32)
+        square_sum = np.zeros((5, 5, 5, 5)).astype(np.float32)
+        for n, c, h, w in np.ndindex(x.shape):
+            square_sum[n, c, h, w] = sum(x[n, max(0, c - int(math.floor((nsize - 1) / 2))):
+                                              min(5, c + int(math.ceil((nsize - 1) / 2)) + 1),
+                                         h,
+                                         w] ** 2)
+        y = x / ((bias + (alpha / nsize) * square_sum) ** beta)
+
+        output = OnnxLoader.run_node(node, [x])
+        np.testing.assert_almost_equal(output["y"], y, decimal=5)
+
+    def test_lrn(self):
+        import math
+        alpha = 0.0002
+        beta = 0.5
+        bias = 2.0
+        nsize = 3
+        node = onnx.helper.make_node(
+            'LRN',
+            inputs=['x'],
+            outputs=['y'],
+            alpha=alpha,
+            beta=beta,
+            bias=bias,
+            size=nsize
+        )
+        x = np.random.randn(5, 5, 5, 5).astype(np.float32)
+        square_sum = np.zeros((5, 5, 5, 5)).astype(np.float32)
+        for n, c, h, w in np.ndindex(x.shape):
+            square_sum[n, c, h, w] = sum(x[n,
+                                         max(0, c - int(math.floor((nsize - 1) / 2))):min(5, c + int(
+                                             math.ceil((nsize - 1) / 2)) + 1),
+                                         h,
+                                         w] ** 2)
+        y = x / ((bias + (alpha / nsize) * square_sum) ** beta)
+
+        output = OnnxLoader.run_node(node, [x])
+        np.testing.assert_almost_equal(output["y"], y, decimal=5)
+
     def test_resnet50(self):
         import torchvision
         import caffe2.python.onnx.backend as backend
@@ -1387,12 +1439,12 @@ class TestModelLoading(OnnxTestCase):
         outputs = rep.run(ndarray_input)
         print(outputs[0])
 
-    def test_alexnet(self):
+    def test_inception_v2(self):
         import torchvision
         import caffe2.python.onnx.backend as backend
 
         ndarray_input = np.random.randn(1, 3, 224, 224).astype(np.float32)
-        onnx_model = onnx.load("/home/xinqi/densenet121/model.onnx")
+        onnx_model = onnx.load("/home/xinqi/inception_v2/model.onnx")
         zmodel = OnnxLoader(onnx_model.graph).to_keras()
         zoutput = zmodel.forward(ndarray_input)
 
