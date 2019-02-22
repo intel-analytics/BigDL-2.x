c93745d7860ce4b202d6b2c3988bb36e93a9dc36
hkvision <huangkaivision@gmail.com>
2019-02-22 16:36:41 +0800

    code update

--- pyzoo/zoo/examples/tensorflow/distributed_training/run_classifier_bert.py
+++ pyzoo/zoo/examples/tensorflow/distributed_training/run_classifier_bert.py
@@ -14,9 +14,6 @@
 # limitations under the License.
 #
 
-import os
-# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-import numpy as np
 import tensorflow as tf
 from bigdl.optim.optimizer import Adam, MaxEpoch
 from zoo.common.nncontext import *
@@ -31,59 +28,66 @@ def to_list_numpy(feature):
             np.array(feature.segment_ids), np.array(feature.label_id)]
 
 
-# Model and data files
-bert_base_dir = "/home/kai/Downloads/bert_models/uncased_L-12_H-768_A-12/"
-bert_config_file = bert_base_dir + "bert_config.json"
-vocab_file = bert_base_dir + "vocab.txt"
-init_checkpoint = bert_base_dir + "bert_model.ckpt"
-data_dir = "/home/kai/Downloads/glue_data/MRPC/"
-
-# Options
-do_lower_case = False
-max_seq_length = 128
-train_batch_size = 32
-eval_batch_size = 8
-predict_batch_size = 8
-learning_rate = 1e-6
-nb_epoch = 50
+def generate_tf_dataset(examples, seq_len, batch_size):
+    features = convert_examples_to_features(examples, label_list, seq_len, tokenizer)
+    features = [to_list_numpy(feature) for feature in features]
+    rdd = getOrCreateSparkContext().parallelize(features)
+    return TFDataset.from_rdd(rdd,
+                              names=["input_ids", "input_mask", "segment_ids", "label_id"],
+                              shapes=[[seq_len], [seq_len], [seq_len], [1]],
+                              types=[tf.int32, tf.int32, tf.int32, tf.int32],
+                              batch_size=batch_size)
 
 
-sc = init_nncontext("BERT Classification Example")
+if __name__ == '__main__':
+    # Model and data files
+    bert_base_dir = "/home/kai/Downloads/bert_models/uncased_L-12_H-768_A-12/"
+    bert_config_file = bert_base_dir + "bert_config.json"
+    vocab_file = bert_base_dir + "vocab.txt"
+    init_checkpoint = bert_base_dir + "bert_model.ckpt"
+    data_dir = "/home/kai/Downloads/glue_data/MRPC/"
 
-# Data preparation and preprocessing
-processor = MrpcProcessor()
-label_list = processor.get_labels()
-tokenizer = FullTokenizer(vocab_file, do_lower_case)
-examples = processor.get_train_examples(data_dir)
-features = convert_examples_to_features(examples, label_list, max_seq_length, tokenizer)
-features = [to_list_numpy(feature) for feature in features]
-feature_rdd = sc.parallelize(features)
-train_dataset = TFDataset.from_rdd(feature_rdd,
-                                   names=["input_ids", "input_mask", "segment_ids", "label_id"],
-                                   shapes=[[max_seq_length], [max_seq_length], [max_seq_length], [1]],
-                                   types=[tf.int32, tf.int32, tf.int32, tf.int32],
-                                   batch_size=train_batch_size)
+    # Options
+    do_lower_case = False
+    max_seq_length = 128
+    train_batch_size = 32
+    eval_batch_size = 8
+    predict_batch_size = 8
+    learning_rate = 1e-6
+    nb_epoch = 50
 
-# Model loading and construction
-input_ids, input_mask, segment_ids, label_ids = train_dataset.tensors
-bert_config = BertConfig.from_json_file(bert_config_file)
-loss, per_example_loss, logits, probabilities = create_model(bert_config, True, input_ids, input_mask, segment_ids,
-                                                             label_ids, len(label_list), False)
-tvars = tf.trainable_variables()
-initialized_variable_names = {}
-scaffold_fn = None
-if init_checkpoint:
-    assignment_map, initialized_variable_names = get_assignment_map_from_checkpoint(tvars, init_checkpoint)
-    tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
-tf.logging.info("**** Trainable Variables ****")
-for var in tvars:
-    init_string = ""
-    if var.name in initialized_variable_names:
-        init_string = ", *INIT_FROM_CKPT*"
-        tf.logging.info("  name = %s, shape = %s%s", var.name, var.shape, init_string)
+    sc = init_nncontext("BERT Classification Example")
 
-# Training
-optimizer = TFOptimizer(loss, Adam(learning_rate))
-optimizer.optimize(end_trigger=MaxEpoch(nb_epoch))
+    # Data preparation and preprocessing
+    processor = MrpcProcessor()
+    label_list = processor.get_labels()
+    tokenizer = FullTokenizer(vocab_file, do_lower_case)
+    train_examples = processor.get_train_examples(data_dir)
+    train_dataset = generate_tf_dataset(train_examples, max_seq_length, train_batch_size)
+    eval_examples = processor.get_dev_examples(data_dir)
+    eval_dataset = generate_tf_dataset(eval_examples, max_seq_length, eval_batch_size)
 
-print("Finished")
+    # Model loading and construction
+    input_ids, input_mask, segment_ids, label_ids = train_dataset.tensors
+    label_ids = tf.squeeze(label_ids)
+    bert_config = BertConfig.from_json_file(bert_config_file)
+    loss, per_example_loss, logits, probabilities = create_model(bert_config, True, input_ids, input_mask, segment_ids,
+                                                                 label_ids, len(label_list), False)
+    tvars = tf.trainable_variables()
+    initialized_variable_names = {}
+    scaffold_fn = None
+    if init_checkpoint:
+        assignment_map, initialized_variable_names = get_assignment_map_from_checkpoint(tvars, init_checkpoint)
+        tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
+    tf.logging.info("**** Trainable Variables ****")
+    for var in tvars:
+        init_string = ""
+        if var.name in initialized_variable_names:
+            init_string = ", *INIT_FROM_CKPT*"
+            tf.logging.info("  name = %s, shape = %s%s", var.name, var.shape, init_string)
+
+    # Training
+    optimizer = TFOptimizer(loss, Adam(learning_rate))
+    optimizer.optimize(end_trigger=MaxEpoch(nb_epoch))
+
+    print("Finished")
