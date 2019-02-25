818811f47bbd38cfd10476883639e02ffe321122
dding3 <ding.ding@intel.com>
2019-02-25 12:15:21 -0800

    fix ut

--- zoo/src/test/scala/com/intel/analytics/zoo/pipeline/api/keras2/layers/AvtivationSpec.scala
+++ zoo/src/test/scala/com/intel/analytics/zoo/pipeline/api/keras2/layers/AvtivationSpec.scala
@@ -207,36 +207,6 @@ class ActivationSpec extends KerasBaseSpec {
     val input = Tensor[Float](Array(2, 10)).rand()
     compareOutputAndGradInput(blayer, zlayer, input)
   }
-
-  "softmax with 4d input" should "be the same as Keras" in {
-    val kerasCode =
-      """
-        |input_tensor = Input(shape=[2, 2, 2])
-        |input = np.random.random([2, 2, 2, 2])
-        |output_tensor = Activation('softmax')(input_tensor)
-        |model = Model(input=input_tensor, output=output_tensor)
-      """.stripMargin
-    val seq = Sequential[Float]()
-    val layer = Activation[Float]("softmax", inputShape = Shape(2, 2, 2))
-    seq.add(layer)
-    checkOutputAndGrad(seq.asInstanceOf[AbstractModule[Tensor[Float], Tensor[Float], Float]],
-      kerasCode)
-  }
-
-  "softmax with 5d input" should "be the same as Keras" in {
-    val kerasCode =
-      """
-        |input_tensor = Input(shape=[2, 2, 2, 2])
-        |input = np.random.random([2, 2, 2, 2, 2])
-        |output_tensor = Activation('softmax')(input_tensor)
-        |model = Model(input=input_tensor, output=output_tensor)
-      """.stripMargin
-    val seq = Sequential[Float]()
-    val layer = Activation[Float]("softmax", inputShape = Shape(2, 2, 2, 2))
-    seq.add(layer)
-    checkOutputAndGrad(seq.asInstanceOf[AbstractModule[Tensor[Float], Tensor[Float], Float]],
-      kerasCode)
-  }
 }
 
 class ActivationSerialTest extends ModuleSerializationTest {
--- zoo/src/test/scala/com/intel/analytics/zoo/pipeline/api/keras2/layers/SoftmaxSpec.scala
+++ zoo/src/test/scala/com/intel/analytics/zoo/pipeline/api/keras2/layers/SoftmaxSpec.scala
@@ -16,9 +16,46 @@
 
 package com.intel.analytics.zoo.pipeline.api.keras2.layers
 
+import com.intel.analytics.bigdl.nn.abstractnn.AbstractModule
 import com.intel.analytics.bigdl.utils.Shape
 import com.intel.analytics.bigdl.tensor.Tensor
+import com.intel.analytics.zoo.pipeline.api.keras.layers.{Keras2Test, KerasBaseSpec}
+import com.intel.analytics.zoo.pipeline.api.keras.models.Sequential
 import com.intel.analytics.zoo.pipeline.api.keras.serializer.ModuleSerializationTest
+
+class Softmax extends KerasBaseSpec {
+
+  "softmax with 4d input" should "be the same as Keras" taggedAs(Keras2Test) in {
+    val kerasCode =
+      """
+        |input_tensor = Input(shape=[2, 2, 2])
+        |input = np.random.random([2, 2, 2, 2])
+        |output_tensor = Activation('softmax')(input_tensor)
+        |model = Model(input=input_tensor, output=output_tensor)
+      """.stripMargin
+    val seq = Sequential[Float]()
+    val layer = Activation[Float]("softmax", inputShape = Shape(2, 2, 2))
+    seq.add(layer)
+    checkOutputAndGrad(seq.asInstanceOf[AbstractModule[Tensor[Float], Tensor[Float], Float]],
+      kerasCode)
+  }
+
+  "softmax with 5d input" should "be the same as Keras" taggedAs(Keras2Test) in {
+    val kerasCode =
+      """
+        |input_tensor = Input(shape=[2, 2, 2, 2])
+        |input = np.random.random([2, 2, 2, 2, 2])
+        |output_tensor = Activation('softmax')(input_tensor)
+        |model = Model(input=input_tensor, output=output_tensor)
+      """.stripMargin
+    val seq = Sequential[Float]()
+    val layer = Activation[Float]("softmax", inputShape = Shape(2, 2, 2, 2))
+    seq.add(layer)
+    checkOutputAndGrad(seq.asInstanceOf[AbstractModule[Tensor[Float], Tensor[Float], Float]],
+      kerasCode)
+  }
+
+}
 
 class SoftMaxSerialTest extends ModuleSerializationTest {
   override def test(): Unit = {
