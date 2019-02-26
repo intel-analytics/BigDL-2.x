86f30a3a91b98b4c8adb787fd19f1875431bc0f0
hkvision <huangkaivision@gmail.com>
2019-02-26 09:39:37 +0800

    update

--- pyzoo/zoo/examples/tensorflow/distributed_training/bert_run_classifier.py
+++ pyzoo/zoo/examples/tensorflow/distributed_training/bert_run_classifier.py
@@ -30,7 +30,7 @@ def to_list_numpy(feature):
             np.array(feature.segment_ids), np.array(feature.label_id)]
 
 
-def generate_tf_dataset(examples, seq_len, batch_per_thread):
+def generate_tf_dataset(examples, seq_len, batch_size):
     features = convert_examples_to_features(examples, label_list, seq_len, tokenizer)
     features = [to_list_numpy(feature) for feature in features]
     rdd = getOrCreateSparkContext().parallelize(features)
@@ -38,17 +38,18 @@ def generate_tf_dataset(examples, seq_len, batch_per_thread):
                               names=["input_ids", "input_mask", "segment_ids", "label_id"],
                               shapes=[[seq_len], [seq_len], [seq_len], [1]],
                               types=[tf.int32, tf.int32, tf.int32, tf.int32],
-                              batch_per_thread=batch_per_thread)
+                              batch_size=batch_size)
 
 
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("--bert_base_dir", dest="bert_base_dir")
     parser.add_option("--data_dir", dest="data_dir")
-    parser.add_option("--batch_per_thread", dest="batch_per_thread", type=int, default=8)
+    parser.add_option("--output_dir", dest="output_dir")
+    parser.add_option("--batch_size", dest="batch_size", type=int, default=112)
     parser.add_option("--max_seq_length", dest="max_seq_length", type=int, default=128)
-    parser.add_option("-e", "--nb_epoch", dest="nb_epoch", type=int, default=50)
-    parser.add_option("-l", "--learning_rate", dest="learning_rate", type=float, default=1e-6)
+    parser.add_option("-e", "--nb_epoch", dest="nb_epoch", type=int, default=3)
+    parser.add_option("-l", "--learning_rate", dest="learning_rate", type=float, default=2e-5)
 
     (options, args) = parser.parse_args(sys.argv)
     # Model and data files
@@ -63,9 +64,9 @@ if __name__ == '__main__':
     label_list = processor.get_labels()
     tokenizer = FullTokenizer(vocab_file, False)
     train_examples = processor.get_train_examples(options.data_dir)
-    train_dataset = generate_tf_dataset(train_examples, options.max_seq_length, options.batch_per_thread)
+    train_dataset = generate_tf_dataset(train_examples, options.max_seq_length, options.batch_size)
     eval_examples = processor.get_dev_examples(options.data_dir)
-    eval_dataset = generate_tf_dataset(eval_examples, options.max_seq_length, options.batch_per_thread)
+    eval_dataset = generate_tf_dataset(eval_examples, options.max_seq_length, options.batch_size)
 
     # Model loading and construction
     input_ids, input_mask, segment_ids, label_ids = train_dataset.tensors
@@ -89,5 +90,7 @@ if __name__ == '__main__':
     # Training
     optimizer = TFOptimizer(loss, Adam(options.learning_rate))
     optimizer.optimize(end_trigger=MaxEpoch(options.nb_epoch))
+    saver = tf.train.Saver()
+    saver.save(optimizer.sess, options.output_dir + "/model")
 
     print("Finished")
