0f6471b8787de61c19460c56e224bada6f5f7f81
Deliadd <chinadingdi@gmail.com>
2019-03-06 15:57:39 +0800

    update

--- pyzoo/test/zoo/pipeline/autograd/test_operator.py
+++ pyzoo/test/zoo/pipeline/autograd/test_operator.py
@@ -537,6 +537,16 @@ class TestOperator(ZooTestCase):
         self.compare_binary_op(k_func,
                                Lambda(function=z_func, ), [[2, 3, 4], [2, 3, 4]])
 
+    def test_ExpandDim(self):
+        input = Parameter(shape=(3,))
+        expand = ExpandDim(dim=0)(input)
+        model = Model(input, expand)
+        assert model.get_output_shape() == (1, 3)
+        inputdata = np.array([2, 1, 6])
+        desired = inputdata.reshape(1, 3)
+        outputdata = model.forward(inputdata)
+        np.testing.assert_almost_equal(outputdata, desired, decimal=4)
+
 
 if __name__ == "__main__":
     pytest.main([__file__])
