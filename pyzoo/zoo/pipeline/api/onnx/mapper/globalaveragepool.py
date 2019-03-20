4d513895dd18cc3f693f9db5499d8475aee14ae1
Xinqi Liu <40779025+cynthia-liu@users.noreply.github.com>
2019-03-20 11:01:13 +0800

    Update globalaveragepool.py

--- pyzoo/zoo/pipeline/api/onnx/mapper/globalaveragepool.py
+++ pyzoo/zoo/pipeline/api/onnx/mapper/globalaveragepool.py
@@ -27,5 +27,9 @@ class GlobalAveragePoolMapper(OperatorMapper):
 
     def _to_tensor(self):
         x = self.model_inputs[0].zvalue
-        y = zautograd.expand_dims(zlayers.GlobalAveragePooling2D()(x), 2)
-        return zautograd.expand_dims(y, 3)
+        y = zlayers.GlobalAveragePooling2D()(x)
+        # Input data tensor from the previous operator; dimensions for image case are (N x C x H x W),
+        # where N is the batch size, C is the number of channels, and H and W are the height and the width of the data.
+        # Output data tensor from pooling across the input tensor. Dimensions will be N x C x 1 x 1
+        return zautograd.expand_dims(zautograd.expand_dims(y, 2), 3)
+ 
