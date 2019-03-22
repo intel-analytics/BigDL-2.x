29a9b48e57b7370a747e2d44b43f1279b389bc62
Deliadd <chinadingdi@gmail.com>
2019-03-22 16:47:49 +0800

    update

--- pyzoo/test/zoo/pipeline/onnx/test_model_loading.py
+++ pyzoo/test/zoo/pipeline/onnx/test_model_loading.py
@@ -16,11 +16,9 @@
 
 
 from test.zoo.pipeline.utils.test_utils_onnx import OnnxTestCase
-from zoo.pipeline.api.autograd import Parameter, Constant
 from zoo.pipeline.api.keras.layers import *
 import numpy as np
 
-from zoo.pipeline.api.keras.models import Model
 
 np.random.seed(1337)  # for reproducibility
 import torch
@@ -1433,67 +1431,3 @@ class TestModelLoading(OnnxTestCase):
 
         output = OnnxLoader.run_node(node, [x])
         np.testing.assert_almost_equal(output["y"], y, decimal=5)
-
-    def test_mean_1D(self):
-        data = np.random.randn(2,)
-        parameter = Parameter(shape=(2,), init_weight=data)
-        out = autograd.mean(parameter, axis=0)
-        model = Model(input=parameter, output=out)
-        result = model.forward(data)
-        np.testing.assert_almost_equal(result, data.mean(axis=0), decimal=5)
-
-    def test_mean_2D(self):
-        data = np.random.randn(2, 3)
-        parameter = Parameter(shape=(2, 3), init_weight=data)
-        out = autograd.mean(parameter, axis=0)
-        model = Model(input=parameter, output=out)
-        result = model.forward(data)
-        np.testing.assert_almost_equal(result, data.mean(axis=0), decimal=5)
-
-    def test_slice_1D(self):
-        data = np.random.randn(4, )
-        parameter = Parameter(shape=(4,), init_weight=data)
-        out = parameter.slice(0, 0, 2)
-        model = Model(input=parameter, output=out)
-        result = model.forward(data)
-        np.testing.assert_almost_equal(result, data[0:2], decimal=5)
-
-    def test_slice_2D(self):
-        data = np.random.randn(2, 3)
-        parameter = Parameter(shape=(2, 3), init_weight=data)
-        out = parameter.slice(0, 0, 2)
-        model = Model(input=parameter, output=out)
-        result = model.forward(data)
-        np.testing.assert_almost_equal(result, data[0:2], decimal=5)
-
-    def test_unsqueeze_1D(self):
-        data = np.random.randn(4, )
-        parameter = Parameter(shape=(4,), init_weight=data)
-        out = autograd.expand_dims(parameter, axis=0)
-        model = Model(input=parameter, output=out)
-        result = model.forward(data)
-        np.testing.assert_almost_equal(result, np.expand_dims(data, axis=0), decimal=5)
-
-    def test_unsqueeze_2D(self):
-        data = np.random.randn(2, 3)
-        parameter = Parameter(shape=(2, 3), init_weight=data)
-        out = autograd.expand_dims(parameter, axis=0)
-        model = Model(input=parameter, output=out)
-        result = model.forward(data)
-        np.testing.assert_almost_equal(result, np.expand_dims(data, axis=0), decimal=5)
-
-    def test_sum_1D(self):
-        data = np.random.randn(2, )
-        parameter = Parameter(shape=(2,), init_weight=data)
-        out = autograd.sum(parameter, axis=0)
-        model = Model(input=parameter, output=out)
-        result = model.forward(data)
-        np.testing.assert_almost_equal(result, data.sum(axis=0), decimal=5)
-
-    def test_sum_2D(self):
-        data = np.random.randn(2, 3)
-        parameter = Parameter(shape=(2, 3), init_weight=data)
-        out = autograd.sum(parameter, axis=0)
-        model = Model(input=parameter, output=out)
-        result = model.forward(data)
-        np.testing.assert_almost_equal(result, data.sum(axis=0), decimal=5)
