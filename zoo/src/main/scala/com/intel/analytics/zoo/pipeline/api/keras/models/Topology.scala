2e6f9f624fd34019d427466e93e7d4c14520088f
Jiaming <litchilitchy@qq.com>
2019-03-27 14:43:04 +0800

    change encap

--- zoo/src/main/scala/com/intel/analytics/zoo/pipeline/api/keras/models/Topology.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/pipeline/api/keras/models/Topology.scala
@@ -382,9 +382,13 @@ abstract class KerasNet[T](implicit val tag: ClassTag[T], implicit val ev: Tenso
    *
    * @param dataSet Target DataSet to release
    */
-  def releaseDataSet(dataSet: DataSet[MiniBatch[T]]): Unit = {
-    dataSet.toDistributed().unpersist()
-    dataSet.toDistributed().originRDD().unpersist()
+  def releaseDataSets(dataSets: Array[DataSet[MiniBatch[T]]]): Unit = {
+    for (ds <- dataSets) {
+      if (ds != null) {
+        ds.toDistributed().unpersist()
+        ds.toDistributed().originRDD().unpersist()
+      }
+    }
   }
 
   /**
@@ -407,10 +411,7 @@ abstract class KerasNet[T](implicit val tag: ClassTag[T], implicit val ev: Tenso
     val valData = toDataSet(validationData, batchSize, featurePaddingParam, labelPaddingParam)
     this.fit(trainData, nbEpoch, valData)
 
-    releaseDataSet(trainData)
-    if (valData != null) {
-      releaseDataSet(valData)
-    }
+    releaseDataSets(Array(trainData, valData))
   }
 
   /**
@@ -431,12 +432,7 @@ abstract class KerasNet[T](implicit val tag: ClassTag[T], implicit val ev: Tenso
     val valData = toDataSet(validationData, batchSize)
     this.fit(trainData, nbEpoch, valData)
 
-    if (x.isDistributed()) {
-      releaseDataSet(trainData)
-      if (valData != null) {
-        releaseDataSet(valData)
-      }
-    }
+    releaseDataSets(Array(trainData, valData))
   }
 
   def fit(
