e334dcd319e257e5017dc3bd0cb4f246bcc7caec
jenniew <jenniewang123@gmail.com>
2019-04-08 11:37:09 -0700

    update test case

--- zoo/src/test/scala/com/intel/analytics/zoo/models/image/objectdetection/ssd/SSDSpec.scala
+++ zoo/src/test/scala/com/intel/analytics/zoo/models/image/objectdetection/ssd/SSDSpec.scala
@@ -17,12 +17,15 @@
 package com.intel.analytics.zoo.models.image.objectdetection.ssd
 
 import com.intel.analytics.bigdl.tensor.Tensor
+import com.intel.analytics.bigdl.utils.Table
 import com.intel.analytics.zoo.common.NNContext
 import com.intel.analytics.zoo.feature.image._
 import com.intel.analytics.zoo.models.common.ZooModel
+import com.intel.analytics.zoo.models.image.objectdetection.ObjectDetector
 import com.intel.analytics.zoo.models.image.objectdetection.common.dataset.Imdb
 import com.intel.analytics.zoo.pipeline.api.keras.ZooSpecHelper
 import com.intel.analytics.zoo.pipeline.api.keras.serializer.ModuleSerializationTest
+import org.apache.log4j.Logger
 import org.apache.spark.{SparkConf, SparkContext}
 import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
@@ -59,14 +62,16 @@ class SSDSpec extends FlatSpec with Matchers with BeforeAndAfter {
       RoiImageToSSDBatch(2)
     val out = imgAug(roidb)
     val input = out.next()
-    val output = model.forward(input.input)
+    val output = model.forward(input.getInput)
+
   }
 
 }
 
 class SSDVGGSerialTest extends ModuleSerializationTest {
+  protected val logger = Logger.getLogger(getClass)
   override def test(): Unit = {
-    val model = SSDVGG[Float](20)
+    val model1 = SSDVGG[Float](20)
 
     val resource = getClass().getClassLoader().getResource("VOCdevkit")
     val voc = Imdb.getImdb("voc_2007_testcode", resource.getPath)
@@ -83,7 +88,14 @@ class SSDVGGSerialTest extends ModuleSerializationTest {
       RoiImageToSSDBatch(2)
     val out = imgAug(roidb)
     val input = out.next()
-    ZooSpecHelper.testZooModelLoadSave(model.asInstanceOf[ZooModel[Tensor[Float], Tensor[Float],
-      Float]], input.input, ZooModel.loadModel[Float])
+    val serFile = java.io.File.createTempFile("UnitTest", "SSDSpecBase")
+    logger.info(s"created file $serFile")
+    model1.saveModel(serFile.getAbsolutePath, overWrite = true)
+    val model2 = ZooModel.loadModel[Float](serFile.getAbsolutePath)
+    val output1 = model1.forward(input.getInput)
+    val output2 = model2.forward(input.getInput)
+    output2.asInstanceOf[Table].keySet.sameElements(output1.asInstanceOf[Table].keySet) should be (true)
+    output2.asInstanceOf[Table].equals(output1.asInstanceOf[Table]) should be (true)
+    if (serFile.exists()) serFile.delete()
   }
 }
