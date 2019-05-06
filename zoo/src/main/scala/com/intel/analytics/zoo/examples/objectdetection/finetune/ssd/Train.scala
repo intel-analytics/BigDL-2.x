df863e6eba50616bad2405f6787bb2fa9be1ec6e
jenniew <jenniewang123@gmail.com>
2019-05-06 13:18:29 -0700

    remove retry setting

--- zoo/src/main/scala/com/intel/analytics/zoo/examples/objectdetection/finetune/ssd/Train.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/examples/objectdetection/finetune/ssd/Train.scala
@@ -119,8 +119,6 @@ object Train {
 
   def main(args: Array[String]): Unit = {
     trainParser.parse(args, TrainParams()).map(param => {
-      System.setProperty("bigdl.failure.retryTimes", "1")
-
       // initial zoo context
       val conf = new SparkConf().setAppName(param.jobName)
       val sc = NNContext.initNNContext(conf)
