197daa19f03bf546d7e699bb9224af0f8acad6a2
hkvision <huangkaivision@gmail.com>
2019-05-13 20:44:30 +0800

    change to image resize

--- zoo/src/main/scala/com/intel/analytics/zoo/models/image/imageclassification/ImageClassificationConfig.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/models/image/imageclassification/ImageClassificationConfig.scala
@@ -135,7 +135,7 @@ object ImagenetConfig {
 
   // Preprocessor for ResNet50 pre-trained in BigDL
   def bigdlResNetPreprocessor(): Preprocessing[ImageFeature, ImageFeature] = {
-      ImageRandomResize(256, 256) ->
+      ImageResize(256, 256) ->
       ImageCenterCrop(224, 224) ->
       ImageChannelScaledNormalizer(104, 117, 123, 0.0078125) ->
       ImageMatToTensor() -> ImageSetToSample()
