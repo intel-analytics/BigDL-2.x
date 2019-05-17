a6462db5a78ac8a6b4ec28f83e1221fac07c825b
Guoqiong Song <guoqiongsong@gmail.com>
2019-05-17 07:37:54 -0700

    add recommenders in Keras style (#1137)
    * add keras style recommender, python + scala, and related notebooks, will have a separate PR for doc

--- scala/com/intel/analytics/zoo/models/recommendation/WideAndDeepSpec.scala
+++ scala/com/intel/analytics/zoo/models/recommendation/WideAndDeepSpec.scala
@@ -20,11 +20,12 @@ import java.net.URL
 
 import com.intel.analytics.bigdl.nn.ClassNLLCriterion
 import com.intel.analytics.bigdl.nn.abstractnn.Activity
-import com.intel.analytics.bigdl.optim.{Adam, Optimizer, Trigger}
+import com.intel.analytics.bigdl.optim.{Adam, Optimizer, Top1Accuracy, Trigger}
 import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.bigdl.utils.{T, Table}
 import com.intel.analytics.zoo.common.NNContext
 import com.intel.analytics.zoo.pipeline.api.keras.ZooSpecHelper
+import com.intel.analytics.zoo.pipeline.api.keras.objectives.SparseCategoricalCrossEntropy
 import com.intel.analytics.zoo.pipeline.api.keras.serializer.ModuleSerializationTest
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.{SparkConf, SparkContext}
@@ -71,30 +72,63 @@ class WideAndDeepSpec extends ZooSpecHelper {
 
     val data = datain
       .rdd.map(r => Utils.getWideTensor(r, columnInfo))
-    data.map { input =>
+    data.take(10).map { x =>
+      val input = x.resize(Array(1, 124), 3)
       val output = model.forward(input)
       val gradInput = model.backward(input, output)
-    }.count()
-
+    }
   }
 
-
-  "WideAndDeep deep model indicator forward and backward" should "work properly" in {
+  "WideAndDeep indicator forward and backward" should "work properly" in {
     val columnInfo = ColumnFeatureInfo(
       indicatorCols = Array("occupation", "gender"),
       indicatorDims = Array(21, 3))
     val model = WideAndDeep[Float]("deep", 5, columnInfo)
 
     val data = datain
-      .rdd.map(r => Utils.getDeepTensor(r, columnInfo))
-    data.map{ input =>
-      val feature : Tensor[Float] = input.reshape(Array(1, input.size(1)))
+      .rdd.map(r => Utils.row2Sample(r, columnInfo, "deep").feature())
+    data.map { input =>
+      val feature: Tensor[Float] = input.reshape(Array(1, input.size(1)))
       val output = model.forward(feature)
       val gradInput = model.backward(feature, output)
     }.count()
   }
 
-  "WideAndDeep deep model embedding and continuous part" should "work properly" in {
+  "WideAndDeep embedding forward and backward" should "work properly" in {
+    val columnInfo = ColumnFeatureInfo(
+      embedCols = Array("userId", "itemId"),
+      embedInDims = Array(100, 100),
+      embedOutDims = Array(20, 20))
+
+    val model = WideAndDeep[Float]("deep", 5, columnInfo)
+    val data = datain
+      .rdd.map(r => Utils.row2Sample(r, columnInfo, "deep").feature())
+
+    data.map { input =>
+      val feature: Tensor[Float] = input.reshape(Array(1, input.size(1)))
+      val output = model.forward(feature)
+      val gradInput = model.backward(feature, output)
+    }.count()
+  }
+
+  "WideAndDeep continuous forward and backward" should "work properly" in {
+    val columnInfo = ColumnFeatureInfo(
+      continuousCols = Array("age")
+    )
+    val model = WideAndDeep[Float]("deep", 5, columnInfo)
+
+    val data = datain
+      .rdd.map(r =>
+      Utils.row2Sample(r, columnInfo, "deep").feature())
+
+    data.map { input =>
+      val feature: Tensor[Float] = input.reshape(Array(1, input.size(1)))
+      val output = model.forward(feature)
+      val gradInput = model.backward(feature, output)
+    }.count()
+  }
+
+  "WideAndDeep deep model forward and backward" should "work properly" in {
     val columnInfo = ColumnFeatureInfo(
       indicatorCols = Array("occupation", "gender"),
       indicatorDims = Array(21, 3),
@@ -105,11 +139,11 @@ class WideAndDeepSpec extends ZooSpecHelper {
     )
     val model = WideAndDeep[Float]("deep", 5, columnInfo)
 
-    val data: RDD[Tensor[Float]] = datain
-      .rdd.map(r => Utils.getDeepTensor(r, columnInfo))
+    val data = datain
+      .rdd.map(r => Utils.getDeepTensors(r, columnInfo))
 
-    data.map{ input =>
-      val feature : Tensor[Float] = input.reshape(Array(1, input.size(1)))
+    data.map { input =>
+      val feature: Table = T.array(input.map(x => x.reshape(Array(1, x.size(1)))))
       val output = model.forward(feature)
       val gradInput = model.backward(feature, output)
     }.count()
@@ -131,9 +165,10 @@ class WideAndDeepSpec extends ZooSpecHelper {
 
     val data: RDD[Activity] = datain
       .rdd.map(r => {
-      val wideTensor: Tensor[Float] = Utils.getWideTensor(r, columnInfo)
-      val deepTensor: Tensor[Float] = Utils.getDeepTensor(r, columnInfo)
-      T(T(wideTensor, deepTensor))
+      val wideTensor: Tensor[Float] = Utils.getWideTensor(r, columnInfo).resize(Array(1, 124), 3)
+      val deepTensor: Array[Tensor[Float]] = Utils.getDeepTensors(r, columnInfo)
+          .map(x => x.reshape(Array(1, x.size(1))))
+      T.array(Array(wideTensor) ++ deepTensor)
     })
     data.map { input =>
       val output = model.forward(input)
@@ -141,139 +176,136 @@ class WideAndDeepSpec extends ZooSpecHelper {
     }.count()
   }
 
-  "WideAndDeep full model implicit forward and backward" should "work properly" in {
-    val columnInfo = ColumnFeatureInfo(
-      wideBaseCols = Array("occupation", "gender"),
-      wideBaseDims = Array(21, 3),
-      wideCrossCols = Array("occupation-gender"),
-      wideCrossDims = Array(100),
-      indicatorCols = Array("occupation", "gender"),
-      indicatorDims = Array(21, 3),
-      embedCols = Array("userId", "itemId"),
-      embedInDims = Array(100, 100),
-      embedOutDims = Array(20, 20),
-      continuousCols = Array("age"))
-    val model = WideAndDeep[Float]("wide_n_deep", 2, columnInfo)
+    "WideAndDeep wide fit and compile" should "work properly" in {
 
-    val negativeDF = Utils.getNegativeSamples(datain)
-    val userDF = datain.select("userId", "occupation", "gender", "age")
+      val columnInfo = ColumnFeatureInfo(
+        wideBaseCols = Array("occupation", "gender"),
+        wideBaseDims = Array(21, 3),
+        wideCrossCols = Array("occupation-gender"),
+        wideCrossDims = Array(100))
+      val widendeep = WideAndDeep[Float]("wide", 5, columnInfo)
 
-    val unioned = negativeDF.join(userDF, Array("userId"))
-      .select("occupation", "gender", "age", "userId", "itemId")
-      .unionAll(datain.select("occupation", "gender", "age", "userId", "itemId"))
-      .withColumn("occupation-gender", bucketUDF(col("occupation"), col("gender")))
+      val data = datain
+        .withColumn("occupation-gender", bucketUDF(col("occupation"), col("gender")))
+        .rdd.map(r => {
+        val uid = r.getAs[Int]("userId")
+        val iid = r.getAs[Int]("itemId")
+        UserItemFeature(uid, iid, Utils.row2Sample(r, columnInfo, "wide"))
+      })
+      val trainRdds = data.map(x => x.sample)
 
-    val data: RDD[Activity] = unioned
-      .rdd.map(r => {
-      val wideTensor: Tensor[Float] = Utils.getWideTensor(r, columnInfo)
-      val deepTensor: Tensor[Float] = Utils.getDeepTensor(r, columnInfo)
-      T(T(wideTensor, deepTensor))
-    })
-    data.map { input =>
-      val output = model.forward(input)
-      val gradInput = model.backward(input, output)
-    }.count()
-  }
+      val optimMethod = new Adam[Float](
+        learningRate = 1e-2,
+        learningRateDecay = 1e-5)
 
-  "WideAndDeep predictUserItemPair" should "have correct predictions" in {
-    val columnInfo = ColumnFeatureInfo(
-      wideBaseCols = Array("occupation", "gender"),
-      wideBaseDims = Array(21, 3),
-      wideCrossCols = Array("occupation-gender"),
-      wideCrossDims = Array(100),
-      indicatorCols = Array("occupation", "gender"),
-      indicatorDims = Array(21, 3),
-      embedCols = Array("userId", "itemId"),
-      embedInDims = Array(100, 100),
-      embedOutDims = Array(20, 20),
-      continuousCols = Array("age"))
-    val widendeep = WideAndDeep[Float]("wide_n_deep", 5, columnInfo)
+      widendeep.compile(optimizer = optimMethod,
+        loss = SparseCategoricalCrossEntropy[Float](zeroBasedLabel = false),
+        metrics = List(new Top1Accuracy[Float]())
+      )
+      widendeep.fit(trainRdds, batchSize = 458,
+        nbEpoch = 1, validationData = trainRdds)
+    }
 
-    val data = datain
-      .withColumn("occupation-gender", bucketUDF(col("occupation"), col("gender")))
-      .rdd.map(r => {
-      val uid = r.getAs[Int]("userId")
-      val iid = r.getAs[Int]("itemId")
-      UserItemFeature(uid, iid, Utils.row2Sample(r, columnInfo, "wide_n_deep"))
-    })
-    val trainRdds = data.map(x => x.sample)
+    "WideAndDeep fit and compile" should " work properly" in {
+      val columnInfo = ColumnFeatureInfo(
+        wideBaseCols = Array("occupation", "gender"),
+        wideBaseDims = Array(21, 3),
+        wideCrossCols = Array("occupation-gender"),
+        wideCrossDims = Array(100),
+        indicatorCols = Array("occupation", "gender"),
+        indicatorDims = Array(21, 3),
+        embedCols = Array("userId", "itemId"),
+        embedInDims = Array(100, 100),
+        embedOutDims = Array(20, 20),
+        continuousCols = Array("age"))
+      val widendeep = WideAndDeep[Float]("wide_n_deep", 5, columnInfo)
 
-    val optimizer = Optimizer(
-      model = widendeep,
-      sampleRDD = trainRdds,
-      criterion = ClassNLLCriterion[Float](),
-      batchSize = 458)
+      val data = datain
+        .withColumn("occupation-gender", bucketUDF(col("occupation"), col("gender")))
+        .rdd.map(r => {
+        val uid = r.getAs[Int]("userId")
+        val iid = r.getAs[Int]("itemId")
+        UserItemFeature(uid, iid, Utils.row2Sample(r, columnInfo, "wide_n_deep"))
+      })
+      val trainRdds = data.map(x => x.sample)
 
-    optimizer
-      .setOptimMethod(new Adam[Float](learningRate = 1e-2, learningRateDecay = 1e-5))
-      .setEndWhen(Trigger.maxEpoch(100))
-      .optimize()
+      val optimMethod = new Adam[Float](
+        learningRate = 1e-2,
+        learningRateDecay = 1e-5)
 
-    val pairPredictions = widendeep.predictUserItemPair(data)
-    val pairPredictionsDF = sqlContext.createDataFrame(pairPredictions).toDF()
-    val out = pairPredictionsDF.join(datain, Array("userId", "itemId"))
-    val correctCounts = out.filter(col("prediction") === col("label")).count()
+      widendeep.compile(optimizer = optimMethod,
+        loss = SparseCategoricalCrossEntropy[Float](zeroBasedLabel = false),
+        metrics = List(new Top1Accuracy[Float]())
+      )
+      widendeep.fit(trainRdds, batchSize = 458,
+        nbEpoch = 20, validationData = trainRdds)
 
-    val accuracy = correctCounts.toDouble / 458
-    // the reference accuracy in this testcase is 0.856
-    assert(accuracy >= 0.40)
-  }
+      val pairPredictions = widendeep.predictUserItemPair(data)
+      val pairPredictionsDF = sqlContext.createDataFrame(pairPredictions).toDF()
+      val out = pairPredictionsDF.join(datain, Array("userId", "itemId"))
+      val correctCounts = out.filter(col("prediction") === col("label")).count()
 
-  "WideAndDeep recommendForItem and recommendForItem" should "work properly" in {
-    val columnInfo = ColumnFeatureInfo(
-      wideBaseCols = Array("occupation", "gender"),
-      wideBaseDims = Array(21, 3),
-      wideCrossCols = Array("occupation-gender"),
-      wideCrossDims = Array(100),
-      indicatorCols = Array("occupation", "gender"),
-      indicatorDims = Array(21, 3),
-      embedCols = Array("userId", "itemId"),
-      embedInDims = Array(100, 100),
-      embedOutDims = Array(20, 20),
-      continuousCols = Array("age"))
-    val widendeep = WideAndDeep[Float]("wide_n_deep", 5, columnInfo)
+      val accuracy = correctCounts.toDouble / 458
+      println(accuracy)
+      // the reference accuracy in this testcase is 0.858
+      assert(accuracy >= 0.30)
+    }
 
-    val data = datain
-      .withColumn("occupation-gender", bucketUDF(col("occupation"), col("gender")))
-      .rdd.map(r => {
-      val uid = r.getAs[Int]("userId")
-      val iid = r.getAs[Int]("itemId")
-      UserItemFeature(uid, iid, Utils.row2Sample(r, columnInfo, "wide_n_deep"))
-    })
-    val trainRdds = data.map(x => x.sample)
+    "WideAndDeep recommendForItem and recommendForUser" should "work properly" in {
+      val columnInfo = ColumnFeatureInfo(
+        wideBaseCols = Array("occupation", "gender"),
+        wideBaseDims = Array(21, 3),
+        wideCrossCols = Array("occupation-gender"),
+        wideCrossDims = Array(100),
+        indicatorCols = Array("occupation", "gender"),
+        indicatorDims = Array(21, 3),
+        embedCols = Array("userId", "itemId"),
+        embedInDims = Array(100, 100),
+        embedOutDims = Array(20, 20),
+        continuousCols = Array("age"))
+      val widendeep = WideAndDeep[Float]("wide_n_deep", 5, columnInfo)
 
-    val optimizer = Optimizer(
-      model = widendeep,
-      sampleRDD = trainRdds,
-      criterion = ClassNLLCriterion[Float](),
-      batchSize = 458)
+      val data = datain
+        .withColumn("occupation-gender", bucketUDF(col("occupation"), col("gender")))
+        .rdd.map(r => {
+        val uid = r.getAs[Int]("userId")
+        val iid = r.getAs[Int]("itemId")
+        UserItemFeature(uid, iid, Utils.row2Sample(r, columnInfo, "wide_n_deep"))
+      })
+      val trainRdds = data.map(x => x.sample)
 
-    optimizer
-      .setOptimMethod(new Adam[Float](learningRate = 1e-2, learningRateDecay = 1e-5))
-      .setEndWhen(Trigger.maxEpoch(100))
-      .optimize()
+      val optimMethod = new Adam[Float](
+        learningRate = 1e-2,
+        learningRateDecay = 1e-5)
 
-    val itemRecs = widendeep.recommendForItem(data, 2)
-    val userRecs = widendeep.recommendForUser(data, 2)
+      widendeep.compile(optimizer = optimMethod,
+        loss = SparseCategoricalCrossEntropy[Float](zeroBasedLabel = false),
+        metrics = List(new Top1Accuracy[Float]())
+      )
+      widendeep.fit(trainRdds, batchSize = 458,
+        nbEpoch = 20, validationData = trainRdds)
 
-    itemRecs.map(x => x.probability).sample(false, 0.1, 1L).collect()
-      .map(x => assert(x >= 0.2))
-    userRecs.map(x => x.probability).sample(false, 0.1, 1L).collect()
-      .map(x => assert(x >= 0.2))
+      val itemRecs = widendeep.recommendForItem(data, 2)
+      val userRecs = widendeep.recommendForUser(data, 2)
 
-    assert(itemRecs.count() <= 200)
-    assert(userRecs.count() <= 200)
-  }
+      itemRecs.map(x => x.probability).sample(false, 0.1, 1L).collect()
+        .map(x => assert(x >= 0.2))
+      userRecs.map(x => x.probability).sample(false, 0.1, 1L).collect()
+        .map(x => assert(x >= 0.2))
 
+      assert(itemRecs.count() <= 200)
+      assert(userRecs.count() <= 200)
+    }
 }
 
 class WideAndDeepSerialTest extends ModuleSerializationTest {
   override def test(): Unit = {
     val columnInfo = ColumnFeatureInfo(
-      indicatorCols = Array("occupation", "gender"),
-      indicatorDims = Array(21, 3))
+      embedCols = Array("occupation", "gender"),
+      embedInDims = Array(21, 21),
+      embedOutDims = Array(4, 4))
     val model = WideAndDeep[Float]("deep", 5, columnInfo)
-    val input = Tensor[Float](Array(100, 50))
+    val input = Tensor[Float](Array(100, 2))
       .fill(new Random(System.nanoTime()).nextInt(20).toFloat + 1)
     ZooSpecHelper.testZooModelLoadSave(model, input, WideAndDeep.loadModel[Float])
   }
