382134ee0a914fc36901cb6fe47c8a7fb27886df
Yuhao Yang <yuhao.yang@intel.com>
2019-05-21 16:07:40 -0700

    lower gcc

--- zoo/src/main/scala/com/intel/analytics/zoo/pipeline/api/net/TorchNet.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/pipeline/api/net/TorchNet.scala
@@ -16,7 +16,10 @@
 
 package com.intel.analytics.zoo.pipeline.api.net
 
+import java.io._
 import java.nio.file.{Files, Paths}
+import java.util.UUID
+import java.util.zip.ZipInputStream
 
 import com.intel.analytics.bigdl.Module
 import com.intel.analytics.bigdl.nn.abstractnn.AbstractModule
@@ -25,25 +28,21 @@ import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
 import com.intel.analytics.zoo.pipeline.api.Predictable
 import org.apache.commons.io.FileUtils
 
+import scala.collection.mutable
 import scala.reflect.ClassTag
 
 /**
  * [[TorchNet]] wraps a TorchScript model as a single layer.
  */
-class TorchNet private(private val path: String)
+class TorchNet private(private val registryID: String)
   extends AbstractModule[Tensor[Float], Tensor[Float], Float] with Predictable[Float] {
-
-  /**
-   * binary content of model in {path}, used as model serialization during broadcast.
-   */
-  private val modelbytes = Files.readAllBytes(Paths.get(path))
 
   /**
    * mark the model as transient and reload TorchNet from byteArray on executors
    */
   @transient
   private lazy val torchModel = {
-    TorchNet.load(modelbytes)
+    TorchNet.load(registryID)
   }
 
   override def parameters(): (Array[Tensor[Float]], Array[Tensor[Float]]) = {
@@ -71,7 +70,9 @@ class TorchNet private(private val path: String)
 
 object TorchNet {
 
-  loadPytorch() // run once per JVM
+  loadPytorch() // load once per JVM
+
+  private val modelRegistry = mutable.HashMap.empty[String, Array[Byte]]
 
   /**
    * Create a TorchNet from a saved TorchScript Model
@@ -80,14 +81,17 @@ object TorchNet {
    */
   def apply(path: String): TorchNet = {
     //TODO: add support for HDFS path
-    new TorchNet(path)
+    val modelbytes = Files.readAllBytes(Paths.get(path))
+    val uuid = UUID.randomUUID().toString
+    modelRegistry.put(uuid, modelbytes)
+    new TorchNet(uuid)
   }
 
   // extract libs from zoo jar file
   private def loadPytorch(): Unit = {
     val tmpDir = com.google.common.io.Files.createTempDir()
     val libStream = TorchNet.getClass.getResourceAsStream(s"/pytorch/lib.zip")
-    TARCompress.unzip(libStream, tmpDir)
+    unzip(libStream, tmpDir)
 
     try {
       System.load(tmpDir + "/libtorch.so")
@@ -98,8 +102,36 @@ object TorchNet {
     }
   }
 
-  private def load(bytes: Array[Byte]): PytorchModel = {
-    new PytorchModel().load(bytes)
+  private def unzip(inputStream: InputStream, outputPath: File) {
+    try {
+      val buffer = new Array[Byte](2048)
+      val istream = new ZipInputStream(inputStream)
+      var entry = istream.getNextEntry
+      while (entry != null) {
+        val entryDestination = new File(outputPath, entry.getName)
+        if (entry.isDirectory) entryDestination.mkdirs
+        else {
+          entryDestination.getParentFile.mkdirs
+          val fos = new FileOutputStream(entryDestination)
+          val bos = new BufferedOutputStream(fos, buffer.length)
+          var len = istream.read(buffer)
+          while (len > 0) {
+            bos.write(buffer, 0, len)
+            len = istream.read(buffer)
+          }
+          bos.close()
+        }
+        entry = istream.getNextEntry
+      }
+    } catch {
+      case io: IOException =>
+        System.out.println("error during loading loading pytorch libs")
+        throw io
+    }
+  }
+
+  private def load(uuid: String): PytorchModel = {
+    new PytorchModel().load(modelRegistry(uuid))
   }
 
 }
