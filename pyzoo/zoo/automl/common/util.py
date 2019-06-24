5ba748b652ef7d4cf7abd650ecc6141f6b43b62b
Shan Yu <yushan@ShandeMacBook-Pro-2.local>
2019-06-24 14:09:51 +0800

    Add rules for np.integer and np.floating in NumpyEncoder

--- pyzoo/zoo/automl/common/util.py
+++ pyzoo/zoo/automl/common/util.py
@@ -90,7 +90,11 @@ class NumpyEncoder(json.JSONEncoder):
     convert numpy array to list for JSON serialize
     """
     def default(self, obj):
-        if isinstance(obj, np.ndarray):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
 
