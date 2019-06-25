eb768df7cd862c3a4fe5b186e9ee97b66d270df7
Xin Qiu <qiuxin2012@users.noreply.github.com>
2019-06-25 11:12:03 +0800

    change to 0.0.0.0 (#1476)

--- scripts/jupyter-with-zoo.sh
+++ scripts/jupyter-with-zoo.sh
@@ -13,7 +13,7 @@ fi
 
 # setup paths
 export PYSPARK_DRIVER_PYTHON=jupyter
-export PYSPARK_DRIVER_PYTHON_OPTS="notebook --notebook-dir=./ --ip=* --no-browser --allow-root --NotebookApp.token=''"
+export PYSPARK_DRIVER_PYTHON_OPTS="notebook --notebook-dir=./ --ip=0.0.0.0 --no-browser --allow-root --NotebookApp.token=''"
 export ANALYTICS_ZOO_JAR=`find ${ANALYTICS_ZOO_HOME}/lib -type f -name "analytics-zoo*jar-with-dependencies.jar"`
 export ANALYTICS_ZOO_PY_ZIP=`find ${ANALYTICS_ZOO_HOME}/lib -type f -name "analytics-zoo*python-api.zip"`
 export ANALYTICS_ZOO_CONF=${ANALYTICS_ZOO_HOME}/conf/spark-analytics-zoo.conf
