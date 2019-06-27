3229e58300570b6fab2d4403e6504d4c66bd9b1a
Yuhao Yang <yuhao.yang@intel.com>
2019-06-27 16:34:44 -0700

    [Pytorch] correct offset for inference (#1481)
    * correct offset
    
    * unit test
    
    * style

--- test_torch_net.py
+++ test_torch_net.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 #
 
+import torch
 import torchvision
 import numpy as np
 import pytest
@@ -23,13 +24,17 @@ from zoo.pipeline.api.net.torch_net import TorchNet
 
 
 class TestTF(ZooTestCase):
-
     def test_torch_net_predict(self):
-        model = torchvision.models.resnet18()
+        model = torchvision.models.resnet18(pretrained=True).eval()
         net = TorchNet.from_pytorch(model, [1, 3, 224, 224])
 
-        result = net.predict(np.random.rand(1, 3, 224, 224))
-        print(result.collect())
+        dummpy_input = torch.ones(1, 3, 224, 224)
+        result = net.predict(dummpy_input.numpy()).collect()
+        assert np.allclose(result[0][0:5].tolist(),
+                           np.asarray(
+                               [-0.03913354128599167, 0.11446280777454376, -1.7967549562454224,
+                                -1.2342952489852905, -0.819004476070404]))
+
 
 if __name__ == "__main__":
     pytest.main([__file__])
