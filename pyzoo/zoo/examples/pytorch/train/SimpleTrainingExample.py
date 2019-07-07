22fff6809810e7f2944436e2dd76f811e8827121
Yuhao Yang <yuhao.yang@intel.com>
2019-07-06 22:31:29 -0700

    update example

--- pyzoo/zoo/examples/pytorch/train/SimpleTrainingExample.py
+++ pyzoo/zoo/examples/pytorch/train/SimpleTrainingExample.py
@@ -1,37 +1,31 @@
-import torch
-
-from zoo.pipeline.api.net.torch_net import TorchNet, TorchIdentityCriterion
-from zoo import init_nncontext
-import numpy as np
-
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 from bigdl.nn.criterion import *
 from bigdl.nn.layer import *
 from bigdl.optim.optimizer import Adam
 from pyspark.sql.types import *
+from zoo import init_nncontext
 from zoo.common.nncontext import *
+from zoo.pipeline.api.net.torch_net import TorchNet, TorchIdentityCriterion
 from zoo.pipeline.nnframes import *
 
 
 # create training data as Spark DataFrame
-def get_classifier_df(sqlContext):
+def get_df(sqlContext):
     data = sc.parallelize([
-        ((2.0, 1.0), 1.0),
-        ((1.0, 2.0), 0.0),
-        ((2.0, 1.0), 1.0),
-        ((1.0, 2.0), 0.0),
-        ((2.0, 1.0), 1.0),
-        ((1.0, 2.0), 0.0),
-        ((2.0, 1.0), 1.0),
-        ((1.0, 2.0), 0.0)
+        ((2.0, 1.0), (1.0, 0.0, 3.0)),
+        ((1.0, 2.0), (1.0, 2.0, 3.0)),
+        ((2.0, 1.0), (1.0, 0.0, 3.0)),
+        ((1.0, 2.0), (1.0, 2.0, 3.0)),
+        ((2.0, 1.0), (1.0, 0.0, 3.0)),
+        ((1.0, 2.0), (1.0, 2.0, 3.0)),
+        ((2.0, 1.0), (1.0, 0.0, 3.0)),
+        ((1.0, 2.0), (1.0, 2.0, 3.0))
     ])
 
     schema = StructType([
         StructField("features", ArrayType(DoubleType(), False), False),
-        StructField("label", DoubleType(), False)])
+        StructField("label", ArrayType(DoubleType(), False), False)])
     df = sqlContext.createDataFrame(data, schema)
     return df
 
@@ -39,29 +33,33 @@ def get_classifier_df(sqlContext):
 class SimpleTorchModel(nn.Module):
     def __init__(self):
         super(SimpleTorchModel, self).__init__()
-        self.dense1 = nn.Linear(2, 1)
+        self.dense1 = nn.Linear(2, 4)
+        self.dense2 = nn.Linear(4, 8)
+        self.dense3 = nn.Linear(8, 3)
 
     def forward(self, x):
         x = self.dense1(x)
+        x = self.dense2(x)
+        x = F.relu(self.dense3(x))
         return x
 
 if __name__ == '__main__':
     sparkConf = init_spark_conf().setAppName("testNNClassifer").setMaster('local[1]')
     sc = init_nncontext(sparkConf)
     sqlContext = SQLContext(sc)
-    df = get_classifier_df(sqlContext)
+    df = get_df(sqlContext)
 
     torch_model = SimpleTorchModel()
     def customLoss(output, label):
         return ((output - label) * (output - label)).sum()
 
     model = TorchNet.from_pytorch(torch_model, [1, 2], customLoss, [1, 1], [1, 1])
-    classifier = NNClassifier(model, TorchIdentityCriterion(), SeqToTensor([2])) \
+    classifier = NNEstimator(model, TorchIdentityCriterion(), SeqToTensor([2]), SeqToTensor([3])) \
         .setBatchSize(4) \
         .setOptimMethod(Adam()) \
-        .setLearningRate(0.1).setMaxEpoch(40)
+        .setLearningRate(0.1).setMaxEpoch(10)
 
     nnClassifierModel = classifier.fit(df)
     res = nnClassifierModel.transform(df)
-    res.show()
+    res.show(10, False)
 
