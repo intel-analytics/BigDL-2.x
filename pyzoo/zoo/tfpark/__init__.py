da1bce5c995fd7946b6386e9045d588c062b534b
bozhou <bo.zhou@intel.com>
2019-07-10 15:59:41 +0800

    modified tfpark init

--- pyzoo/zoo/tfpark/__init__.py
+++ pyzoo/zoo/tfpark/__init__.py
@@ -14,6 +14,5 @@
 # limitations under the License.
 #
 
-from zoo.pipeline.api.net import TFDataset
-from zoo.pipeline.api.net import TFOptimizer
+from zoo.pipeline.api.net import TFDataset, TFOptimizer
 from zoo.tfpark.model import KerasModel
