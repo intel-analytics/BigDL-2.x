8566116899df6331e0ab3ca3332ce214e622a0c0
Shan Yu <shan.yu@intel.com>
2019-07-18 13:52:42 +0800

    [unfinished] modify UT to cut down run time. Pipeline time sequence UT need merge new smoke recipe). Refer to last commit for reliable version

--- pyzoo/test/zoo/automl/model/test_VanillaLSTM.py
+++ pyzoo/test/zoo/automl/model/test_VanillaLSTM.py
@@ -23,17 +23,25 @@ from zoo.automl.feature.time_sequence import TimeSequenceFeatureTransformer
 
 class TestVanillaLSTM(ZooTestCase):
 
-    def test_fit_eval(self):
+    def setup_method(self, method):
+        super().setup_method(method)
         train_data = pd.DataFrame(data=np.random.randn(64, 4))
+        val_data = pd.DataFrame(data=np.random.randn(16, 4))
+        test_data = pd.DataFrame(data=np.random.randn(16, 4))
+
         future_seq_len = 1
         past_seq_len = 6
 
         # use roll method in time_sequence
         tsft = TimeSequenceFeatureTransformer()
-        x_train, y_train = tsft._roll_train(train_data,
-                                            past_seq_len=past_seq_len,
-                                            future_seq_len=future_seq_len)
-        config = {
+        self.x_train, self.y_train = tsft._roll_train(train_data,
+                                                      past_seq_len=past_seq_len,
+                                                      future_seq_len=future_seq_len)
+        self.x_val, self.y_val = tsft._roll_train(val_data,
+                                                  past_seq_len=past_seq_len,
+                                                  future_seq_len=future_seq_len)
+        self.x_test = tsft._roll_test(test_data, past_seq_len=past_seq_len)
+        self.config = {
             'epochs': 2,
             "lr": 0.001,
             "lstm_1_units": 16,
@@ -42,100 +50,51 @@ class TestVanillaLSTM(ZooTestCase):
             "dropout_2": 0.2,
             "batch_size": 32,
         }
-        model = VanillaLSTM(check_optional_config=False, future_seq_len=future_seq_len)
-        print("fit_eval:", model.fit_eval(x_train, y_train, **config))
+        self.model = VanillaLSTM(check_optional_config=False, future_seq_len=future_seq_len)
+        self.y_pred = None
+        self.fitted = False
+
+    def test_fit_eval(self):
+        print("fit_eval:", self.model.fit_eval(self.x_train,
+                                               self.y_train,
+                                               **self.config))
+        self.fitted = True
 
     def test_evaluate(self):
-        train_data = pd.DataFrame(data=np.random.randn(64, 4))
-        val_data = pd.DataFrame(data=np.random.randn(16, 4))
-        future_seq_len = 1
-        past_seq_len = 6
-        # use roll method in time_sequence
-        tsft = TimeSequenceFeatureTransformer()
-        x_train, y_train = tsft._roll_train(train_data,
-                                            past_seq_len=past_seq_len,
-                                            future_seq_len=future_seq_len)
-        x_val, y_val = tsft._roll_train(val_data,
-                                        past_seq_len=past_seq_len,
-                                        future_seq_len=future_seq_len)
-        config = {
-            'epochs': 1,
-            "lr": 0.001,
-            "lstm_1_units": 16,
-            "dropout_1": 0.2,
-            "lstm_2_units": 10,
-            "dropout_2": 0.2,
-            "batch_size": 32,
-        }
-        model = VanillaLSTM(check_optional_config=False, future_seq_len=future_seq_len)
-        model.fit_eval(x_train, y_train, **config)
-        mse, rs = model.evaluate(x_val, y_val, metric=['mean_squared_error', 'r_square'])
+        if not self.fitted:
+            self.model.fit_eval(self.x_train,
+                                self.y_train,
+                                **self.config)
+        mse, rs = self.model.evaluate(self.x_val,
+                                      self.y_val,
+                                      metric=['mean_squared_error', 'r_square'])
         print("Mean squared error is:", mse)
         print("R square is:", rs)
 
     def test_predict(self):
-        train_data = pd.DataFrame(data=np.random.randn(64, 4))
-        test_data = pd.DataFrame(data=np.random.randn(16, 4))
-        future_seq_len = 1
-        past_seq_len = 6
-
-        # use roll method in time_sequence
-        tsft = TimeSequenceFeatureTransformer()
-        x_train, y_train = tsft._roll_train(train_data,
-                                            past_seq_len=past_seq_len,
-                                            future_seq_len=future_seq_len)
-        x_test = tsft._roll_test(test_data, past_seq_len=past_seq_len)
-
-        config = {
-            'epochs': 2,
-            "lr": 0.001,
-            "lstm_1_units": 16,
-            "dropout_1": 0.2,
-            "lstm_2_units": 10,
-            "dropout_2": 0.2,
-            "batch_size": 32,
-        }
-        model = VanillaLSTM(check_optional_config=False, future_seq_len=future_seq_len)
-        model.fit_eval(x_train, y_train, **config)
-        y_pred = model.predict(x_test)
-        assert y_pred.shape == (x_test.shape[0], 1)
+        if not self.fitted:
+            self.model.fit_eval(self.x_train,
+                                self.y_train,
+                                **self.config)
+        self.y_pred = self.model.predict(self.x_test)
+        assert self.y_pred.shape == (self.x_test.shape[0], 1)
 
     def test_save_restore(self):
-        train_data = pd.DataFrame(data=np.random.randn(64, 4))
-        test_data = pd.DataFrame(data=np.random.randn(16, 4))
-        future_seq_len = 1
-        past_seq_len = 6
-
-        # use roll method in time_sequence
-        tsft = TimeSequenceFeatureTransformer()
-        x_train, y_train = tsft._roll_train(train_data,
-                                            past_seq_len=past_seq_len,
-                                            future_seq_len=future_seq_len)
-        x_test = tsft._roll_test(test_data, past_seq_len=past_seq_len)
-
-        config = {
-            'epochs': 2,
-            "lr": 0.001,
-            "lstm_1_units": 16,
-            "dropout_1": 0,
-            "lstm_2_units": 10,
-            "dropout_2": 0,
-            "batch_size": 8,
-        }
-
-        model = VanillaLSTM(check_optional_config=False, future_seq_len=future_seq_len)
         new_model = VanillaLSTM(check_optional_config=False)
-        model.fit_eval(x_train, y_train, **config)
-        predict_before = model.predict(x_test)
+        if self.y_pred:
+            predict_before = self.y_pred
+        else:
+            self.model.fit_eval(self.x_train, self.y_train, **self.config)
+            predict_before = self.model.predict(self.x_test)
 
         dirname = tempfile.mkdtemp(prefix="automl_test_vanilla")
         try:
-            save(dirname, model=model)
-            restore(dirname, model=new_model, config=config)
-            predict_after = new_model.predict(x_test)
+            save(dirname, model=self.model)
+            restore(dirname, model=new_model, config=self.config)
+            predict_after = new_model.predict(self.x_test)
             assert np.allclose(predict_before, predict_after)
             new_config = {'epochs': 2}
-            new_model.fit_eval(x_train, y_train, **new_config)
+            new_model.fit_eval(self.x_train, self.y_train, **new_config)
 
         finally:
             shutil.rmtree(dirname)
