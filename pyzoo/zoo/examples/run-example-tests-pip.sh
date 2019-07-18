daea15d6b4ef8da6a8a11f205c5499ef9c07c14f
li,zhichao <zhichao.li@intel.com>
2019-07-18 13:14:34 +0800

    Revert "[WIP] Add RayOnSpark examples (#1420)"
    This reverts commit 4df37acc1bb6cc9a35bac1927950f78ceea64082.

--- pyzoo/zoo/examples/run-example-tests-pip.sh
+++ pyzoo/zoo/examples/run-example-tests-pip.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 clear_up () {
     echo "Clearing up environment. Uninstalling analytics-zoo"
     pip uninstall -y analytics-zoo
@@ -375,38 +374,6 @@ unset SPARK_DRIVER_MEMORY
 now=$(date "+%s")
 time9=$((now-start))
 echo "qaranker time used:$time9 seconds"
-
-execute_ray_test () {
-    echo "start example $1"
-    start=$(date "+%s")
-    python $2
-    exit_status=$?
-    if [ $exit_status -ne 0 ];
-    then
-        clear_up
-        echo "$1 failed"
-        exit $exit_status
-    fi
-    now=$(date "+%s")
-    return $((now-start))
-}
-
-execute_ray_test rl_pong ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/ray/rl_pong/rl_pong.py
-time9 = $?
-
-execute_ray_test sync_parameter_server ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/ray/parameter_server/sync_parameter_server.py
-time10 = $?
-
-execute_ray_test async_parameter_server ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/ray/parameter_server/async_parameter_server.py
-time11 = $?
-
-execute_ray_test multiagent_two_trainers ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/ray/rllib/multiagent_two_trainers.py
-time12 = $?
-
-echo "#9 rl_pong time used:$time9 seconds"
-echo "#10 sync_parameter_server time used:$time10 seconds"
-echo "#11 async_parameter_server time used:$time11 seconds"
-echo "#12 multiagent_two_trainers time used:$time12 seconds"
 
 
 # This should be done at the very end after all tests finish.
