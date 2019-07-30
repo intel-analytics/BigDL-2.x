3c4f518223844fdb2fdc621afba26ad6fe5d037b
qiuxin2012 <qiuxin2012cs@gmail.com>
2019-07-30 15:49:49 +0800

    fix typo

--- pyzoo/zoo/feature/common.py
+++ pyzoo/zoo/feature/common.py
@@ -223,7 +223,7 @@ class FeatureSet(DataSet):
                             If it's DRAM, will cache dataset into dynamic random-access memory
                             If it's PMEM, will cache dataset into Intel Optane DC Persistent Memory
                             If it's a Int number n, will cache dataset into disk, and only hold 1/n of
-                              the data into memory during the training. After going throught the 1/n,
+                              the data into memory during the training. After going through the 1/n,
                               we will release the current cache, and load another 1/n into memory.
         :param bigdl_type: numeric type
         :return: A feature set
@@ -241,7 +241,7 @@ class FeatureSet(DataSet):
                             If it's DRAM, will cache dataset into dynamic random-access memory
                             If it's PMEM, will cache dataset into Intel Optane DC Persistent Memory
                             If it's a Int number n, will cache dataset into disk, and only hold 1/n of
-                              the data into memory during the training. After going throught the 1/n,
+                              the data into memory during the training. After going through the 1/n,
                               we will release the current cache, and load another 1/n into memory.
         :param bigdl_type:numeric type
         :return: A feature set
