850e4768b9d7a8966cdb2f634dd476531f97c6d6
YinaChen <yitastudy@gmail.com>
2019-08-05 15:55:35 +0800

    test

--- pyzoo/zoo/examples/inception/inception.py
+++ pyzoo/zoo/examples/inception/inception.py
@@ -158,39 +158,40 @@ if __name__ == "__main__":
     sc = init_nncontext("inception v1")
 
     image_size = 224  # create dataset
-    # train_transformer = ChainedPreprocessing([ImagePixelBytesToMat(),
-    #                                           ImageRandomCrop(image_size, image_size),
-    #                                           ImageRandomPreprocessing(ImageHFlip(), 0.5),
-    #                                           ImageChannelNormalize(123.0, 117.0, 104.0),
-    #                                           ImageMatToTensor(format="NHWC", to_RGB=True),
-    #                                           ImageSetToSample(input_keys=["imageTensor"], target_keys=["label"])
-    #                                           ])
-    train_transformer = Pipeline([PixelBytesToMat(),
-                                  Resize(256, 256),
-                                  RandomCropper(image_size, image_size, True, "Random", 3),
-                                  ChannelNormalize(123.0, 117.0, 104.0),
-                                  MatToTensor(to_rgb=False),
-                                  ImageFrameToSample(input_keys=["imageTensor"], target_keys=["label"])
-                                  ])
+    train_transformer = ChainedPreprocessing([ImagePixelBytesToMat(),
+                                              ImageResize(256, 256),
+                                              ImageRandomCrop(image_size, image_size),
+                                              ImageRandomPreprocessing(ImageHFlip(), 0.5),
+                                              ImageChannelNormalize(123.0, 117.0, 104.0),
+                                              ImageMatToTensor(format="NHWC", to_RGB=True),
+                                              ImageSetToSample(input_keys=["imageTensor"], target_keys=["label"])
+                                              ])
+    # train_transformer = Pipeline([PixelBytesToMat(),
+    #                               Resize(256, 256),
+    #                               RandomCropper(image_size, image_size, True, "Random", 3),
+    #                               ChannelNormalize(123.0, 117.0, 104.0),
+    #                               MatToTensor(to_rgb=False),
+    #                               ImageFrameToSample(input_keys=["imageTensor"], target_keys=["label"])
+    #                               ])
 
     raw_train_data = get_inception_data(options.folder, sc, "train")
     train_data = FeatureSet.image_frame(raw_train_data).transform(train_transformer)
 
-    # val_transformer = ChainedPreprocessing([ImagePixelBytesToMat(),
-    #                                         ImageRandomCrop(image_size, image_size),
-    #                                         ImageRandomPreprocessing(ImageHFlip(), 0.5),
-    #                                         ImageChannelNormalize(123.0, 117.0, 104.0),
-    #                                         ImageMatToTensor(format="NHWC", to_RGB=True),
-    #                                         ImageSetToSample(input_keys=["imageTensor"], target_keys=["label"])
-    #                                         ])
+    val_transformer = ChainedPreprocessing([ImagePixelBytesToMat(),
+                                            ImageRandomCrop(image_size, image_size),
+                                            ImageRandomPreprocessing(ImageHFlip(), 0.5),
+                                            ImageChannelNormalize(123.0, 117.0, 104.0),
+                                            ImageMatToTensor(format="NHWC", to_RGB=True),
+                                            ImageSetToSample(input_keys=["imageTensor"], target_keys=["label"])
+                                            ])
 
-    val_transformer = Pipeline([PixelBytesToMat(),
-                                Resize(256, 256),
-                                RandomCropper(image_size, image_size, False, "Center", 3),
-                                ChannelNormalize(123.0, 117.0, 104.0),
-                                MatToTensor(to_rgb=False),
-                                ImageFrameToSample(input_keys=["imageTensor"], target_keys=["label"])
-                                ])
+    # val_transformer = Pipeline([PixelBytesToMat(),
+    #                             Resize(256, 256),
+    #                             RandomCropper(image_size, image_size, False, "Center", 3),
+    #                             ChannelNormalize(123.0, 117.0, 104.0),
+    #                             MatToTensor(to_rgb=False),
+    #                             ImageFrameToSample(input_keys=["imageTensor"], target_keys=["label"])
+    #                             ])
     raw_val_data = get_inception_data(options.folder, sc, "val")
     val_data = FeatureSet.image_frame(raw_val_data).transform(val_transformer)
 
