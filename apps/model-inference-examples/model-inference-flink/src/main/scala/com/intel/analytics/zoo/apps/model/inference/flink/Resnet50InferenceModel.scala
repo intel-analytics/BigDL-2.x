85eb035e54bee73525d55ee0e42b2685b91150dd
joy <joy@127.0.0.1localhost>
2019-08-16 15:16:30 +0800

    resnet50

---
+++ apps/model-inference-examples/model-inference-flink/src/main/scala/com/intel/analytics/zoo/apps/model/inference/flink/Resnet50InferenceModel.scala
@@ -0,0 +1,13 @@
+package com.intel.analytics.zoo.apps.model.inference.flink
+
+import java.io.{ByteArrayInputStream, File, FileOutputStream}
+import java.nio.channels.Channels
+
+import com.google.common.io.Files
+import com.intel.analytics.zoo.pipeline.inference.InferenceModel
+
+class Resnet50InferenceModel(var concurrentNum: Int = 1, modelType: String, modelBytes: Array[Byte], inputShape: Array[Int], ifReverseInputChannels: Boolean, meanValues: Array[Float], scale: Float) extends InferenceModel(concurrentNum) with Serializable {
+  doLoadTF(null, modelType, modelBytes, inputShape, ifReverseInputChannels, meanValues, scale)
+  println(this)
+}
+
