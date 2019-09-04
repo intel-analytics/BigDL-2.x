8173ce1a52829d9d1bae494489b85f3cd8698f9f
Jiaming <litchilitchy@qq.com>
2019-09-04 12:41:36 +0800

    yaml parsing and bash rename

--- zoo/src/main/scala/com/intel/analytics/zoo/serving/docker-run.sh
+++ zoo/src/main/scala/com/intel/analytics/zoo/serving/docker-run.sh
@@ -22,37 +22,6 @@ if [ -z "${model_path}" ]; then
     echo "Please set model path"
     exit 1
 fi
-if [ -z "${spark_master}" ]; then
-    echo "master of spark cluster not set, using default value local[*]"
-    spark_master=local[*]
-fi
-if [ -z "${spark_driver_memory}" ]; then
-    echo "spark driver memory not set, using default value 4g"
-    spark_driver_memory=4g
-fi
-if [ -z "${spark_executor_memory}" ]; then
-    echo "spark executor memory not set, using default value 1g"
-    spark_executor_memory=1g
-fi
-if [ -z "${spark_num_executors}" ]; then
-    echo "spark num-executors not set, using default value 1"
-    spark_num_executors=1
-fi
-if [ -z "${spark_executor_cores}" ]; then
-    echo "spark executor-cores not set, using default value 4"
-    spark_executor_cores=4
-fi
-if [ -z "${params_batch_size}" ]; then
-    echo "batch size of inference not set, using default value 4"
-    params_batch_size=4
-fi
-if [ -z "${params_mkl_threads}" ]; then    
-    params_mkl_threads=4
-fi
-if [ -z "${params_engine_type}" ]; then    
-    params_engine_type=mkldnn
-fi
 
 
-
-docker run --net=host -v $(pwd)/${model_path}:/opt/work/resources -e ModelFolder="resources" -e Master=${spark_master} -e DriverMemory=${spark_driver_memory} -e ExecutorMemory=${spark_executor_memory} -e NumExecutors=${spark_num_executors} -e ExecutorCores=${spark_executor_cores} -e BatchSize=${params_batch_size} -e MklThreads=${params_mkl_threads} -e EngineType=${params_engine_type} intelanalytics/analytics-zoo-serving-pub-sub:0.5.1-spark_2.4.0
+docker run --net=host -v $(pwd)/${model_path}:/opt/work/resources -e ModelFolder="resources"  intelanalytics/analytics-zoo-serving-pub-sub:0.5.1-spark_2.4.0
