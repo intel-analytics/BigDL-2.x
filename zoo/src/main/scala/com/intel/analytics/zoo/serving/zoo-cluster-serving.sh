ff958fa8497d636f8ade2ffce396ee5442c675a0
Jiaming <litchilitchy@qq.com>
2019-09-17 15:58:10 +0800

    naming

--- zoo/src/main/scala/com/intel/analytics/zoo/serving/zoo-cluster-serving.sh
+++ zoo/src/main/scala/com/intel/analytics/zoo/serving/zoo-cluster-serving.sh
@@ -67,7 +67,12 @@ if [ -z "${ModelFolder}" ]; then
     exit 1
 fi
 
+if [ -z "${ANALYTICS_ZOO_HOME}" ]; then
+    echo "Please set ANALYTICS_ZOO_HOME environment variable"
+    exit 1
+fi
+export ANALYTICS_ZOO_JAR=`find ${ANALYTICS_ZOO_HOME}/lib -type f -name "analytics-zoo*jar-with-dependencies.jar"`
 
-${SPARK_HOME}/bin/spark-submit --master ${spark_master} --driver-memory ${spark_driver_memory} --executor-memory ${spark_executor_memory} --num-executors ${spark_num_executors} --executor-cores ${spark_executor_cores} --conf "spark.driver.extraJavaOptions=-Dbigdl.engineType=${params_engine_type} -Dbigdl.mklNumThreads=${params_mkl_threads}"  --jars ./packages/spark-redis-2.4.0-SNAPSHOT-jar-with-dependencies.jar --class com.intel.analytics.zoo.serving.ZooServing ./packages/serving-0.1.0-SNAPSHOT-jar-with-dependencies.jar -f ${ModelFolder} -b ${params_batch_size}
+${SPARK_HOME}/bin/spark-submit --master ${spark_master} --driver-memory ${spark_driver_memory} --executor-memory ${spark_executor_memory} --num-executors ${spark_num_executors} --executor-cores ${spark_executor_cores} --conf "spark.driver.extraJavaOptions=-Dbigdl.engineType=${params_engine_type} -Dbigdl.mklNumThreads=${params_mkl_threads}"  --jars ./packages/spark-redis-2.4.0-SNAPSHOT-jar-with-dependencies.jar --class com.intel.analytics.zoo.serving.ClusterServing ${ANALYTICS_ZOO_JAR} -f ${ModelFolder} -b ${params_batch_size}
 
 
