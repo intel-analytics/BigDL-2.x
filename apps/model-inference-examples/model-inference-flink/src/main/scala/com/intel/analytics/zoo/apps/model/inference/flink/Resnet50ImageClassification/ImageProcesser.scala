d1aff33d36c4449ccd85c21b380384acb6573d03
joy <joy@127.0.0.1localhost>
2019-09-19 18:28:27 +0800

    solve image preprocess

--- apps/model-inference-examples/model-inference-flink/src/main/scala/com/intel/analytics/zoo/apps/model/inference/flink/Resnet50ImageClassification/ImageProcesser.scala
+++ apps/model-inference-examples/model-inference-flink/src/main/scala/com/intel/analytics/zoo/apps/model/inference/flink/Resnet50ImageClassification/ImageProcesser.scala
@@ -1,12 +1,10 @@
 package com.intel.analytics.zoo.apps.model.inference.flink.Resnet50ImageClassification
 
-class ImageProcesser(bytes: Array[Byte], cropWidth: Int, cropHeight: Int, meanR: Int, meanG: Int, meanB: Int, scale: Double) extends ImageProcessing {
+class ImageProcessor extends ImageProcessing {
   def preProcess(bytes: Array[Byte], cropWidth: Int, cropHeight: Int, meanR: Int, meanG: Int, meanB: Int, scale: Double) = {
-    val imageMatt = byteArrayToMat(bytes)
-    val imageCenti = centerCrop(imageMatt, cropWidth, cropHeight)
-    val imageTensors = matToNCHWAndRGBTensor(imageCenti)
-    val norImaTensors = channelScaledNormalize(imageTensors, meanR, meanG, meanB, scale)
- //   imageTensors
-    norImaTensors
+    val imageMat = byteArrayToMat(bytes)
+    val imageCent = centerCrop(imageMat, cropWidth, cropHeight)
+    val imageArray = matToNCHWAndArray(imageCent)
+    imageArray
   }
 }
