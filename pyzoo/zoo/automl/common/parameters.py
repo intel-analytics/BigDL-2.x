dcca0b1d3dfa3f90153b1a9d2abe8b137e4b973b
Shan Yu <shan.yu@intel.com>
2019-10-11 10:33:18 +0800

    add ppl.fit_with_fixed_configs and ppl.config_save

---
+++ pyzoo/zoo/automl/common/parameters.py
@@ -0,0 +1,7 @@
+# where pipeline saves file by default
+DEFAULT_PPL_DIR = "./saved_pipeline"
+DEFAULT_CONFIG_DIR = "./saved_configs"
+
+# reward metric in tune reporter
+REWARD_METRIC = "reward_metric"
+
