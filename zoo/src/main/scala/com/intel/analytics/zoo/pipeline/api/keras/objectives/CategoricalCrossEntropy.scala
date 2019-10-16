67606d277f3762594cd36c6095b019f217447cd6
qiuxin2012 <qiuxin2012cs@gmail.com>
2019-10-16 15:11:28 +0800

    add unit test

--- zoo/src/main/scala/com/intel/analytics/zoo/pipeline/api/keras/objectives/CategoricalCrossEntropy.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/pipeline/api/keras/objectives/CategoricalCrossEntropy.scala
@@ -29,16 +29,15 @@ import scala.reflect.ClassTag
 class CategoricalCrossEntropy[@specialized(Float, Double) T: ClassTag]()
   (implicit ev: TensorNumeric[T]) extends TensorLossFunction[T] {
 
-  protected val eps = ev.fromType(1e-8)
-
-  protected val oneMinusEps = ev.minus(ev.one, eps)
 
   override val loss: AbstractCriterion[Tensor[T], Tensor[T], T] =
     com.intel.analytics.bigdl.nn.CategoricalCrossEntropy[T]()
 
   override def updateOutput(input: Tensor[T], target: Tensor[T]): T = {
+    val eps = ev.fromType(1e-8)
+    val maxFloat = ev.fromType(Float.MaxValue)
     // avoid NaN when compute input's log in BigDL's CategoricalCrossEntropy
-    input.apply1(ev.clip(_, eps, oneMinusEps))
+    input.apply1(ev.clip(_, eps, maxFloat))
     output = loss.updateOutput(input, target)
     output
   }
