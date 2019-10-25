b4a01b616303302738de5c7e931ae18abd05cecb
glorysdj <glorysdj@gmail.com>
2019-10-24 21:49:44 -0400

    add params to examples

--- zoo/src/main/scala/com/intel/analytics/zoo/examples/localEstimator/LenetLocalEstimator.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/examples/localEstimator/LenetLocalEstimator.scala
@@ -22,30 +22,59 @@ import com.intel.analytics.bigdl.nn.ClassNLLCriterion
 import com.intel.analytics.bigdl.optim.{Adam, Loss, Top1Accuracy, Top5Accuracy}
 import com.intel.analytics.zoo.pipeline.estimator.LocalEstimator
 import org.slf4j.LoggerFactory
+import scopt.OptionParser
 
-object LenetLocalEstimator extends App {
+case class LenetLocalEstimatorParams(imageDirPath: String = "./",
+                                     batchSize: Int = 1000,
+                                     epoch: Int = 15,
+                                     threadNum: Int = 44)
+
+object LenetLocalEstimator {
 
   val logger = LoggerFactory.getLogger(getClass)
 
-  val imageDirPath = "hdfs://172.16.0.110:9000/mnist"
+  def main(args: Array[String]) : Unit = {
+    val parser = new OptionParser[LenetLocalEstimatorParams]("LenetLocalEstimator Example") {
+      opt[String]('d', "imageDirPath")
+        .required()
+        .text("The directory of mnist dataset")
+        .action((x, c) => c.copy(imageDirPath = x))
+      opt[Int]('b', "batchSize")
+        .required()
+        .text("The number of batchSize")
+        .action((x, c) => c.copy(batchSize = x))
+      opt[Int]('e', "epoch")
+        .required()
+        .text("The number of epoch")
+        .action((x, c) => c.copy(epoch = x))
+      opt[Int]('t', "threadNum")
+        .required()
+        .text("The number of threadNum")
+        .action((x, c) => c.copy(threadNum = x))
+    }
 
-  val batchSize = 1000
-  val epoch = 15
-  val threadNum = 44
+    parser.parse(args, LenetLocalEstimatorParams()).map { params =>
+      logger.info(s"params parsed as $params")
 
-  val model: Module[Float] = LeNet5(10)
-  val criterion = ClassNLLCriterion[Float]()
-  val adam = new Adam[Float]()
-  val validations = Array(new Top1Accuracy[Float], new Top5Accuracy[Float], new Loss[Float])
-  val localEstimator = LocalEstimator(model, criterion, adam, validations, threadNum)
-  logger.info(s"LocalEstimator loaded as $localEstimator")
+      val imageDirPath = params.imageDirPath
+      val batchSize = params.batchSize
+      val epoch = params.epoch
+      val threadNum = params.threadNum
 
-  val trainData = MnistDataLoader.loadTrainData(imageDirPath)
-  val testData = MnistDataLoader.loadTestData(imageDirPath)
-  localEstimator.fit(trainData,
-    testData,
-    ImageProcessing.labeledGreyImageToMiniBatchTransformer,
-    batchSize,
-    epoch)
+      val model: Module[Float] = LeNet5(10)
+      val criterion = ClassNLLCriterion[Float]()
+      val adam = new Adam[Float]()
+      val validations = Array(new Top1Accuracy[Float], new Top5Accuracy[Float], new Loss[Float])
+      val localEstimator = LocalEstimator(model, criterion, adam, validations, threadNum)
+      logger.info(s"LocalEstimator loaded as $localEstimator")
 
+      val trainData = MnistDataLoader.loadTrainData(imageDirPath)
+      val testData = MnistDataLoader.loadTestData(imageDirPath)
+      localEstimator.fit(trainData,
+        testData,
+        ImageProcessing.labeledGreyImageToMiniBatchTransformer,
+        batchSize,
+        epoch)
+    }
+  }
 }
