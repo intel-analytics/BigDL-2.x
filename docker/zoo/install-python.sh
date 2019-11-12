adf67c74c35f35d9fb58869f1bae10fe72a5444c
le-zheng <alettazheng@hotmail.com>
2019-11-12 15:40:06 +0800

    update Dockerfile python

--- docker/zoo/install-python.sh
+++ docker/zoo/install-python.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-PY_VERSION_SUFFIX=${PY_VERSION_3:+3}
+PY_VERSION_SUFFIX=3
 PYTHON=python${PY_VERSION_SUFFIX}
 PIP=pip${PY_VERSION_SUFFIX}
 
@@ -14,15 +14,10 @@ ${PIP} install numpy scipy
 ${PIP} install --no-binary pandas -I pandas
 ${PIP} install scikit-learn matplotlib seaborn jupyter wordcloud moviepy requests h5py opencv-python tensorflow==1.10.0
 
-if [[ "$PYTHON" != "python3" ]]; then
-    ${PYTHON} -m pip install -U ipykernel
-    ${PYTHON} -m ipykernel install --user
-else
-    ln -s /usr/bin/python3 /usr/bin/python
-    ln -s /usr/bin/pip3 /usr/local/bin/pip
-    #Fix tornado await process
-    pip uninstall -y -q tornado
-    pip install tornado==5.1.1
-fi
+ln -s /usr/bin/python3 /usr/bin/python
+ln -s /usr/bin/pip3 /usr/local/bin/pip
+#Fix tornado await process
+pip uninstall -y -q tornado
+pip install tornado==5.1.1
 
 ${PYTHON} -m ipykernel.kernelspec
