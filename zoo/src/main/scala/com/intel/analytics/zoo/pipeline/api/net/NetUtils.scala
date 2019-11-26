1c5cb0e654d93b2e417a81593a7ff6e04dbacb01
yangw <yang3.wang@intel.com>
2019-11-26 13:14:03 +0800

    enhance error messages

--- zoo/src/main/scala/com/intel/analytics/zoo/pipeline/api/net/NetUtils.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/pipeline/api/net/NetUtils.scala
@@ -283,7 +283,19 @@ private[zoo] case class Meta(inputNames: Array[String],
                              variables: Option[Array[String]] = None,
                              gradVariables: Option[Array[String]] = None,
                              gradInputs: Option[Array[String]] = None
-                             )
+                             ) {
+
+  for (name <- inputNames) {
+    require(name.split(":").length == 2, s"Input names require to be Tensor names, " +
+      s"but \"$name\" looks like a Operation name")
+  }
+
+  for (name <- outputNames) {
+    require(name.split(":").length == 2, s"Output names require to be Tensor names, " +
+      s"but \"$name\" looks like a Operation name")
+  }
+
+}
 
 
 trait NetUtils[T, D <: Module[T] with NetUtils[T, D]] {
