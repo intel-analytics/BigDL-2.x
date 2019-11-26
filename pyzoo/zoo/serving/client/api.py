bd33ebb017fd02af8a99de8a08318c4bb30b330e
Jiaming <litchilitchy@qq.com>
2019-11-26 10:16:05 +0800

    fix invalid code

--- pyzoo/zoo/serving/client/api.py
+++ pyzoo/zoo/serving/client/api.py
@@ -19,24 +19,25 @@ from zoo.serving.client.utils.helpers import RedisQueue
 if __name__ == "__main__":
     # if you do not specify file_path, it will use default config
     # file_path = "/path/to/analytics-zoo-cluster-serving/config.yaml"
-    file_path = None
 
+    """
     redis_queue = RedisQueue(file_path)
     redis_queue.enqueue_image()
     redis_queue.get_results("result:*")
+    """
 
-    '''
+    """
     following lines demonstrate how to push data to redis
-    '''
+    """
     # img_path = "/path/to/image"
     # img = cv2.imread(img_path)
     # img = cv2.resize(img, (224, 224))
 
-    # redis_queue.enqueue_image()
+    # redis_queue.enqueue_image(img_path, img)
 
-    '''
+    """
     following lines demonstrate how to get data from redis
-    '''
+    """
     # res_list = DB.keys('result:*')
     # for res in res_list:
     #
