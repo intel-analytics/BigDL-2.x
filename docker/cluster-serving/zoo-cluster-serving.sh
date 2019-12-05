2df1ee205b3336ff08d1a24f7bedfe7d37a426be
Jiaming <litchilitchy@qq.com>
2019-12-05 15:37:16 +0800

    docker file

--- docker/cluster-serving/zoo-cluster-serving.sh
+++ docker/cluster-serving/zoo-cluster-serving.sh
@@ -60,7 +60,5 @@ if [ -z "${params_engine_type}" ]; then
 fi
 
 
-${SPARK_HOME}/bin/spark-submit --master ${spark_master} --driver-memory ${spark_driver_memory} --executor-memory ${spark_executor_memory} --num-executors ${spark_num_executors} --executor-cores ${spark_executor_cores} --total-executor-cores ${spark_total_executor_cores} --conf "spark.executor.extraJavaOptions=-Dbigdl.engineType=${params_engine_type}" --conf "spark.driver.extraJavaOptions=-Dbigdl.engineType=${params_engine_type}" --jars docker/spark-redis-2.4.0-SNAPSHOT-jar-with-dependencies.jar --class com.intel.analytics.zoo.serving.ClusterServing docker/analytics-zoo-bigdl_0.10.0-spark_2.4.0-0.7.0-SNAPSHOT-jar-with-dependencies.jar
-# -f ${model_path} -b ${params_batch_size} -n ${params_top_n} -r ${data_src} -s ${data_shape}
-
+${SPARK_HOME}/bin/spark-submit --master ${spark_master} --driver-memory ${spark_driver_memory} --executor-memory ${spark_executor_memory} --num-executors ${spark_num_executors} --executor-cores ${spark_executor_cores} --total-executor-cores ${spark_total_executor_cores} --conf "spark.executor.extraJavaOptions=-Dbigdl.engineType=${params_engine_type}" --conf "spark.driver.extraJavaOptions=-Dbigdl.engineType=${params_engine_type}" --jars ${SPARK_REDIS_JAR} --class com.intel.analytics.zoo.serving.ClusterServing ${ZOO_JAR}
 
