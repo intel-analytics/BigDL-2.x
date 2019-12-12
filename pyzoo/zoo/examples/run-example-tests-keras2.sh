91d11f24ebb724e1e976282ff8630a84e8a46f77
aqtjin <aqtjin@outlook.com>
2019-12-12 16:59:19 +0800

    fix k2

--- pyzoo/zoo/examples/run-example-tests-keras2.sh
+++ pyzoo/zoo/examples/run-example-tests-keras2.sh
@@ -15,7 +15,7 @@ set -e
 echo "#1 start example test for attention"
 #timer
 start=$(date "+%s")
-sed "s/max_features = 20000/max_features = 200/g;s/max_len = 200/max_len = 20/g;s/hidden_size=128/hidden_size=8/g" \
+sed "s/max_features = 20000/max_features = 100/g;s/max_len = 200/max_len = 10/g;s/hidden_size=128/hidden_size=8/g" \
     ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/attention/transformer.py \
     > ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/attention/tmp.py
 
