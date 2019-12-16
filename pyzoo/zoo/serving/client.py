7bfa3d02b94423d61dc7324c46cd1e256be1673a
Jiaming <litchilitchy@qq.com>
2019-12-16 12:34:42 +0800

    delete shape check and use force resize, add read from image path support

---
+++ pyzoo/zoo/serving/client.py
@@ -0,0 +1,137 @@
+#
+# Copyright 2018 Analytics Zoo Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import base64
+import cv2
+import yaml
+import redis
+import datetime
+import time
+
+
+class API:
+    """
+    base level of API control
+    select data pipeline here, Redis/Kafka/...
+    interface preserved for API class
+    """
+    def __init__(self):
+        dt = datetime.datetime.now()
+        self.log_file = open("./cluster-serving.log", "w")
+        import os
+        a = os.listdir("../../..")
+
+        try:
+            file_path = "../../../docker/cluster-serving/config.yaml"
+        except:
+            raise EOFError("config file does not exist!")
+        with open(file_path) as f:
+            config = yaml.load(f)
+            host_port = config['data']['src'].split(":")
+            config['data']['host'] = host_port[0]
+            config['data']['port'] = host_port[1]
+
+        self.db = redis.StrictRedis(host=config['data']['host'],
+                                    port=config['data']['port'], db=0)
+
+        self.data_shape = config['data']['shape'].split(",")
+        for i in range(len(self.data_shape)):
+            self.data_shape[i] = int(self.data_shape[i])
+
+
+class InputQueue(API):
+    def __init__(self):
+        super().__init__()
+        self.c, self.h, self.w = None, None, None
+        self.data_shape_check()
+
+    def data_shape_check(self):
+        for num in self.data_shape:
+            if num <= 0:
+                raise Exception("Your image shape config is invalid, please check.")
+            if 0 < num < 5:
+                self.c = num
+                continue
+            if not self.h:
+                self.h = num
+            else:
+                self.w = num
+        return
+
+    def enqueue_image(self, uri, img):
+        """
+        :param id: String you use to identify this record
+        :param data: Data, ndarray type
+        :return:
+        """
+        if isinstance(img, str):
+            img = cv2.imread(str)
+            if not img:
+                print("You have pushed an image with invalid path, skipped.")
+                return
+
+        # force resize here to avoid input image shape inconsistent
+        # if the shape is consistent, it would not affect the data
+        img = cv2.resize(img, (self.h, self.w))
+        data = cv2.imencode(".jpg", img)[1]
+
+        img_encoded = self.base64_encode_image(data)
+
+        d = {"uri": uri, "image": img_encoded}
+
+        inf = self.db.info()
+
+        try:
+            if inf['used_memory'] >= inf['maxmemory'] * 0.6:
+                raise redis.exceptions.ConnectionError
+            self.db.xadd("image_stream", d)
+            print("Write to Redis successful")
+        except redis.exceptions.ConnectionError:
+            print("Redis queue is full, please wait for inference "
+                  "or delete the unprocessed records.")
+            time.sleep(1)
+
+        except redis.exceptions.ResponseError as e:
+            print(e, "Redis memory is full, please dequeue or delete.")
+            time.sleep(1)
+
+    @staticmethod
+    def base64_encode_image(img):
+        # base64 encode the input NumPy array
+        return base64.b64encode(img).decode("utf-8")
+
+
+class OutputQueue(API):
+    def __init__(self):
+        super().__init__()
+
+    def dequeue(self):
+        res_list = self.db.keys('result:*')
+        decoded = {}
+        for res in res_list:
+            res_dict = self.db.hgetall(res.decode('utf-8'))
+            res_id = res.decode('utf-8').split(":")[1]
+            res_value = res_dict[b'value'].decode('utf-8')
+            decoded[res_id] = res_value
+            self.db.delete(res)
+        return decoded
+
+    def query(self, uri):
+        res_dict = self.db.hgetall("result:"+uri)
+
+        if not res_dict or len(res_dict) == 0:
+            return "{}"
+        return res_dict[b'value'].decode('utf-8')
