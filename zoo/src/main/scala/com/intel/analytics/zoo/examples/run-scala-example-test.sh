95ac105d4dbc4c0919b79cca3143545576622309
respecteverything <374376070@qq.com>
2019-12-26 17:30:20 +0800

    modify parameters

--- zoo/src/main/scala/com/intel/analytics/zoo/examples/run-scala-example-test.sh
+++ zoo/src/main/scala/com/intel/analytics/zoo/examples/run-scala-example-test.sh
@@ -16,10 +16,10 @@ start=$(date "+%s")
 ${ANALYTICS_ZOO_HOME}/bin/spark-shell-with-zoo.sh \
 --master ${MASTER} \
 --executor-cores 4 --total-executor-cores 4 \
---driver-memory 50g \
+--driver-memory 25g \
 --class com.intel.analytics.zoo.examples.resnet.TrainImageNet \
 -f hdfs://172.168.2.181:9000/imagenet-zl \
---batchSize 2048 --nEpochs 2 --learningRate 0.1 --warmupEpoch 1 \
+--batchSize 128 --nEpochs 2 --learningRate 0.1 --warmupEpoch 1 \
 --maxLr 3.2 --cache /cache  --depth 50 --classes 1000
 
 now=$(date "+%s")
