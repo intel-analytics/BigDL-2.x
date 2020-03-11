0ba198601776be48f4fc216bf02f9d841773b157
Yu Shan <yushan0105@163.com>
2020-03-11 08:25:22 +0800

    change MTNet license to apache for tmp

--- pyzoo/zoo/automl/model/MTNet_keras.py
+++ pyzoo/zoo/automl/model/MTNet_keras.py
@@ -1,4 +1,5 @@
-# Copyright (c) 2018 Roland Zimmermann, 2019 Analytics Zoo Authors.
+#
+# Copyright 2018 Analytics Zoo Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,27 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# MIT License
-#
-# Copyright (c) 2018 Roland Zimmermann
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 
 import numpy as np
 import time
