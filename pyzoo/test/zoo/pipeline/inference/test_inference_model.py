4ee59f4f36e79ebf7911795a8a86cde962568d80
Qiyuan Gong <qiyuan.gong@intel.com>
2020-03-16 14:50:42 +0800

    Remove calibration from python api, doc and Unit test

--- pyzoo/test/zoo/pipeline/inference/test_inference_model.py
+++ pyzoo/test/zoo/pipeline/inference/test_inference_model.py
@@ -101,36 +101,8 @@ class TestInferenceModel(ZooTestCase):
             scale=1)
         print(model)
         input_data = np.random.random([4, 1, 224, 224, 3])
-        s3url = "https://s3-ap-southeast-1.amazonaws.com/"
-        var_url = s3url + "analytics-zoo-models/openvino/val_bmp_32.tar"
-        lib_url = s3url + "analytics-zoo-models/openvino/opencv_4.0.0_ubuntu_lib.tar"
-        var_file_abs_path = maybe_download("val_bmp_32.tar", local_path, var_url)
-        lib_file_abs_path = maybe_download("opencv_4.0.0_ubuntu_lib.tar", local_path, lib_url)
-        var_tar = tarfile.open(var_file_abs_path, "r")
-        print("Extracting %s to %s" % (var_file_abs_path, local_path))
-        var_tar.extractall(local_path)
-        var_tar.close()
-        lib_tar = tarfile.open(lib_file_abs_path, "r")
-        print("Extracting %s to %s" % (lib_file_abs_path, local_path))
-        lib_tar.extractall(local_path)
-        lib_tar.close()
-        validation_file_path = local_path + "/val_bmp_32/val.txt"
-        opencv_lib_path = local_path + "/lib"
-        model2 = InferenceModel(3)
-        model2.load_tf_as_calibrated_openvino(
-            model_path=None,
-            model_type="resnet_v1_50",
-            checkpoint_path=local_path + "/resnet_v1_50.ckpt",
-            input_shape=[4, 224, 224, 3],
-            if_reverse_input_channels=True,
-            mean_values=[123.68, 116.78, 103.94],
-            scale=1,
-            network_type='C',
-            validation_file_path=validation_file_path,
-            subset=32,
-            opencv_lib_path=opencv_lib_path)
-        print(model2)
-        model2.predict(input_data)
+        model.predict(input_data)
+
 
 if __name__ == "__main__":
     pytest.main([__file__])
