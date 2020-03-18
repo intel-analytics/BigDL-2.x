c5df67b52263c68d34bf8438d94a1ad4973e7da3
Qiyuan Gong <qiyuan.gong@intel.com>
2020-03-18 15:50:45 +0800

    Fix a bug in openvino perf

--- zoo/src/main/scala/com/intel/analytics/zoo/examples/vnni/openvino/Perf.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/examples/vnni/openvino/Perf.scala
@@ -62,7 +62,6 @@ object Perf {
     parser.parse(args, ResNet50PerfParams()).foreach { param =>
       if (!param.onSpark) {
         System.setProperty("bigdl.localMode", "true")
-        System.setProperty("bigdl.engineType", "mkldnn")
       }
 
       val batchSize = param.batchSize
