880434794549635905b4c0bde9b16903bae2f8f7
Kai Huang <huangkaivision@gmail.com>
2020-03-18 15:23:22 +0800

    Add long description for pypi (#2018)
    * add long description
    
    * update long description
    
    * trial
    
    * update
    
    * fix style

--- pyzoo/setup.py
+++ pyzoo/setup.py
@@ -22,6 +22,29 @@ from shutil import copyfile, copytree, rmtree
 
 from setuptools import setup
 
+long_description = '''
+Analytics Zoo: A unified Data Analytics and AI platform for distributed TensorFlow,
+ Keras, PyTorch, Apache Spark/Flink and Ray.
+
+You may want to develop your AI solutions using Analytics Zoo if:
+
+- You want to easily prototype the entire end-to-end pipeline that applies AI models
+ (e.g., TensorFlow, Keras, PyTorch, BigDL, OpenVINO, etc.) to production big data.
+- You want to transparently scale your AI applications from a laptop to large clusters with "zero"
+ code changes.
+- You want to deploy your AI pipelines to existing YARN or K8S clusters *WITHOUT* any modifications
+ to the clusters.
+- You want to automate the process of applying machine learning (such as feature engineering,
+ hyperparameter tuning, model selection and distributed inference).
+
+Find instructions to install analytics-zoo via pip, please visit our documentation page:
+ https://analytics-zoo.github.io/master/#PythonUserGuide/install/
+
+For source code and more information, please visit our GitHub page:
+ https://github.com/intel-analytics/analytics-zoo
+'''
+
+
 TEMP_PATH = "zoo/share"
 analytics_zoo_home = os.path.abspath(__file__ + "/../../")
 SCRIPTS_TARGET = os.path.join(TEMP_PATH, "bin/cluster-serving")
@@ -94,6 +117,8 @@ def setup_package():
         version=VERSION,
         description='A unified Data Analytics and AI platform for distributed TensorFlow, Keras, '
                     'PyTorch, Apache Spark/Flink and Ray',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
         author='Analytics Zoo Authors',
         author_email='bigdl-user-group@googlegroups.com',
         license='Apache License, Version 2.0',
