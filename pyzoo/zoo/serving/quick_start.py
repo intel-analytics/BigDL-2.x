b9cf6827e33375ef601b37750fba8b4e440f8996
Song Jiaming <litchilitchy@qq.com>
2020-03-20 12:22:25 +0800

    cluster serving leave only one copy of quick start example (#2086)

--- pyzoo/zoo/serving/quick_start.py
+++ pyzoo/zoo/serving/quick_start.py
@@ -19,13 +19,12 @@ import os
 import cv2
 import json
 import time
+from optparse import OptionParser
 
 
-def run():
+def run(path):
     input_api = InputQueue()
-
-    base_path = os.path.abspath(
-        __file__ + "/../../../test/zoo/resources/serving_quick_start")
+    base_path = path
 
     if not base_path:
         raise EOFError("You have to set your image path")
@@ -39,7 +38,7 @@ def run():
         img = cv2.resize(img, (224, 224))
         input_api.enqueue_image(p, img)
 
-    time.sleep(5)
+    time.sleep(10)
 
     # get all result and dequeue
     result = output_api.dequeue()
@@ -53,4 +52,8 @@ def run():
 
 
 if __name__ == "__main__":
-    run()
+    parser = OptionParser()
+    parser.add_option("-i", "--image_path", dest="path", default="test_image")
+    import sys
+    (options, args) = parser.parse_args(sys.argv)
+    run(options.path)
