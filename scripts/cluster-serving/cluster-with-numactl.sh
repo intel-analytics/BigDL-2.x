8107c436d3304e16550225c5176a35bf8b742c28
cyita <yitastudy@gmail.com>
2020-03-20 11:59:32 +0800

    fix

--- scripts/cluster-serving/cluster-with-numactl.sh
+++ scripts/cluster-serving/cluster-with-numactl.sh
@@ -27,7 +27,7 @@ fi
 
 _WORKER_CORE_NUM_LESS_THAN=24
 _MODE=$1 # start or stop
-_WORKER_NUM=$2  # worker num on each numa node
+_WORKER_PER_SOCKET=$2  # worker num on each numa node
 
 if [ "${_MODE}" != "start" ] && [ "${_MODE}" != "stop" ]; then
   echo "failed,mode should be start or stop."
@@ -39,7 +39,7 @@ if ht_enabled; then
   TOTAL_CORE_NUM=$((TOTAL_CORE_NUM / 2))
 fi
 
-if [ $TOTAL_CORE_NUM -lt $_WORKER_CORE_NUM_LESS_THAN ] && [ -z "${_WORKER_NUM}" ]; then
+if [ $TOTAL_CORE_NUM -lt $_WORKER_CORE_NUM_LESS_THAN ] && [ -z "${_WORKER_PER_SOCKET}" ]; then
   # use local mode
   echo "local[*]"
   exit 1
@@ -66,9 +66,6 @@ if [ "${_MODE}" == "start" ] && [ -n "$grep_port" ]; then
   echo "failed,Spark master port $SPARK_MASTER_PORT is in use"
   exit 1
 fi
-
-# Start or stop master node
-"${SPARK_HOME}/sbin"/spark-daemon.sh "${_MODE}" org.apache.spark.deploy.master.Master spark-master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT
 
 # NOTE: This exact class name is matched downstream by SparkSubmit.
 # Any changes need to be reflected there.
@@ -122,7 +119,14 @@ _TOTAL_MEM=`grep MemTotal /proc/meminfo | awk '{print $2}'`
 _1G=1048576
 _MEMORY_FOR_DRIVER=2  # reserve 2g memory for the driver
 _NUMA_MEM=$((((_TOTAL_MEM - _1G - (_1G * _MEMORY_FOR_DRIVER)) / _1G) / $_NUMA_NODE_NUM))
-  
+if [[ $_NUMA_MEM -le 0 ]]; then
+  echo "failed,Not enough memory for cluster serving performance mode"
+  exit 1
+fi
+
+# Start or stop master node
+"${SPARK_HOME}/sbin"/spark-daemon.sh "${_MODE}" org.apache.spark.deploy.master.Master spark-master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT
+
 _WORKER_NAME_NO=1
 
 # Load NUMA configurations line-by-line and set `numactl` options
@@ -132,16 +136,19 @@ for nnode in ${_NUMA_HARDWARE_INFO[@]}; do
     IFS=' ' _NUMA_CPUS=(${BASH_REMATCH[2]})
     _LENGTH=${#_NUMA_CPUS[@]}
     if ht_enabled; then _LENGTH=$((_LENGTH / 2)); fi
-    if [[ -z "${_WORKER_NUM}" ]]; then
+    if [[ -z "${_WORKER_PER_SOCKET}" ]]; then
       # calculate worker num on this numa node, 12 ~ 23 core/worker
-      _WORKER_NUM=$((_LENGTH / $((_WORKER_CORE_NUM_LESS_THAN / 2))))
+      _WORKER_PER_SOCKET=$((_LENGTH / $((_WORKER_CORE_NUM_LESS_THAN / 2))))
     fi
-    if [[ $_WORKER_NUM -eq 0 ]]; then
-      _WORKER_NUM=1
+    if [[ $_WORKER_PER_SOCKET -eq 0 ]]; then
+      _WORKER_PER_SOCKET=1
     fi
-    _LENGTH=$((_LENGTH / _WORKER_NUM))
+    if [[ $_WORKER_PER_SOCKET -gt $_NUMA_MEM ]]; then
+      _WORKER_PER_SOCKET=$_NUMA_MEM
+    fi
+    _LENGTH=$((_LENGTH / _WORKER_PER_SOCKET))
 
-    for ((i = 0; i < $((_WORKER_NUM)); i ++)); do
+    for ((i = 0; i < $((_WORKER_PER_SOCKET)); i ++)); do
       if [ "${_MODE}" == "start" ]; then
         core_start=$(( i * _LENGTH ))
         _NUMACTL="numactl -m ${_NUMA_NO} -C $(join_by , ${_NUMA_CPUS[@]:${core_start}:${_LENGTH}})"
@@ -149,7 +156,7 @@ for nnode in ${_NUMA_HARDWARE_INFO[@]}; do
 
         # Launch a worker with numactl
         export SPARK_WORKER_CORES=${_LENGTH} # core num per worker
-        export SPARK_WORKER_MEMORY="$((_NUMA_MEM / _WORKER_NUM))g"
+        export SPARK_WORKER_MEMORY="$((_NUMA_MEM / _WORKER_PER_SOCKET))g"
         start_instance "$_NUMACTL" "$_WORKER_NAME_NO"
       else
         stop_instance "$_WORKER_NAME_NO"
