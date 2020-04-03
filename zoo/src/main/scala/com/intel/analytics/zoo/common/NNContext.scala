1f2e5d78b0b3fb76ccac348ca00f15e1dcad2b00
Qiyuan Gong <qiyuan.gong@intel.com>
2020-04-03 16:16:37 +0800

    Fix nncontext conf

--- zoo/src/main/scala/com/intel/analytics/zoo/common/NNContext.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/common/NNContext.scala
@@ -232,8 +232,8 @@ object NNContext {
       zooConf.setExecutorEnv("KMP_BLOCKTIME", kmpBlockTime)
       zooConf.setExecutorEnv("KMP_SETTINGS", kmpSettings)
       zooConf.setExecutorEnv("OMP_NUM_THREADS", ompNumThreads)
-      zooConf.setExecutorEnv("spark.executor.extraJavaOptions",
-        "-Dbigdl.mklNumThreads" + env("OMP_NUM_THREADS").toString)
+      zooConf.set("spark.executor.extraJavaOptions",
+        "-Dbigdl.mklNumThreads=" + ompNumThreads.toString)
     }
   }
 
