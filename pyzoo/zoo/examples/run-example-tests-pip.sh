0b808109c3f92474ac0d83ab8d8cd5224f339000
Le-Zheng <30695225+Le-Zheng@users.noreply.github.com>
2020-04-07 08:21:00 +0800

    fix python examples on k8s (#2127)
    * fix python examples on k8s

--- pyzoo/zoo/examples/run-example-tests-pip.sh
+++ pyzoo/zoo/examples/run-example-tests-pip.sh
@@ -596,6 +596,27 @@ unset SPARK_DRIVER_MEMORY
 now=$(date "+%s")
 time14=$((now-start))
 
+echo "#15 start example test for attention"
+start=$(date "+%s")
+sed "s/max_features = 20000/max_features = 200/g;s/max_len = 200/max_len = 20/g;s/hidden_size=128/hidden_size=8/g" \
+    ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/attention/transformer.py \
+    > ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/attention/tmp.py
+export SPARK_DRIVER_MEMORY=20g
+python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/attention/tmp.py
+
+exit_status=$?
+if [ $exit_status -ne 0 ];
+then
+    clear_up
+    echo "attention failed"
+    exit $exit_status
+fi
+
+unset SPARK_DRIVER_MEMORY
+now=$(date "+%s")
+time15=$((now-start))
+echo "attention time used:$time15 seconds"
+
 # This should be done at the very end after all tests finish.
 clear_up
 
@@ -612,3 +633,4 @@ echo "#10 qaranker time used: $time10 seconds"
 echo "#12 vnni/openvino time used: $time12 seconds"
 echo "#13 streaming Object Detection time used: $time13 seconds"
 echo "#14 streaming text classification time used: $time14 seconds"
+echo "#15 start example test for attention time used: $time15 seconds"
