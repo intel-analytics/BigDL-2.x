c486c21da38863fbe4bc6e2ad24814b4c0e7e96f
Shane Huang <shengsheng.huang@intel.com>
2020-04-09 13:30:58 +0800

    add anomaly detector

---
+++ pyzoo/zoo/zouwu/model/anomaly.py
@@ -0,0 +1,138 @@
+#
+# Copyright 2018 Analytics Zoo Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import math
+import numpy as np
+
+from abc import ABCMeta, abstractmethod
+
+
+class Distance(metaclass=ABCMeta):
+    """
+    The Base Distance Class.
+    """
+    @abstractmethod
+    def distance(self, x, y):
+        """
+        Calculate the distance between x and y. a and b should be in same shape.
+        :param x: the first tensor
+        :param y: the second tensor
+        :return: the absolute distance between x and y
+        """
+        pass
+
+
+class EuclideanDistance(Distance):
+    """
+    Euclidean Distance Measure
+    """
+
+    def __init__(self):
+        pass
+
+    def distance(self, x, y):
+        return np.linalg.norm(x - y)
+
+
+class ThresholdFitter:
+    def fit(self,
+            y,
+            yhat,
+            mode="percentile"):
+        """
+        fit the y and yhat and find the proper threshold
+        :param y:
+        :param yhat:
+        :param mode:
+        :return: threshold
+        """
+        pass
+
+
+class DetectorBase(metaclass=ABCMeta):
+    """
+    Base class for detector
+    """
+    @abstractmethod
+    def detect(self, y, **kwargs):
+        """
+        Detect anomalies in dataset
+        :param y: the dataset
+        :return: the anomaly indexes in y
+        """
+        pass
+
+
+class ThresholdDetector(DetectorBase):
+    """
+    Anomaly detector
+    """
+
+    def __init__(self):
+        pass
+
+    def detect(self,
+               y,
+               yhat=None,
+               threshold=math.inf,
+               dist_measure: Distance = EuclideanDistance()):
+        """
+        Detect anomalies. Each sample can have 1 or more dimensions.
+        :param dist_measure:
+        :param y: the values to detect. shape could be
+                1-D (num_samples,)
+                or 2-D array (num_samples, features)
+        :param yhat: the estimated values, a tensor with same shape as y,
+                could be None when threshold is a tuple
+        :param threshold: threshold, could be
+            1. a single value -  absolute distance threshold, same for all samples
+            2. a 1-D array in shape (num_samples,) - per sample absolute distance threshold
+            3. a tensor in same shape as y and yhat - per dimension absolute distance threshold
+            4. a tuple (min, max) min and max tensors, same shape as y, yhat is ignored in this case
+        :return: the anomaly values indexes in the samples, i.e. num_samples dimension.
+        """
+        if isinstance(threshold, int) or \
+                isinstance(threshold, float):
+            self._check_all_distance(y, yhat, threshold)
+        elif isinstance(threshold, np.array):
+            if len(threshold.shape) == 2:
+                self._check_per_dim_distance(y, yhat, threshold)
+            elif len(threshold.shape) == 1:
+                self._check_per_sample_distance(y, yhat, threshold)
+            else:
+                raise ValueError(
+                    "threshold shape", str(
+                        threshold.shape), "is not valid")
+        elif isinstance(threshold, tuple) \
+                and len(threshold) == 2 \
+                and threshold.shape == y.shape:
+            return self._check_range(y, threshold)
+        else:
+            raise ValueError(
+                "threshold shape", str(threshold),
+                "is not valid")
+
+    def _check_all_distance(self, y, yhat, threshold):
+        pass
+
+    def _check_per_dim_distance(self, y, yhat, threshold):
+        pass
+
+    def _check_per_sample_distance(self, y, yhat, threshold):
+        pass
+
+    def _check_range(self, y, threshold):
+        pass
