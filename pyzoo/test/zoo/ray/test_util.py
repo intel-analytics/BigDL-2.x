70f2407ebdecabdb721db67e47804c9cba365d9c
Kai Huang <huangkaivision@gmail.com>
2020-04-09 18:06:51 +0800

    revert

--- pyzoo/test/zoo/ray/test_util.py
+++ pyzoo/test/zoo/ray/test_util.py
@@ -25,20 +25,11 @@ np.random.seed(1337)  # for reproducibility
 
 class TestUtil(TestCase):
 
-    # def test_split(self):
-    #     vector = np.ones([10])
-    #     result = rutils.split(vector, 4)
-    #     assert len(result) == 4
-    #     assert len(result[0]) == 3
-    #     assert len(result[1]) == 3
-    #     assert len(result[2]) == 2
-    #     assert len(result[3]) == 2
-
-    # def test_resource_to_bytes(self):
-    #     assert 10 == rutils.resourceToBytes("10b")
-    #     assert 10000 == rutils.resourceToBytes("10k")
-    #     assert 10000000 == rutils.resourceToBytes("10m")
-    #     assert 10000000000 == rutils.resourceToBytes("10g")
+    def test_resource_to_bytes(self):
+        assert 10 == rutils.resourceToBytes("10b")
+        assert 10000 == rutils.resourceToBytes("10k")
+        assert 10000000 == rutils.resourceToBytes("10m")
+        assert 10000000000 == rutils.resourceToBytes("10g")
 
 
 if __name__ == "__main__":
