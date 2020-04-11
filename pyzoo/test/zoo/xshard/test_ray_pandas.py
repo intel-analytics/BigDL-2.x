5e79ff35d0b01ea0aafb1568bd5aad7b31f97bdc
jenniew <jenniewang123@gmail.com>
2020-04-10 17:28:47 -0700

    update test

--- pyzoo/test/zoo/xshard/test_ray_pandas.py
+++ pyzoo/test/zoo/xshard/test_ray_pandas.py
@@ -40,62 +40,56 @@ class TestDataShards(ZooTestCase):
         """
         pass
 
-    # def test_read_local_csv(self):
-    #     file_path = os.path.join(self.resource_path, "xshard")
-    #     data_shard = zoo.xshard.pandas.read_csv(file_path, self.ray_ctx)
-    #     data = data_shard.collect()
-    #     assert len(data) == 2, "number of shard should be 2"
-    #     df = data[0]
-    #     assert "location" in df.columns, "location is not in columns"
-    #
-    # def test_read_local_json(self):
-    #     file_path = os.path.join(self.resource_path, "xshard")
-    #     data_shard = zoo.xshard.pandas.read_json(file_path, ray_ctx)
-    #     data = data_shard.collect()
-    #     assert len(data) == 2, "number of shard should be 2"
-    #     df = data[0]
-    #     assert "value" in df.columns, "value is not in columns"
+    def test_read_local_csv(self):
+        file_path = os.path.join(self.resource_path, "xshard")
+        data_shard = zoo.xshard.pandas.read_csv(file_path, self.ray_ctx)
+        data = data_shard.collect()
+        assert len(data) == 2, "number of shard should be 2"
+        df = data[0]
+        assert "location" in df.columns, "location is not in columns"
 
-    # def test_read_s3(self):
-    #
-    #     access_key_id = os.environ["AWS_ACCESS_KEY_ID"]
-    #     secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"]
-    #     self.ray_ctx.stop()
-    #     self.ray_ctx = RayContext(sc=self.sc,
-    #                               object_store_memory="1g",
-    #                               env={"AWS_ACCESS_KEY_ID":access_key_id,
-    #                                    "AWS_SECRET_ACCESS_KEY":secret_access_key}
-    #                               )
-    #     self.ray_ctx.init()
-    #     file_path = "s3://analytics-zoo-data/nyc_taxi.csv"
-    #     data_shard = zoo.xshard.pandas.read_csv(file_path, self.ray_ctx)
-    #     data = data_shard.collect()
-    #     df = data[0]
-    #     assert "value" in df.columns, "value is not in columns"
+    def test_read_local_json(self):
+        file_path = os.path.join(self.resource_path, "xshard")
+        data_shard = zoo.xshard.pandas.read_json(file_path, self.ray_ctx)
+        data = data_shard.collect()
+        assert len(data) == 2, "number of shard should be 2"
+        df = data[0]
+        assert "value" in df.columns, "value is not in columns"
 
-    # def test_repartition(self):
-    #     file_path = os.path.join(self.resource_path, "xshard")
-    #     data_shard = zoo.xshard.pandas.read_json(file_path, ray_ctx)
-    #     partitions1 = data_shard.get_partitions()
-    #     assert len(partitions1) == 2, "number of partition should be 2"
-    #     data_shard.repartition(1)
-    #     partitions2 = data_shard.get_partitions()
-    #     assert len(partitions2) == 1, "number of partition should be 1"
-    #     partition_data = ray.get(partitions2[0].get_data())
-    #     assert len(partition_data) == 2, "partition 0 should have 2 objects"
-    #
-    # def test_apply(self):
-    #     file_path = os.path.join(self.resource_path, "xshard")
-    #     data_shard = zoo.xshard.pandas.read_json(file_path, ray_ctx)
-    #     data = data_shard.collect()
-    #     assert data[0]["value"].values[0] > 0, "value should be positive"
-    #
-    #     def negative(df, column_name):
-    #         df[column_name] = df[column_name] * (-1)
-    #         return df
-    #     data_shard.apply(negative, "value")
-    #     data2 = data_shard.collect()
-    #     assert data2[0]["value"].values[0] < 0, "value should be negative"
+    def test_read_s3(self):
+        access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+        secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+        if access_key_id and secret_access_key:
+            file_path = "s3://analytics-zoo-data/nyc_taxi.csv"
+            data_shard = zoo.xshard.pandas.read_csv(file_path, self.ray_ctx)
+            data = data_shard.collect()
+            df = data[0]
+            assert "value" in df.columns, "value is not in columns"
+
+
+    def test_repartition(self):
+        file_path = os.path.join(self.resource_path, "xshard")
+        data_shard = zoo.xshard.pandas.read_json(file_path, self.ray_ctx)
+        partitions1 = data_shard.get_partitions()
+        assert len(partitions1) == 2, "number of partition should be 2"
+        data_shard.repartition(1)
+        partitions2 = data_shard.get_partitions()
+        assert len(partitions2) == 1, "number of partition should be 1"
+        partition_data = ray.get(partitions2[0].get_data())
+        assert len(partition_data) == 2, "partition 0 should have 2 objects"
+
+    def test_apply(self):
+        file_path = os.path.join(self.resource_path, "xshard")
+        data_shard = zoo.xshard.pandas.read_json(file_path, self.ray_ctx)
+        data = data_shard.collect()
+        assert data[0]["value"].values[0] > 0, "value should be positive"
+
+        def negative(df, column_name):
+            df[column_name] = df[column_name] * (-1)
+            return df
+        data_shard.apply(negative, "value")
+        data2 = data_shard.collect()
+        assert data2[0]["value"].values[0] < 0, "value should be negative"
 
 
 if __name__ == "__main__":
