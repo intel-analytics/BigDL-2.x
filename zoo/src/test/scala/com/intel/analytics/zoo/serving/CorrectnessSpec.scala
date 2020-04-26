b06e1a20c3c460104f361409056e404522edfde5
Jiaming <litchilitchy@qq.com>
2020-04-26 09:26:22 +0800

    unit test spec mod for uploaded http file and dynamic download every time

--- zoo/src/test/scala/com/intel/analytics/zoo/serving/CorrectnessSpec.scala
+++ zoo/src/test/scala/com/intel/analytics/zoo/serving/CorrectnessSpec.scala
@@ -12,28 +12,37 @@ import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class CorrectnessSpec extends FlatSpec with Matchers {
-  val configPath = "/home/litchy/pro/analytics-zoo/config.yaml"
+  val configPath = "/tmp/config.yaml"
   val logger = Logger.getLogger(getClass)
   def runServingBg(): Future[Unit] = Future {
     ClusterServing.run(configPath)
   }
   "Cluster Serving result" should "be correct" in {
     val cli = new Jedis()
-    runServingBg().onComplete(_ => None)
+
     // call push method in python
-    val imagePath = "/home/litchy/val_img"
+    "wget -O /tmp/serving_val.tar http://10.239.45.10:8081/repository/raw/analytics-zoo-data/imagenet_1k.tar".!
+    "tar -xvf /tmp/serving_val.tar -C /tmp/".!
+    runServingBg().onComplete(_ => None)
+
+    val imagePath = "/tmp/imagenet_1k"
     val lsCmd = "ls " + imagePath
 
-    val totalNum = (lsCmd #| "wc").!!.split(" ")(5).toInt
+    val totalNum = (lsCmd #| "wc").!!.split(" +").filter(_ != "").head.toInt
+
 
     val enqueueScriptPathCmd = "python3 " + getClass.getClassLoader.getResource("serving/enqueue_image_in_path.py").getPath +
       " --img_path " + imagePath + " --img_num " + totalNum.toString
-    val p = Process(enqueueScriptPathCmd, None,
-      "PYTHONPATH" -> "$PYTHONPATH:/home/litchy/pro/analytics-zoo/dist/lib/analytics-zoo-bigdl_0.10.0-spark_2.4.3-0.8.0-SNAPSHOT-python-api.zip",
-    "SPARK_HOME" -> "/home/litchy/Programs/spark-2.4.0-bin-hadoop2.7")
+    val p = Process(enqueueScriptPathCmd, None
+//      ,
+//      "PYTHONPATH" -> "$PYTHONPATH:/home/litchy/pro/analytics-zoo/dist/lib/analytics-zoo-bigdl_0.10.0-spark_2.4.3-0.8.0-SNAPSHOT-python-api.zip",
+//    "SPARK_HOME" -> "/home/litchy/Programs/spark-2.4.0-bin-hadoop2.7"
+    )
     p.!
+    ("rm -rf /tmp/" + imagePath + "*").!
+    "rm -rf /tmp/serving_val_*".!
+    "rm -rf /tmp/config.yaml".!
     // check if record is enough
-
     var cnt = 0
     var res_length: Int = 0
     while (res_length != totalNum) {
@@ -71,7 +80,7 @@ class CorrectnessSpec extends FlatSpec with Matchers {
     }
     val acc = cN / tN
     logger.info(s"Top 1 Accuracy of serving, Openvino ResNet50 Model on ImageNet is ${acc}")
-    assert(acc > 0.7)
+    assert(acc > 0.72)
   }
 
 
