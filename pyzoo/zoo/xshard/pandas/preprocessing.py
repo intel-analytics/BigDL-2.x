47a45f3dfe3a67aae996185c6966cd5e41aab64c
dding3 <ding.ding@intel.com>
2020-04-29 10:19:01 -0700

    fix comments

--- pyzoo/zoo/xshard/pandas/preprocessing.py
+++ pyzoo/zoo/xshard/pandas/preprocessing.py
@@ -140,7 +140,7 @@ def read_file_spark(context, file_path, file_type):
         data_paths = list_s3_file(file_url_splits, file_type)
     else:
         data_paths = get_file_list(file_path)
-    rdd = context.parallelize(data_paths, node_num * 20)
+    rdd = context.parallelize(data_paths, core_num * 2)
 
     if prefix == "hdfs":
         def loadFile(iterator):
