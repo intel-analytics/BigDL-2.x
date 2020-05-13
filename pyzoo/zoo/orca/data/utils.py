023693b9a67574c7e0a0387d36f80fbf3e701ca3
jenniew <jenniewang123@gmail.com>
2020-05-13 11:48:38 -0700

    fix util

--- pyzoo/zoo/orca/data/utils.py
+++ pyzoo/zoo/orca/data/utils.py
@@ -75,4 +75,4 @@ def get_class_name(obj):
     module = obj.__class__.__module__
     if module is None or module == str.__class__.__module__:
         return obj.__class__.__name__
-    return module + '.' + obj.__class__.__name_
+    return module + '.' + obj.__class__.__name__
