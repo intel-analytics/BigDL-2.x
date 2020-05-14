be3f2b3ab46f4c45704bd02dc0937029ccc509d8
Jiaming <litchilitchy@qq.com>
2020-05-14 15:01:19 +0800

    style

--- zoo/src/main/scala/com/intel/analytics/zoo/serving/ClusterServing.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/serving/ClusterServing.scala
@@ -30,7 +30,8 @@ object ClusterServing {
   Logger.getLogger("org").setLevel(Level.ERROR)
   Logger.getLogger("com.intel.analytics.zoo").setLevel(Level.INFO)
   var params: SerParams = null
-  def run(configPath: String = "config.yaml", redisHost: String = null, redisPort: Int = -1): Unit = {
+  def run(configPath: String = "config.yaml",
+          redisHost: String = null, redisPort: Int = -1): Unit = {
     val helper = new ClusterServingHelper(configPath)
     helper.initArgs()
     params = new SerParams(helper)
