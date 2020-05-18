bbdb903b785d99cea5f84a066606794d9e3e4ddd
Wang, Yang <yang3.wang@intel.com>
2020-05-18 13:10:25 +0800

    align tranform_shard api

--- pyzoo/zoo/orca/data/shard.py
+++ pyzoo/zoo/orca/data/shard.py
@@ -105,7 +105,7 @@ class SparkDataShards(DataShards):
         self.rdd = rdd
 
     def transform_shard(self, func, *args):
-        self.rdd = self.rdd.map(func(*args))
+        self.rdd = self.rdd.map(lambda data: func(data, *args))
         return self
 
     def collect(self):
