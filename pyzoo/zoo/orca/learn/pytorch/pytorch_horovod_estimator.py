2898d3d5ae0137b1eb7b9ab11e88529704ffbb66
Wang, Yang <yang3.wang@intel.com>
2020-07-05 14:42:15 +0800

    pytorch horovod ray 0.8.6

--- pyzoo/zoo/orca/learn/pytorch/pytorch_horovod_estimator.py
+++ pyzoo/zoo/orca/learn/pytorch/pytorch_horovod_estimator.py
@@ -20,11 +20,12 @@ import torch.nn as nn
 import collections
 import numpy as np
 import numbers
+import io
 
 from ray.util.sgd.torch import TrainingOperator
 from ray.util.sgd.utils import check_for_failure
-from ray.util.sgd.torch.distributed_torch_runner import DistributedTorchRunner
-from torch.utils.data import DataLoader
+from ray.util.sgd.torch.torch_runner import TorchRunner
+from torch.utils.data import DataLoader, IterableDataset
 from torch.utils.data.distributed import DistributedSampler
 from zoo.orca.learn.horovod.horovod_ray_runner import HorovodRayRunner, HorovodWorker
 from zoo.ray import RayContext
@@ -33,16 +34,17 @@ import ray
 logger = logging.getLogger(__name__)
 
 
-class TorchWorker(HorovodWorker, DistributedTorchRunner):
+class TorchWorker(HorovodWorker, TorchRunner):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, backend="gloo", **kwargs)
+        super().__init__(*args, **kwargs)
 
-    def _setup_distributed_pytorch(self, url, world_rank, world_size):
+    def setup_components(self):
         import horovod.torch as hvd
-        hvd.init()
 
-    def _setup_training(self):
-        import horovod.torch as hvd
+        logger.debug("Loading data.")
+        if self.data_creator and callable(self.data_creator):
+            self._initialize_dataloaders()
+
         logger.debug("Creating model")
         self.models = self.model_creator(self.config)
         if not isinstance(self.models, collections.Iterable):
@@ -70,27 +72,11 @@ class TorchWorker(HorovodWorker, DistributedTorchRunner):
 
         self._create_schedulers_if_available()
         self._try_setup_apex()
-
         self._create_loss()
-        self._initialize_dataloaders()
 
-        self.training_operator = self.training_operator_cls(
-            self.config,
-            models=self.models,
-            optimizers=self.optimizers,
-            criterion=self.criterion,
-            train_loader=self.train_loader,
-            validation_loader=self.validation_loader,
-            world_rank=hvd.rank(),
-            schedulers=self.schedulers,
-            use_fp16=self.use_fp16,
-            use_tqdm=self.use_tqdm)
-
-    def _initialize_dataloaders(self):
-        import horovod.torch as hvd
-        super(DistributedTorchRunner, self)._initialize_dataloaders()
-
+    def _wrap_dataloaders(self):
         def with_sampler(loader):
+            import horovod.torch as hvd
             # Automatically set the DistributedSampler
             data_loader_args = {
                 "dataset": loader.dataset,
@@ -108,12 +94,50 @@ class TorchWorker(HorovodWorker, DistributedTorchRunner):
             }
             return DataLoader(**data_loader_args)
 
-        if isinstance(self.train_loader, DataLoader):
-            self.train_loader = with_sampler(self.train_loader)
+        self.train_loader = with_sampler(self.train_loader)
 
-        if self.validation_loader and isinstance(self.validation_loader,
-                                                 DataLoader):
+        if self.validation_loader:
             self.validation_loader = with_sampler(self.validation_loader)
+
+    def setup_ddp_and_operator(self):
+        """Runs distributed coordination components.
+
+        This helps avoid timeouts due to creator functions (perhaps
+        downloading data or models).
+        """
+        import horovod.torch as hvd
+
+        # Wrap dataloaders
+        self._wrap_dataloaders()
+
+        self.training_operator = self.training_operator_cls(
+            self.config,
+            models=self.models,
+            optimizers=self.optimizers,
+            criterion=self.criterion,
+            train_loader=self.train_loader,
+            validation_loader=self.validation_loader,
+            world_rank=hvd.rank(),
+            schedulers=self.schedulers,
+            use_fp16=self.use_fp16,
+            use_tqdm=self.use_tqdm)
+
+    def load_state_stream(self, byte_obj):
+        """Loads a bytes object the training state dict.
+
+        This is needed because we don't want to deserialize the tensor
+        onto the same device (which is from the driver process). We want to
+        map it onto the actor's specific device.
+
+        From: github.com/pytorch/pytorch/issues/10622#issuecomment-474733769
+        """
+        _buffer = io.BytesIO(byte_obj)
+        to_gpu = self.use_gpu and torch.cuda.is_available()
+        state_dict = torch.load(
+            _buffer,
+            map_location=("cpu" if not to_gpu else
+                          lambda storage, loc: storage.cuda()))
+        return self.load_state_dict(state_dict)
 
 
 class PyTorchHorovodEstimator(HorovodRayRunner):
@@ -162,18 +186,35 @@ class PyTorchHorovodEstimator(HorovodRayRunner):
             scheduler_step_freq=self.scheduler_step_freq)
         super().__init__(RayContext.get(), worker_cls=TorchWorker, worker_param=self.param)
 
-        def init_func():
+        def setup_pytorch():
             import torch
             torch.set_num_threads(self.cores_per_node)
             print("Worker initialized")
 
-        self.run(init_func)
-        remote_setups = [
-            worker.setup.remote(None, None, None)
+        self.run(setup_pytorch)
+
+        def setup_horovod():
+            import horovod.torch as hvd
+            hvd.init()
+            print("Horovod initialized")
+
+        self.run(setup_horovod)
+
+        # Runs the creator functions.
+        remote_component_setup = [
+            worker.setup_components.remote()
             for i, worker in enumerate(self.remote_workers)
         ]
         # Get setup tasks in order to throw errors on failure
-        ray.get(remote_setups)
+        ray.get(remote_component_setup)
+
+        # Runs code that requires all creator functions to have run.
+        remote_operator_setups = [
+            worker.setup_ddp_and_operator.remote()
+            for worker in self.remote_workers
+        ]
+        # Get setup tasks in order to throw errors on failure
+        ray.get(remote_operator_setups)
 
     def train(self,
               num_steps=None,
