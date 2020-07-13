8a49d0432b72a6e08d3fb26fb5b355a58c49bb9b
dding3 <ding.ding@intel.com>
2020-07-13 10:16:07 -0700

    exclude xgbtrain test from spark2._x

--- zoo/src/test/scala/com/intel/analytics/zoo/pipeline/nnframes/NNClassifierSpec.scala
+++ zoo/src/test/scala/com/intel/analytics/zoo/pipeline/nnframes/NNClassifierSpec.scala
@@ -466,32 +466,6 @@ class NNClassifierSpec extends ZooSpecHelper {
 
     model.transform(xgbInput).count()
   }
-
-  "XGBRegressor" should "work" in {
-    val spark = SparkSession.builder().getOrCreate()
-    import spark.implicits._
-
-    val df = Seq(
-      (1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 1.0f, 2.0f, 4.0f, 8.0f, 3.0f, 116.3668f),
-      (1.0f, 3.0f, 8.0f, 6.0f, 5.0f, 9.0f, 5.0f, 6.0f, 7.0f, 4.0f, 116.367f),
-      (2.0f, 1.0f, 5.0f, 7.0f, 6.0f, 7.0f, 4.0f, 1.0f, 2.0f, 3.0f, 116.367f),
-      (2.0f, 1.0f, 4.0f, 3.0f, 6.0f, 1.0f, 3.0f, 2.0f, 1.0f, 3.0f, 116.3668f)
-    ).toDF("f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "label")
-
-    val vectorAssembler = new VectorAssembler()
-      .setInputCols(Array("f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10"))
-      .setOutputCol("features")
-    val assembledDf = vectorAssembler.transform(df).select("features", "label").cache()
-
-    val xgbRf0 = new XGBRegressor()
-    val xgbRegressorModel0 = xgbRf0.fit(assembledDf)
-    val y0 = xgbRegressorModel0.transform(assembledDf)
-
-    xgbRegressorModel0.save("/tmp/test")
-    val model = XGBRegressorModel.load("/tmp/test")
-    val y0_0 = model.transform(assembledDf)
-    assert(y0_0.except(y0).count()==0)
-  }
 }
 
 private case class Data(label: Double, features: Array[Double])
