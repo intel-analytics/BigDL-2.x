0384e0c98f68a9bf5fe5d8e91038037b95ae8619
Wang, Yang <yang3.wang@intel.com>
2020-07-16 10:32:12 +0800

    add license

--- pyzoo/zoo/orca/learn/tf/tf_ray_estimator.py
+++ pyzoo/zoo/orca/learn/tf/tf_ray_estimator.py
@@ -1,3 +1,19 @@
+#
+# Copyright 2018 Analytics Zoo Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 from zoo.orca.learn.tf.tf_trainer import TFTrainer
 from zoo.ray import RayContext
 
@@ -26,10 +42,10 @@ class TFRayEstimator():
                                     num_cpus_per_worker=self.ray_ctx.ray_node_cpu_cores,
                                     use_gpu=False,
                                     verbose=verbose)
-    def train(self):
+    def fit(self):
         return self.tf_trainer.train()
 
-    def validate(self):
+    def evaluates(self):
         return self.tf_trainer.validate()
 
     def get_model(self):
