ec6d48a096aa44d828232463abd9f62caf091b9e
Wang, Yang <yang3.wang@intel.com>
2020-07-16 15:09:02 +0800

    add tests

---
+++ pyzoo/test/zoo/orca/learn/ray/tf/test_tf_ray_estimator.py
@@ -0,0 +1,107 @@
+#
+# Copyright 2018 Analytics Zoo Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from unittest import TestCase
+from zoo.orca.learn.tf.tf_ray_estimator import TFRayEstimator
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import numpy as np
+
+NUM_TRAIN_SAMPLES = 1000
+NUM_TEST_SAMPLES = 400
+
+
+def create_config(batch_size):
+    return {
+        # todo: batch size needs to scale with # of workers
+        "batch_size": batch_size,
+        "fit_config": {
+            "steps_per_epoch": NUM_TRAIN_SAMPLES // batch_size
+        },
+        "evaluate_config": {
+            "steps": NUM_TEST_SAMPLES // batch_size,
+        }
+    }
+
+
+def linear_dataset(a=2, size=1000):
+    x = np.random.rand(size)
+    y = x / 2
+
+    x = x.reshape((-1, 1))
+    y = y.reshape((-1, 1))
+
+    return x, y
+
+
+def simple_dataset(config):
+    batch_size = config["batch_size"]
+    x_train, y_train = linear_dataset(size=NUM_TRAIN_SAMPLES)
+    x_test, y_test = linear_dataset(size=NUM_TEST_SAMPLES)
+
+    train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+    train_dataset = train_dataset.shuffle(NUM_TRAIN_SAMPLES).repeat().batch(
+        batch_size)
+    test_dataset = test_dataset.repeat().batch(batch_size)
+
+    return train_dataset, test_dataset
+
+
+def simple_model(config):
+    model = Sequential([Dense(10, input_shape=(1, )), Dense(1)])
+    return model
+
+
+def compile_args(config):
+    args = {
+        "optimizer": "sgd",
+        "loss": "mean_squared_error",
+        "metrics": ["mean_squared_error"]
+    }
+    return args
+
+
+class TestTFRayEstimator(TestCase):
+
+    def test_fit_and_evaluate(self):
+
+        trainer = TFRayEstimator(
+            model_creator=simple_model,
+            compile_args_creator=compile_args,
+            data_creator=simple_dataset,
+            verbose=True,
+            config=create_config(32))
+
+        # model baseline performance
+        start_stats = trainer.evaluate()
+        print(start_stats)
+
+        # train for 2 epochs
+        trainer.fit()
+        trainer.fit()
+
+        # model performance after training (should improve)
+        end_stats = trainer.evaluate()
+        print(end_stats)
+
+        # sanity check that training worked
+        dloss = end_stats["validation_loss"] - start_stats["validation_loss"]
+        dmse = (end_stats["validation_mean_squared_error"] -
+                start_stats["validation_mean_squared_error"])
+        print(f"dLoss: {dloss}, dMSE: {dmse}")
+
+        assert dloss < 0 and dmse < 0, "training sanity check failed. loss increased!"
--- pyzoo/zoo/orca/learn/tf/tf_ray_estimator.py
+++ pyzoo/zoo/orca/learn/tf/tf_ray_estimator.py
@@ -17,35 +17,37 @@
 from zoo.orca.learn.tf.tf_trainer import TFTrainer
 from zoo.ray import RayContext
 
-class TFRayEstimator():
-    
+
+class TFRayEstimator:
     def __init__(self,
                  model_creator,
                  compile_args_creator,
                  data_creator,
                  config=None,
                  verbose=False
-                ):
-        
+                 ):
         def trainer_model_creator(config):
             model = model_creator(config)
-            compile_args = compile_args_creator(config),
+            compile_args = compile_args_creator(config)
             # to support horovod, we need to wrap hvd.DistributedOptimizer on
             # compile_args["optimizer"]
+            print(compile_args)
             model.compile(**compile_args)
             return model
+
         self.ray_ctx = RayContext.get()
         self.tf_trainer = TFTrainer(trainer_model_creator,
                                     data_creator,
                                     config=config,
-                                    num_replicas=self.num_ray_nodes,
+                                    num_replicas=self.ray_ctx.num_ray_nodes,
                                     num_cpus_per_worker=self.ray_ctx.ray_node_cpu_cores,
                                     use_gpu=False,
                                     verbose=verbose)
+
     def fit(self):
         return self.tf_trainer.train()
 
-    def evaluates(self):
+    def evaluate(self):
         return self.tf_trainer.validate()
 
     def get_model(self):
