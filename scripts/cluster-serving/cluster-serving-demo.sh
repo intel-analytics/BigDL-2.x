bf4512a13b8b6eefd44a13e24771c0205797c987
chengwei <cw.peng@foxmail.com>
2020-07-28 20:54:49 +0800

    rename to zoo.jar and add a demo script

--- scripts/cluster-serving/cluster-serving-demo.sh
+++ scripts/cluster-serving/cluster-serving-demo.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-chmod a+x ./*
-
-export PYTHONPATH=$PYTHONPATH:./analytics-zoo-bigdl_0.10.0-spark_2.4.3-0.9.0-SNAPSHOT-python-api.zip
-
-mv analytics-zoo-bigdl_0.10.0-spark_2.4.3-0.9.0-SNAPSHOT-serving.jar zoo.jar
+./cluster-serving-prepare.sh
 
 ./cluster-serving-start -c ./config.yaml & sleep 20
 
