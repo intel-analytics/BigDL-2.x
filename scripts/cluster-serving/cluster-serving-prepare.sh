367f08b3aa9af9c852fd2a70446fed2dceef0227
Jiaming <litchilitchy@qq.com>
2020-08-03 10:35:31 +0800

    sh ref

--- scripts/cluster-serving/cluster-serving-prepare.sh
+++ scripts/cluster-serving/cluster-serving-prepare.sh
@@ -5,4 +5,8 @@ chmod a+x ./*
 export PYTHONPATH=$PYTHONPATH:./analytics-zoo-bigdl_0.10.0-spark_2.4.3-0.9.0-SNAPSHOT-python-api.zip
 mv analytics-zoo-bigdl_0.10.0-spark_2.4.3-0.9.0-SNAPSHOT-serving.jar zoo.jar
 
-echo "cluster serving env is ready"
+chmod a+x cluster-serving-*
+export CS_PATH=$(pwd)
+export PATH=$PATH:$CS_PATH
+
+echo "cluster serving environment is ready"
