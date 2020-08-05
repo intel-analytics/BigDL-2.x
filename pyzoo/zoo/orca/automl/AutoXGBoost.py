b96e79b92dfc146c3362ea9650c8b980e481f334
dding3 <ding.ding@intel.com>
2020-08-05 11:57:44 -0700

    fix style check

--- pyzoo/zoo/orca/automl/AutoXGBoost.py
+++ pyzoo/zoo/orca/automl/AutoXGBoost.py
@@ -19,39 +19,10 @@ from zoo.automl.config.recipe import *
 
 
 class AutoXGBoost(object):
-    def regressor(self,
-                  feature_cols,
+    @staticmethod
+    def regressor(feature_cols,
                   target_col,
                   name="automl",
                   logs_dir="~/zoo_automl_logs"):
-        # models
-        self.tsp = XgbRegressorPredictor(feature_cols, target_col,
-                                         name, logs_dir)
-        return self.tsp
-
-    def fit(self,
-            input_df,
-            validation_df,
-            metric="rmse",
-            recipe=XgbRegressorGridRandomRecipe(),
-            mc=False,
-            resources_per_trial={"cpu": 2},
-            distributed=False,
-            hdfs_url=None):
-        pipeline = self.tsp.fit(input_df, validation_df, metric,
-                                recipe, mc, resources_per_trial,
-                                distributed, hdfs_url)
-        return pipeline
-
-    def predict(self, input_df):
-        output = self.tsp.predict(input_df)
-        return output
-
-    def evaluate(self,
-                 input_df,
-                 metric=None):
-        res = self.tsp.fit(input_df, metric)
-        return res
-
-    def getModel(self):
-        return self.tsp.pipeline.model
+        tsp = XgbRegressorPredictor(feature_cols, target_col, name, logs_dir)
+        return tsp
