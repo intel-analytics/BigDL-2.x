b14802d74ec966bdfcc530edd9dfac6316f95d4c
Song Jiaming <litchy233@gmail.com>
2020-08-05 15:03:58 +0800

    Cluster Serving postprocessing pick topN filter (can use in fasterrcnn) (#2678)

--- zoo/src/main/scala/com/intel/analytics/zoo/serving/engine/InferenceSupportive.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/serving/engine/InferenceSupportive.scala
@@ -61,9 +61,10 @@ object InferenceSupportive {
          * original Tensor, thus if reuse of Tensor is needed,
          * have to squeeze it back.
          */
-        //      dimCheck(t, "add", params)
+        dimCheck(t, "add", params)
         val result = params.model.doPredict(t)
         dimCheck(result, "remove", params)
+        dimCheck(t, "remove", params)
         val t3 = System.nanoTime()
         println(s"Inference and Dim check time ${(t3 - t2) / 1e9} s")
         val kvResult = if (result.isTensor) {
@@ -123,9 +124,6 @@ object InferenceSupportive {
         newSize = newSize :+ elem
       }
       t(key).asInstanceOf[Tensor[Float]].resize(newSize)
-      if (params.modelType == "openvino") {
-        t(key).asInstanceOf[Tensor[Float]].addSingletonDimension()
-      }
     })
     if (params.dataShape.size == 1) {
       t.keySet.foreach(key => {
