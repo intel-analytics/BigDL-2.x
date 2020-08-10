e3bced99c7e195ffa1129ddc29a3888ab51d7b62
cyita <yitastudy@gmail.com>
2020-08-10 17:02:19 +0800

    add dataloader testcase

--- pyzoo/test/zoo/orca/learn/spark/test_pytorch_estimator_for_spark.py
+++ pyzoo/test/zoo/orca/learn/spark/test_pytorch_estimator_for_spark.py
@@ -19,6 +19,7 @@ from unittest import TestCase
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 
 from zoo.orca.data.pandas import read_csv
 from zoo.orca.learn.pytorch import Estimator
@@ -38,8 +39,23 @@ class SimpleModel(nn.Module):
         return F.log_softmax(x, dim=1)
 
 
+class LinearDataset(torch.utils.data.Dataset):
+    """y = a * x + b"""
+
+    def __init__(self, a, b, size=1000):
+        x = np.arange(0, 10, 10 / size, dtype=np.float32)
+        self.x = torch.from_numpy(x)
+        self.y = torch.from_numpy(a * x + b)
+
+    def __getitem__(self, index):
+        return self.x[index, None], self.y[index, None]
+
+    def __len__(self):
+        return len(self.x)
+
+
 class TestEstimatorForSpark(TestCase):
-    def test_bigdl_pytorch_estimator(self):
+    def test_bigdl_pytorch_estimator_shard(self):
         model = nn.Linear(1, 1)
 
         def loss_func(input, target):
@@ -59,6 +75,25 @@ class TestEstimatorForSpark(TestCase):
         estimator = Estimator.from_torch(model_creator=model, loss_creator=loss_func,
                                          optimizer_creator=SGD(), backend="bigdl")
         estimator.fit(data=data_shard, epochs=2, batch_size=8, validation_data=data_shard,
+                      validation_methods=[Accuracy()], checkpoint_trigger=EveryEpoch())
+
+    def test_bigdl_pytorch_estimator_dataloader(self):
+        model = nn.Linear(1, 1)
+
+        estimator = Estimator.from_torch(model_creator=model, loss_creator=nn.MSELoss,
+                                         optimizer_creator=SGD(), backend="bigdl")
+
+        train_dataset = LinearDataset(2, 5, size=1000)
+        train_loader = torch.utils.data.DataLoader(
+            train_dataset,
+            batch_size=32,
+        )
+        val_dataset = LinearDataset(2, 5, size=400)
+        val_loader = torch.utils.data.DataLoader(
+            val_dataset,
+            batch_size=32,
+        )
+        estimator.fit(data=train_loader, epochs=2, validation_data=val_loader,
                       validation_methods=[Accuracy()], checkpoint_trigger=EveryEpoch())
 
 
