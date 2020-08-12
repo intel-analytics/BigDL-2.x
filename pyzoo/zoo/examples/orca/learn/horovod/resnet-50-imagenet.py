6f7ead90e2e07479fd84ab13425b0848a407ecdf
Wang, Yang <yang3.wang@intel.com>
2020-08-12 22:02:03 +0800

    fix metrics name

--- pyzoo/zoo/examples/orca/learn/horovod/resnet-50-imagenet.py
+++ pyzoo/zoo/examples/orca/learn/horovod/resnet-50-imagenet.py
@@ -266,7 +266,7 @@ def compile_args_creator(config):
     import tensorflow.keras as keras
     opt = keras.optimizers.SGD(momentum=momentum)
     param = dict(loss=keras.losses.sparse_categorical_crossentropy, optimizer=opt,
-                 metrics=['accuracy', 'top_k_sparse_categorical_accuracy'])
+                 metrics=['accuracy', 'sparse_top_k_categorical_accuracy'])
     return param
 
 
