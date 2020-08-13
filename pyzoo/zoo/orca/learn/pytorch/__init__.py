7252caa3a9b661b4fc4507806836c1cd5320bd2f
cyita <yitastudy@gmail.com>
2020-08-13 14:10:06 +0800

    remove Trainer in init

--- pyzoo/zoo/orca/learn/pytorch/__init__.py
+++ pyzoo/zoo/orca/learn/pytorch/__init__.py
@@ -13,6 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from .pytorch_trainer import PyTorchTrainer
 from .training_operator import TrainingOperator
 from .estimator import Estimator
