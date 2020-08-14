003bf223f00f8a8004972271c5dc700cc3cebb01
Kai Huang <huangkaivision@gmail.com>
2020-08-14 18:17:14 +0800

    add ut

--- pyzoo/test/zoo/orca/learn/jep/test_pytorch_estimator_for_spark_creator.py
+++ pyzoo/test/zoo/orca/learn/jep/test_pytorch_estimator_for_spark_creator.py
@@ -72,6 +72,7 @@ class TestEstimatorForSparkCreator(TestCase):
 
         estimator.fit(data=get_dataloader, epochs=2, validation_data=get_dataloader,
                       validation_methods=[Accuracy()], checkpoint_trigger=EveryEpoch())
+        model = estimator.get_model()
 
 
 if __name__ == "__main__":
