b9bb2c62654b9a7f494199afc5dac68459c950da
jenniew <jenniewang123@gmail.com>
2020-09-24 17:06:18 -0700

    update

--- pyzoo/test/zoo/orca/learn/ray/pytorch/test_pytorch_estimator.py
+++ pyzoo/test/zoo/orca/learn/ray/pytorch/test_pytorch_estimator.py
@@ -28,72 +28,72 @@ from tempfile import TemporaryDirectory
 
 
 class TestPyTorchEstimator(TestCase):
-    # def test_train(self):
-    #     estimator = Estimator.from_torch(
-    #         model=model_creator,
-    #         optimizer=optimizer_creator,
-    #         loss=nn.MSELoss,
-    #         scheduler_creator=scheduler_creator,
-    #         config={
-    #             "lr": 1e-2,  # used in optimizer_creator
-    #             "hidden_size": 1,  # used in model_creator
-    #             "batch_size": 4,  # used in data_creator
-    #         })
-    #     stats1 = estimator.fit(train_data_creator, epochs=5)
-    #     train_loss1 = stats1[-1]["train_loss"]
-    #     validation_loss1 = estimator.evaluate(validation_data_creator)["val_loss"]
-    #
-    #     stats2 = estimator.fit(train_data_creator, epochs=3)
-    #     train_loss2 = stats2[-1]["train_loss"]
-    #     validation_loss2 = estimator.evaluate(validation_data_creator)["val_loss"]
-    #
-    #     assert train_loss2 <= train_loss1, (train_loss2, train_loss1)
-    #     assert validation_loss2 <= validation_loss1, (validation_loss2,
-    #                                                   validation_loss1)
-    #     estimator.shutdown()
-    #
-    # def test_save_and_restore(self):
-    #     estimator1 = Estimator.from_torch(
-    #         model=model_creator,
-    #         optimizer=optimizer_creator,
-    #         loss=nn.MSELoss,
-    #         scheduler_creator=scheduler_creator,
-    #         config={
-    #             "lr": 1e-2,  # used in optimizer_creator
-    #             "hidden_size": 1,  # used in model_creator
-    #             "batch_size": 4,  # used in data_creator
-    #         })
-    #     with TemporaryDirectory() as tmp_path:
-    #         estimator1.fit(train_data_creator, epochs=1)
-    #         checkpoint_path = os.path.join(tmp_path, "checkpoint")
-    #         estimator1.save(checkpoint_path)
-    #
-    #         model1 = estimator1.get_model()
-    #
-    #         estimator1.shutdown()
-    #
-    #         estimator2 = Estimator.from_torch(
-    #             model=model_creator,
-    #             optimizer=optimizer_creator,
-    #             loss=nn.MSELoss,
-    #             scheduler_creator=scheduler_creator,
-    #             config={
-    #                 "lr": 1e-2,  # used in optimizer_creator
-    #                 "hidden_size": 1,  # used in model_creator
-    #                 "batch_size": 4,  # used in data_creator
-    #             })
-    #         estimator2.load(checkpoint_path)
-    #
-    #         model2 = estimator2.get_model()
-    #
-    #     model1_state_dict = model1.state_dict()
-    #     model2_state_dict = model2.state_dict()
-    #
-    #     assert set(model1_state_dict.keys()) == set(model2_state_dict.keys())
-    #
-    #     for k in model1_state_dict:
-    #         assert torch.equal(model1_state_dict[k], model2_state_dict[k])
-    #     estimator2.shutdown()
+    def test_train(self):
+        estimator = Estimator.from_torch(
+            model=model_creator,
+            optimizer=optimizer_creator,
+            loss=nn.MSELoss,
+            scheduler_creator=scheduler_creator,
+            config={
+                "lr": 1e-2,  # used in optimizer_creator
+                "hidden_size": 1,  # used in model_creator
+                "batch_size": 4,  # used in data_creator
+            })
+        stats1 = estimator.fit(train_data_creator, epochs=5)
+        train_loss1 = stats1[-1]["train_loss"]
+        validation_loss1 = estimator.evaluate(validation_data_creator)["val_loss"]
+
+        stats2 = estimator.fit(train_data_creator, epochs=3)
+        train_loss2 = stats2[-1]["train_loss"]
+        validation_loss2 = estimator.evaluate(validation_data_creator)["val_loss"]
+
+        assert train_loss2 <= train_loss1, (train_loss2, train_loss1)
+        assert validation_loss2 <= validation_loss1, (validation_loss2,
+                                                      validation_loss1)
+        estimator.shutdown()
+
+    def test_save_and_restore(self):
+        estimator1 = Estimator.from_torch(
+            model=model_creator,
+            optimizer=optimizer_creator,
+            loss=nn.MSELoss,
+            scheduler_creator=scheduler_creator,
+            config={
+                "lr": 1e-2,  # used in optimizer_creator
+                "hidden_size": 1,  # used in model_creator
+                "batch_size": 4,  # used in data_creator
+            })
+        with TemporaryDirectory() as tmp_path:
+            estimator1.fit(train_data_creator, epochs=1)
+            checkpoint_path = os.path.join(tmp_path, "checkpoint")
+            estimator1.save(checkpoint_path)
+
+            model1 = estimator1.get_model()
+
+            estimator1.shutdown()
+
+            estimator2 = Estimator.from_torch(
+                model=model_creator,
+                optimizer=optimizer_creator,
+                loss=nn.MSELoss,
+                scheduler_creator=scheduler_creator,
+                config={
+                    "lr": 1e-2,  # used in optimizer_creator
+                    "hidden_size": 1,  # used in model_creator
+                    "batch_size": 4,  # used in data_creator
+                })
+            estimator2.load(checkpoint_path)
+
+            model2 = estimator2.get_model()
+
+        model1_state_dict = model1.state_dict()
+        model2_state_dict = model2.state_dict()
+
+        assert set(model1_state_dict.keys()) == set(model2_state_dict.keys())
+
+        for k in model1_state_dict:
+            assert torch.equal(model1_state_dict[k], model2_state_dict[k])
+        estimator2.shutdown()
 
     def test_train_ray_sgd(self):
         def data_creator(config):
