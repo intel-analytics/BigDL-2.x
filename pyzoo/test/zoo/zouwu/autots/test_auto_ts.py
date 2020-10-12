72ca9d88bd263d640bd6ef337d2312103dc05576
Yu Shan <shan.yu@intel.com>
2020-10-12 12:23:18 +0800

    add predict and evaluate result check in zouwu tests

--- pyzoo/test/zoo/zouwu/autots/test_auto_ts.py
+++ pyzoo/test/zoo/zouwu/autots/test_auto_ts.py
@@ -53,8 +53,10 @@ class TestZouwuAutoTS(ZooTestCase):
         pipeline = tsp.fit(self.train_df)
         assert isinstance(pipeline, TSPipeline)
         assert pipeline.internal.config is not None
-        pipeline.evaluate(self.validation_df)
-        pipeline.predict(self.validation_df)
+        evaluate_result = pipeline.evaluate(self.validation_df)
+        assert evaluate_result[0].shape[0] == horizon
+        predict_df = pipeline.predict(self.validation_df)
+        assert not predict_df.empty
 
     def test_AutoTrainer_LstmRecipe(self):
         horizon = np.random.randint(1, 6)
@@ -74,8 +76,10 @@ class TestZouwuAutoTS(ZooTestCase):
                            ))
         assert isinstance(pipeline, TSPipeline)
         assert pipeline.internal.config is not None
-        pipeline.evaluate(self.validation_df)
-        pipeline.predict(self.validation_df)
+        evaluate_result = pipeline.evaluate(self.validation_df)
+        assert evaluate_result[0].shape[0] == horizon
+        predict_df = pipeline.predict(self.validation_df)
+        assert not predict_df.empty
 
     def test_AutoTrainer_MTNetRecipe(self):
         horizon = np.random.randint(1, 6)
@@ -97,8 +101,10 @@ class TestZouwuAutoTS(ZooTestCase):
                            ))
         assert isinstance(pipeline, TSPipeline)
         assert pipeline.internal.config is not None
-        pipeline.evaluate(self.validation_df)
-        pipeline.predict(self.validation_df)
+        evaluate_result = pipeline.evaluate(self.validation_df)
+        assert evaluate_result[0].shape[0] == horizon
+        predict_df = pipeline.predict(self.validation_df)
+        assert not predict_df.empty
 
 
 if __name__ == "__main__":
