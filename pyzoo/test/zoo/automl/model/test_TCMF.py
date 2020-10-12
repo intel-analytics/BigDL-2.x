b7ee3e147a225ab350a46a34ec002732191dc378
Yu Shan <shan.yu@intel.com>
2020-10-12 10:33:32 +0800

    add tcmf fit incremental (#2929)
    * initial changes from PR 2560
    
    * fixes and add UTs
    
    * update example for fit_incremental
    
    * update docs
    
    * fix ut fail

--- pyzoo/test/zoo/automl/model/test_TCMF.py
+++ pyzoo/test/zoo/automl/model/test_TCMF.py
@@ -37,16 +37,16 @@ class TestTCMF(ZooTestCase):
         }
         self.model = TCMF()
         self.Ymat = np.random.rand(self.num_samples, seq_len)
-        self.horizon = np.random.randint(1, 50)
+        self.horizon = np.random.randint(2, 50)
 
     def teardown_method(self, method):
         del self.model
         del self.Ymat
 
-    def test_fit_predict_evaluate(self):
+    def test_tcmf(self):
         self.model.fit_eval(x=self.Ymat, y=None, **self.config)
         # test predict
-        result = self.model.predict(x=None, horizon=self.horizon)
+        result = self.model.predict(horizon=self.horizon)
         assert result.shape[1] == self.horizon
         # test evaluate
         target = np.random.rand(self.num_samples, self.horizon)
@@ -54,33 +54,54 @@ class TestTCMF(ZooTestCase):
         assert len(evaluate_result) == 2
         assert len(evaluate_result[0]) == self.horizon
         assert len(evaluate_result[1]) == self.horizon
+        # test fit_incremental
+        Ymat_before = self.model.model.Ymat
+        self.model.fit_incremental(target)
+        Ymat_after = self.model.model.Ymat
+        assert Ymat_after.shape[1] - Ymat_before.shape[1] == self.horizon
+        incr_result = self.model.predict(horizon=self.horizon)
+        np.testing.assert_raises(AssertionError, np.testing.assert_array_equal, result, incr_result)
 
-    def test_predict_evaluate_error(self):
-        with pytest.raises(ValueError):
+    def test_error(self):
+        with pytest.raises(ValueError, match="We don't support input x directly"):
             self.model.predict(x=1)
 
-        with pytest.raises(ValueError):
+        with pytest.raises(ValueError, match="We don't support input x directly"):
             self.model.evaluate(x=1, y=np.random.rand(self.num_samples, self.horizon))
 
-        with pytest.raises(ValueError):
-            self.model.evaluate(x=None, y=None)
+        with pytest.raises(ValueError, match="Input invalid y of None"):
+            self.model.evaluate(y=None)
 
-        with pytest.raises(Exception):
+        with pytest.raises(Exception,
+                           match="Needs to call fit_eval or restore first before calling predict"):
             self.model.predict(x=None)
 
-        with pytest.raises(Exception):
-            self.model.evaluate(x=None, y=np.random.rand(self.num_samples, self.horizon))
+        with pytest.raises(Exception,
+                           match="Needs to call fit_eval or restore first before calling predict"):
+            self.model.evaluate(y=np.random.rand(self.num_samples, self.horizon))
+
+        with pytest.raises(ValueError, match="Input invalid x of None"):
+            self.model.fit_incremental(x=None)
+
+        with pytest.raises(Exception, match="Needs to call fit_eval or restore first before "
+                                            "calling fit_incremental"):
+            self.model.fit_incremental(x=np.random.rand(self.num_samples, self.horizon))
+
+        with pytest.raises(Exception, match=f"Expected incremental input with {self.num_samples} "
+                                            f"time series, got {self.num_samples - 1} instead"):
+            self.model.fit_eval(x=self.Ymat, y=None, **self.config)
+            self.model.fit_incremental(x=np.random.rand(self.num_samples - 1, self.horizon))
 
     def test_save_restore(self):
         self.model.fit_eval(x=self.Ymat, y=None, **self.config)
-        result_save = self.model.predict(x=None, horizon=self.horizon)
+        result_save = self.model.predict(horizon=self.horizon)
         model_file = "tmp.pkl"
         self.model.save(model_file)
         assert os.path.isfile(model_file)
         new_model = TCMF()
         new_model.restore(model_file)
         assert new_model.model
-        result_restore = new_model.predict(x=None, horizon=self.horizon)
+        result_restore = new_model.predict(horizon=self.horizon)
         assert_array_almost_equal(result_save, result_restore, decimal=2), \
             "Prediction values are not the same after restore: " \
             "predict before is {}, and predict after is {}".format(result_save, result_restore)
--- pyzoo/test/zoo/zouwu/model/forecast/test_tcmf_forecaster.py
+++ pyzoo/test/zoo/zouwu/model/forecast/test_tcmf_forecaster.py
@@ -23,90 +23,130 @@ import tempfile
 
 class TestZouwuModelTCMFForecaster(TestCase):
 
-    def test_forecast_tcmf(self):
-        model = TCMFForecaster(y_iters=1,
-                               init_FX_epoch=1,
-                               max_FX_epoch=1,
-                               max_TCN_epoch=1,
-                               alt_iters=2)
-        horizon = np.random.randint(1, 50)
-        # construct data
-        id = np.arange(300)
-        data = np.random.rand(300, 480)
-        input = dict({'data': data})
-        with self.assertRaises(Exception) as context:
-            model.fit(input)
-        self.assertTrue("key `y` doesn't exist in x" in str(context.exception))
-        input = dict({'id': id, 'y': data})
-        with self.assertRaises(Exception) as context:
-            model.is_distributed()
-        self.assertTrue('You should run fit before calling is_distributed()'
-                        in str(context.exception))
-        model.fit(input)
-        assert not model.is_distributed()
-        with self.assertRaises(Exception) as context:
-            model.fit(input)
-        self.assertTrue('This model has already been fully trained' in str(context.exception))
-        with self.assertRaises(Exception) as context:
-            model.fit(input, incremental=True)
-        self.assertTrue('NotImplementedError' in context.exception.__class__.__name__)
+    def setUp(self):
+        self.model = TCMFForecaster(y_iters=1,
+                                    init_FX_epoch=1,
+                                    max_FX_epoch=1,
+                                    max_TCN_epoch=1,
+                                    alt_iters=2)
+        self.num_samples = 300
+        self.horizon = np.random.randint(1, 50)
+        seq_len = 480
+        self.data = np.random.rand(self.num_samples, seq_len)
+        self.id = np.arange(self.num_samples)
+        self.data_new = np.random.rand(self.num_samples, self.horizon)
+
+    def test_ndarray_local(self):
+        ndarray_input = {'id': self.id, 'y': self.data}
+        self.model.fit(ndarray_input)
+        assert not self.model.is_xshards_distributed()
+        # test predict
+        yhat = self.model.predict(horizon=self.horizon)
+
+        # test save load
         with tempfile.TemporaryDirectory() as tempdirname:
-            model.save(tempdirname)
-            loaded_model = TCMFForecaster.load(tempdirname, distributed=False)
-        yhat = model.predict(x=None, horizon=horizon)
-        yhat_loaded = loaded_model.predict(x=None, horizon=horizon)
+            self.model.save(tempdirname)
+            loaded_model = TCMFForecaster.load(tempdirname, is_xshards_distributed=False)
+        yhat_loaded = loaded_model.predict(horizon=self.horizon)
         yhat_id = yhat_loaded["id"]
-        assert (yhat_id == id).all()
+        np.testing.assert_equal(yhat_id, self.id)
         yhat = yhat["prediction"]
         yhat_loaded = yhat_loaded["prediction"]
-        assert yhat.shape == (300, horizon)
-        assert (yhat == yhat_loaded).all()
-        target_value = np.random.rand(300, horizon)
-        target_value = dict({"y": target_value})
-        assert model.evaluate(x=None, target_value=target_value, metric=['mse'])
+        assert yhat.shape == (self.num_samples, self.horizon)
+        np.testing.assert_array_almost_equal(yhat, yhat_loaded, decimal=4)
+        # test evaluate
+        target_value = dict({"y": self.data_new})
+        assert self.model.evaluate(target_value=target_value, metric=['mse'])
+        # test fit_incremental
+        self.model.fit_incremental({'y': self.data_new})  # 1st time
+        self.model.fit_incremental({'y': self.data_new})  # 2nd time
+        yhat_incr = self.model.predict(horizon=self.horizon)
+        yhat_incr = yhat_incr["prediction"]
+        assert yhat_incr.shape == (self.num_samples, self.horizon)
+        np.testing.assert_raises(AssertionError, np.testing.assert_array_equal, yhat, yhat_incr)
 
-    def test_forecast_tcmf_without_id(self):
-        model = TCMFForecaster(y_iters=1,
-                               init_FX_epoch=1,
-                               max_FX_epoch=1,
-                               max_TCN_epoch=1,
-                               alt_iters=2)
-        horizon = np.random.randint(1, 50)
-        # construct data
-        id = np.arange(200)
-        data = np.random.rand(300, 480)
+    def test_forecast_ndarray_error(self):
+        # is_xshards_distributed
+        with self.assertRaises(Exception) as context:
+            self.model.is_xshards_distributed()
+        self.assertTrue('You should run fit before calling is_xshards_distributed()'
+                        in str(context.exception))
+
+        # fit
+        input = dict({'data': self.data})
+        with self.assertRaises(Exception) as context:
+            self.model.fit(input)
+        self.assertTrue("key `y` doesn't exist in x" in str(context.exception))
+
         input = dict({'y': "abc"})
         with self.assertRaises(Exception) as context:
-            model.fit(input)
+            self.model.fit(input)
         self.assertTrue("the value of y should be an ndarray" in str(context.exception))
-        input = dict({'id': id, 'y': data})
+
+        id_diff = np.arange(200)
+        input = dict({'id': id_diff, 'y': self.data})
         with self.assertRaises(Exception) as context:
-            model.fit(input)
+            self.model.fit(input)
         self.assertTrue("the length of the id array should be equal to the number of"
                         in str(context.exception))
-        input = dict({'y': data})
-        model.fit(input)
-        assert not model.is_distributed()
+
+        input = dict({'id': self.id, 'y': self.data})
+        self.model.fit(input)
         with self.assertRaises(Exception) as context:
-            model.fit(input)
+            self.model.fit(input)
         self.assertTrue('This model has already been fully trained' in str(context.exception))
+
+        # fit_incremental
+        data_id_diff = {'id': self.id - 1, 'y': self.data_new}
+        with self.assertRaises(ValueError) as context:
+            self.model.fit_incremental(data_id_diff)
+        self.assertTrue('The input ids in fit_incremental differs from input ids in fit'
+                        in str(context.exception))
+
+        model1 = TCMFForecaster(y_iters=1, init_FX_epoch=1, max_FX_epoch=1, max_TCN_epoch=1,
+                                alt_iters=2)
+        model1.fit({'y': self.data})
+        with self.assertRaises(ValueError) as context:
+            model1.fit_incremental(data_id_diff)
+        self.assertTrue('Got valid id in fit_incremental and invalid id in fit.'
+                        in str(context.exception))
+
+        # evaluate
+        target_value_fake = dict({"data": self.data_new})
+        with self.assertRaises(Exception) as context:
+            self.model.evaluate(target_value=target_value_fake, metric=['mse'])
+        self.assertTrue("key `y` doesn't exist in x" in str(context.exception))
+
+    def test_forecast_tcmf_without_id(self):
+        # construct data
+        input = dict({'y': self.data})
+        self.model.fit(input)
+        assert not self.model.is_xshards_distributed()
         with tempfile.TemporaryDirectory() as tempdirname:
-            model.save(tempdirname)
-            loaded_model = TCMFForecaster.load(tempdirname, distributed=False)
-        yhat = model.predict(x=None, horizon=horizon)
-        yhat_loaded = loaded_model.predict(x=None, horizon=horizon)
+            self.model.save(tempdirname)
+            loaded_model = TCMFForecaster.load(tempdirname, is_xshards_distributed=False)
+        yhat = self.model.predict(horizon=self.horizon)
+        yhat_loaded = loaded_model.predict(horizon=self.horizon)
         assert "id" not in yhat_loaded
         yhat = yhat["prediction"]
         yhat_loaded = yhat_loaded["prediction"]
-        assert yhat.shape == (300, horizon)
-        assert (yhat == yhat_loaded).all()
-        target_value = np.random.rand(300, horizon)
-        target_value_fake = dict({"data": target_value})
-        with self.assertRaises(Exception) as context:
-            model.evaluate(x=None, target_value=target_value_fake, metric=['mse'])
-        self.assertTrue("key y doesn't exist in y" in str(context.exception))
-        target_value = dict({"y": target_value})
-        model.evaluate(x=None, target_value=target_value, metric=['mse'])
+        assert yhat.shape == (self.num_samples, self.horizon)
+        np.testing.assert_array_almost_equal(yhat, yhat_loaded, decimal=4)
+
+        target_value = dict({"y": self.data_new})
+        self.model.evaluate(target_value=target_value, metric=['mse'])
+
+        self.model.fit_incremental({'y': self.data_new})  # 1st time
+        yhat_incr = self.model.predict(horizon=self.horizon)
+        yhat_incr = yhat_incr["prediction"]
+        assert yhat_incr.shape == (self.num_samples, self.horizon)
+        np.testing.assert_raises(AssertionError, np.testing.assert_array_equal, yhat, yhat_incr)
+
+        data_new_id = {'id': self.id, 'y': self.data_new}
+        with self.assertRaises(ValueError) as context:
+            self.model.fit_incremental(data_new_id)
+        self.assertTrue('Got valid id in fit_incremental and invalid id in fit.'
+                        in str(context.exception))
 
     def test_forecast_tcmf_xshards(self):
         from zoo.orca import OrcaContext
@@ -134,12 +174,6 @@ class TestZouwuModelTCMFForecaster(TestCase):
         def get_pred(d):
             return d["prediction"]
 
-        model = TCMFForecaster(y_iters=1,
-                               init_FX_epoch=1,
-                               max_FX_epoch=1,
-                               max_TCN_epoch=1,
-                               alt_iters=2)
-
         with tempfile.NamedTemporaryFile() as temp:
             data = np.random.rand(300, 480)
             df = pd.DataFrame(data)
@@ -148,39 +182,39 @@ class TestZouwuModelTCMFForecaster(TestCase):
         shard.cache()
         shard_train = shard.transform_shard(preprocessing, 'id', 'data')
         with self.assertRaises(Exception) as context:
-            model.fit(shard_train)
+            self.model.fit(shard_train)
         self.assertTrue("key `y` doesn't exist in x" in str(context.exception))
         shard_train = shard.transform_shard(preprocessing, 'cid', 'y')
         with self.assertRaises(Exception) as context:
-            model.fit(shard_train)
+            self.model.fit(shard_train)
         self.assertTrue("key `id` doesn't exist in x" in str(context.exception))
         with self.assertRaises(Exception) as context:
-            model.is_distributed()
-        self.assertTrue('You should run fit before calling is_distributed()'
+            self.model.is_xshards_distributed()
+        self.assertTrue('You should run fit before calling is_xshards_distributed()'
                         in str(context.exception))
         shard_train = shard.transform_shard(preprocessing, 'id', 'y')
-        model.fit(shard_train)
-        assert model.is_distributed()
+        self.model.fit(shard_train)
+        assert self.model.is_xshards_distributed()
         with self.assertRaises(Exception) as context:
-            model.fit(shard_train)
+            self.model.fit(shard_train)
         self.assertTrue('This model has already been fully trained' in str(context.exception))
         with self.assertRaises(Exception) as context:
-            model.fit(shard_train, incremental=True)
+            self.model.fit_incremental(shard_train)
         self.assertTrue('NotImplementedError' in context.exception.__class__.__name__)
         with tempfile.TemporaryDirectory() as tempdirname:
-            model.save(tempdirname + "/model")
-            loaded_model = TCMFForecaster.load(tempdirname + "/model", distributed=True)
+            self.model.save(tempdirname + "/model")
+            loaded_model = TCMFForecaster.load(tempdirname + "/model", is_xshards_distributed=True)
         horizon = np.random.randint(1, 50)
-        yhat_shard_origin = model.predict(x=None, horizon=horizon)
+        yhat_shard_origin = self.model.predict(horizon=horizon)
         yhat_list_origin = yhat_shard_origin.collect()
         yhat_list_origin = list(map(get_pred, yhat_list_origin))
-        yhat_shard = loaded_model.predict(x=None, horizon=horizon)
+        yhat_shard = loaded_model.predict(horizon=horizon)
         yhat_list = yhat_shard.collect()
         yhat_list = list(map(get_pred, yhat_list))
         yhat_origin = np.concatenate(yhat_list_origin)
         yhat = np.concatenate(yhat_list)
         assert yhat.shape == (300, horizon)
-        assert (yhat == yhat_origin).all()
+        np.testing.assert_equal(yhat, yhat_origin)
         output_dt_col_name = pd.date_range(start='2020-05-01', periods=horizon, freq='H').to_list()
         yhat_df_shards = yhat_shard.transform_shard(postprocessing, output_dt_col_name)
         final_df_list = yhat_df_shards.collect()
@@ -190,37 +224,34 @@ class TestZouwuModelTCMFForecaster(TestCase):
         OrcaContext.pandas_read_backend = "spark"
 
     def test_forecast_tcmf_distributed(self):
-        model = TCMFForecaster(y_iters=1,
-                               init_FX_epoch=1,
-                               max_FX_epoch=1,
-                               max_TCN_epoch=1,
-                               alt_iters=2)
-        horizon = np.random.randint(1, 50)
-        # construct data
-        id = np.arange(300)
-        data = np.random.rand(300, 480)
-        input = dict({'id': id, 'y': data})
+        input = dict({'id': self.id, 'y': self.data})
 
         from zoo.orca import init_orca_context, stop_orca_context
 
         init_orca_context(cores=4, spark_log_level="INFO", init_ray_on_spark=True,
                           object_store_memory="1g")
-        model.fit(input, num_workers=4)
+        self.model.fit(input, num_workers=4)
 
         with tempfile.TemporaryDirectory() as tempdirname:
-            model.save(tempdirname)
-            loaded_model = TCMFForecaster.load(tempdirname, distributed=False)
-        yhat = model.predict(x=None, horizon=horizon, num_workers=4)
-        yhat_loaded = loaded_model.predict(x=None, horizon=horizon, num_workers=4)
+            self.model.save(tempdirname)
+            loaded_model = TCMFForecaster.load(tempdirname, is_xshards_distributed=False)
+        yhat = self.model.predict(horizon=self.horizon, num_workers=4)
+        yhat_loaded = loaded_model.predict(horizon=self.horizon, num_workers=4)
         yhat_id = yhat_loaded["id"]
-        assert (yhat_id == id).all()
+        np.testing.assert_equal(yhat_id, self.id)
         yhat = yhat["prediction"]
         yhat_loaded = yhat_loaded["prediction"]
-        assert yhat.shape == (300, horizon)
+        assert yhat.shape == (self.num_samples, self.horizon)
         np.testing.assert_equal(yhat, yhat_loaded)
-        target_value = np.random.rand(300, horizon)
-        target_value = dict({"y": target_value})
-        assert model.evaluate(x=None, target_value=target_value, metric=['mse'])
+
+        self.model.fit_incremental({'y': self.data_new})
+        yhat_incr = self.model.predict(horizon=self.horizon)
+        yhat_incr = yhat_incr["prediction"]
+        assert yhat_incr.shape == (self.num_samples, self.horizon)
+        np.testing.assert_raises(AssertionError, np.testing.assert_array_equal, yhat, yhat_incr)
+
+        target_value = dict({"y": self.data_new})
+        assert self.model.evaluate(target_value=target_value, metric=['mse'])
         stop_orca_context()
 
 
