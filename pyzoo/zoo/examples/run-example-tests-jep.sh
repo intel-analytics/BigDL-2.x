e73e5d129cbe3b27227535614e03cae4f1c54c6e
GavinGu07 <55721214+GavinGu07@users.noreply.github.com>
2020-10-12 15:22:52 +0800

    Update run-example-tests-jep.sh

--- pyzoo/zoo/examples/run-example-tests-jep.sh
+++ pyzoo/zoo/examples/run-example-tests-jep.sh
@@ -2,7 +2,7 @@
 
 # export SPARK_HOME=$SPARK_HOME
 # export MASTER=local[4]
-export ANALYTICS_ZOO_ROOT=$ANALYTICS_ZOO_ROOT
+# export ANALYTICS_ZOO_ROOT=$ANALYTICS_ZOO_ROOT
 # export ANALYTICS_ZOO_HOME=$ANALYTICS_ZOO_ROOT/dist
 # export ANALYTICS_ZOO_JAR=`find ${ANALYTICS_ZOO_HOME}/lib -type f -name "analytics-zoo*jar-with-dependencies.jar"`
 # export ANALYTICS_ZOO_PYZIP=`find ${ANALYTICS_ZOO_HOME}/lib -type f -name "analytics-zoo*python-api.zip"`
@@ -14,12 +14,13 @@ set -e
 echo "#1 start example for MNIST"
 #timer
 start=$(date "+%s")
-if [ -f analytics-zoo-data/data/mnist ]
+if [ -f ${ANALYTICS_ZOO_ROOT}/data/mnist.zip ]
 then
-    echo "analytics-zoo-data/data/mnist already exists" 
+    echo "mnist.zip already exists"
 else
-    wget -nv http://10.239.45.10:8081/repository/raw/analytics-zoo-data/mnist -P analytics-zoo-data/data
+    wget -nv $FTP_URI/analytics-zoo-data/mnist.zip -P ${ANALYTICS_ZOO_ROOT}/data
 fi
+unzip -q ${ANALYTICS_ZOO_ROOT}/data/mnist.zip -d ${ANALYTICS_ZOO_ROOT}/data
 
-python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/pytorch/train/mnist/main.py --dir analytics-zoo-data/data/mnist
+python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/pytorch/train/mnist/main.py --dir analytics-zoo-data/data
 
