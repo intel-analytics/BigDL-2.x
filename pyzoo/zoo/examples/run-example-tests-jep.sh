6d193aafa22e3070fa0cb078fe1c80d9f07731a7
GavinGu07 <55721214+GavinGu07@users.noreply.github.com>
2020-10-13 09:49:27 +0800

    Update run-example-tests-jep.sh

--- pyzoo/zoo/examples/run-example-tests-jep.sh
+++ pyzoo/zoo/examples/run-example-tests-jep.sh
@@ -18,10 +18,10 @@ if [ -f analytics-zoo-data/data/mnist ]
 then
     echo "mnist already exists"
 else
-    wget -nv http://10.239.45.10:8081/repository/raw/analytics-zoo-data/mnist/train-labels-idx1-ubyte.gz -P analytics-zoo-data/data/mnist/raw
-    wget -nv http://10.239.45.10:8081/repository/raw/analytics-zoo-data/mnist/train-images-idx3-ubyte.gz -P analytics-zoo-data/data/mnist/raw
-    wget -nv http://10.239.45.10:8081/repository/raw/analytics-zoo-data/mnist/t10k-labels-idx1-ubyte.gz -P analytics-zoo-data/data/mnist/raw
-    wget -nv http://10.239.45.10:8081/repository/raw/analytics-zoo-data/mnist/t10k-images-idx3-ubyte.gz -P analytics-zoo-data/data/mnist/raw
+    wget -nv http://10.239.45.10:8081/repository/raw/analytics-zoo-data/mnist/train-labels-idx1-ubyte.gz -P analytics-zoo-data/data/MNIST/raw
+    wget -nv http://10.239.45.10:8081/repository/raw/analytics-zoo-data/mnist/train-images-idx3-ubyte.gz -P analytics-zoo-data/data/MNIST/raw
+    wget -nv http://10.239.45.10:8081/repository/raw/analytics-zoo-data/mnist/t10k-labels-idx1-ubyte.gz -P analytics-zoo-data/data/MNIST/raw
+    wget -nv http://10.239.45.10:8081/repository/raw/analytics-zoo-data/mnist/t10k-images-idx3-ubyte.gz -P analytics-zoo-data/data/MNIST/raw
 fi
 
 python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/pytorch/train/mnist/main.py --dir analytics-zoo-data/data
