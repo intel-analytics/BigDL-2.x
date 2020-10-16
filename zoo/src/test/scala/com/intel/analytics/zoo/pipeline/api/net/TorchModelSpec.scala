99e9e3c4d9a6c2b300574b2b903c8fb0e98f3c60
ManfeiBai <ludviqbai@outlook.com>
2020-10-16 09:27:58 +0800

    add AbstructModel and FloatModel

--- zoo/src/test/scala/com/intel/analytics/zoo/pipeline/api/net/TorchModelSpec.scala
+++ zoo/src/test/scala/com/intel/analytics/zoo/pipeline/api/net/TorchModelSpec.scala
@@ -19,7 +19,7 @@ import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.zoo.common.{PythonInterpreter, PythonInterpreterTest}
 import com.intel.analytics.zoo.core.TFNetNative
 import com.intel.analytics.zoo.pipeline.api.keras.ZooSpecHelper
-import com.intel.analytics.zoo.pipeline.inference.InferenceModel
+import com.intel.analytics.zoo.pipeline.inference.{InferenceModel, AbstractModel, FloatModel}
 import jep.NDArray
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.{SparkConf, SparkContext}
