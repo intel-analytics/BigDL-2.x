38ea7ca48f2de0c0312bd289e92c494d4ca3f8bf
Jiaming <litchilitchy@qq.com>
2020-10-20 16:10:04 +0800

    ut

--- zoo/src/test/scala/com/intel/analytics/zoo/serving/models/OpenVINOModelSpec.scala
+++ zoo/src/test/scala/com/intel/analytics/zoo/serving/models/OpenVINOModelSpec.scala
@@ -16,7 +16,12 @@
 
 package com.intel.analytics.zoo.serving.models
 
+import com.intel.analytics.zoo.serving.PreProcessing
+import com.intel.analytics.zoo.serving.arrow.ArrowDeserializer
+import com.intel.analytics.zoo.serving.engine.{ClusterServingInference, ModelHolder}
+import com.intel.analytics.zoo.serving.utils.{ClusterServingHelper, SerParams}
 import org.scalatest.{FlatSpec, Matchers}
+
 import sys.process._
 
 class OpenVINOModelSpec extends FlatSpec with Matchers {
@@ -26,9 +31,27 @@ class OpenVINOModelSpec extends FlatSpec with Matchers {
     "tar -xvf /tmp/serving_val.tar -C /tmp/".!
     val resource = getClass().getClassLoader().getResource("serving")
     val dataPath = resource.getPath + "/image-3_224_224-base64"
-    val modelPath = "/tmp/"
+    val b64string = scala.io.Source.fromFile(dataPath).mkString
 
+    val helper = new ClusterServingHelper()
+    helper.modelType = "openvino"
+    helper.weightPath = "/tmp/serving_val_openvino_res50/resnet_v1_50.bin"
+    helper.defPath = "/tmp/serving_val_openvino_res50/resnet_v1_50.xml"
+    ModelHolder.model = helper.loadInferenceModel()
+    ("rm -rf /tmp/imagenet_1k*").!
+    "rm -rf /tmp/serving_val_*".!
+    "rm -rf /tmp/config.yaml".!
+    val params = new SerParams(helper)
+    val inference = new ClusterServingInference(new PreProcessing(params.chwFlag),
+      params.modelType, "", params.coreNum, params.resize)
+    val in = List(("1", b64string), ("2", b64string), ("3", b64string))
+    val postProcessed = inference.multiThreadPipeline(in)
 
+    postProcessed.foreach(x => {
+      val result = ArrowDeserializer.getArray(x._2)
+      require(result(0)._1.length == 1000, "result length wrong")
+      require(result(0)._2.length == 1, "result shape wrong")
+    })
   }
 
 }
