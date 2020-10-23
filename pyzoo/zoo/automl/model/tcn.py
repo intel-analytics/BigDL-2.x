4e7e49ed71853380c401272a24f72f5d04775d9f
Yu Shan <shan.yu@intel.com>
2020-10-23 12:49:41 +0800

    refactor automl pytorch model

--- pyzoo/zoo/automl/model/tcn.py
+++ pyzoo/zoo/automl/model/tcn.py
@@ -42,9 +42,7 @@
 import torch
 import torch.nn as nn
 from torch.nn.utils import weight_norm
-from zoo.automl.model.abstract import BaseModel
-from zoo.automl.common.util import *
-from zoo.automl.common.metrics import Evaluator
+from zoo.automl.model.base_pytorch_model import PytorchBaseModel
 
 
 class Chomp1d(nn.Module):
@@ -117,7 +115,7 @@ class TemporalConvNet(nn.Module):
 def model_creator(config):
     return TemporalConvNet(input_size=config["input_size"],
                            output_size=config["output_size"],
-                           num_channels=config.get("nhid", 30) * config.get("levels", 8),
+                           num_channels=[config.get("nhid", 30)] * config.get("levels", 8),
                            kernel_size=config.get("kernel_size", 7),
                            dropout=config.get("dropout", 0.2))
 
@@ -131,102 +129,13 @@ def loss_creator(config):
     return nn.MSELoss()
 
 
-class TCNPytorch(BaseModel):
+class TCNPytorch(PytorchBaseModel):
     def __init__(self, config, check_optional_config=False):
-        self.check_optional_config = check_optional_config
-        self._check_config(**config)
-        self.model = model_creator(config)
-        self.optimizer = optimizer_creator(self.model, config)
-        self.criterion = loss_creator(config)
-        self.config = config
-
-    def fit_eval(self, x, y, validation_data=None, mc=False, verbose=0, epochs=1, metric=["mse"],
-                 **config):
-        epoch_losses = []
-        for i in range(epochs):
-            train_loss = self.train_epoch(x, y)
-            epoch_losses.append(train_loss)
-        train_stats = {"loss": np.mean(epoch_losses), "last_loss": epoch_losses[-1]}
-        # todo: support input validation data None
-        assert validation_data is not None, "You must input validation data!"
-        val_stats = self._validate(validation_data[0], validation_data[1], metric=metric)
-        return train_stats.update(val_stats)
-
-    def train_epoch(self, x, y):
-        # todo: support torch data loader
-        batch_size = self.config["batch_size"]
-        self.model.train()
-        batch_idx = 0
-        total_loss = 0
-        for i in range(0, x.size(0), batch_size):
-            if i + batch_size > x.size(0):
-                x, y = x[i:], y[i:]
-            else:
-                x, y = x[i:(i + batch_size)], y[i:(i + batch_size)]
-            self.optimizer.zero_grad()
-            yhat = self.model(x)
-            loss = self.criterion(yhat, y)
-            loss.backward()
-            self.optimizer.step()
-            batch_idx += 1
-            total_loss += loss.item()
-        train_loss = total_loss / batch_idx
-        return train_loss
-
-    def _validate(self, x, y, metric):
-        self.model.eval()
-        with torch.no_grad():
-            yhat = self.model(x)
-            val_loss = self.criterion(yhat, y)
-            eval_result = Evaluator.evaluate(metric=metric,
-                                             y_true=y, y_pred=yhat.numpy(),
-                                             multioutput='uniform_average')
-        return {"val_loss": val_loss.item(),
-                metric: eval_result}
-
-    def evaluate(self, x, y, metric=['mse']):
-        yhat = self.predict(x)
-        eval_result = [Evaluator.evaluate(m, y_true=y, y_pred=yhat, multioutput="raw_values")
-                       for m in metric]
-        return eval_result
-
-    def predict(self, x, mc=False):
-        if mc:
-            self.model.train()
-        else:
-            self.model.eval()
-        yhat = self.model(x)
-        return yhat
-
-    def predict_with_uncertainty(self, x, n_iter=100):
-        result = np.zeros((n_iter,) + (x.shape[0], self.config["output_size"]))
-        for i in range(n_iter):
-            result[i, :, :] = self.predict(x, mc=True)
-
-        prediction = result.mean(axis=0)
-        uncertainty = result.std(axis=0)
-        return prediction, uncertainty
-
-    def state_dict(self):
-        state = {
-            "config": self.config,
-            "model": self.model.state_dict(),
-            "optimizer": self.optimizer.state_dict(),
-        }
-        return state
-
-    def load_state_dict(self, state):
-        self.model.load_state_dict(state["model"])
-        self.config = state["config"]
-        self.optimizer.load_state_dict(state["optimizer"])
-
-    def save(self, model_path, config_path, **config):
-        state_dict = self.state_dict()
-        torch.save(state_dict, model_path)
-
-    def restore(self, model_path, **config):
-        state_dict = torch.load(model_path)
-        self.load_state_dict(state_dict)
+        super().__init__(model_creator=model_creator,
+                         optimizer_creator=optimizer_creator,
+                         loss_creator=loss_creator,
+                         config=config,
+                         check_optional_config=check_optional_config)
 
     def _get_required_parameters(self):
         return {
@@ -239,7 +148,4 @@ class TCNPytorch(BaseModel):
             "nhid",
             "levels",
             "kernel_size",
-            'lr',
-            "dropout",
-            'batch_size'
-        }
+        } | super()._get_optional_parameters()
