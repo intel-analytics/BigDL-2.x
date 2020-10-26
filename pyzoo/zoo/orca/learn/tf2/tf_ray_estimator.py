4179f770bbd4befd45dbd8421d8dbdb38de56575
Wang, Yang <yang3.wang@intel.com>
2020-10-26 23:01:45 +0800

    fix batch size

--- pyzoo/zoo/orca/learn/tf2/tf_ray_estimator.py
+++ pyzoo/zoo/orca/learn/tf2/tf_ray_estimator.py
@@ -247,7 +247,7 @@ class Estimator:
         return stats
 
 
-    def predict(self, data_creator, batch_size, verbose=1,
+    def predict(self, data_creator, batch_size=None, verbose=1,
                  steps=None, callbacks=None, data_config=None):
         """Evaluates the model on the validation data set."""
         logger.info("Starting predict step.")
