caa3a0e8eb6eff581ccffada5978036feb4aef01
theaperdeng <theaperdeng@outlook.com>
2020-11-04 15:47:32 +0800

    remove featuretools import

--- pyzoo/zoo/automl/feature/time_sequence.py
+++ pyzoo/zoo/automl/feature/time_sequence.py
@@ -21,9 +21,6 @@ from zoo.automl.feature.abstract import BaseFeatureTransformer
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 import pandas as pd
 import numpy as np
-import featuretools as ft
-from featuretools.primitives import make_agg_primitive, make_trans_primitive
-from featuretools.variable_types import Text, Numeric, DatetimeTimeIndex
 import json
 
 time_attr = ("MINUTE", "DAY", "DAYOFYEAR", "HOUR", "WEEKDAY", "WEEKOFYEAR", "MONTH")
