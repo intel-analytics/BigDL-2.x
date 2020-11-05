7ddfe91e21cc9c08065e2b53c6c331a2d0638287
jenniew <jenniewang123@gmail.com>
2020-11-05 14:10:43 -0800

    remove print

--- pyzoo/zoo/orca/data/utils.py
+++ pyzoo/zoo/orca/data/utils.py
@@ -301,9 +301,6 @@ def merge(data_list):
             elif isinstance(data_list[0][key], np.ndarray):
                 merged_value = np.concatenate([data[key] for data in data_list], axis=0)
                 result[key] = merged_value
-            print("key is: ", key)
-            print("result after set is: ", result)
-        print("result is: ", result)
         return result
     elif get_class_name(data_list[0]) == 'pandas.core.frame.DataFrame':
         import pandas as pd
