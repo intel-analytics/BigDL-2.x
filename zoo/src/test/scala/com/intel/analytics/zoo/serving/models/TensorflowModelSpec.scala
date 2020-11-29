b959e480ad25679a1cea2cb6a3e35abcade39d83
qihong <qihong.liu@intel.com>
2020-11-29 20:19:15 +0800

    add tensorflow specific test and some ndarray test case file

--- zoo/src/test/scala/com/intel/analytics/zoo/serving/models/TensorflowModelSpec.scala
+++ zoo/src/test/scala/com/intel/analytics/zoo/serving/models/TensorflowModelSpec.scala
@@ -16,7 +16,6 @@
 
 package com.intel.analytics.zoo.serving.models
 
-
 import com.intel.analytics.zoo.serving.PreProcessing
 import com.intel.analytics.zoo.serving.arrow.ArrowDeserializer
 import com.intel.analytics.zoo.serving.engine.{ClusterServingInference, ModelHolder}
@@ -26,17 +25,17 @@ import org.scalatest.{FlatSpec, Matchers}
 import scala.collection.JavaConverters._
 
 class TensorflowModelSpec extends FlatSpec with Matchers {
-  "TensorflowModel" should "work" in {
+
+  "TensorflowModel Inception v1" should "work" in {
     val resource = getClass().getClassLoader().getResource("serving")
     val dataPath = resource.getPath + "/image-3_224_224-base64"
     val b64string = scala.io.Source.fromFile(dataPath).mkString
 
     val helper = new ClusterServingHelper()
+    helper.chwFlag=false
     helper.modelType = "tensorflowFrozenModel"
-    helper.weightPath = "/home/arda/intel-analytics/model/models/resnet50/tf_res50"
-    //helper.defPath = "/tmp/openvino_inception_v1/inception_v1.xml"
+    helper.weightPath = "/home/arda/intel-analytics/model/models/inception_v1/tensorflow/tf_inception/"
     ModelHolder.model = helper.loadInferenceModel()
-
 
     val params = new SerParams(helper)
     val inference = new ClusterServingInference(new PreProcessing(params.chwFlag),
@@ -48,6 +47,156 @@ class TensorflowModelSpec extends FlatSpec with Matchers {
       val result = ArrowDeserializer.getArray(x._2)
       require(result(0)._1.length == 1001, "result length wrong")
       require(result(0)._2.length == 1, "result shape wrong")
+    })
+  }
+
+
+  "TensorflowModel MobileNet v1" should "work" in {
+    val resource = getClass().getClassLoader().getResource("serving")
+    val dataPath = resource.getPath + "/image-3_224_224-base64"
+    val b64string = scala.io.Source.fromFile(dataPath).mkString
+
+    val helper = new ClusterServingHelper()
+    helper.chwFlag=false
+    helper.modelType = "tensorflowFrozenModel"
+    helper.weightPath = "/home/arda/intel-analytics/model/models/mobilenet_v1/tensorflow/tf_mobilenet/"
+    ModelHolder.model = helper.loadInferenceModel()
+
+    val params = new SerParams(helper)
+    val inference = new ClusterServingInference(new PreProcessing(params.chwFlag),
+      params.modelType, "", params.coreNum, params.resize)
+    val in = List(("1", b64string), ("2", b64string), ("3", b64string))
+    val postProcessed = inference.multiThreadPipeline(in)
+
+    postProcessed.foreach(x => {
+      val result = ArrowDeserializer.getArray(x._2)
+      require(result(0)._1.length == 1001, "result length wrong")
+      require(result(0)._2.length == 1, "result shape wrong")
+    })
+  }
+
+
+  "TensorflowModel MobileNet v2" should "work" in {
+    val resource = getClass().getClassLoader().getResource("serving")
+    val dataPath = resource.getPath + "/image-3_224_224-base64"
+    val b64string = scala.io.Source.fromFile(dataPath).mkString
+
+    val helper = new ClusterServingHelper()
+    helper.chwFlag=false
+    helper.modelType = "tensorflowFrozenModel"
+    helper.weightPath = "/home/arda/intel-analytics/model/models/mobilenet_v2/tensorflow/tf_mobilenet/"
+    ModelHolder.model = helper.loadInferenceModel()
+
+    val params = new SerParams(helper)
+    val inference = new ClusterServingInference(new PreProcessing(params.chwFlag),
+      params.modelType, "", params.coreNum, params.resize)
+    val in = List(("1", b64string), ("2", b64string), ("3", b64string))
+    val postProcessed = inference.multiThreadPipeline(in)
+
+    postProcessed.foreach(x => {
+      val result = ArrowDeserializer.getArray(x._2)
+      require(result(0)._1.length == 1001, "result length wrong")
+      require(result(0)._2.length == 1, "result shape wrong")
+    })
+  }
+
+
+  "TensorflowModel ResNet 50" should "work" in {
+    val resource = getClass().getClassLoader().getResource("serving")
+    val dataPath = resource.getPath + "/image-3_224_224-base64"
+    val b64string = scala.io.Source.fromFile(dataPath).mkString
+
+    val helper = new ClusterServingHelper()
+    helper.chwFlag=false
+    helper.modelType = "tensorflowFrozenModel"
+    helper.weightPath = "/home/arda/intel-analytics/model/models/resnet50/tf_res50/"
+    ModelHolder.model = helper.loadInferenceModel()
+
+    val params = new SerParams(helper)
+    val inference = new ClusterServingInference(new PreProcessing(params.chwFlag),
+      params.modelType, "", params.coreNum, params.resize)
+    val in = List(("1", b64string), ("2", b64string), ("3", b64string))
+    val postProcessed = inference.multiThreadPipeline(in)
+
+    postProcessed.foreach(x => {
+      val result = ArrowDeserializer.getArray(x._2)
+      require(result(0)._1.length == 1000, "result length wrong")
+      require(result(0)._2.length == 1, "result shape wrong")
+    })
+  }
+
+
+  "TensorflowModel tf auto" should "work" in {
+    val resource = getClass().getClassLoader().getResource("serving")
+    val dataPath = resource.getPath + "/ndarray-128-base64"
+    val b64string = scala.io.Source.fromFile(dataPath).mkString
+
+    val helper = new ClusterServingHelper()
+    helper.chwFlag=false
+    helper.modelType = "tensorflowSavedModel"
+    helper.weightPath = "/home/arda/intel-analytics/model/models/tf_auto/"
+    ModelHolder.model = helper.loadInferenceModel()
+
+    val params = new SerParams(helper)
+    val inference = new ClusterServingInference(new PreProcessing(params.chwFlag),
+      params.modelType, "", params.coreNum, params.resize)
+    val in = List(("1", b64string), ("2", b64string), ("3", b64string))
+    val postProcessed = inference.multiThreadPipeline(in)
+
+    postProcessed.foreach(x => {
+      val result = ArrowDeserializer.getArray(x._2)
+      require(result(0)._1.length == 128, "result length wrong")
+      require(result(0)._2.length == 1, "result shape wrong")
+    })
+  }
+
+
+  "TensorflowModel VGG16" should "work" in {
+    val resource = getClass().getClassLoader().getResource("serving")
+    val dataPath = resource.getPath + "/image-3_224_224-base64"
+    val b64string = scala.io.Source.fromFile(dataPath).mkString
+
+    val helper = new ClusterServingHelper()
+    helper.chwFlag=false
+    helper.modelType = "tensorflowFrozenModel"
+    helper.weightPath = "/home/arda/intel-analytics/model/models/vgg16/tensorflow/tf_vgg16/"
+    ModelHolder.model = helper.loadInferenceModel()
+
+    val params = new SerParams(helper)
+    val inference = new ClusterServingInference(new PreProcessing(params.chwFlag),
+      params.modelType, "", params.coreNum, params.resize)
+    val in = List(("1", b64string), ("2", b64string), ("3", b64string))
+    val postProcessed = inference.multiThreadPipeline(in)
+
+    postProcessed.foreach(x => {
+      val result = ArrowDeserializer.getArray(x._2)
+      require(result(0)._1.length == 1000, "result length wrong")
+      require(result(0)._2.length == 1, "result shape wrong")
+    })
+  }
+
+
+  "TensorflowModel tf_2out" should "work" in {
+    val resource = getClass().getClassLoader().getResource("serving")
+    val dataPath = resource.getPath + "/ndarray-2-base64"
+    val b64string = scala.io.Source.fromFile(dataPath).mkString
+
+    val helper = new ClusterServingHelper()
+    helper.chwFlag=false
+    helper.modelType = "tensorflowSavedModel"
+    helper.weightPath = "/home/arda/intel-analytics/model/models/tf_2out/"
+    ModelHolder.model = helper.loadInferenceModel()
+
+    val params = new SerParams(helper)
+    val inference = new ClusterServingInference(new PreProcessing(params.chwFlag),
+      params.modelType, "", params.coreNum, params.resize)
+    val in = List(("1", b64string), ("2", b64string), ("3", b64string))
+    val postProcessed = inference.multiThreadPipeline(in)
+
+    postProcessed.foreach(x => {
+      val result = ArrowDeserializer.getArray(x._2)
+      require(result(0)._1.length == 2, "result length wrong")
+      require(result(0)._2.length == 2, "result shape wrong")
     })
   }
 
