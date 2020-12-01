92c5f6f65a4025259430a0f94a1e69f045197e9c
jing-xu <jingx1998@outlook.com>
2020-12-01 10:08:58 +0800

    reorder

--- pyzoo/zoo/examples/orca/learn/pytorch/cifar10/cifar10.py
+++ pyzoo/zoo/examples/orca/learn/pytorch/cifar10/cifar10.py
@@ -36,12 +36,52 @@ from zoo.orca.learn.metrics import Accuracy
 from zoo.orca.learn.trigger import EveryEpoch
 
 
+parser = argparse.ArgumentParser(description='PyTorch Cifar10 Example')
+parser.add_argument('--cluster_mode', type=str, default="local",
+                    help='The mode for the Spark cluster. local or yarn.')
+args = parser.parse_args()
+    
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
-         
+               
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+# show images
+imshow(torchvision.utils.make_grid(images))
+# print labels
+print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+               
+if args.cluster_mode == "local":
+    init_orca_context(cores=1, memory="20g")
+elif args.cluster_mode == "yarn":
+    init_orca_context(
+        cluster_mode="yarn-client", cores=4, num_nodes=2, memory="2g",
+        driver_memory="10g", driver_cores=1,
+        conf={"spark.rpc.message.maxSize": "1024",
+            "spark.task.maxFailures": "1",
+            "spark.driver.extraJavaOptions": "-Dbigdl.failure.retryTimes=1"})
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -60,82 +100,25 @@ class Net(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+   
+net = Net()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-def main():
-    parser = argparse.ArgumentParser(description='PyTorch Cifar10 Example')
-    parser.add_argument('--dir', default='./data', metavar='N',
-                        help='the folder store mnist data')
-    parser.add_argument('--batch-size', type=int, default=4, metavar='N',
-                        help='input batch size for training per executor(default: 256)')
-    parser.add_argument('--test-batch-size', type=int, default=4, metavar='N',
-                        help='input batch size for testing per executor(default: 1000)')
-    parser.add_argument('--epochs', type=int, default=2, metavar='N',
-                        help='number of epochs to train (default: 2)')
-    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
-                        help='learning rate (default: 0.001)')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    parser.add_argument('--cluster_mode', type=str, default="local",
-                        help='The mode for the Spark cluster. local or yarn.')
-    args = parser.parse_args()
+net.train()  
+zoo_estimator = Estimator.from_torch(model=net, optimizer=optimizer, loss=criterion,
+                                     backend="bigdl")
+zoo_estimator.fit(data=trainloader, epochs=2, validation_data=testloader,
+                  validation_methods=[Accuracy()], checkpoint_trigger=EveryEpoch())
+print('Finished Training')
     
-    print(args)
+dataiter = iter(testloader)
+images, labels = dataiter.next()
 
-    torch.manual_seed(args.seed)
-    
-    transform = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+# print images
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
-    trainset = torchvision.datasets.CIFAR10(root=args.dir, train=True,
-                                            download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
-                                              shuffle=True, num_workers=2)
-
-    testset = torchvision.datasets.CIFAR10(root=args.dir, train=False,
-                                           download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch_size,
-                                             shuffle=False, num_workers=2)
-
-    classes = ('plane', 'car', 'bird', 'cat',
-               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-               
-    dataiter = iter(trainloader)
-    images, labels = dataiter.next()
-
-    # show images
-    imshow(torchvision.utils.make_grid(images))
-    # print labels
-    print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
-               
-    if args.cluster_mode == "local":
-        init_orca_context(cores=1, memory="20g")
-    elif args.cluster_mode == "yarn":
-        init_orca_context(
-            cluster_mode="yarn-client", cores=4, num_nodes=2, memory="2g",
-            driver_memory="10g", driver_cores=1,
-            conf={"spark.rpc.message.maxSize": "1024",
-                  "spark.task.maxFailures": "1",
-                  "spark.driver.extraJavaOptions": "-Dbigdl.failure.retryTimes=1"})
-    
-    model = Net()
-    model.train()
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
-    zoo_estimator = Estimator.from_torch(model=model, optimizer=optimizer, loss=criterion,
-                                         backend="bigdl")
-    zoo_estimator.fit(data=trainloader, epochs=args.epochs, validation_data=testloader,
-                      validation_methods=[Accuracy()], checkpoint_trigger=EveryEpoch())
-    
-    dataiter = iter(testloader)
-    images, labels = dataiter.next()
-
-    # print images
-    imshow(torchvision.utils.make_grid(images))
-    print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
-    zoo_estimator.evaluate(data=testloader, validation_methods=[Accuracy()])
-    stop_orca_context()
-
-
-if __name__ == '__main__':
-    main()
+zoo_estimator.evaluate(data=testloader, validation_methods=[Accuracy()])
+zoo_estimator.shutdown()
+stop_orca_context()
