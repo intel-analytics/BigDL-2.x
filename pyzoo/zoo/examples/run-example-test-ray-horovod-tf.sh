e844bbadb40a533377ed7a675f03ccce5b272889
Wang, Yang <yang3.wang@intel.com>
2020-12-07 12:05:22 +0800

    remove test

--- pyzoo/zoo/examples/run-example-test-ray-horovod-tf.sh
+++ pyzoo/zoo/examples/run-example-test-ray-horovod-tf.sh
@@ -14,19 +14,12 @@ export BIGDL_JARS=`find ${ANALYTICS_ZOO_HOME}/lib -type f -name "analytics-zoo*j
 set -e
 
 echo "Start ray horovod tf example tests"
-#start execute
-echo "#1 tf2 estimator resnet 50 example"
-start=$(date "+%s")
-python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/orca/learn/tf2/resnet/resnet-50-imagenet.py --use_dummy_data --benchmark
-now=$(date "+%s")
-time1=$((now-start))
 
-echo "#2 Start tf2 estimator lenet"
+echo "#1 Start tf2 estimator lenet"
 start=$(date "+%s")
 python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/orca/learn/tf2/mnist/lenet_mnist_keras.py --cluster_mode local --max_epoch 1
 now=$(date "+%s")
-time2=$((now-start))
+time1=$((now-start))
 
 echo "Ray example tests finished"
-echo "#1 tf2 estimator resnet 50 time used:$time1 seconds"
-echo "#2 tf2 estimator lenet used:$time2 seconds"
+echo "#1 tf2 estimator lenet used:$time1 seconds"
