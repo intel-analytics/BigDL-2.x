223827df4794489c970746426bdfa234957ef90b
leonardozcm <597714572@qq.com>
2020-12-07 15:47:41 +0800

    fine tune

--- pyzoo/zoo/examples/orca/learn/tf/transform_learning/transform_learning.py
+++ pyzoo/zoo/examples/orca/learn/tf/transform_learning/transform_learning.py
@@ -1,21 +1,24 @@
 import os
-
 import tensorflow as tf
-
 from tensorflow import keras as K
 
 from zoo.orca import init_orca_context, stop_orca_context
+from zoo.orca.learn.tf.estimator import Estimator
 
 
 def train_val(cluster_mode, epochs, batch_size):
-    dataset_dir="datasets"
+
+    zip_file = tf.keras.utils.get_file(
+        origin="https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip",
+        fname="cats_and_dogs_filtered.zip", extract=True)
+    base_dir, _ = os.path.splitext(zip_file)
+
     if cluster_mode == "local":
         init_orca_context(cluster_mode="local", cores=4, memory="3g")
     elif cluster_mode == "yarn":
-        additional =  "datasets/cats_and_dogs_filtered.zip#" + dataset_dir
+        additional ="datasets/cats_and_dogs_filtered.zip#datasets"
         init_orca_context(cluster_mode="yarn-client", num_nodes=2, cores=2, driver_memory="3g",
                           additional_archive=additional)
-    base_dir="datasets/cats_and_dogs_filtered"
 
     train_dir = os.path.join(base_dir, 'train')
     validation_dir = os.path.join(base_dir, 'validation')
@@ -46,6 +49,7 @@ def train_val(cluster_mode, epochs, batch_size):
         image_resized = tf.image.resize(image_decoded, [image_size, image_size])
         return tf.cast(tf.reshape(image_resized, (image_size, image_size, 3)), dtype=tf.float32) / 255.0, label
 
+    # datasets
     train_list = [os.path.join(train_cats_dir, name) for name in os.listdir(train_cats_dir)] + [
         os.path.join(train_dogs_dir, name) for name in os.listdir(train_dogs_dir)]
     train_filenames = tf.constant(train_list)
@@ -63,25 +67,24 @@ def train_val(cluster_mode, epochs, batch_size):
     validation_dataset = tf.data.Dataset.from_tensor_slices((validation_filenames, validation_labels))
     validation_dataset = validation_dataset.map(_parse_function).shuffle(buffer_size=1000)
 
+    # build models
     inputs = K.layers.Input(shape=IMG_SHAPE)
     mnet = K.applications.MobileNetV2(input_tensor=inputs,
                                       include_top=False,
                                       weights='imagenet')
     mnet.trainable = False
-
     outputs_0 = mnet(inputs)
     outputs_1 = K.layers.GlobalAveragePooling2D()(outputs_0)
     outputs_2 = K.layers.Dense(1, activation='sigmoid')(outputs_1)
     model = K.models.Model(inputs=[inputs], outputs=[outputs_2])
-
     model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=0.0001),
                   loss='binary_crossentropy',
                   metrics=['accuracy'])
     model.summary()
-
     len(model.trainable_variables)
 
-    from zoo.orca.learn.tf.estimator import Estimator
+    # train
+    print("Freeze Network")
     est = Estimator.from_keras(keras_model=model)
     est.fit(train_dataset,
             batch_size=batch_size,
@@ -109,13 +112,13 @@ def train_val(cluster_mode, epochs, batch_size):
     model.summary()
     print(len(model.trainable_variables))
 
+    print("Unfreeze Network")
     est = Estimator.from_keras(keras_model=model)
     est.fit(data=train_dataset,
             batch_size=batch_size,
             epochs=epochs,
             validation_data=validation_dataset
             )
-    print("==>unfreeze")
     result = est.evaluate(validation_dataset)
     print(result)
     stop_orca_context()
