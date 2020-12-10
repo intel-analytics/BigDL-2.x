acad298f28f737d5259b912f45af59c745bffc94
Qiyuan Gong <qiyuan.gong@intel.com>
2020-12-10 17:18:44 +0800

    Add ProcessLookupError

--- pyzoo/zoo/ray/raycontext.py
+++ pyzoo/zoo/ray/raycontext.py
@@ -72,7 +72,7 @@ def kill_redundant_log_monitors(redis_address):
             is_same_redis = "--redis-address={}".format(redis_address)
             if is_log_monitor and is_same_redis in cmdline:
                 log_monitor_processes.append(proc)
-        except (psutil.AccessDenied, psutil.ZombieProcess):
+        except (psutil.AccessDenied, psutil.ZombieProcess, psutil.ProcessLookupError):
             # psutil may encounter AccessDenied or ZombieProcess exceptions
             # when it's trying to visit some MacOS core services
             if psutil.MACOS:
