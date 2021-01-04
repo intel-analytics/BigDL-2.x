0a5e10acd400d292de902923575b391de4be4ad4
Le-Zheng <alettazheng@hotmail.com>
2021-01-04 21:25:23 +0800

    add auto-test script

---
+++ docs/docs/colab-notebook/run-notebook-test.sh
@@ -0,0 +1,4 @@
+# Conversion to py file
+${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/lenet_mnist_graph
+# run with ipython
+ipython lenet_mnist_graph.py
