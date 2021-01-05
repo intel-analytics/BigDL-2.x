d08266d0861dc5491abc44443b7c72087eb4e2c1
Song Jiaming <litchy233@gmail.com>
2021-01-05 15:28:20 +0800

    serving only run in linux fix (#3320)

--- pyzoo/dev/release.sh
+++ pyzoo/dev/release.sh
@@ -102,11 +102,10 @@ if [ ${upload} == true ]; then
     $upload_command
 fi
 # build and upload serving wheel
-
 if [ "$platform" == "linux" ]; then
     cd ${ANALYTICS_ZOO_PYTHON_DIR}/zoo/serving
     if [ -d "${ANALYTICS_ZOO_PYTHON_DIR}/zoo/serving/dist" ]; then
-       rm -f ${ANALYTICS_ZOO_PYTHON_DIR}/zoo/serving/dist
+       rm -r ${ANALYTICS_ZOO_PYTHON_DIR}/zoo/serving/dist
     fi
     python setup.py bdist_wheel --universal
 
