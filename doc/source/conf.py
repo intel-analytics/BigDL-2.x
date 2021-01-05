c6813bfe8184c7d222c1bdad73c72ee52b3286c6
helenlly <helenlly@yahoo.com>
2021-01-05 14:11:13 +0800

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -39,15 +39,7 @@ html_theme_options = {
 }
 
 # The suffix of source filenames.
-from recommonmark.parser import CommonMarkParser
-source_parsers = {
-    '.md': CommonMarkParser,
-}
-source_suffix = ['.rst', '.md']
 
-source_parsers = {
-    '.md': CommonMarkParser,
-}
 master_doc = 'index'
 
 project = 'analytics-zoo'
