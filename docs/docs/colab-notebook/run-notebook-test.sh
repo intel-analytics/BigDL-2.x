6b68bf86b249ef625f6972dd722fd41ddef5d5da
Le-Zheng <alettazheng@hotmail.com>
2021-01-06 15:20:10 +0800

    update testing script, running with ipython directly

--- docs/docs/colab-notebook/run-notebook-test.sh
+++ docs/docs/colab-notebook/run-notebook-test.sh
@@ -1,4 +1,2 @@
-# Conversion to py file
-${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/lenet_mnist_graph
 # run with ipython
-ipython lenet_mnist_graph.py
+ipython ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/tf_lenet_mnist.ipynb
