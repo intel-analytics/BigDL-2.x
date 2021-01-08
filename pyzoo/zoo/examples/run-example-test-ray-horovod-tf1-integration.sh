54675158f17b74c06c9815ca90fa4f5ee457e218
leonardozcm <597714572@qq.com>
2021-01-08 15:12:31 +0800

    clear_up

--- pyzoo/zoo/examples/run-example-test-ray-horovod-tf1-integration.sh
+++ pyzoo/zoo/examples/run-example-test-ray-horovod-tf1-integration.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
+clear_up () {
+    echo "Clearing up environment. Uninstalling analytics-zoo"
+    pip uninstall -y analytics-zoo
+    pip uninstall -y bigdl
+    pip uninstall -y pyspark
+}
 
-export SPARK_HOME=$SPARK_HOME
-export MASTER=local[4]
-export FTP_URI=$FTP_URI
-export ANALYTICS_ZOO_ROOT=$ANALYTICS_ZOO_ROOT
-export ANALYTICS_ZOO_HOME=$ANALYTICS_ZOO_ROOT/dist
-export ANALYTICS_ZOO_JAR=`find ${ANALYTICS_ZOO_HOME}/lib -type f -name "analytics-zoo*jar-with-dependencies.jar"`
-export ANALYTICS_ZOO_PYZIP=`find ${ANALYTICS_ZOO_HOME}/lib -type f -name "analytics-zoo*python-api.zip"`
-export ANALYTICS_ZOO_CONF=${ANALYTICS_ZOO_HOME}/conf/spark-analytics-zoo.conf
-export PYTHONPATH=${ANALYTICS_ZOO_PYZIP}:$PYTHONPATH
-export BIGDL_JARS=`find ${ANALYTICS_ZOO_HOME}/lib -type f -name "analytics-zoo*jar-with-dependencies.jar"`
-
-set -e
-
-echo "Start ray horovod tf1 example tests"
 
 echo "#1 start test for orca tf transfer_learning"
 #timer
@@ -30,4 +22,6 @@ then
 fi
 now=$(date "+%s")
 time1=$((now-start))
+
+
 echo "#1 orca tf transfer_learning time used:$time1 seconds"
