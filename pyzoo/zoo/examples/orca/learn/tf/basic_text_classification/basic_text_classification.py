79d836c684cab6f499172e6b862b3a20e86fba93
leonardozcm <597714572@qq.com>
2021-01-11 15:51:48 +0800

    @title

--- pyzoo/zoo/examples/orca/learn/tf/basic_text_classification/basic_text_classification.py
+++ pyzoo/zoo/examples/orca/learn/tf/basic_text_classification/basic_text_classification.py
@@ -16,7 +16,7 @@
 #
 # Copyright 2018 The TensorFlow Authors.
 #
-# @title Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
@@ -28,7 +28,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-# @title MIT License
+# MIT License
 #
 # Copyright (c) 2017 François Chollet
 #
