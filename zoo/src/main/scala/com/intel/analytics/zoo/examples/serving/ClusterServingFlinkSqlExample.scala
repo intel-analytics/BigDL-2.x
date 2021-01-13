5ede3d9d515fb41ccf6d6f119bf1fa965c4b86c7
Jiaming <litchilitchy@qq.com>
2021-01-13 13:18:58 +0800

    flink udf maven

--- zoo/src/main/scala/com/intel/analytics/zoo/examples/serving/ClusterServingFlinkSqlExample.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/examples/serving/ClusterServingFlinkSqlExample.scala
@@ -17,13 +17,15 @@
 package com.intel.analytics.zoo.examples.serving
 
 
-import com.intel.analytics.zoo.serving.operator.{ClusterServingFunction, ClusterServingInferenceOperator, ClusterServingInput, ClusterServingParams}
+import com.intel.analytics.zoo.serving.operator.ClusterServingFunction
 import org.apache.flink.table.api.{EnvironmentSettings, TableEnvironment}
 import org.apache.log4j.{Level, Logger}
 import scopt.OptionParser
 
 object ClusterServingFlinkSqlExample {
+
   case class ExampleParams(modelPath: String = null, inputFile: String = null)
+
   val parser = new OptionParser[ExampleParams]("Cluster Serving Operator Usage Example") {
     opt[String]('m', "modelPath")
       .text("Model Path of Cluster Serving")
@@ -35,6 +37,7 @@ object ClusterServingFlinkSqlExample {
       .required()
   }
   Logger.getLogger("org").setLevel(Level.ERROR)
+
   def main(args: Array[String]): Unit = {
     val arg = parser.parse(args, ExampleParams()).head
     val settings = EnvironmentSettings.newInstance().build()
