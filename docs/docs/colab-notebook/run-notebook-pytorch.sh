c4fb2fcc761d1209675e24da777023802777bb9d
Le-Zheng <alettazheng@hotmail.com>
2021-01-19 15:56:33 +0800

    add pytorch quick start test

---
+++ docs/docs/colab-notebook/run-notebook-pytorch.sh
@@ -0,0 +1,22 @@
+echo "#2 start example for pytorch minist"
+#timer
+start=$(date "+%s")	
+#if [ -f analytics-zoo-data/data/MNIST ]	
+#then	
+#    echo "MNIST already exists"	
+#else	
+#    wget -nv $FTP_URI/analytics-zoo-data/mnist/train-labels-idx1-ubyte.gz -P analytics-zoo-data/data/MNIST/raw	
+#    wget -nv $FTP_URI/analytics-zoo-data/mnist/train-images-idx3-ubyte.gz -P analytics-zoo-data/data/MNIST/raw	
+#    wget -nv $FTP_URI/analytics-zoo-data/mnist/t10k-labels-idx1-ubyte.gz -P analytics-zoo-data/data/MNIST/raw	
+#    wget -nv $FTP_URI/analytics-zoo-data/mnist/t10k-images-idx3-ubyte.gz -P analytics-zoo-data/data/MNIST/raw	
+#fi	
+${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/pytorch_lenet_mnist
+sed -i "s/get_ipython()/#/g"  ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/pytorch_lenet_mnist.py
+sed -i "s/import os/#import os/g" ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/pytorch_lenet_mnist.py
+sed -i "s/import sys/#import sys/g" ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/pytorch_lenet_mnist.py
+python ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/pytorch_lenet_mnist.py
+
+now=$(date "+%s")	
+time2=$((now-start))	
+
+echo "#3 start example for orca Cifar10"
