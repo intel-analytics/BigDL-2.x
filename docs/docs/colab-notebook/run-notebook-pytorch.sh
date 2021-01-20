08d1387524c57f8fe3e1295e9c214545541bec40
Le-Zheng <alettazheng@hotmail.com>
2021-01-20 15:00:42 +0800

    update

--- docs/docs/colab-notebook/run-notebook-pytorch.sh
+++ docs/docs/colab-notebook/run-notebook-pytorch.sh
@@ -19,6 +19,11 @@ sed -i 's/^[^#].*__future__ */#&/g' ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebo
 sed -i "s/_ = (sys.path/#_ = (sys.path/g" ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/pytorch_lenet_mnist.py
 sed -i 's/^[^#].*site-packages*/#&/g' ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/pytorch_lenet_mnist.py
 
+wget -nv $FTP_URI/analytics-zoo-data/mnist/train-labels-idx1-ubyte.gz -P ./dataset
+wget -nv $FTP_URI/analytics-zoo-data/mnist/train-images-idx3-ubyte.gz -P ./dataset
+wget -nv $FTP_URI/analytics-zoo-data/mnist/t10k-labels-idx1-ubyte.gz -P ./dataset
+wget -nv $FTP_URI/analytics-zoo-data/mnist/t10k-images-idx3-ubyte.gz -P ./dataset
+
 python ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/pytorch_lenet_mnist.py
 
 now=$(date "+%s")	
