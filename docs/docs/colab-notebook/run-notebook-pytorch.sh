b5c25ef1e4f4654c6275a9522156d541e3b51a3f
Le-Zheng <alettazheng@hotmail.com>
2021-01-20 22:57:16 +0800

    update

--- docs/docs/colab-notebook/run-notebook-pytorch.sh
+++ docs/docs/colab-notebook/run-notebook-pytorch.sh
@@ -1,15 +1,7 @@
 echo "#2 start example for pytorch minist"
 #timer
 start=$(date "+%s")	
-#if [ -f analytics-zoo-data/data/MNIST ]	
-#then	
-#    echo "MNIST already exists"	
-#else	
-#    wget -nv $FTP_URI/analytics-zoo-data/mnist/train-labels-idx1-ubyte.gz -P analytics-zoo-data/data/MNIST/raw	
-#    wget -nv $FTP_URI/analytics-zoo-data/mnist/train-images-idx3-ubyte.gz -P analytics-zoo-data/data/MNIST/raw	
-#    wget -nv $FTP_URI/analytics-zoo-data/mnist/t10k-labels-idx1-ubyte.gz -P analytics-zoo-data/data/MNIST/raw	
-#    wget -nv $FTP_URI/analytics-zoo-data/mnist/t10k-images-idx3-ubyte.gz -P analytics-zoo-data/data/MNIST/raw	
-#fi	
+
 ${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/pytorch_lenet_mnist
 sed -i "s/get_ipython()/#/g"  ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/pytorch_lenet_mnist.py
 sed -i "s/import os/#import os/g" ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/pytorch_lenet_mnist.py
