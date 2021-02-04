ad83f135225c4b05cd7d8ed7ae5cc17c876eaa75
leonardozcm <597714572@qq.com>
2021-02-05 00:17:33 +0800

    debug

--- pyzoo/zoo/examples/run-example-tests.sh
+++ pyzoo/zoo/examples/run-example-tests.sh
@@ -646,15 +646,28 @@ mkdir analytics-zoo-data/data/cats_and_dogs_filtered/samples
 cp analytics-zoo-data/data/cats_and_dogs_filtered/train/cats/cat.7* analytics-zoo-data/data/cats_and_dogs_filtered/samples
 cp analytics-zoo-data/data/cats_and_dogs_filtered/train/dogs/dog.7* analytics-zoo-data/data/cats_and_dogs_filtered/samples
 #prepare model
-if [ -d ${HOME}/.cache/torch/hub/checkpoints/resnet18-5c106cde.pth ]
-then
+if [ -d ${HOME}/.cache/torch/hub/checkpoints/resnet18-5c106cde.pth ]; then
   echo "resnet model found."
 else
-  if [ ! -d ${HOME}/.cache/torch/hub/checkpoints ];then
+  if [ ! -d ${HOME}/.cache/torch/hub/checkpoints ]; then
     mkdir ${HOME}/.cache/torch/hub/checkpoints
   fi
   wget $FTP_URI/analytics-zoo-models/pytorch/resnet18-5c106cde.pth -P ${HOME}/.cache/torch/hub/checkpoints
 fi
+#run the example
+${ANALYTICS_ZOO_HOME}/bin/spark-submit-python-with-zoo.sh \
+  --master ${MASTER} \
+  --driver-memory 2g \
+  --executor-memory 2g \
+  ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/orca/learn/bigdl/resnet_finetune/resnet_finetune.py \
+  analytics-zoo-data/data/cats_and_dogs_filtered/samples
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+  echo "orca bigdl resnet-finetune"
+  exit $exit_status
+fi
+now=$(date "+%s")
+time20=$((now - start))
 
 echo "#1 textclassification time used: $time1 seconds"
 echo "#2 autograd time used: $time2 seconds"
