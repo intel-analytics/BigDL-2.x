9d06a9108d0310b6f25b83d55a287290192eb0f6
leonardozcm <597714572@qq.com>
2021-02-04 21:54:41 +0800

    debug

--- pyzoo/zoo/examples/run-example-tests.sh
+++ pyzoo/zoo/examples/run-example-tests.sh
@@ -13,25 +13,6 @@ export BIGDL_CLASSPATH=${ANALYTICS_ZOO_JAR}
 
 set -e
 
-echo "#20 start test for orca bigdl resnet-finetune"
-#timer
-start=$(date "+%s")
-#prepare dataset
-wget $FTP_URI/analytics-zoo-data/data/cats_and_dogs_filtered.zip -P analytics-zoo-data/data
-unzip -q analytics-zoo-data/data/cats_and_dogs_filtered.zip -d analytics-zoo-data/data
-mkdir analytics-zoo-data/data/cats_and_dogs_filtered/samples
-cp analytics-zoo-data/data/cats_and_dogs_filtered/train/cats/cat.7* analytics-zoo-data/data/cats_and_dogs_filtered/samples
-cp analytics-zoo-data/data/cats_and_dogs_filtered/train/dogs/dog.7* analytics-zoo-data/data/cats_and_dogs_filtered/samples
-#prepare model
-if [ -d ${HOME}/.cache/torch/hub/checkpoints/resnet18-5c106cde.pth ]
-then
-  echo "resnet model found."
-else
-  if [ ! -d ${HOME}/.cache/torch/hub/checkpoints ];then
-    mkdir ${HOME}/.cache/torch/hub/checkpoints
-  fi
-  wget $FTP_URI/analytics-zoo-models/pytorch/resnet18-5c106cde.pth -P ${HOME}/.cache/torch/hub/checkpoints
-fi
 #run the example
 ${ANALYTICS_ZOO_HOME}/bin/spark-submit-python-with-zoo.sh \
   --master ${MASTER} \
@@ -670,7 +651,25 @@ fi
 now=$(date "+%s")
 time19=$((now - start))
 
-
+echo "#20 start test for orca bigdl resnet-finetune"
+#timer
+start=$(date "+%s")
+#prepare dataset
+wget $FTP_URI/analytics-zoo-data/data/cats_and_dogs_filtered.zip -P analytics-zoo-data/data
+unzip -q analytics-zoo-data/data/cats_and_dogs_filtered.zip -d analytics-zoo-data/data
+mkdir analytics-zoo-data/data/cats_and_dogs_filtered/samples
+cp analytics-zoo-data/data/cats_and_dogs_filtered/train/cats/cat.7* analytics-zoo-data/data/cats_and_dogs_filtered/samples
+cp analytics-zoo-data/data/cats_and_dogs_filtered/train/dogs/dog.7* analytics-zoo-data/data/cats_and_dogs_filtered/samples
+#prepare model
+if [ -d ${HOME}/.cache/torch/hub/checkpoints/resnet18-5c106cde.pth ]
+then
+  echo "resnet model found."
+else
+  if [ ! -d ${HOME}/.cache/torch/hub/checkpoints ];then
+    mkdir ${HOME}/.cache/torch/hub/checkpoints
+  fi
+  wget $FTP_URI/analytics-zoo-models/pytorch/resnet18-5c106cde.pth -P ${HOME}/.cache/torch/hub/checkpoints
+fi
 
 echo "#1 textclassification time used: $time1 seconds"
 echo "#2 autograd time used: $time2 seconds"
