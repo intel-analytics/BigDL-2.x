3644e686a8dd630b942633798e10da5ba338c800
Le-Zheng <30695225+Le-Zheng@users.noreply.github.com>
2021-02-07 16:07:45 +0800

    fix python style check (#3474)

--- pyzoo/zoo/examples/run-example-tests-pip-ray.sh
+++ pyzoo/zoo/examples/run-example-tests-pip-ray.sh
@@ -46,9 +46,6 @@ unzip -q ${ANALYTICS_ZOO_ROOT}/data/mnist.zip -d ${ANALYTICS_ZOO_ROOT}/data
 execute_ray_test lenet_mnist ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/orca/learn/mxnet/lenet_mnist.py -e 1 -b 256
 time5=$?
 
-execute_ray_test tf2_mnist ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/orca/learn/tf2/mnist/lenet_mnist_keras.py --cluster_mode local --max_epoch 1
-time6=$?
-
 if [ -d ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/orca/learn/pytorch/fashion_mnist/data ]
 then
     echo "fashion-mnist dataset already exists"
@@ -62,7 +59,7 @@ sed "s/epochs=5/epochs=1/g;s/batch_size=4/batch_size=256/g" \
     > ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/orca/learn/pytorch/fashion_mnist/fashion_mnist_tmp.py
 
 execute_ray_test fashion_mnist ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/orca/learn/pytorch/fashion_mnist/fashion_mnist_tmp.py
-time7=$?
+time6=$?
 
 if [ ! -f BSDS300-images.tgz ]; then
   wget -nv $FTP_URI/analytics-zoo-data/BSDS300-images.tgz
@@ -73,15 +70,14 @@ if [ ! -d dataset/BSDS300/images ]; then
 fi
 
 execute_ray_test super_resolution_BSDS3000 ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/orca/learn/pytorch/super_resolution/super_resolution.py
-time8=$?
+time7=$?
 
 echo "#1 rl_pong time used:$time1 seconds"
 echo "#2 sync_parameter_server time used:$time2 seconds"
 echo "#3 async_parameter_server time used:$time3 seconds"
 echo "#4 multiagent_two_trainers time used:$time4 seconds"
 echo "#5 mxnet_lenet time used:$time5 seconds"
-echo "#6 tf2_lenet time used:$time6 seconds"
-echo "#7 fashion-mnist time used:$time7 seconds"
-echo "#8 super-resolution time used:$time8 seconds"
+echo "#6 fashion-mnist time used:$time6 seconds"
+echo "#7 super-resolution time used:$time7 seconds"
 
 clear_up
