b356ad02929a6ad261330d5a3abf694749be7214
glorysdj <glorysdj@gmail.com>
2021-02-23 23:05:12 -0800

    add scripts and init.sh

--- ppml/trusted-big-data-ml/scala/docker-graphene/init.sh
+++ ppml/trusted-big-data-ml/scala/docker-graphene/init.sh
@@ -2,16 +2,11 @@
 
 set -x
 
-if [ $# -ne 2 ]; then
-    echo $0: usage: init.sh local_ip sgx_mem_size, example: init.sh 172.168.10.102 64G
-    exit 1
-fi
+local_ip=$LOCAL_IP
+sgx_mem_size=$SGX_MEM_SIZE
 
-local_ip=$1
-sgx_mem_size=$2
-
-make SGX=1 GRAPHENEDIR=/graphene THIS_DIR=/training/jdk8_spark SPARK_LOCAL_IP=$local_ip SPARK_USER=root G_SGX_SIZE=$sgx_mem_size && \
+make SGX=1 GRAPHENEDIR=/graphene THIS_DIR=/ppml/trusted-big-data-ml SPARK_LOCAL_IP=$local_ip SPARK_USER=root G_SGX_SIZE=$sgx_mem_size && \
 cd /opt/jdk8/bin && \
-ln -s /training/jdk8_spark/java.sig java.sig && \
-ln -s /training/jdk8_spark/java.manifest.sgx java.manifest.sgx && \
-ln -s /training/jdk8_spark/java.token java.token
+ln -s /ppml/trusted-big-data-ml/java.sig java.sig && \
+ln -s /ppml/trusted-big-data-ml/java.manifest.sgx java.manifest.sgx && \
+ln -s /ppml/trusted-big-data-ml/java.token java.token
