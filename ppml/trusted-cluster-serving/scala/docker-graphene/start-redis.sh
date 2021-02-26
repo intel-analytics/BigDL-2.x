5f542262eb31c8040196c47dbe1d59d54115194d
glorysdj <glorysdj@gmail.com>
2021-02-26 00:51:18 -0800

    add redis

--- ppml/trusted-cluster-serving/scala/docker-graphene/start-redis.sh
+++ ppml/trusted-cluster-serving/scala/docker-graphene/start-redis.sh
@@ -5,7 +5,7 @@ echo "### Launching Redis ###"
 KEY_PATH=/ppml/trusted-cluster-serving/redis/work/keys
 
 cd /ppml/trusted-cluster-serving/redis
-SGX=1 ./pal_loader redis-server --tls-port 6379 --port 0 \
+SGX=1 ./pal_loader redis-server --tls-port $REDIS_PORT --port 0 \
     --tls-cert-file $KEY_PATH/server.crt \
     --tls-key-file $KEY_PATH/server.key \
     --tls-ca-cert-file $KEY_PATH/server.crt \
