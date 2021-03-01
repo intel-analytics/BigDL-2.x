35473753e0d63c9c7e0d89de75325b1ce2a1c388
Le-Zheng <alettazheng@hotmail.com>
2021-03-01 12:32:55 +0800

    address comments

--- pyzoo/zoo/util/spark.py
+++ pyzoo/zoo/util/spark.py
@@ -31,6 +31,9 @@ class SparkRunner:
                  redirect_spark_log=True):
         self.spark_log_level = spark_log_level
         self.redirect_spark_log = redirect_spark_log
+        with SparkContext._lock:
+            if SparkContext._active_spark_context:
+                print("WARNING: There's existing SparkContext. Please close it first.")
         import pyspark
         print("Current pyspark location is : {}".format(pyspark.__file__))
 
