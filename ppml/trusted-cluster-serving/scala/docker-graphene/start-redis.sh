1f78983e6911585a931655bb46f586ee113b5146
glorysdj <glorysdj@gmail.com>
2021-03-04 17:15:28 -0800

    add http

--- ppml/trusted-cluster-serving/scala/docker-graphene/start-redis.sh
+++ ppml/trusted-cluster-serving/scala/docker-graphene/start-redis.sh
@@ -2,11 +2,10 @@
 # set -x
 
 echo "### Launching Redis ###"
-KEY_PATH=/ppml/trusted-cluster-serving/redis/work/keys
 
 cd /ppml/trusted-cluster-serving/redis
 SGX=1 ./pal_loader redis-server --tls-port $REDIS_PORT --port 0 \
-    --tls-cert-file $KEY_PATH/server.crt \
-    --tls-key-file $KEY_PATH/server.key \
-    --tls-ca-cert-file $KEY_PATH/server.crt \
+    --tls-cert-file /ppml/trusted-cluster-serving/redis/work/keys/server.crt \
+    --tls-key-file /ppml/trusted-cluster-serving/redis/work/keys/server.key \
+    --tls-ca-cert-file /ppml/trusted-cluster-serving/redis/work/keys/server.crt \
     --protected-mode no --maxmemory 10g | tee ./redis-sgx.log
