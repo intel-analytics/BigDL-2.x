6dcefa1f44b334827bd9e349e2acdc6d4b1809ad
glorysdj <glorysdj@gmail.com>
2021-03-11 18:19:52 -0800

    add cluster serving all-in-one mode

--- ppml/trusted-cluster-serving/scala/docker-graphene/init-cluster-serving.sh
+++ ppml/trusted-cluster-serving/scala/docker-graphene/init-cluster-serving.sh
@@ -2,7 +2,7 @@
 set -x
 
 core_num=$CORE_NUM
-secure_passowrd=$SECURE_PASSWORD
+secure_passowrd=$REDIS_SECURE_PASSWORD
 redis_host=$REDIS_HOST
 
 sed -i "s#core_number:#core_number: ${core_num}#" config.yaml
