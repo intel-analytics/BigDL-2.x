34945adf3d954dae79a7c4d21945bfcf200e8cb2
glorysdj <glorysdj@gmail.com>
2021-03-14 17:55:13 -0700

    rm unused env

--- ppml/trusted-cluster-serving/scala/docker-graphene/start-cluster-serving-job.sh
+++ ppml/trusted-cluster-serving/scala/docker-graphene/start-cluster-serving-job.sh
@@ -10,7 +10,6 @@ flink_job_manager_port=$FLINK_JOB_MANAGER_REST_PORT
 echo "### Launching Cluster Serving Job ###"
 
 export FLINK_CONF_DIR=${flink_home}/conf && \
-    export TF_MKL_ALLOC_MAX_BYTES=10737418240 && \
     ${flink_home}/bin/flink run \
     -c com.intel.analytics.zoo.serving.ClusterServing -p 1 -m $flink_job_manager_ip:$flink_job_manager_port \
     /ppml/trusted-cluster-serving/java/work/analytics-zoo-bigdl_${BIGDL_VERSION}-spark_${SPARK_VERSION}-${ANALYTICS_ZOO_VERSION}-serving.jar | tee ./cluster-serving-job-sgx.log
