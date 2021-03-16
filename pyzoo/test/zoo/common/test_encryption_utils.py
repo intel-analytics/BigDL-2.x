090fffb835a005367365897e85543197118c7154
Qiyuan Gong <qiyuan.gong@intel.com>
2021-03-16 19:36:19 +0800

    Finished AES CBC and GCM based on cryptography

--- pyzoo/test/zoo/common/test_encryption_utils.py
+++ pyzoo/test/zoo/common/test_encryption_utils.py
@@ -17,28 +17,82 @@ import pytest
 import string
 import random
 
-from zoo.common.encryption_utils import encrypt_with_AES_CBC, decrypt_with_AES_CBC
+from zoo.common.encryption_utils import *
+
 
 class TestEncryption(object):
 
-    def test_aes128(self):
+    def test_aes128_cbc_bytes(self):
         letters = string.ascii_lowercase
         random_str = ''.join(random.choice(letters) for i in range(100))
         # random_str = "hello world, hello scala, hello encrypt, come on UNITED!!!"
-        enc_bytes = encrypt_with_AES_CBC(random_str.encode("utf-8"), 'analytics-zoo', 'intel-analytics')
-        dec_bytes = decrypt_with_AES_CBC(enc_bytes, 'analytics-zoo', 'intel-analytics')
-        print(dec_bytes)
-        assert dec_bytes == random_str.encode("utf-8"), \
-            "Check AES GCM 128 encryption and decryption result"
+        enc_bytes = encrypt_bytes_with_AES_CBC(random_str.encode(), 'analytics-zoo', 'intel-analytics')
+        dec_bytes = decrypt_bytes_with_AES_CBC(enc_bytes, 'analytics-zoo', 'intel-analytics')
+        assert dec_bytes == random_str.encode(), \
+            "Check AES CBC 128 encryption and decryption result"
 
-    def test_aes256(self):
+    def test_aes256_cbc_bytes(self):
         letters = string.ascii_lowercase
         random_str = ''.join(random.choice(letters) for i in range(100))
-        enc_bytes = encrypt_with_AES_CBC(random_str.encode("utf-8"), 'analytics-zoo', 'intel-analytics', 256)
-        dec_bytes = decrypt_with_AES_CBC(enc_bytes, 'analytics-zoo', 'intel-analytics', 256)
-        print(dec_bytes)
+        enc_bytes = encrypt_bytes_with_AES_CBC(random_str.encode("utf-8"), 'analytics-zoo', 'intel-analytics', 256)
+        dec_bytes = decrypt_bytes_with_AES_CBC(enc_bytes, 'analytics-zoo', 'intel-analytics', 256)
         assert dec_bytes == random_str.encode("utf-8"), \
-            "Check AES GCM 256 encryption and decryption result"
+            "Check AES CBC 256 encryption and decryption result"
+
+    def test_aes128_gcm_bytes(self):
+        letters = string.ascii_lowercase
+        random_str = ''.join(random.choice(letters) for i in range(100))
+        # random_str = "hello world, hello scala, hello encrypt, come on UNITED!!!"
+        enc_bytes = encrypt_bytes_with_AES_GCM(random_str.encode(), 'analytics-zoo', 'intel-analytics')
+        dec_bytes = decrypt_bytes_with_AES_GCM(enc_bytes, 'analytics-zoo', 'intel-analytics')
+        assert dec_bytes == random_str.encode(), \
+            "Check AES GCM 128 encryption and decryption result"
+
+    def test_aes256_gcm_bytes(self):
+        letters = string.ascii_lowercase
+        random_str = ''.join(random.choice(letters) for i in range(100))
+        # random_str = "hello world, hello scala, hello encrypt, come on UNITED!!!"
+        enc_bytes = encrypt_bytes_with_AES_GCM(random_str.encode(), 'analytics-zoo', 'intel-analytics', 256)
+        dec_bytes = decrypt_bytes_with_AES_GCM(enc_bytes, 'analytics-zoo', 'intel-analytics', 256)
+        assert dec_bytes == random_str.encode(), \
+            "Check AES GCM 128 encryption and decryption result"
+
+    def test_aes128_cbc(self):
+        letters = string.ascii_lowercase
+        random_str = ''.join(random.choice(letters) for i in range(100))
+        # random_str = "hello world, hello scala, hello encrypt, come on UNITED!!!"
+        enc_str = encrypt_with_AES_CBC(random_str, 'analytics-zoo', 'intel-analytics')
+        dec_str = decrypt_with_AES_CBC(enc_str, 'analytics-zoo', 'intel-analytics')
+        assert dec_str == random_str, \
+            "Check AES CBC 128 encryption and decryption result"
+
+    def test_aes256_cbc(self):
+        letters = string.ascii_lowercase
+        random_str = ''.join(random.choice(letters) for i in range(100))
+        # random_str = "hello world, hello scala, hello encrypt, come on UNITED!!!"
+        enc_str = encrypt_with_AES_CBC(random_str, 'analytics-zoo', 'intel-analytics', 256)
+        dec_str = decrypt_with_AES_CBC(enc_str, 'analytics-zoo', 'intel-analytics', 256)
+        assert dec_str == random_str, \
+            "Check AES CBC 128 encryption and decryption result"
+
+    def test_aes128_gcm(self):
+        letters = string.ascii_lowercase
+        random_str = ''.join(random.choice(letters) for i in range(100))
+        # random_str = "hello world, hello scala, hello encrypt, come on UNITED!!!"
+        enc_str = encrypt_with_AES_GCM(random_str, 'analytics-zoo', 'intel-analytics')
+        dec_str = decrypt_with_AES_GCM(enc_str, 'analytics-zoo', 'intel-analytics')
+        assert dec_str == random_str, \
+            "Check AES GCM 128 encryption and decryption result"
+
+    def test_aes256_gcm(self):
+        letters = string.ascii_lowercase
+        random_str = ''.join(random.choice(letters) for i in range(100))
+        # random_str = "hello world, hello scala, hello encrypt, come on UNITED!!!"
+        enc_str = encrypt_with_AES_GCM(random_str, 'analytics-zoo', 'intel-analytics', 256)
+        dec_str = decrypt_with_AES_GCM(enc_str, 'analytics-zoo', 'intel-analytics', 256)
+        assert dec_str == random_str, \
+            "Check AES GCM 128 encryption and decryption result"
+
 
 
 if __name__ == "__main__":
--- pyzoo/zoo/common/encryption_utils.py
+++ pyzoo/zoo/common/encryption_utils.py
@@ -20,24 +20,70 @@ def get_private_key(secret_key, salt, key_len=128):
 
 
 def encrypt_with_AES_CBC(plain_text, secret_key, salt, key_len=128, block_size=16):
+    ct_bytes = encrypt_bytes_with_AES_CBC(plain_text.encode(),
+                                          secret_key, salt, key_len, block_size)
+    return base64.b64encode(ct_bytes).decode()
+
+
+def decrypt_with_AES_CBC(cipher_text, secret_key, salt, key_len=128, block_size=16):
+    plain_bytes = decrypt_bytes_with_AES_CBC(base64.b64decode(cipher_text),
+                                             secret_key, salt, key_len, block_size)
+    return plain_bytes.decode()
+
+
+def encrypt_bytes_with_AES_CBC(plain_text_bytes, secret_key, salt, key_len=128, block_size=16):
     key = get_private_key(secret_key, salt, key_len)
     iv = os.urandom(block_size)
     # Align with Java AES/CBC/PKCS5PADDING
     padder = padding.PKCS7(key_len).padder()
-    data = padder.update(plain_text)
+    data = padder.update(plain_text_bytes)
+    data += padder.finalize()
     # create Cipher
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
-    encryptor = cipher.encryptor()
+    encryptor = Cipher(algorithms.AES(key), modes.CBC(iv)).encryptor()
     ct = encryptor.update(data) + encryptor.finalize()
     return iv + ct
 
 
-def decrypt_with_AES_CBC(cipher_text, secret_key, salt, key_len=128, block_size=16):
+def decrypt_bytes_with_AES_CBC(cipher_text_bytes, secret_key, salt, key_len=128, block_size=16):
     key = get_private_key(secret_key, salt, key_len)
-    iv = cipher_text[:block_size]
-    unpadder = padding.PKCS7(key_len).unpadder()
-    ct = unpadder.update(cipher_text[block_size:])
+    iv = cipher_text_bytes[:block_size]
     # create Cipher
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
-    decryptor = cipher.decryptor()
-    return decryptor.update(ct) + decryptor.finalize()
+    decryptor = Cipher(algorithms.AES(key), modes.CBC(iv)).decryptor()
+    ct = decryptor.update(cipher_text_bytes[block_size:]) + decryptor.finalize()
+    # unpadding
+    unpadder = padding.PKCS7(key_len).unpadder()
+    ct = unpadder.update(ct)
+    ct += unpadder.finalize()
+    return ct
+
+
+def encrypt_with_AES_GCM(plain_text, secret_key, salt, key_len=128, block_size=12):
+    ct_bytes = encrypt_bytes_with_AES_GCM(plain_text.encode(),
+                                          secret_key, salt, key_len, block_size)
+    return base64.b64encode(ct_bytes).decode()
+
+
+def decrypt_with_AES_GCM(cipher_text, secret_key, salt, key_len=128, block_size=12):
+    plain_bytes = decrypt_bytes_with_AES_GCM(base64.b64decode(cipher_text),
+                                             secret_key, salt, key_len, block_size)
+    return plain_bytes.decode()
+
+
+def encrypt_bytes_with_AES_GCM(plain_text_bytes, secret_key, salt, key_len=128, block_size=12):
+    key = get_private_key(secret_key, salt, key_len)
+    iv = os.urandom(block_size)
+    # create Cipher
+    encryptor = Cipher(algorithms.AES(key), modes.GCM(iv)).encryptor()
+    ct = encryptor.update(plain_text_bytes) + encryptor.finalize()
+    return iv + ct + encryptor.tag
+
+
+def decrypt_bytes_with_AES_GCM(cipher_text_bytes, secret_key, salt, key_len=128, block_size=12):
+    key = get_private_key(secret_key, salt, key_len)
+    tag = cipher_text_bytes[-16:]
+    iv = cipher_text_bytes[:block_size]
+    # create Cipher
+    decryptor = Cipher(algorithms.AES(key), modes.GCM(iv, tag)).decryptor()
+    # 16 for tag
+    ct = decryptor.update(cipher_text_bytes[block_size:-16]) + decryptor.finalize()
+    return ct
