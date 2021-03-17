7ba22a7ab9fff2b3700d91c075c079256a8bb8b9
Qiyuan Gong <qiyuan.gong@intel.com>
2021-03-17 17:05:50 +0800

    Refine unit test

--- pyzoo/test/zoo/common/test_encryption_utils.py
+++ pyzoo/test/zoo/common/test_encryption_utils.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from unittest import TestCase
 
 import pytest
 import string
@@ -21,79 +22,68 @@ import random
 from zoo.common.encryption_utils import *
 
 
-class TestEncryption(object):
+class TestEncryption(TestCase):
+
+    def setUp(self) -> None:
+        # self.random_str = "hello world, hello scala, hello encrypt, come on UNITED!!!"
+        letters = string.ascii_lowercase
+        self.random_str = ''.join(random.choice(letters) for _ in range(100))
 
     def test_aes128_cbc_bytes(self):
-        letters = string.ascii_lowercase
-        random_str = ''.join(random.choice(letters) for i in range(100))
-        # random_str = "hello world, hello scala, hello encrypt, come on UNITED!!!"
-        enc_bytes = encrypt_bytes_with_AES_CBC(random_str.encode(), 'analytics-zoo', 'intel-analytics')
-        dec_bytes = decrypt_bytes_with_AES_CBC(enc_bytes, 'analytics-zoo', 'intel-analytics')
-        assert dec_bytes == random_str.encode(), \
+        enc_bytes = encrypt_bytes_with_AES_CBC(self.random_str.encode(),
+                                               'analytics-zoo', 'intel-analytics')
+        dec_bytes = decrypt_bytes_with_AES_CBC(enc_bytes,
+                                               'analytics-zoo', 'intel-analytics')
+        assert dec_bytes == self.random_str.encode(), \
             "Check AES CBC 128 encryption and decryption result"
 
     def test_aes256_cbc_bytes(self):
-        letters = string.ascii_lowercase
-        random_str = ''.join(random.choice(letters) for i in range(100))
-        enc_bytes = encrypt_bytes_with_AES_CBC(random_str.encode("utf-8"), 'analytics-zoo', 'intel-analytics', 256)
-        dec_bytes = decrypt_bytes_with_AES_CBC(enc_bytes, 'analytics-zoo', 'intel-analytics', 256)
-        assert dec_bytes == random_str.encode("utf-8"), \
+        enc_bytes = encrypt_bytes_with_AES_CBC(self.random_str.encode("utf-8"),
+                                               'analytics-zoo', 'intel-analytics', 256)
+        dec_bytes = decrypt_bytes_with_AES_CBC(enc_bytes,
+                                               'analytics-zoo', 'intel-analytics', 256)
+        assert dec_bytes == self.random_str.encode("utf-8"), \
             "Check AES CBC 256 encryption and decryption result"
 
     def test_aes128_gcm_bytes(self):
-        letters = string.ascii_lowercase
-        random_str = ''.join(random.choice(letters) for i in range(100))
-        # random_str = "hello world, hello scala, hello encrypt, come on UNITED!!!"
-        enc_bytes = encrypt_bytes_with_AES_GCM(random_str.encode(), 'analytics-zoo', 'intel-analytics')
-        dec_bytes = decrypt_bytes_with_AES_GCM(enc_bytes, 'analytics-zoo', 'intel-analytics')
-        assert dec_bytes == random_str.encode(), \
+        enc_bytes = encrypt_bytes_with_AES_GCM(self.random_str.encode(),
+                                               'analytics-zoo', 'intel-analytics')
+        dec_bytes = decrypt_bytes_with_AES_GCM(enc_bytes,
+                                               'analytics-zoo', 'intel-analytics')
+        assert dec_bytes == self.random_str.encode(), \
             "Check AES GCM 128 encryption and decryption result"
 
     def test_aes256_gcm_bytes(self):
-        letters = string.ascii_lowercase
-        random_str = ''.join(random.choice(letters) for i in range(100))
-        # random_str = "hello world, hello scala, hello encrypt, come on UNITED!!!"
-        enc_bytes = encrypt_bytes_with_AES_GCM(random_str.encode(), 'analytics-zoo', 'intel-analytics', 256)
-        dec_bytes = decrypt_bytes_with_AES_GCM(enc_bytes, 'analytics-zoo', 'intel-analytics', 256)
-        assert dec_bytes == random_str.encode(), \
+        enc_bytes = encrypt_bytes_with_AES_GCM(self.random_str.encode(),
+                                               'analytics-zoo', 'intel-analytics', 256)
+        dec_bytes = decrypt_bytes_with_AES_GCM(enc_bytes,
+                                               'analytics-zoo', 'intel-analytics', 256)
+        assert dec_bytes == self.random_str.encode(), \
             "Check AES GCM 128 encryption and decryption result"
 
     def test_aes128_cbc(self):
-        letters = string.ascii_lowercase
-        random_str = ''.join(random.choice(letters) for i in range(100))
-        # random_str = "hello world, hello scala, hello encrypt, come on UNITED!!!"
-        enc_str = encrypt_with_AES_CBC(random_str, 'analytics-zoo', 'intel-analytics')
+        enc_str = encrypt_with_AES_CBC(self.random_str, 'analytics-zoo', 'intel-analytics')
         dec_str = decrypt_with_AES_CBC(enc_str, 'analytics-zoo', 'intel-analytics')
-        assert dec_str == random_str, \
+        assert dec_str == self.random_str, \
             "Check AES CBC 128 encryption and decryption result"
 
     def test_aes256_cbc(self):
-        letters = string.ascii_lowercase
-        random_str = ''.join(random.choice(letters) for i in range(100))
-        # random_str = "hello world, hello scala, hello encrypt, come on UNITED!!!"
-        enc_str = encrypt_with_AES_CBC(random_str, 'analytics-zoo', 'intel-analytics', 256)
+        enc_str = encrypt_with_AES_CBC(self.random_str, 'analytics-zoo', 'intel-analytics', 256)
         dec_str = decrypt_with_AES_CBC(enc_str, 'analytics-zoo', 'intel-analytics', 256)
-        assert dec_str == random_str, \
+        assert dec_str == self.random_str, \
             "Check AES CBC 128 encryption and decryption result"
 
     def test_aes128_gcm(self):
-        letters = string.ascii_lowercase
-        random_str = ''.join(random.choice(letters) for i in range(100))
-        # random_str = "hello world, hello scala, hello encrypt, come on UNITED!!!"
-        enc_str = encrypt_with_AES_GCM(random_str, 'analytics-zoo', 'intel-analytics')
+        enc_str = encrypt_with_AES_GCM(self.random_str, 'analytics-zoo', 'intel-analytics')
         dec_str = decrypt_with_AES_GCM(enc_str, 'analytics-zoo', 'intel-analytics')
-        assert dec_str == random_str, \
+        assert dec_str == self.random_str, \
             "Check AES GCM 128 encryption and decryption result"
 
     def test_aes256_gcm(self):
-        letters = string.ascii_lowercase
-        random_str = ''.join(random.choice(letters) for i in range(100))
-        # random_str = "hello world, hello scala, hello encrypt, come on UNITED!!!"
-        enc_str = encrypt_with_AES_GCM(random_str, 'analytics-zoo', 'intel-analytics', 256)
+        enc_str = encrypt_with_AES_GCM(self.random_str, 'analytics-zoo', 'intel-analytics', 256)
         dec_str = decrypt_with_AES_GCM(enc_str, 'analytics-zoo', 'intel-analytics', 256)
-        assert dec_str == random_str, \
+        assert dec_str == self.random_str, \
             "Check AES GCM 128 encryption and decryption result"
-
 
 
 if __name__ == "__main__":
