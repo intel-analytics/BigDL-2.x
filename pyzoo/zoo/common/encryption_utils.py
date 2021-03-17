fb0cd38d56f6aeea08ba37c6cede4f630e37b6d2
Qiyuan Gong <qiyuan.gong@intel.com>
2021-03-17 20:58:57 +0800

    Fix backend error in py36

--- pyzoo/zoo/common/encryption_utils.py
+++ pyzoo/zoo/common/encryption_utils.py
@@ -17,6 +17,7 @@
 import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
 import base64
 import hashlib
 
