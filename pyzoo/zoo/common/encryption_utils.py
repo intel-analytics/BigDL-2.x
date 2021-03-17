ce01db83ac4748eac95a7834a64497157c11d59f
Qiyuan Gong <qiyuan.gong@intel.com>
2021-03-17 10:52:50 +0800

    Encryption with cryptography passed

--- pyzoo/zoo/common/encryption_utils.py
+++ pyzoo/zoo/common/encryption_utils.py
@@ -16,22 +16,49 @@ def get_private_key(secret_key, salt, key_len=128):
     # AES key_len means key bit length, not string/char length
     # 16 for 128 and 32 for 256
     bit_len = (key_len / 128) * 16
-    return hashlib.pbkdf2_hmac('SHA256', secret_key.encode(), salt.encode(), 65536, bit_len)
+    return hashlib.pbkdf2_hmac('SHA256', secret_key.encode(), salt.encode(), 65536, int(bit_len))
 
 
 def encrypt_with_AES_CBC(plain_text, secret_key, salt, key_len=128, block_size=16):
+    """
+    encrypt string plain text with AES CBC
+    :param plain_text: plain test in string
+    :param secret_key: secret key in string
+    :param salt: secret slat in string
+    :param key_len: key len (128 or 256)
+    :param block_size: lv size (default 16 for CBC)
+    :return: cipher text in string
+    """
     ct_bytes = encrypt_bytes_with_AES_CBC(plain_text.encode(),
                                           secret_key, salt, key_len, block_size)
     return base64.b64encode(ct_bytes).decode()
 
 
 def decrypt_with_AES_CBC(cipher_text, secret_key, salt, key_len=128, block_size=16):
+    """
+    decrypt string cipher text with AES CBC
+    :param cipher_text: cipher text in string
+    :param secret_key: secret key in string
+    :param salt: secret slat in string
+    :param key_len: key len (128 or 256)
+    :param block_size: lv size (default 16 for CBC)
+    :return: plain test in string
+    """
     plain_bytes = decrypt_bytes_with_AES_CBC(base64.b64decode(cipher_text),
                                              secret_key, salt, key_len, block_size)
     return plain_bytes.decode()
 
 
 def encrypt_bytes_with_AES_CBC(plain_text_bytes, secret_key, salt, key_len=128, block_size=16):
+    """
+    encrypt bytes plain text with AES CBC
+    :param plain_text_bytes: plain test in bytes
+    :param secret_key: secret key in string
+    :param salt: secret slat in string
+    :param key_len: key len (128 or 256)
+    :param block_size: lv size (default 16 for CBC)
+    :return: cipher text in bytes
+    """
     key = get_private_key(secret_key, salt, key_len)
     iv = os.urandom(block_size)
     # Align with Java AES/CBC/PKCS5PADDING
@@ -45,6 +72,15 @@ def encrypt_bytes_with_AES_CBC(plain_text_bytes, secret_key, salt, key_len=128, 
 
 
 def decrypt_bytes_with_AES_CBC(cipher_text_bytes, secret_key, salt, key_len=128, block_size=16):
+    """
+    decrypt bytes cipher text with AES CBC
+    :param cipher_text_bytes: cipher text in bytes
+    :param secret_key: secret key in string
+    :param salt: secret slat in string
+    :param key_len: key len (128 or 256)
+    :param block_size: lv size (default 16 for CBC)
+    :return: plain test in bytes
+    """
     key = get_private_key(secret_key, salt, key_len)
     iv = cipher_text_bytes[:block_size]
     # create Cipher
@@ -58,18 +94,45 @@ def decrypt_bytes_with_AES_CBC(cipher_text_bytes, secret_key, salt, key_len=128,
 
 
 def encrypt_with_AES_GCM(plain_text, secret_key, salt, key_len=128, block_size=12):
+    """
+    encrypt string plain text with AES GCM
+    :param plain_text: plain test in string
+    :param secret_key: secret key in string
+    :param salt: secret slat in string
+    :param key_len: key len (128 or 256)
+    :param block_size: lv size (default 12 for GCM)
+    :return: cipher text in string
+    """
     ct_bytes = encrypt_bytes_with_AES_GCM(plain_text.encode(),
                                           secret_key, salt, key_len, block_size)
     return base64.b64encode(ct_bytes).decode()
 
 
 def decrypt_with_AES_GCM(cipher_text, secret_key, salt, key_len=128, block_size=12):
+    """
+    decrypt string cipher text with AES GCM
+    :param cipher_text: cipher text in string
+    :param secret_key: secret key in string
+    :param salt: secret slat in string
+    :param key_len: key len (128 or 256)
+    :param block_size: lv size (default 12 for GCM)
+    :return: plain test in string
+    """
     plain_bytes = decrypt_bytes_with_AES_GCM(base64.b64decode(cipher_text),
                                              secret_key, salt, key_len, block_size)
     return plain_bytes.decode()
 
 
 def encrypt_bytes_with_AES_GCM(plain_text_bytes, secret_key, salt, key_len=128, block_size=12):
+    """
+    encrypt bytes plain text with AES GCM
+    :param plain_text_bytes: plain test in bytes
+    :param secret_key: secret key in string
+    :param salt: secret slat in string
+    :param key_len: key len (128 or 256)
+    :param block_size: lv size (default 12 for GCM)
+    :return: cipher text in bytes
+    """
     key = get_private_key(secret_key, salt, key_len)
     iv = os.urandom(block_size)
     # create Cipher
@@ -79,6 +142,15 @@ def encrypt_bytes_with_AES_GCM(plain_text_bytes, secret_key, salt, key_len=128, 
 
 
 def decrypt_bytes_with_AES_GCM(cipher_text_bytes, secret_key, salt, key_len=128, block_size=12):
+    """
+    decrypt bytes cipher text with AES GCM
+    :param cipher_text_bytes: cipher text in bytes
+    :param secret_key: secret key in string
+    :param salt: secret slat in string
+    :param key_len: key len (128 or 256)
+    :param block_size: lv size (default 12 for GCM)
+    :return: plain test in bytes
+    """
     key = get_private_key(secret_key, salt, key_len)
     tag = cipher_text_bytes[-16:]
     iv = cipher_text_bytes[:block_size]
