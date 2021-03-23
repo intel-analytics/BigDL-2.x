46630ee6d2ddc3b0433408c0315df4baba91b486
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2021-03-23 09:37:25 +0800

    Update run-docker-local-example.sh

--- ppml/trusted-cluster-serving/scala/docker-graphene/run-docker-local-example.sh
+++ ppml/trusted-cluster-serving/scala/docker-graphene/run-docker-local-example.sh
@@ -1,6 +1,3 @@
-export KEYS_PATH=$1 #the_dir_path_of_your_prepared_keys
-export SECURE_PASSWORD_PATH=$2 #the_dir_path_of_your_prepared_password
-export LOCAL_IP=$3 #your_local_ip_of_the_sgx_server
 sudo docker run -itd \
     --privileged \
     --net=host \
