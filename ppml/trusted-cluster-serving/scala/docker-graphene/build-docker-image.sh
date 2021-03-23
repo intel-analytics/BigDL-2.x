86ddc3a786322a8706783f003b238746559b9890
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2021-03-23 14:01:07 +0800

    Update build-docker-image.sh

--- ppml/trusted-cluster-serving/scala/docker-graphene/build-docker-image.sh
+++ ppml/trusted-cluster-serving/scala/docker-graphene/build-docker-image.sh
@@ -1,3 +1,9 @@
+export HTTP_PROXY_HOST=your_http_proxy_host
+export HTTP_PROXY_PORT=your_http_proxy_port
+export HTTPS_PROXY_HOST=your_https_proxy_host
+export HTTPS_PROXY_PORT=your_https_proxy_port
+export JDK_URL=http://your-http-url-to-download-jdk
+
 sudo docker build \
     --build-arg http_proxy=http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT \
     --build-arg https_proxy=http://$HTTPS_PROXY_HOST:$HTTPS_PROXY_PORT \
