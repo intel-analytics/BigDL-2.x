9136fd106d4e2c018db33d61c5eca1bec472df9f
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2021-03-24 11:02:44 +0800

    Delete generate-password.sh

--- ppml/generate-password.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir password && cd password
-export YOUR_PASSWORD=$1 #used_password_in_generate-keys.sh
-openssl genrsa -out key.txt 2048
-echo $YOUR_PASSWORD | openssl rsautl -inkey key.txt -encrypt >output.bin
