735f433a9ee18b24641ecc52c1e75b1696e9b7c1
theaperdeng <theaperdeng@outlook.com>
2021-03-30 16:55:23 +0800

    metric param fix typo

--- pyzoo/zoo/zouwu/model/forecast/seq2seq_forecaster.py
+++ pyzoo/zoo/zouwu/model/forecast/seq2seq_forecaster.py
@@ -84,7 +84,7 @@ class Seq2SeqForecaster(Forecaster):
     def evaluate(self, x, y, metrics=['mse']):
         if not self.internal.model_built:
             raise RuntimeError("You must call fit or restore first before calling evaluate!")
-        return self.internal.evaluate(x, y, metric=metrics)
+        return self.internal.evaluate(x, y, metrics=metrics)
     
     def save(self, checkpoint_file):
         if not self.internal.model_built:
