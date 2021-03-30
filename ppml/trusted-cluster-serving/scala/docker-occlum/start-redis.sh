bb02e34b10dbf8b4ebd801ad7deb71dd466c60af
Qiyuan Gong <qiyuan.gong@intel.com>
2021-03-30 18:48:18 +0800

    Refine redis and fix cp

--- ppml/trusted-cluster-serving/scala/docker-occlum/start-redis.sh
+++ ppml/trusted-cluster-serving/scala/docker-occlum/start-redis.sh
@@ -4,8 +4,5 @@
 echo "### Launching Redis ###"
 
 cd /opt/redis
-./redis-server --tls-port $REDIS_PORT --port 0 \
-    --tls-cert-file /opt/keys/server.crt \
-    --tls-key-file /opt/keys/server.key \
-    --tls-ca-cert-file /opt/keys/server.crt \
+./redis-server -p $REDIS_PORT \
     --protected-mode no --maxmemory 10g | tee ./redis-sgx.log
