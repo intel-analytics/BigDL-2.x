791cc543ce73c3e6ac643fa33797a85b61fe5a03
yaxinchen666 <leepace666666@sjtu.edu.cn>
2021-04-01 14:07:23 +0800

    raise error when backend is not supported

--- pyzoo/zoo/examples/orca/learn/pytorch/cifar10/cifar10.py
+++ pyzoo/zoo/examples/orca/learn/pytorch/cifar10/cifar10.py
@@ -165,6 +165,6 @@ elif args.backend == "torch_distributed":
     for r in res:
         print(r, ":", res[r])
 else:
-    print("Only bigdl and torch_distributed are supported as the backend, but got {}".format(args.backend))
+    raise NotImplementedError("Only bigdl and torch_distributed are supported as the backend, but got {}".format(args.backend))
 
 stop_orca_context()
