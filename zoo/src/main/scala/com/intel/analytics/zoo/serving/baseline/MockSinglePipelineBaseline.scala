045fc24106208f5d8ae122f716206cbca98ce28a
Song Jiaming <litchy233@gmail.com>
2021-04-07 14:48:40 +0800

    Revert "Cluster Serving add batch inference (#3732)" (#3734)
    This reverts commit f03c71d0ff0caf4b90fc4c9de373659c5d161900.

--- zoo/src/main/scala/com/intel/analytics/zoo/serving/baseline/MockSinglePipelineBaseline.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/serving/baseline/MockSinglePipelineBaseline.scala
@@ -80,7 +80,7 @@ object MockSinglePipelineBaseline extends Supportive {
 
     val model = helper.loadInferenceModel()
     val warmT = makeTensorFromShape(param.inputShape)
-    val clusterServingInference = new ClusterServingInference(null, helper)
+    val clusterServingInference = new ClusterServingInference(null, "openvino")
     clusterServingInference.typeCheck(warmT)
     clusterServingInference.dimCheck(warmT, "add", helper.modelType)
     (0 until 10).foreach(_ => {
