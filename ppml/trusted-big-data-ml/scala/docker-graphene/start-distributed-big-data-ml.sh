76566bc8594edec5a454c04bdeab311a0f5d23a3
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2021-04-08 14:33:32 +0800

    Update start-distributed-big-data-ml.sh

--- ppml/trusted-big-data-ml/scala/docker-graphene/start-distributed-big-data-ml.sh
+++ ppml/trusted-big-data-ml/scala/docker-graphene/start-distributed-big-data-ml.sh
@@ -6,14 +6,14 @@ source ./environment.sh
 
 echo "### phase.1 distribute the keys and password and data"
 echo ">>> $MASTER"
-ssh root@$MASTER "rm -rf $KEYS_PATH && rm -rf $SECURE_PASSWORD_PATH && rm -rf $DATA_PATH && mkdir -p $AZ_PPML_PATH"
+sshpass -p 'intel@123' ssh root@$MASTER "rm -rf $KEYS_PATH && rm -rf $SECURE_PASSWORD_PATH && rm -rf $DATA_PATH && mkdir -p $AZ_PPML_PATH"
 scp -r $SOURCE_KEYS_PATH root@$MASTER:$KEYS_PATH
 scp -r $SOURCE_SECURE_PASSWORD_PATH root@$MASTER:$SECURE_PASSWORD_PATH
 scp -r $SOURCE_DATA_PATH root@$MASTER:$DATA_PATH
 for worker in ${WORKERS[@]}
   do
     echo ">>> $worker"
-    ssh root@$worker "rm -rf $KEYS_PATH && rm -rf $SECURE_PASSWORD_PATH && rm -rf $DATA_PATH && mkdir -p $AZ_PPML_PATH"
+    sshpass -p 'intel@123' ssh root@$worker "rm -rf $KEYS_PATH && rm -rf $SECURE_PASSWORD_PATH && rm -rf $DATA_PATH && mkdir -p $AZ_PPML_PATH"
     scp -r $SOURCE_KEYS_PATH root@$worker:$KEYS_PATH
     scp -r $SOURCE_SECURE_PASSWORD_PATH root@$worker:$SECURE_PASSWORD_PATH
     scp -r $SOURCE_DATA_PATH root@$worker:$DATA_PATH
@@ -22,17 +22,17 @@ echo "### phase.1 distribute the keys and password finished successfully"
 
 echo "### phase.2 pull the docker image"
 echo ">>> $MASTER"
-ssh root@$MASTER "docker pull $TRUSTED_BIGDATA_ML_DOCKER"
+sshpass -p 'intel@123' ssh root@$MASTER "docker pull $TRUSTED_BIGDATA_ML_DOCKER"
 for worker in ${WORKERS[@]}
   do
     echo ">>> $worker"
-    ssh root@$worker "docker pull $TRUSTED_BIGDATA_ML_DOCKER"
+    sshpass -p 'intel@123' ssh root@$worker "docker pull $TRUSTED_BIGDATA_ML_DOCKER"
   done
 echo "### phase.2 pull the docker image finished successfully"
 
 echo "### phase.3 deploy the spark components"
 echo ">>> $MASTER, start spark master"
-ssh root@$MASTER "docker run -itd \
+sshpass -p 'intel@123' ssh root@$MASTER "docker run -itd \
       --privileged \
       --net=host \
       --cpuset-cpus="0-1" \
@@ -42,7 +42,7 @@ ssh root@$MASTER "docker run -itd \
       --device=/dev/sgx/provision \
       -v /var/run/aesmd/aesm.socket:/var/run/aesmd/aesm.socket \
       -v $KEYS_PATH:/ppml/trusted-big-data-ml/work/keys \
-      -v $SECURE_PASSWORD_PATH:/ppml/trusted-big-data-ml/work/passowrd \
+      -v $SECURE_PASSWORD_PATH:/ppml/trusted-big-data-ml/work/password \
       --name=spark-master \
       -e LOCAL_IP=$MASTER \
       -e SGX_MEM_SIZE=16G \
@@ -50,7 +50,7 @@ ssh root@$MASTER "docker run -itd \
       -e SPARK_MASTER_PORT=7077 \
       -e SPARK_MASTER_WEBUI_PORT=8080 \
       $TRUSTED_BIGDATA_ML_DOCKER bash -c 'cd /ppml/trusted-big-data-ml && ./init.sh && ./start-spark-standalone-master-sgx.sh'"
-while ! ssh root@$MASTER "nc -z $MASTER 8080"; do
+while ! sshpass -p 'intel@123' ssh root@$MASTER "nc -z $MASTER 8080"; do
   sleep 10
 done
 echo ">>> $MASTER, redis started successfully."
@@ -58,17 +58,17 @@ echo ">>> $MASTER, redis started successfully."
 for worker in ${WORKERS[@]}
   do
     echo ">>> $worker"
-    ssh root@$worker "docker run -itd \
+    sshpass -p 'intel@123' ssh root@$worker "docker run -itd \
           --privileged \
           --net=host \
-          --cpuset-cpus="10-30" \
+          --cpuset-cpus="6-10" \
           --oom-kill-disable \
           --device=/dev/gsgx \
           --device=/dev/sgx/enclave \
           --device=/dev/sgx/provision \
           -v /var/run/aesmd/aesm.socket:/var/run/aesmd/aesm.socket \
           -v $KEYS_PATH:/ppml/trusted-big-data-ml/work/keys \
-          -v $SECURE_PASSWORD_PATH:/ppml/trusted-big-data-ml/work/passowrd \
+          -v $SECURE_PASSWORD_PATH:/ppml/trusted-big-data-ml/work/password \
           --name=spark-worker-$worker \
           -e LOCAL_IP=$worker \
           -e SGX_MEM_SIZE=64G \
@@ -80,14 +80,14 @@ for worker in ${WORKERS[@]}
 
 for worker in ${WORKERS[@]}
   do
-    while ! ssh root@$worker "nc -z $worker 8081"; do
+    while ! sshpass -p 'intel@123' ssh root@$worker "nc -z $worker 8081"; do
       sleep 10
     done
     echo ">>> $worker, spark-worker-$worker started successfully."
   done
 
 echo ">>> $MASTER, start spark-driver"
-ssh root@$MASTER "docker run -itd \
+sshpass -p 'intel@123' ssh root@$MASTER "docker run -itd \
       --privileged \
       --net=host \
       --cpuset-cpus="2-5" \
@@ -98,7 +98,7 @@ ssh root@$MASTER "docker run -itd \
       -v /var/run/aesmd/aesm.socket:/var/run/aesmd/aesm.socket \
       -v $DATA_PATH:/ppml/trusted-big-data-ml/work/data \
       -v $KEYS_PATH:/ppml/trusted-big-data-ml/work/keys \
-      -v $SECURE_PASSWORD_PATH:/ppml/trusted-big-data-ml/work/passowrd \
+      -v $SECURE_PASSWORD_PATH:/ppml/trusted-big-data-ml/work/password \
       --name=spark-driver \
       -e LOCAL_IP=$MASTER \
       -e SGX_MEM_SIZE=32G \
@@ -106,7 +106,7 @@ ssh root@$MASTER "docker run -itd \
       -e SPARK_DRIVER_PORT=10027 \
       -e SPARK_DRIVER_BLOCK_MANAGER_PORT=10026 \
       $TRUSTED_BIGDATA_ML_DOCKER bash -c 'cd /ppml/trusted-big-data-ml && ./init.sh && ./start-spark-standalone-driver-sgx.sh'"
-while ! ssh root@$MASTER "docker logs spark-driver | grep 'model saved'"; do
+while ! sshpass -p 'intel@123' ssh root@$MASTER "docker logs spark-driver | grep 'model saved'"; do
   sleep 10
 done
 echo ">>> $MASTER, cluster-serving started successfully."
