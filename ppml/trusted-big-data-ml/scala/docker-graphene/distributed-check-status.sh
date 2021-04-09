af00954876af0d782dbfd7641f58e5cfc61c6184
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2021-04-09 10:16:48 +0800

    delete driver check

--- ppml/trusted-big-data-ml/scala/docker-graphene/distributed-check-status.sh
+++ ppml/trusted-big-data-ml/scala/docker-graphene/distributed-check-status.sh
@@ -19,16 +19,15 @@ else
 fi
 
 
-if [ "$#" -gt 5 ]; then
-    echo "Acceptable arguments: \"all\", or one or more among \"master\", \"worker\", \"driver\"."
-elif [ "$all" -eq 1 ]; then 
+if [ "$#" -gt 2 ]; then
+    echo "Acceptable arguments: \"all\", or one or more among \"master\", \"worker\"."
+elif [ "$all" -eq 1 ]; then
     ssh root@$MASTER "docker exec spark-master bash /ppml/trusted-big-data-ml/check-status.sh master"
     for worker in ${WORKERS[@]}
     do
         ssh root@$worker "docker exec spark-worker-$worker bash /ppml/trusted-big-data-ml/check-status.sh worker"
     done
-    ssh root@$MASTER "docker exec spark-driver bash /ppml/trusted-big-data-ml/check-status.sh driver"
-else 
+else
     for arg in "$@"
     do
         if [ "$arg" == master ]; then
@@ -38,10 +37,8 @@ else
             do
                 ssh root@$worker "docker exec spark-worker-$worker bash /ppml/trusted-big-data-ml/check-status.sh worker"
             done
-        elif [ "$arg" == driver ]; then
-            ssh root@$MASTER "docker exec spark-driver bash /ppml/trusted-big-data-ml/check-status.sh driver"
-        else 
-            echo "Acceptable arguments: \"all\", or one or more among \"master\", \"worker\", \"driver\"."
+        else
+            echo "Acceptable arguments: \"all\", or one or more among \"master\", \"worker\"."
         fi
     done
 fi
