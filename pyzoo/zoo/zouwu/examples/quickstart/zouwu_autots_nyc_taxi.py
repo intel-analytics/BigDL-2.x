22013858105c27146d192f193794014d55ded92d
PeterQiu0516 <peterqiu0516@gmail.com>
2021-04-15 11:27:38 +0800

    Add License

--- pyzoo/zoo/zouwu/examples/quickstart/zouwu_autots_nyc_taxi.py
+++ pyzoo/zoo/zouwu/examples/quickstart/zouwu_autots_nyc_taxi.py
@@ -1,3 +1,18 @@
+#
+# Copyright 2018 Analytics Zoo Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 import pandas as pd
 from zoo.zouwu.preprocessing.utils import train_val_test_split
 from zoo.orca import init_orca_context, stop_orca_context
@@ -8,6 +23,7 @@ from zoo.zouwu.autots.forecast import TSPipeline
 import os
 import argparse
 import tempfile
+
 
 os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
 
