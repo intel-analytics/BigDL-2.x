f490fcf118896352965024bbf4a994b07d486a4b
Yu Shan <shan.yu@intel.com>
2021-04-15 20:02:47 +0800

    refactor upload store

--- pyzoo/zoo/orca/data/ray_xshards.py
+++ pyzoo/zoo/orca/data/ray_xshards.py
@@ -30,42 +30,38 @@ logger = logging.getLogger(__name__)
 class LocalStore:
 
     def __init__(self):
-        self.shards = {}
-        self.shard_count = {}
         self.partitions = {}
 
-    def upload_shards(self, shard_id, shard):
-        partition_idx, shard_idx = shard_id
-        self.shards[shard_id] = shard
-        if partition_idx in self.shard_count:
-            self.shard_count[partition_idx] += 1
-        else:
-            self.shard_count[partition_idx] = 1
+    def upload_shards(self, part_shard_id, shard):
+        partition_idx, shard_idx = part_shard_id
+        if partition_idx not in self.partitions:
+            self.partitions[partition_idx] = {}
+        self.partitions[partition_idx][shard_idx] = shard
         return 0
 
     def upload_partition(self, partition_id, partition):
         self.partitions[partition_id] = partition
 
-    def get_shards(self, shard_id):
-        return self.shards[shard_id]
+    def get_shards(self, part_shard_id):
+        partition_idx, shard_idx = part_shard_id
+        return self.partitions[part_shard_id][shard_idx]
 
     def get_partition(self, partition_id):
-        if not self.partitions:
+        part = self.partitions[partition_id]
+        if isinstance(part, dict):
             partition = []
-            for shard_idx in range(self.shard_count[partition_id]):
-                shard = self.shards[(partition_id, shard_idx)]
+            for shard_idx in range(len(part)):
+                shard = part[shard_idx]
                 partition.append(shard)
             return partition
         else:
-            return self.partitions[partition_id]
+            return part
 
     def get_partitions(self):
-        if not self.partitions:
-            result = {partition_id: self.get_partition(partition_id)
-                      for partition_id in self.shard_count.keys()}
-            return result
-        else:
-            return self.partitions
+        result = {}
+        for k in self.partitions.keys():
+            result[k] = self.get_partition(k)
+        return result
 
 
 def write_to_ray(idx, partition, redis_address, redis_password, partition_store_names):
@@ -91,8 +87,8 @@ def write_to_ray(idx, partition, redis_address, redis_password, partition_store_
         result.append(local_store.upload_shards.remote((idx, shard_id), shard_ref))
     is_empty = len(result) == 0
     if is_empty:
-        shard_ref = ray.put([])
-        result.append(local_store.upload_shards.remote((idx, 0), shard_ref))
+        partition_ref = ray.put([])
+        result.append(local_store.upload_partition.remote(idx, partition_ref))
     ray.get(result)
 
     return [(idx, local_store_name.split(":")[-1], local_store_name, is_empty)]
