3b2b283a8a8795f7181112a4961195e9bc8d59cd
Yu Shan <shan.yu@intel.com>
2021-04-20 23:43:14 +0800

    add default values

--- pyzoo/zoo/orca/automl/auto_estimator.py
+++ pyzoo/zoo/orca/automl/auto_estimator.py
@@ -18,7 +18,11 @@ from zoo.automl.search import SearchEngineFactory
 
 
 class AutoEstimator:
-    def __init__(self, model_builder, logs_dir, resources_per_trial, name):
+    def __init__(self,
+                 model_builder,
+                 logs_dir="/tmp/auto_estimator_logs",
+                 resources_per_trial=None,
+                 name=None):
         self.model_builder = model_builder
         self.searcher = SearchEngineFactory.create_engine(backend="ray",
                                                           logs_dir=logs_dir,
@@ -31,9 +35,9 @@ class AutoEstimator:
                    model_creator,
                    optimizer,
                    loss,
-                   logs_dir,
-                   resources_per_trial,
-                   name,
+                   logs_dir="/tmp/auto_estimator_logs",
+                   resources_per_trial=None,
+                   name=None,
                    ):
         """
         Create an AutoEstimator for torch.
@@ -46,9 +50,11 @@ class AutoEstimator:
             will be used for all estimators.
         :param loss: PyTorch loss instance or PyTorch loss creator function
             or pytorch loss name (string).
-        :param logs_dir: Local directory to save logs and results.
+        :param logs_dir: Local directory to save logs and results. It defaults to
+            "/tmp/auto_estimator_logs"
         :param resources_per_trial: Dict. resources for each trial. e.g. {"cpu": 2}.
         :param name: Name of the auto estimator.
+
         :return: an AutoEstimator object.
         """
         from zoo.orca.automl.pytorch_utils import validate_pytorch_loss, \
@@ -68,17 +74,19 @@ class AutoEstimator:
     @staticmethod
     def from_keras(*,
                    model_creator,
-                   logs_dir,
-                   resources_per_trial,
-                   name,
+                   logs_dir="/tmp/auto_estimator_logs",
+                   resources_per_trial=None,
+                   name=None,
                    ):
         """
         Create an AutoEstimator for tensorflow keras.
 
         :param model_creator: Tensorflow keras model creator function.
-        :param logs_dir: Local directory to save logs and results.
+        :param logs_dir: Local directory to save logs and results. It defaults to
+            "/tmp/auto_estimator_logs"
         :param resources_per_trial: Dict. resources for each trial. e.g. {"cpu": 2}.
         :param name: Name of the auto estimator.
+
         :return: an AutoEstimator object.
         """
         from zoo.automl.model import KerasModelBuilder
