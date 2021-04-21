b71bf26eb0a047c6ac6f659c123cfc2099f5688d
Yu Shan <shan.yu@intel.com>
2021-04-21 15:16:48 +0800

    Move create searcher to auto estimator constructor  (#3829)
    * move create searcher in auto estimator constructor
    
    * add default values

--- pyzoo/zoo/orca/automl/auto_estimator.py
+++ pyzoo/zoo/orca/automl/auto_estimator.py
@@ -14,11 +14,20 @@
 # limitations under the License.
 #
 
+from zoo.automl.search import SearchEngineFactory
+
 
 class AutoEstimator:
-    def __init__(self, model_builder, searcher):
+    def __init__(self,
+                 model_builder,
+                 logs_dir="/tmp/auto_estimator_logs",
+                 resources_per_trial=None,
+                 name=None):
         self.model_builder = model_builder
-        self.searcher = searcher
+        self.searcher = SearchEngineFactory.create_engine(backend="ray",
+                                                          logs_dir=logs_dir,
+                                                          resources_per_trial=resources_per_trial,
+                                                          name=name)
         self._fitted = False
 
     @staticmethod
@@ -26,9 +35,9 @@ class AutoEstimator:
                    model_creator,
                    optimizer,
                    loss,
-                   logs_dir,
-                   resources_per_trial,
-                   name,
+                   logs_dir="/tmp/auto_estimator_logs",
+                   resources_per_trial=None,
+                   name=None,
                    ):
         """
         Create an AutoEstimator for torch.
@@ -41,50 +50,51 @@ class AutoEstimator:
             will be used for all estimators.
         :param loss: PyTorch loss instance or PyTorch loss creator function
             or pytorch loss name (string).
-        :param logs_dir: Local directory to save logs and results.
+        :param logs_dir: Local directory to save logs and results. It defaults to
+            "/tmp/auto_estimator_logs"
         :param resources_per_trial: Dict. resources for each trial. e.g. {"cpu": 2}.
         :param name: Name of the auto estimator.
+
         :return: an AutoEstimator object.
         """
         from zoo.orca.automl.pytorch_utils import validate_pytorch_loss, \
             validate_pytorch_optim
         from zoo.automl.model import PytorchModelBuilder
-        from zoo.automl.search import SearchEngineFactory
         loss = validate_pytorch_loss(loss)
         optimizer = validate_pytorch_optim(optimizer)
         model_builder = PytorchModelBuilder(model_creator=model_creator,
                                             optimizer_creator=optimizer,
                                             loss_creator=loss)
-        searcher = SearchEngineFactory.create_engine(backend="ray",
-                                                     logs_dir=logs_dir,
-                                                     resources_per_trial=resources_per_trial,
-                                                     name=name)
-        return AutoEstimator(model_builder=model_builder, searcher=searcher)
+
+        return AutoEstimator(model_builder=model_builder,
+                             logs_dir=logs_dir,
+                             resources_per_trial=resources_per_trial,
+                             name=name)
 
     @staticmethod
     def from_keras(*,
                    model_creator,
-                   logs_dir,
-                   resources_per_trial,
-                   name,
+                   logs_dir="/tmp/auto_estimator_logs",
+                   resources_per_trial=None,
+                   name=None,
                    ):
         """
         Create an AutoEstimator for tensorflow keras.
 
         :param model_creator: Tensorflow keras model creator function.
-        :param logs_dir: Local directory to save logs and results.
+        :param logs_dir: Local directory to save logs and results. It defaults to
+            "/tmp/auto_estimator_logs"
         :param resources_per_trial: Dict. resources for each trial. e.g. {"cpu": 2}.
         :param name: Name of the auto estimator.
+
         :return: an AutoEstimator object.
         """
         from zoo.automl.model import KerasModelBuilder
-        from zoo.automl.search import SearchEngineFactory
         model_builder = KerasModelBuilder(model_creator=model_creator)
-        searcher = SearchEngineFactory.create_engine(backend="ray",
-                                                     logs_dir=logs_dir,
-                                                     resources_per_trial=resources_per_trial,
-                                                     name=name)
-        return AutoEstimator(model_builder=model_builder, searcher=searcher)
+        return AutoEstimator(model_builder=model_builder,
+                             logs_dir=logs_dir,
+                             resources_per_trial=resources_per_trial,
+                             name=name)
 
     def fit(self,
             data,
