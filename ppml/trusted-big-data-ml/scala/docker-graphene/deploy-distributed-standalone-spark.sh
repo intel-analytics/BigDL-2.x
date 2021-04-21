f6dc1f485856f824bfbba10d2f75d017b1285409
colossuschang <colossuschang@sjtu.edu.cn>
2021-04-21 16:13:53 +0800

    Fixed spark-driver bugs

--- ppml/trusted-big-data-ml/scala/docker-graphene/deploy-distributed-standalone-spark.sh
+++ ppml/trusted-big-data-ml/scala/docker-graphene/deploy-distributed-standalone-spark.sh
@@ -53,6 +53,9 @@ ssh root@$MASTER "docker run -itd \
       -e SPARK_MASTER_PORT=7077 \
       -e SPARK_MASTER_WEBUI_PORT=8080 \
       $TRUSTED_BIGDATA_ML_DOCKER bash -c 'cd /ppml/trusted-big-data-ml && ./init.sh && ./start-spark-standalone-master-sgx.sh'"
+while ! ssh root@$MASTER "nc -z $MASTER 8080"; do
+  sleep 10
+done
 
 for worker in ${WORKERS[@]}
   do
@@ -78,4 +81,12 @@ for worker in ${WORKERS[@]}
           $TRUSTED_BIGDATA_ML_DOCKER bash -c 'cd /ppml/trusted-big-data-ml && ./init.sh && ./start-spark-standalone-worker-sgx.sh'"
   done
 
+for worker in ${WORKERS[@]}
+  do
+    while ! ssh root@$worker "nc -z $worker 8081"; do
+      sleep 10
+    done
+  done
+
 ./distributed-check-status.sh
+
