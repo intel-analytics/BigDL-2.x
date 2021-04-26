baf7dd02b4706d84d70727384722871af356d439
theaperdeng <theaperdeng@outlook.com>
2021-04-26 14:30:56 +0800

    change logger as review

--- pyzoo/zoo/automl/search/ray_tune_search_engine.py
+++ pyzoo/zoo/automl/search/ray_tune_search_engine.py
@@ -260,7 +260,9 @@ class RayTuneSearchEngine(SearchEngine):
         # catch the ImportError Since it has been processed in TensorboardXLogger
         tf_config, tf_metric = self._log_adapt(analysis)
 
-        self.logger = TensorboardXLogger(os.path.join(self.logs_dir, self.name+"_leaderboard"))
+        self.logger = TensorboardXLogger(logs_dir=os.path.join(self.logs_dir,
+                                                               self.name+"_leaderboard"),
+                                         name=self.name)
         self.logger.run(tf_config, tf_metric)
         self.logger.close()
 
