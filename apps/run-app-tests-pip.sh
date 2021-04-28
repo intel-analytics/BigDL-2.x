2a6e3647afcb2205d1485e0501250058b898b04e
Yu Shan <shan.yu@intel.com>
2021-04-28 15:12:10 +0800

    add download data in app test

--- apps/run-app-tests-pip.sh
+++ apps/run-app-tests-pip.sh
@@ -794,6 +794,18 @@ ${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/../pyzoo/zoo/zouwu/
 sed -i '/get_ipython()/d; /plot./d; /plt./d' ${ANALYTICS_ZOO_HOME}/../pyzoo/zoo/zouwu/use-case/fsi/stock_prediction.py
 cd ${ANALYTICS_ZOO_HOME}/../pyzoo/zoo/zouwu/use-case/fsi/
 
+# download data
+if ! [ -d "data" ];
+then
+    mkdir data
+    cd data
+    wget https://github.com/CNuge/kaggle-code/raw/master/stock_data/individual_stocks_5yr.zip
+    wget https://raw.githubusercontent.com/CNuge/kaggle-code/master/stock_data/merge.sh
+    chmod +x merge.sh
+    unzip individual_stocks_5yr.zip
+    ./merge.sh
+fi
+
 python ${ANALYTICS_ZOO_HOME}/../pyzoo/zoo/zouwu/use-case/fsi/stock_prediction.py
 cd -
 
