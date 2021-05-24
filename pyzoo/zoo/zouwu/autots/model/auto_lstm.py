edb5fab0c0b33d70526a6fd3abf5830983a84faa
Yu Shan <shan.yu@intel.com>
2021-05-25 02:13:32 +0800

    add doc

--- pyzoo/zoo/zouwu/autots/model/auto_lstm.py
+++ pyzoo/zoo/zouwu/autots/model/auto_lstm.py
@@ -40,18 +40,18 @@ class AutoLSTM:
         :param input_feature_num: Int. The number of features in the input
         :param output_target_num: Int. The number of targets in the output
         :param optimizer: String or pyTorch optimizer creator function or
-            tf.keras optimizer instance.
+               tf.keras optimizer instance.
         :param loss: String or pytorch/tf.keras loss instance or pytorch loss creator function.
         :param metric: String. The evaluation metric name to optimize. e.g. "mse"
         :param hidden_dim: Int or hp sampling function from an integer space. The number of features
-            in the hidden state `h`. For hp sampling, see zoo.zouwu.orca.automl.hp for more details.
-            e.g. hp.grid_search([32, 64]).
+               in the hidden state `h`. For hp sampling, see zoo.zouwu.orca.automl.hp for more
+               details. e.g. hp.grid_search([32, 64]).
         :param layer_num: Int or hp sampling function from an integer space. Number of recurrent
-            layers. e.g. hp.randint(1, 3)
+               layers. e.g. hp.randint(1, 3)
         :param lr: float or hp sampling function from a float space. Learning rate.
-            e.g. hp.choice([0.001, 0.003, 0.01])
+               e.g. hp.choice([0.001, 0.003, 0.01])
         :param dropout: float or hp sampling function from a float space. Learning rate. Dropout
-            rate. e.g. hp.uniform(0.1, 0.3)
+               rate. e.g. hp.uniform(0.1, 0.3)
         :param backend: The backend of the lstm model. We only support backend as "torch" for now.
         :param logs_dir: Local directory to save logs and results. It defaults to "/tmp/auto_lstm"
         :param cpus_per_trial: Int. Number of cpus for each trial. It defaults to 1.
@@ -90,6 +90,37 @@ class AutoLSTM:
             scheduler=None,
             scheduler_params=None,
             ):
+        """
+        Automatically fit the model and search for the best hyper parameters.
+
+        :param data: train data.
+               For backend of "torch", data can be a tuple of ndarrays or a function that takes a
+               config dictionary as parameter and returns a PyTorch DataLoader.
+               For backend of "keras", data can be a tuple of ndarrays.
+               If data is a tuple of ndarrays, it should be in the form of (x, y),
+                where x is training input data and y is training target data.
+        :param epochs: Max number of epochs to train in each trial. Defaults to 1.
+               If you have also set metric_threshold, a trial will stop if either it has been
+               optimized to the metric_threshold or it has been trained for {epochs} epochs.
+        :param batch_size: Int or hp sampling function from an integer space. Training batch size.
+               It defaults to 32.
+        :param validation_data: Validation data. Validation data type should be the same as data.
+        :param metric_threshold: a trial will be terminated when metric threshold is met
+        :param n_sampling: Number of times to sample from the search_space. Defaults to 1.
+               If hp.grid_search is in search_space, the grid will be repeated n_sampling of times.
+               If this is -1, (virtually) infinite samples are generated
+               until a stopping condition is met.
+        :param search_alg: str, all supported searcher provided by ray tune
+               (i.e."variant_generator", "random", "ax", "dragonfly", "skopt",
+               "hyperopt", "bayesopt", "bohb", "nevergrad", "optuna", "zoopt" and
+               "sigopt")
+        :param search_alg_params: extra parameters for searcher algorithm besides search_space,
+               metric and searcher mode
+        :param scheduler: str, all supported scheduler provided by ray tune
+        :param scheduler_params: parameters for scheduler
+
+        :return:
+        """
         self.search_space["batch_size"] = batch_size
         self.auto_est.fit(
             data=data,
@@ -106,4 +137,7 @@ class AutoLSTM:
         )
 
     def get_best_model(self):
+        """
+        Get the best lstm model.
+        """
         return self.auto_est.get_best_model()
