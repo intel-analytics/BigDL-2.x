2dd8287a0b581d0be81fa69d6e6e9dc8f8e0e7fc
glorysdj <glorysdj@gmail.com>
2021-05-25 18:30:48 -0700

    update makefile and init

--- ppml/trusted-big-data-ml/python/docker-graphene/init.sh
+++ ppml/trusted-big-data-ml/python/docker-graphene/init.sh
@@ -2,6 +2,7 @@
 
 set -x
 
+local_ip=$LOCAL_IP
 sgx_mem_size=$SGX_MEM_SIZE
 
-make SGX=1 GRAPHENEDIR=/graphene THIS_DIR=/ppml/trusted-big-data-ml G_SGX_SIZE=$sgx_mem_size
+make SGX=1 GRAPHENEDIR=/graphene THIS_DIR=/ppml/trusted-big-data-ml  SPARK_LOCAL_IP=$local_ip SPARK_USER=root G_SGX_SIZE=$sgx_mem_size
