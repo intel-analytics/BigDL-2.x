d8835fab9c9d172ff71668b0169b66c6859dc9a1
gaoping <pingx.gao@intel.com>
2021-06-08 07:50:58 +0000

    add release mac script

---
+++ pyzoo/dev/release_mac.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+#
+# Copyright 2018 Analytics Zoo Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This is the default script with maven parameters to release analytics-zoo for mac.
+# Note that if the maven parameters to build analytics-zoo need to be changed,
+# make sure to change this file accordingly.
+# If you want to customize the release, please use release.sh and specify maven parameters instead.
+
+set -e
+RUN_SCRIPT_DIR=$(cd $(dirname $0) ; pwd)
+echo $RUN_SCRIPT_DIR
+
+if (( $# < 1)); then
+  echo "Usage: release_mac.sh version spark_version bigdl_artifactId spark_profile"
+  echo "Usage example: bash release_mac.sh default spark_version bigdl_artifactId spark_profile"
+  echo "Usage example: bash release__mac.sh 0.6.0.dev0 spark_version bigdl_artifactId spark_profile"
+  exit -1
+fi
+
+version=$1
+spark_version=$2
+bigdl_artifactId=$3
+spark_profile=$4
+
+
+bash ${RUN_SCRIPT_DIR}/release.sh mac ${version} false true -Dspark.version=${spark_version} -Dbigdl.artifactId=${bigdl_artifactId} -P ${spark_profile}
