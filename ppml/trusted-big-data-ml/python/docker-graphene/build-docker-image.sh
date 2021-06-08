1655d7c839b25d284d6172d17a97dfb467388f7b
glorysdj <glorysdj@gmail.com>
2021-06-07 19:46:41 -0700

    rm examples

--- ppml/trusted-big-data-ml/python/docker-graphene/build-docker-image.sh
+++ ppml/trusted-big-data-ml/python/docker-graphene/build-docker-image.sh
@@ -1,14 +1,8 @@
-#export HTTP_PROXY_HOST=your_http_proxy_host
-#export HTTP_PROXY_PORT=your_http_proxy_port
-#export HTTPS_PROXY_HOST=your_https_proxy_host
-#export HTTPS_PROXY_PORT=your_https_proxy_port
-#export JDK_URL=http://your-http-url-to-download-jdk
-
-export HTTP_PROXY_HOST=proxy-chain.intel.com
-export HTTP_PROXY_PORT=911
-export HTTPS_PROXY_HOST=proxy-chain.intel.com
-export HTTPS_PROXY_PORT=912
-export JDK_URL=http://10.239.45.10:8081/repository/raw/jdk/jdk-8u192-linux-x64.tar.gz
+export HTTP_PROXY_HOST=your_http_proxy_host
+export HTTP_PROXY_PORT=your_http_proxy_port
+export HTTPS_PROXY_HOST=your_https_proxy_host
+export HTTPS_PROXY_PORT=your_https_proxy_port
+export JDK_URL=http://your-http-url-to-download-jdk
 
 sudo docker build \
     --build-arg http_proxy=http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT \
@@ -19,9 +13,5 @@ sudo docker build \
     --build-arg HTTPS_PROXY_PORT=$HTTPS_PROXY_PORT \
     --build-arg JDK_VERSION=8u192 \
     --build-arg JDK_URL=$JDK_URL \
-    --build-arg no_proxy=10.239.45.10 \
+    --build-arg no_proxy=x.x.x.x \
     -t intelanalytics/analytics-zoo-ppml-trusted-big-data-ml-python-graphene:0.11-SNAPSHOT -f ./Dockerfile .
-
-sudo docker tag intelanalytics/analytics-zoo-ppml-trusted-big-data-ml-python-graphene:0.11-SNAPSHOT \
-    10.239.45.10/arda/intelanalytics/analytics-zoo-ppml-trusted-big-data-ml-python-graphene:0.11-SNAPSHOT
-sudo docker push 10.239.45.10/arda/intelanalytics/analytics-zoo-ppml-trusted-big-data-ml-python-graphene:0.11-SNAPSHOT
--- ppml/trusted-big-data-ml/python/docker-graphene/examples/pytorch/download-pretrained-model.py
+++
@@ -1,8 +0,0 @@
-from torchvision import models
-import torch
-
-output_filename = "alexnet-pretrained.pt"
-alexnet = models.alexnet(pretrained=True)
-torch.save(alexnet, output_filename)
-
-print("Pre-trained model was saved in \"%s\"" % output_filename)
