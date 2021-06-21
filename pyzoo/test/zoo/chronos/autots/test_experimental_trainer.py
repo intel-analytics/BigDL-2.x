5604ae3cba888313d6bf877ae7ed32bf6c10f2fa
theaperdeng <theaperdeng@outlook.com>
2021-06-21 18:19:07 +0800

    add ut for tcn and data creator

--- pyzoo/test/zoo/chronos/autots/test_experimental_trainer.py
+++ pyzoo/test/zoo/chronos/autots/test_experimental_trainer.py
@@ -18,6 +18,7 @@ from unittest import TestCase
 import pytest
 
 import torch
+from torch.utils.data import TensorDataset, DataLoader
 import numpy as np
 from zoo.chronos.autots.experimental_trainer import AutoTSTrainer
 from zoo.chronos.data.tsdataset import TSDataset
@@ -43,6 +44,17 @@ def get_tsdataset():
                                  target_col=["value 1", "value 2"],
                                  extra_feature_col=["extra feature 1", "extra feature 2"],
                                  id_col="id")
+
+
+def get_data_creator():
+    def data_creator(config):
+        tsdata = get_tsdataset()
+        x, y = tsdata.roll(lookback=7, horizon=1).to_numpy()
+        return DataLoader(TensorDataset(torch.from_numpy(x).float(),
+                                        torch.from_numpy(y).float()),
+                            batch_size=config["batch_size"],
+                            shuffle=True)
+    return data_creator
 
 
 class TestAutoTrainer(TestCase):
@@ -83,7 +95,67 @@ class TestAutoTrainer(TestCase):
                          n_sampling=1
                          )
         config = auto_trainer.get_best_config()
+    
+    def test_fit_tcn_feature(self):
+        input_feature_dim = 11  # This param will not be used
+        output_feature_dim = 2  # 2 targets (i.e. "value 1", "value 2") is generated in get_tsdataset
 
+        search_space = {
+            'hidden_units': hp.grid_search([32, 64]),
+            'levels': hp.randint(4, 6),
+            'kernel_size': hp.randint(3, 5),
+            'dropout': hp.uniform(0.1, 0.2),
+            'lr': hp.loguniform(0.001, 0.01)
+        }
+        auto_trainer = AutoTSTrainer(model='tcn',
+                                     search_space=search_space,
+                                     past_seq_len=hp.randint(4, 6),
+                                     future_seq_len=1,
+                                     input_feature_num=input_feature_dim,
+                                     output_target_num=output_feature_dim,
+                                     selected_features="auto",
+                                     metric="mse",
+                                     loss=torch.nn.MSELoss(),
+                                     logs_dir="/tmp/auto_trainer",
+                                     cpus_per_trial=2,
+                                     name="auto_trainer")
+        auto_trainer.fit(data=get_tsdataset().gen_dt_feature(),
+                         epochs=1,
+                         batch_size=hp.choice([32, 64]),
+                         validation_data=get_tsdataset().gen_dt_feature(),
+                         n_sampling=1
+                         )
+        config = auto_trainer.get_best_config()
+
+    def test_fit_lstm_data_creator(self):
+        input_feature_dim = 4
+        output_feature_dim = 2  # 2 targets (i.e. "value 1", "value 2") is generated in get_tsdataset
+
+        search_space = {
+            'hidden_dim': hp.grid_search([32, 64]),
+            'layer_num': hp.randint(1, 3),
+            'lr': hp.choice([0.001, 0.003, 0.01]),
+            'dropout': hp.uniform(0.1, 0.2)
+        }
+        auto_trainer = AutoTSTrainer(model='lstm',
+                                     search_space=search_space,
+                                     past_seq_len=7,
+                                     future_seq_len=1,
+                                     input_feature_num=input_feature_dim,
+                                     output_target_num=output_feature_dim,
+                                     selected_features="auto",
+                                     metric="mse",
+                                     loss=torch.nn.MSELoss(),
+                                     logs_dir="/tmp/auto_trainer",
+                                     cpus_per_trial=2,
+                                     name="auto_trainer")
+        auto_trainer.fit(data=get_data_creator(),
+                         epochs=1,
+                         batch_size=hp.choice([32, 64]),
+                         validation_data=get_data_creator(),
+                         n_sampling=1
+                         )
+        config = auto_trainer.get_best_config()
 
 if __name__ == "__main__":
     pytest.main([__file__])
