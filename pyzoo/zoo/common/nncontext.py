1f3fddfdc90b33a58ce79ebe9ab86451efaa2883
Le-Zheng <alettazheng@hotmail.com>
2021-06-23 09:15:06 +0800

    enable default DistriOptimizer as V2

--- pyzoo/zoo/common/nncontext.py
+++ pyzoo/zoo/common/nncontext.py
@@ -266,6 +266,22 @@ def stop_spark_standalone():
     SparkRunner.stop_spark_standalone()
 
 
+def set_optimizer_version(optimizerVersion, bigdl_type="float"):
+    """
+    Set DistriOptimizer version.
+    param optimizerVersion: should be "OptimizerV1" or "OptimizerV2".
+    """
+    callZooFunc(bigdl_type, "setOptimizerVersion", optimizerVersion)
+
+
+def get_optimizer_version(bigdl_type="float"):
+    """
+    Get DistriOptimizer version.
+    return optimizerVersion
+    """
+    return callZooFunc(bigdl_type, "getOptimizerVersion")
+
+
 class ZooContextMeta(type):
 
     _log_output = False
@@ -584,19 +600,3 @@ def _get_bigdl_verion_conf():
 def load_conf(conf_str, split_char=None):
     return dict(line.split(split_char) for line in conf_str.split("\n") if
                 "#" not in line and line.strip())
-
-
-def set_optimizer_version(optimizerVersion, bigdl_type="float"):
-    """
-    Set DistriOptimizer version.
-    param optimizerVersion: should be "OptimizerV1" or "OptimizerV2".
-    """
-    callZooFunc(bigdl_type, "setOptimizerVersion", optimizerVersion)
-
-
-def get_optimizer_version(bigdl_type="float"):
-    """
-    Get DistriOptimizer version.
-    return optimizerVersion
-    """
-    return callZooFunc(bigdl_type, "getOptimizerVersion")
