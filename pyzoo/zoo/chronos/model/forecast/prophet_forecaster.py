23393996ba69f8673b745ae0d4a8b48cbf62510d
PeterQiu0516 <peterqiu0516@gmail.com>
2021-06-23 11:32:33 +0800

    Follow pep8 practice (2)

--- pyzoo/zoo/chronos/model/forecast/prophet_forecaster.py
+++ pyzoo/zoo/chronos/model/forecast/prophet_forecaster.py
@@ -21,8 +21,8 @@ from zoo.chronos.model.prophet import ProphetModel
 class ProphetForecaster(Forecaster):
     """
     Prophet Forecaster
-    Prophet is a procedure for forecasting time series data based on an additive model
-    where non-linear trends are fit with yearly, weekly, and daily seasonality, plus holiday effects.
+    Prophet is a procedure for forecasting time series data based on an additive model where
+    non-linear trends are fit with yearly, weekly, and daily seasonality, plus holiday effects.
     It works best with time series that have strong seasonal effects and several seasons of
     historical data. Prophet is robust to missing data and shifts in the trend, and
     typically handles outliers well.
