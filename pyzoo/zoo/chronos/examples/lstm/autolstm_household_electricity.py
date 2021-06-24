22c8c45b38484346d9fece61c6d673a3e84cc374
liangs6212 <liangx.shen@intel.com>
2021-06-24 15:18:18 +0800

    changed forecast columns,changed preprocessing to tsdataset interface

--- pyzoo/zoo/chronos/examples/lstm/autolstm_household_electricity.py
+++ pyzoo/zoo/chronos/examples/lstm/autolstm_household_electricity.py
@@ -26,6 +26,7 @@ import argparse
 from sklearn.preprocessing import MinMaxScaler
 
 
+from zoo.chronos.data.tsdataset import TSDataset
 from zoo.chronos.autots.model.auto_lstm import AutoLSTM
 from zoo.orca.automl import hp
 from zoo.orca import init_orca_context, stop_orca_context
@@ -33,22 +34,18 @@ from zoo.orca import init_orca_context, stop_orca_context
 
 class AutoLSTMDataset(Dataset):
 
-    def __init__(self, data_selection, past_seq_len=9, future_seq_len=1):
-        self.data = AutoLSTMDataset.get_data(data_selection)
-        self.past_seq_len = past_seq_len
-        self.future_seq_len = future_seq_len
+    def __init__(self, data_selection, past_seq_len, future_seq_len, **kwargs):
+        self.x, self.y, _ = AutoLSTMDataset.preprocess_data(
+            data_selection, past_seq_len, future_seq_len)
 
     def __len__(self):
-        return self.data.shape[0] - self.past_seq_len - self.future_seq_len + 1
+        return self.x.shape[0]
 
     def __getitem__(self, idx):
-        X = self.data[idx:idx+self.past_seq_len, :input_feature_dim]
-        y = self.data[idx+self.past_seq_len:idx+self.past_seq_len +
-                      self.future_seq_len, 1:3]
-        return torch.from_numpy(X).float(), torch.from_numpy(y).float()
+        return self.x[idx], self.y[idx]
 
     @staticmethod
-    def get_data(data_selection):
+    def preprocess_data(data_selection, past_seq_len, future_seq_len, **kwargs):
         url_base = "https://archive.ics.uci.edu/ml/machine-learning-databases/"
         url_file_path = "00235/household_power_consumption.zip"
         file_name = url_file_path.split('/')[-1].rpartition('.')[0] + '.txt'
@@ -59,25 +56,38 @@ class AutoLSTMDataset(Dataset):
             file = zipfile.ZipFile(io.BytesIO(download_file.content))
             file.extractall(os.path.abspath(os.path.dirname(__file__)))
         df = pd.read_csv(file_path, sep=';', header=0, low_memory=False,
-                         infer_datetime_format=True, parse_dates={'datetime': [0, 1]}, index_col=['datetime'], nrows=2000)
-        df.dropna(axis=0, how='any', inplace=True)
-        df.astype('float32')
-        train_dataset, valid_dataset = mm.fit_transform(
-            df[:1600]), mm.transform(df[1600:])
+                         infer_datetime_format=True, parse_dates={'datetime': [0, 1]}, nrows=2000)
+        train_data = TSDataset.from_pandas(df[:int(len(df)*0.8)], dt_col="datetime",
+                                           target_col=["Global_active_power"],
+                                           extra_feature_col=["Global_reactive_power"])
+        x_train, y_train = train_data.impute(mode="linear")\
+                                     .scale(mm)\
+                                     .roll(lookback=past_seq_len,
+                                           horizon=future_seq_len)\
+                                     .to_numpy()
+        test_data = TSDataset.from_pandas(df[int(len(df)*0.8):], dt_col="datetime",
+                                          target_col=["Global_active_power"],
+                                          extra_feature_col=["Global_reactive_power"])
+        x_test, y_test = test_data.impute(mode="linear")\
+                                  .scale(mm, fit=False)\
+                                  .roll(lookback=past_seq_len,
+                                        horizon=future_seq_len)\
+                                  .to_numpy()
+
         if data_selection == 'train':
-            return train_dataset
-        else:
-            return valid_dataset
+            return torch.from_numpy(x_train).float(), torch.from_numpy(y_train).float(), test_data
+        elif data_selection == 'valid':
+            return torch.from_numpy(x_test).float(), torch.from_numpy(y_test).float(), test_data
 
 
 def train_dataloader_creator(config):
-    return DataLoader(AutoLSTMDataset('train'),
+    return DataLoader(AutoLSTMDataset('train', **config),
                       batch_size=config["batch_size"],
                       shuffle=True)
 
 
 def valid_dataloader_creator(config):
-    return DataLoader(AutoLSTMDataset('valid'),
+    return DataLoader(AutoLSTMDataset('valid', **config),
                       batch_size=config["batch_size"],
                       shuffle=True)
 
@@ -109,11 +119,11 @@ if __name__ == '__main__':
     init_orca_context(cluster_mode=args.cluster_mode, cores=args.cores,
                       memory=args.memory, num_nodes=num_nodes, init_ray_on_spark=True)
 
-    input_feature_dim = 7
-    output_feature_dim = 2
     mm = MinMaxScaler()
-    auto_lstm = AutoLSTM(input_feature_num=7,
-                         output_target_num=2,
+
+    auto_lstm = AutoLSTM(input_feature_num=2,
+                         output_target_num=1,
+                         past_seq_len=14,
                          optimizer='Adam',
                          loss=torch.nn.MSELoss(),
                          metric="mse",
@@ -131,10 +141,17 @@ if __name__ == '__main__':
                   n_sampling=args.n_sampling,
                   )
     best_model = auto_lstm.get_best_model()
-    tsdata_x, tsdata_y = next(
+
+    _, _, test_data = AutoLSTMDataset.preprocess_data(
+        'valid', **best_model.config)
+    test_data_x, test_data_y = next(
         iter(valid_dataloader_creator(best_model.config)))
-    mse, smape = best_model.evaluate(x=np.array(
-        tsdata_x), y=np.array(tsdata_y), metrics=['mse', 'smape'])
+
+    testdata_x_unscale, testdata_y_unscale = test_data._unscale_numpy(
+        test_data_x.numpy()), test_data._unscale_numpy(test_data_y.numpy())
+
+    mse, smape = best_model.evaluate(
+        x=testdata_x_unscale, y=testdata_y_unscale, metrics=['mse', 'smape'])
     print(f'mse is {np.mean(mse)}, smape is {np.mean(smape)}')
     print(
         f"The hyperparameters of the model include lr:{best_model.config['lr']},"
