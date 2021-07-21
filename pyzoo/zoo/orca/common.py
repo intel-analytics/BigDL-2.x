74b53fa87ab66b8eca074fa304123d0caea3dcbf
sgwhat <njusongge@gmail.com>
2021-07-21 13:33:11 +0800

    new update

--- pyzoo/zoo/orca/common.py
+++ pyzoo/zoo/orca/common.py
@@ -199,11 +199,15 @@ def init_orca_context(cluster_mode=None, cores=None, memory=None, num_nodes=None
             cores = conf.get("spark.executor.cores") if cores is None else cores
             memory = conf.get("spark.executor.memory") if memory is None else memory
             num_nodes = conf.get("spark.executor.instances") if num_nodes is None else num_nodes
+        else:
+            cores = 4 if cores is None else cores
+            memory = "2g" if memory is None else memory
+            num_nodes = 4 if num_nodes is None else num_nodes
     spark_args = {}
     for key in ["conf", "spark_log_level", "redirect_spark_log"]:
         if key in kwargs:
             spark_args[key] = kwargs[key]
-    if cluster_mode is None or not activate_sc:
+    if cluster_mode is None:
         cores = 2 if cores is None else cores
         memory = "2g" if memory is None else memory
         num_nodes = 1 if num_nodes is None else num_nodes
