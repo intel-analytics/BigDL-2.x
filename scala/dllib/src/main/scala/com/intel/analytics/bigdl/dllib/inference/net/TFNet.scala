4b3ed5b90ed10e3f4d752adb5812047dbc3a178a
dding3 <ding.ding@intel.com>
2021-07-26 19:20:47 -0700

    fix dllib compile error

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/inference/net/TFNet.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/inference/net/TFNet.scala
@@ -30,7 +30,8 @@ import com.intel.analytics.bigdl.common.zooUtils
 import com.intel.analytics.zoo.core.TFNetNative
 import com.intel.analytics.bigdl.dllib.inference.Predictable
 import com.intel.analytics.bigdl.dllib.inference.net.TFNet.TFGraphHolder
-import com.intel.analytics.bigdl.orca.tfpark.{TFResourceManager, TFUtils}
+import com.intel.analytics.bigdl.orca.tfpark.{TFResourceManager}
+import com.intel.analytics.bigdl.common.TFUtils
 import org.apache.spark.rdd.RDD
 import org.tensorflow.framework.GraphDef
 import org.tensorflow.{DataType, Graph, Session, Tensor => TTensor}
