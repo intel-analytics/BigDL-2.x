97109c8721e25909834f29756a2c7aebfac07158
liangs6212 <liangx.shen@intel.com>
2021-07-30 16:48:13 +0800

    fix error

--- pyzoo/zoo/examples/run-example-tests-pip-ray.sh
+++ pyzoo/zoo/examples/run-example-tests-pip-ray.sh
@@ -116,7 +116,7 @@ if [ ! -f {ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/quickstart/nyc_taxi.cs
   wget -nv $FTP_URI/analytics-zoo-data/apps/nyc-taxi/nyc_taxi.csv -P ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/quickstart/
 fi
 
-execute_ray_test autots_nyc_taxi "${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/quickstart/autots_nyc_taxi.py --datadir ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples6/quickstart/nyc_taxi.csv"
+execute_ray_test autots_nyc_taxi "${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/quickstart/autots_nyc_taxi.py --datadir ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/quickstart/nyc_taxi.csv"
 time13=$?
 
 echo "#1 rl_pong time used:$time1 seconds"
