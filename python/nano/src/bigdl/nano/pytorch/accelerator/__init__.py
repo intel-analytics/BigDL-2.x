24905eadef3967ac6bda9344df14e23fd0142a6b
leonardozcm <597714572@qq.com>
2021-08-16 14:53:19 +0800

    mv ipex tensor saver

--- python/nano/src/bigdl/nano/pytorch/accelerator/__init__.py
+++ python/nano/src/bigdl/nano/pytorch/accelerator/__init__.py
@@ -8,4 +8,41 @@ from intel_pytorch_extension.ops.save import *
 # Note that you need to temporarily store original torch.save,
 # because it will be modified in ipex.ops.save.
 torch.save = _torch_save
-from .nano_specified_saver import nano_save
+
+import copy
+import pickle
+
+DEFAULT_PROTOCOL = 2
+
+torch_save = torch.save
+
+def nano_save(obj, f, pickle_module=pickle, pickle_protocol=DEFAULT_PROTOCOL,
+                   _use_new_zipfile_serialization=False):
+    def to_cpu(obj, iter_type):
+        # Extend original `save` defined in ipex.ops.save
+        # to support converting a list of xpu tensor to cpu in torch.save
+        iter_keys = obj.keys() if iter_type == 'dict' else range(len(obj))
+        for k in iter_keys:
+            if isinstance(obj[k], dict):
+                to_cpu(obj[k], 'dict')
+            elif torch.is_tensor(obj[k]) and obj[k].device.type == 'xpu':
+                obj[k] = obj[k].to('cpu')
+            elif isinstance(obj[k], list):
+                to_cpu(obj[k], 'list')
+            else:
+                pass
+
+    if isinstance(obj, dict):
+        obj_copy = copy.deepcopy(obj)
+        to_cpu(obj_copy, 'dict')
+    elif torch.is_tensor(obj) and obj.device.type == 'xpu':
+        obj_copy = copy.deepcopy(obj).to('cpu')
+    elif isinstance(obj, torch.nn.Module):
+        obj_copy = copy.deepcopy(obj).to('cpu')
+    else:
+        obj_copy = obj
+    return torch_save(obj_copy, f, pickle_module, pickle_protocol, _use_new_zipfile_serialization)
+
+
+torch.save = nano_save
+
