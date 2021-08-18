01d264da5238e45ae94fd19a95b27764aff1ec0a
OpheliaLjh <opheelia@umich.edu>
2021-08-18 09:22:22 +0800

    change

--- ppml/trusted-big-data-ml/scala/docker-graphene/bashexm/runjava.sh
+++
@@ -1,2 +0,0 @@
-#graphene-sgx ./bash -c "${JDK_HOME}/bin/java"
-graphene-sgx ./bash -c "/opt/jdk/bin/java  -Xmx64m -cp /home/sdp/hangrui/myzoo/analytics-zoo/ppml/trusted-big-data-ml/scala/docker-graphene/bashexm/my-app-1.0-CHR-jar-with-dependencies.jar com.mycompany.app.App"
--- ppml/trusted-big-data-ml/scala/docker-graphene/deploy-local-spark-sgx.sh
+++ ppml/trusted-big-data-ml/scala/docker-graphene/deploy-local-spark-sgx.sh
@@ -5,22 +5,22 @@ export DATA_PATH=~/hangrui/myzoo/analytics-zoo/ppml/trusted-big-data-ml/work/dat
 export KEYS_PATH=/home/sdp/qiyuan/keys
 export LOCAL_IP=192.168.0.113
 
-sudo docker pull 10.239.45.10/arda/intelanalytics/analytics-zoo-ppml-trusted-big-data-ml-scala-graphene:0.11-SNAPSHOTchrnew
+sudo docker pull 10.239.45.10/arda/intelanalytics/analytics-zoo-ppml-trusted-big-data-ml-scala-graphene:0.11-SNAPSHOTCHR
 
 sudo docker run -itd \
     --privileged \
     --net=host \
-    --cpuset-cpus="0-5" \
+    --cpuset-cpus="0-3" \
     --oom-kill-disable \
-    --device=/dev/gsgx \
     --device=/dev/sgx/enclave \
     --device=/dev/sgx/provision \
+    -v /dev/sgx/enclave:/dev/sgx/enclave \
+    -v /dev/sgx/provision:/dev/sgx/provision \
     -v $ENCLAVE_KEY_PATH:/graphene/Pal/src/host/Linux-SGX/signer/enclave-key.pem \
     -v /var/run/aesmd/aesm.socket:/var/run/aesmd/aesm.socket \
     -v $DATA_PATH:/ppml/trusted-big-data-ml/work/data \
     -v $KEYS_PATH:/ppml/trusted-big-data-ml/work/keys \
-    --name=spark-local \
+    --name=spark-local-new \
     -e LOCAL_IP=$LOCAL_IP \
     -e SGX_MEM_SIZE=64G \
-     10.239.45.10/arda/intelanalytics/analytics-zoo-ppml-trusted-big-data-ml-scala-graphene:0.11-SNAPSHOTchrnew
-
+     10.239.45.10/arda/intelanalytics/analytics-zoo-ppml-trusted-big-data-ml-scala-graphene:0.11-SNAPSHOTCHR
