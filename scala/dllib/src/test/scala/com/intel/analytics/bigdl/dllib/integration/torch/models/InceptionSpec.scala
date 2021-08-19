f28c225990bc3444749227d48c8c82b19e530d02
Le-Zheng <alettazheng@hotmail.com>
2021-08-19 20:05:45 +0800

    update

--- scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/integration/torch/models/InceptionSpec.scala
+++ scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/integration/torch/models/InceptionSpec.scala
@@ -21,7 +21,7 @@ import com.intel.analytics.bigdl.dllib.nn.{ClassNLLCriterion, Graph, Input}
 import com.intel.analytics.bigdl.dllib.nn.abstractnn.AbstractModule
 import com.intel.analytics.bigdl.dllib.optim.SGD
 import com.intel.analytics.bigdl.dllib.tensor.Tensor
-import com.intel.analytics.bigdl.integration.torch.{TH, TorchSpec}
+import com.intel.analytics.bigdl.dllib.integration.torch.{TH, TorchSpec}
 import com.intel.analytics.bigdl.dllib.models.Inception
 import com.intel.analytics.bigdl.utils.RandomGenerator._
 import com.intel.analytics.bigdl.dllib.utils.{T, Table}
