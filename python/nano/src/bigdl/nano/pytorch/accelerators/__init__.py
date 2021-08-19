e36ad262cfeaa5e9f17d644c42a28f9b2a17b90b
leonardozcm <597714572@qq.com>
2021-08-19 15:51:48 +0800

    diff

--- python/nano/src/bigdl/nano/pytorch/accelerators/__init__.py
+++ python/nano/src/bigdl/nano/pytorch/accelerators/__init__.py
@@ -15,19 +15,18 @@
 #
 
 
-import pickle
-import copy
-from intel_pytorch_extension.ops.save import *
 import torch
 _torch_save = torch.save
 
 # To replace torch.save in ipex, you need to import and exec their __init__.py first.
-
+from intel_pytorch_extension.ops.save import *
 # And then you can replace torch.save with your customized function.
 # Note that you need to temporarily store original torch.save,
 # because it will be modified in ipex.ops.save.
 torch.save = _torch_save
 
+import pickle
+import copy
 
 DEFAULT_PROTOCOL = 2
 
