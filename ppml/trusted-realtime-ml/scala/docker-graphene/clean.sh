4a668523ba526c800fdc89bdea73955252d25ee8
glorysdj <glorysdj@gmail.com>
2021-09-02 01:02:34 -0700

    update

--- ppml/trusted-realtime-ml/scala/docker-graphene/clean.sh
+++ ppml/trusted-realtime-ml/scala/docker-graphene/clean.sh
@@ -4,4 +4,4 @@ set -x
 
 sgx_mem_size=$SGX_MEM_SIZE
 
-make SGX=1 GRAPHENEDIR=/graphene THIS_DIR=/ppml/trusted-big-data-ml G_SGX_SIZE=$sgx_mem_size clean
+make SGX=1 GRAPHENEDIR=/graphene THIS_DIR=/ppml/trusted-realtime-ml G_SGX_SIZE=$sgx_mem_size clean
