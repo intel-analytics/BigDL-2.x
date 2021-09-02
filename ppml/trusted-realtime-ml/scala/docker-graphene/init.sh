c1bb9a9b7cf8cc981402b1dae2c36f898b53dabd
glorysdj <glorysdj@gmail.com>
2021-09-02 01:05:52 -0700

    update

--- ppml/trusted-realtime-ml/scala/docker-graphene/init.sh
+++ ppml/trusted-realtime-ml/scala/docker-graphene/init.sh
@@ -4,4 +4,4 @@ set -x
 
 sgx_mem_size=$SGX_MEM_SIZE
 
-make SGX=1 GRAPHENEDIR=/graphene THIS_DIR=/ppml/trusted-realtime-mlG_SGX_SIZE=$sgx_mem_size
+make SGX=1 GRAPHENEDIR=/graphene THIS_DIR=/ppml/trusted-realtime-ml G_SGX_SIZE=$sgx_mem_size
