06c7bfbf7e8da51c555d3b15ba97faf4c20eaebb
sgwhat <ge.song@intel.com>
2021-09-03 13:43:03 +0800

    tensorflow multi_workers

--- pyzoo/zoo/orca/learn/tf2/demo.py
+++ pyzoo/zoo/orca/learn/tf2/demo.py
@@ -3,7 +3,6 @@ import json
 
 from pyspark.context import SparkContext
 from pyspark.conf import SparkConf
-import itertools
 import tensorflow as tf
 
 #from zoo.orca.learn.tf2.pyspark_estimator import Estimator
@@ -14,45 +13,55 @@ from numpy import array
 import numpy as np
 from keras.models import Sequential
 from keras.layers import LSTM
-from keras.layers import Dense
+from keras.layers import Dense 
+
+workers = 2
+
+def build_and_compile_model():
+    model = Sequential()
+    model.add(LSTM(50, activation='relu', input_shape=(3, 1)))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mse')
+    return model
 
 os.environ['TF_CONFIG'] = json.dumps({
     'cluster': {
-        'worker': ["localhost:12345"]
+        'worker': ["172.168.0.204:20000", "172.168.0.202:20001"]
     },
     'task': {'type': 'worker', 'index': 0}
 })
 
+urls =["172.168.0.204:20000", "172.168.0.202:20001"]
+no_proxy = os.environ.get("no_proxy", "")
+ips = [url.split(":")[0] for url in urls]
+os.environ["no_proxy"] = ",".join(ips) + "," + no_proxy
+
+strategy =  tf.distribute.experimental.MultiWorkerMirroredStrategy()
+print('Number of devices: %d' % strategy.num_replicas_in_sync)
+
 def transform_func(*args):
-    x = array([[10, 20, 30], [20, 30, 40], [30, 40, 50], [40, 50, 60]])
-    y = array([40, 50, 60, 70])
+    train_data = array([[10, 20, 30], [20, 30, 40], [30, 40, 50], [40, 50, 60]])
+    val_data = array([40, 50, 60, 70])
+    train_data = train_data.reshape((4,3,1))
 
-    x = x.reshape((4,3,1))
-
-    strategy =  tf.distribute.experimental.MultiWorkerMirroredStrategy()
-    def build_and_compile_model():
-        model = Sequential()
-        model.add(LSTM(50, activation='relu', input_shape=(3, 1)))
-        model.add(Dense(1))
-        model.compile(optimizer='adam', loss='mse')
-        return model
     with strategy.scope():
         mutil_model = build_and_compile_model()
-    # checkpoint_dir = './training_checkpoints'
-    # checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
-    # callbacks = [
-    #     tf.keras.callbacks.TensorBoard(log_dir='./logs'),
-    #     tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_prefix,
-    #                                         save_weights_only=True)]
-    mutil_model.fit(x, y, epochs=1, verbose=0)
 
-    stats = mutil_model.get_weights()
-    return stats
+    checkpoint_dir = './training_checkpoints'
+    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+    callbacks = [
+        tf.keras.callbacks.TensorBoard(log_dir='./logs'),
+        tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_prefix,
+                                            save_weights_only=True)] 
 
-rdd = sc.parallelize([1, 2], 4)
+    mutil_model.fit(train_data, val_data, epochs=10, verbose=0, callbacks=callbacks)
 
-res = rdd.mapPartitions(transform_func).collect()
-res = list(itertools.chain.from_iterable(res))
-rel = res[0].copy()
+    weights = mutil_model.get_weights()
+    return [weights]
+
+workerRDD = sc.parallelize(list(range(workers)), workers)
+res = workerRDD.mapPartitions(transform_func).collect()
+
 print(res)
+
 sc.stop()
