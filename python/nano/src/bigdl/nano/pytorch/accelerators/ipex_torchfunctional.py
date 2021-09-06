25f5582e3768b7af1372e3a6be794ca2762554e5
leonardozcm <597714572@qq.com>
2021-09-06 10:41:32 +0800

    move to cpu

--- python/nano/src/bigdl/nano/pytorch/accelerators/ipex_torchfunctional.py
+++ python/nano/src/bigdl/nano/pytorch/accelerators/ipex_torchfunctional.py
@@ -13,49 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# 
-# `workaround_cross_entropy` is adapted from pytorch team.
-# https://github.com/pytorch/pytorch/blob/c7c711bfb88fcb0ef573125a5a8655c49156055b
-# /torch/nn/functional.py#L2767
-# 
-# Note: This license has also been called the "New BSD License" or "Modified BSD License". See also
-# the 2-clause BSD License.
-#
-# Copyright (c) 2016 The Pytorch Project.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without modification, are permitted
-# provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice, this list of conditions
-# and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright notice, this list of
-# conditions and the following disclaimer in the documentation and/or other materials provided
-# with the distribution.
-#
-# 3. Neither the name of the copyright holder nor the names of its contributors may be used to
-# endorse or promote products derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-# AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-# OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
 
 
 import torch
-from torch.overrides import has_torch_function_variadic, handle_torch_function
-from typing import Callable, Optional
+from torch.nn import functional as F
+from typing import Callable
 
 Tensor = torch.Tensor
-torch_nn_functional = torch.nn.functional
-_Reduction = torch.nn._reduction
-
+_cross_entropy = F.cross_entropy
 
 def replace_torch_function(function_name: str, replace_func: Callable):
     setattr(torch.nn.functional, function_name, replace_func)
@@ -64,31 +29,12 @@ def replace_torch_function(function_name: str, replace_func: Callable):
 def workaround_cross_entropy(
     input: Tensor,
     target: Tensor,
-    weight: Optional[Tensor] = None,
-    size_average: Optional[bool] = None,
-    ignore_index: int = -100,
-    reduce: Optional[bool] = None,
-    reduction: str = "mean",
+    *args,
+    **kwargs
 ) -> Tensor:
-    if has_torch_function_variadic(input, target):
-        return handle_torch_function(
-            workaround_cross_entropy,
-            (input, target),
-            input,
-            target,
-            weight=weight,
-            size_average=size_average,
-            ignore_index=ignore_index,
-            reduce=reduce,
-            reduction=reduction,
-        )
-    if size_average is not None or reduce is not None:
-        reduction = _Reduction.legacy_get_string(size_average, reduce)
-
-    # `log_softmax(input, 1).contiguous()` makes multidimensional tensors continuous in memory
-    # May fix https://github.com/intel/intel-extension-for-pytorch/issues/175#issue-972312586
-    return torch_nn_functional.nll_loss(torch_nn_functional.log_softmax(input, 1).contiguous(),
-                                        target, weight, None, ignore_index, None, reduction)
+    input = input.cpu()
+    target = target.cpu()
+    return _cross_entropy(input, target, *args, **kwargs)
 
 
 # Usage: append your target method and your own implements to  `replacement_dict`
