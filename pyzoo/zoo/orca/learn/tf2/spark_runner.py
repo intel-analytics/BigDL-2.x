d6887924bb74986b063539c3c2ca524f59174ae6
sgwhat <ge.song@intel.com>
2021-09-07 22:40:39 +0800

    create spark_runner

--- pyzoo/zoo/orca/learn/tf2/spark_runner.py
+++ pyzoo/zoo/orca/learn/tf2/spark_runner.py
@@ -35,26 +35,31 @@ def validation_data_creator(*args):
     val_data = array([40, 50, 60, 70])
     return val_data
 
-def handle_datasets_train(data_creator, validation_data_creator):   
-    train_dataset = data_creator()
-    if validation_data_creator is not None:
-        test_dataset = validation_data_creator()
-    else:
-        test_dataset = None
-    return train_dataset, test_dataset
-
 class SparkRunner:
-    def __init__(self, model_creator, data_creator, validation_data_creator):
+    def __init__(self, model_creator, data_creator, validation_data_creator, config=None):
         self.model_creator = model_creator
         self.data_creator = data_creator
         self.validation_data_creator = validation_data_creator
+        self.config = {} if config is None else config
 
-    def disributed_train_func(self, *args):
+    def handle_datasets_train(self, data_creator, validation_data_creator):   
+        train_dataset = data_creator()
+        if validation_data_creator is not None:
+            test_dataset = validation_data_creator()
+        else:
+            test_dataset = None
+        return train_dataset, test_dataset
+
+    def disributed_train_func(self, *args):#, config):
         tc = BarrierTaskContext().get()
         rank = tc.partitionId()
         free_port = find_free_port(tc)
         cluster = tc.allGather(str(free_port))
         print(cluster)
+
+        #config = self.config.copy()
+        #batch_size=32
+        #config["batch_size"] = batch_size
 
         import os
         os.environ["TF_CONFIG"] = json.dumps({
@@ -69,14 +74,20 @@ class SparkRunner:
         strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
 
         with strategy.scope():
-            model = self.model_creator()
+            model = model_creator()#self.config)
+        
+        #train_dataset = data_creator()
+        #test_dataset = validation_data_creator()
 
-        train_dataset, test_dataset = handle_datasets_train(self.data_creator, self.validation_data_creator)#, config)
+        data_creator = self.data_creator
+        validation_data_creator = self.validation_data_creator
+        train_dataset, test_dataset = self.handle_datasets_train(data_creator, validation_data_creator)#, config)
         
         model.fit(train_dataset, test_dataset, epochs=10, verbose=1)
 
         return [model.get_weights()]
     
+
 
 if __name__ == "__main__":
 
@@ -85,7 +96,7 @@ if __name__ == "__main__":
     sc = SparkContext()
     sparkRDD = sc.parallelize(list(range(40)), 4)
     workerRDD = sparkRDD.repartition(2)
-    spark_func = SparkRunner(model_creator, data_creator, validation_data_creator).disributed_train_func
+    spark_func = SparkRunner().disributed_train_func
     res = workerRDD.barrier().mapPartitions(spark_func).collect()
     assert np.all(res[0][0] == res[1][0])
     sc.stop()
