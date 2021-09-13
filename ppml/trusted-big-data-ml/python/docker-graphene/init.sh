8b3b1156a0b4550b8548ca5245de648848526190
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2021-09-13 16:14:26 +0800

    update for adding soft link for sgx

--- ppml/trusted-big-data-ml/python/docker-graphene/init.sh
+++ ppml/trusted-big-data-ml/python/docker-graphene/init.sh
@@ -6,3 +6,10 @@ local_ip=$LOCAL_IP
 sgx_mem_size=$SGX_MEM_SIZE
 
 make SGX=1 GRAPHENEDIR=/graphene THIS_DIR=/ppml/trusted-big-data-ml  SPARK_LOCAL_IP=$local_ip SPARK_USER=root G_SGX_SIZE=$sgx_mem_size
+
+cd /dev
+mkdir sgx
+ln -s /dev/sgx_enclave /dev/sgx/enclave
+ln -s /dev/sgx_provision /dev/sgx/provision
+
+cd /ppml/trusted-big-data-ml
