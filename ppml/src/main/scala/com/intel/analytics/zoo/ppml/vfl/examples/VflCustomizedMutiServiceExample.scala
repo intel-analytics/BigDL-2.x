9197d0d25548ff0c3efa61354c7805dbbc747c3d
Jiaming <litchilitchy@qq.com>
2021-09-17 15:47:46 +0800

    tmp

---
+++ ppml/src/main/scala/com/intel/analytics/zoo/ppml/vfl/examples/VflCustomizedMutiServiceExample.scala
@@ -0,0 +1,16 @@
+package com.intel.analytics.zoo.ppml.vfl.examples
+
+import com.intel.analytics.zoo.ppml.FLServer
+import com.intel.analytics.zoo.ppml.psi.PSIServiceImpl
+import com.intel.analytics.zoo.ppml.vfl.{NNServiceImpl, VflNNAggregator}
+
+object VflCustomizedMutiServiceExample {
+  def main(args: Array[String]): Unit = {
+//    val flServer = new FLServer()
+//    val psiService = new PSIServiceImpl()
+//    val nnService = new NNServiceImpl()
+//    val aggregator = new VflNNAggregator()
+//    nnService.setAggregator()
+
+  }
+}
