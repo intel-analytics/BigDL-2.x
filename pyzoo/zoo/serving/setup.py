06e555c4f3601b9971c2c8aea21673eda242b6cc
Song Jiaming <litchy233@gmail.com>
2021-09-27 02:07:57 +0000

    Revert "fix pyzoo serving build failure (#4837)" (#4844)

--- pyzoo/zoo/serving/setup.py
+++ pyzoo/zoo/serving/setup.py
@@ -62,7 +62,7 @@ except IOError:
 
 
 def setup_package():
-    script_names = [ f for f in os.listdir(SCRIPTS_TARGET) if os.path.isfile(os.path.join(SCRIPTS_TARGET,f)) ]
+    script_names = os.listdir(SCRIPTS_TARGET)
     scripts = list(map(lambda script: os.path.join(
         SCRIPTS_TARGET, script), script_names))
 
