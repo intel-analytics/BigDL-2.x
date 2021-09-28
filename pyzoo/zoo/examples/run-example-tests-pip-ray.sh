2a723782091bdac429fc1d9ada2bc4b0b0823bce
liangs6212 <80952198+liangs6212@users.noreply.github.com>
2021-09-28 10:22:30 +0800

    Chronos:  add distributed training example (#4749)
    * add example for distributed
    
    * change rmse,smape
    
    * sync md to code
    
    * add example_test_script
    
    * fix line
    
    * sync master code

--- pyzoo/zoo/examples/run-example-tests-pip-ray.sh
+++ pyzoo/zoo/examples/run-example-tests-pip-ray.sh
@@ -133,6 +133,14 @@ fi
 execute_ray_test dpgansimulator_wwt "${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/simulator/dpgansimulator_wwt.py --datadir ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/simulator/data_train_small.npz --epoch 1 --plot_figures False"
 time15=$?
 
+if [ ! -f ~/.chronos/dataset/network_traffic/network_traffic_data.csv ]; then
+  wget -nv $FTP_URI/analytics-zoo-data/network-traffic/data/data.csv -P ~/.chronos/dataset/network_traffic/
+  mv ~/.chronos/dataset/network_traffic/data.csv ~/.chronos/dataset/network_traffic/network_traffic_data.csv
+fi
+
+execute_ray_test distributed_training_network_traffic "${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/distributed/distributed_training_network_traffic.py"
+time16=$?
+
 echo "#1 rl_pong time used:$time1 seconds"
 echo "#2 sync_parameter_server time used:$time2 seconds"
 echo "#3 async_parameter_server time used:$time3 seconds"
@@ -148,5 +156,6 @@ echo "#12 autolstm_nyc_taxi time used:$time12 seconds"
 echo "#13 autoprophet_nyc_taxi time used:$time13 seconds"
 echo "#14 autots_nyc_taxi time used:$time14 seconds"
 echo "#15 dpgansimulator_wwt time used:$time15 seconds"
+echo "#16 distributed_training_network_traffic time used:$time16 seconds"
 
 clear_up
