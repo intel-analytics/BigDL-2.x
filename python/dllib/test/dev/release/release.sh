fb1cd74e5f23f278f6b1ea8a84774a5bc9b4188c
Le-Zheng <alettazheng@hotmail.com>
2021-10-11 08:11:56 +0800

    update

--- python/dllib/test/dev/release/release.sh
+++ python/dllib/test/dev/release/release.sh
@@ -27,7 +27,7 @@ echo $BIGDL_PYTHON_DIR
 if (( $# < 1)); then
   echo "Usage: release.sh platform mvn_parameters"
   echo "Usage example: bash release.sh linux"
-  echo "you can also add other profiles such as: -Dspark.version=2.4.6 -P spark_2.4"
+  echo "you can also add other profiles such as: -Dspark.version=2.4.6 -P spark_2.x"
   exit -1
 fi
 
