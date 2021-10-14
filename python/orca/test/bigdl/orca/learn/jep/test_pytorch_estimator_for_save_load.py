f349c2ebded6dc7b8bdb9c2d15d05dedc1107137
Wang, Yang <yang3.wang@intel.com>
2021-10-14 17:36:53 +0800

    change to dummpy data

--- python/orca/test/bigdl/orca/learn/jep/test_pytorch_estimator_for_save_load.py
+++ python/orca/test/bigdl/orca/learn/jep/test_pytorch_estimator_for_save_load.py
@@ -66,21 +66,12 @@ class TestEstimatorForSaveAndLoad(TestCase):
 
         dir = "/tmp/dataset/"
         batch_size = 320
-        train_loader = torch.utils.data.DataLoader(
-            datasets.MNIST(dir, train=True, download=True,
-                           transform=transforms.Compose([
-                               transforms.ToTensor(),
-                               transforms.Normalize((0.1307,), (0.3081,))
-                           ])),
-            batch_size=batch_size, shuffle=True)
 
-        test_loader = torch.utils.data.DataLoader(
-            datasets.MNIST(dir, train=False,
-                           transform=transforms.Compose([
-                               transforms.ToTensor(),
-                               transforms.Normalize((0.1307,), (0.3081,))
-                           ])),
-            batch_size=batch_size, shuffle=False)
+        images = torch.randn(1000 * 28 * 28, dtype=torch.float32).view(1000, 1, 28, 28)
+        labels = torch.randn(1000, dtype=torch.float32).view(1000, 1)
+        dataset = torch.utils.data.TensorDataset(images, labels)
+        train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+        test_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
         # epoch 1
         est = Estimator.from_torch(model=model, optimizer=adam, loss=criterion,
