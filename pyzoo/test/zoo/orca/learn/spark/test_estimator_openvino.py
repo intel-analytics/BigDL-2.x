0c89be4b54e655cbb2af623e7388629a5a941e28
Yina Chen <33650826+cyita@users.noreply.github.com>
2021-10-15 10:01:30 +0800

    Orca openvino estimator use openvino python (#4882)
    * init
    
    * add xshards and dataframe support
    
    * delete
    
    * multioutput & example
    
    * ignore openvino test
    
    * remove windows
    
    * add init
    
    * remove

--- pyzoo/test/zoo/orca/learn/spark/test_estimator_openvino.py
+++ pyzoo/test/zoo/orca/learn/spark/test_estimator_openvino.py
@@ -14,8 +14,8 @@
 # limitations under the License.
 #
 import os
+import shutil
 import tarfile
-import tempfile
 from unittest import TestCase
 
 import numpy as np
@@ -29,6 +29,7 @@ property_path = os.path.join(os.path.split(__file__)[0],
                              "../../../../../../zoo/target/classes/app.properties")
 data_url = "http://10.239.45.10:8081/repository/raw"
 resource_path = os.path.join(os.path.split(__file__)[0], "../../../resources")
+local_path = "/tmp/tmpyhny717gopenvino"
 
 with open(property_path) as f:
     for _ in range(2):  # skip the first two lines
@@ -48,6 +49,17 @@ def read_file_and_cast(file_path):
 
 class TestEstimatorForOpenVINO(TestCase):
     def setUp(self):
+        pass
+
+    def tearDown(self):
+        shutil.rmtree(local_path)
+
+    def check_result(self, result, length=0):
+        if length == 0:
+            length = len(result)
+        return np.all(list(map(lambda i: np.allclose(result[i], self.output), range(0, length))))
+
+    def load_resnet(self):
         input_file_path = os.path.join(resource_path, "orca/learn/resnet_input")
         output_file_path = os.path.join(resource_path, "orca/learn/resnet_output")
         self.input = read_file_and_cast(input_file_path)
@@ -55,30 +67,49 @@ class TestEstimatorForOpenVINO(TestCase):
         self.input = np.array(self.input).reshape([3, 224, 224])
         self.output = np.array(self.output).reshape([4, 1000])[:1]
 
-        with tempfile.TemporaryDirectory() as local_path:
-            model_url = data_url + "/analytics-zoo-data/openvino2020_resnet50.tar"
-            model_path = maybe_download("openvino2020_resnet50.tar",
-                                        local_path, model_url)
-            tar = tarfile.open(model_path)
-            tar.extractall(path=local_path)
-            tar.close()
-            model_path = os.path.join(local_path, "openvino2020_resnet50/resnet_v1_50.xml")
-            self.est = Estimator.from_openvino(model_path=model_path)
+        os.makedirs(local_path, exist_ok=True)
+        model_url = data_url + "/analytics-zoo-data/openvino2020_resnet50.tar"
+        model_path = maybe_download("openvino2020_resnet50.tar",
+                                    local_path, model_url)
+        tar = tarfile.open(model_path)
+        tar.extractall(path=local_path)
+        tar.close()
+        model_path = os.path.join(local_path, "openvino2020_resnet50/resnet_v1_50.xml")
+        self.est = Estimator.from_openvino(model_path=model_path)
 
-    def check_result(self, result, length=0):
-        if length == 0:
-            length = len(result)
-        return np.all(list(map(lambda i: np.allclose(result[i], self.output), range(0, length))))
+    def load_roberta(self):
+        os.makedirs(local_path, exist_ok=True)
+        model_url = data_url + "/analytics-zoo-data/roberta/roberta.tar"
+        model_path = maybe_download("roberta.tar",
+                                    local_path, model_url)
+        tar = tarfile.open(model_path)
+        tar.extractall(path=local_path)
+        tar.close()
+        model_path = os.path.join(local_path, "roberta/model.xml")
+        self.est = Estimator.from_openvino(model_path=model_path)
 
     def test_openvino_predict_ndarray(self):
+        self.load_resnet()
         input_data = np.array([self.input] * 22)
-        result = self.est.predict(input_data)
+        input_data = np.concatenate([input_data, np.zeros([1, 3, 224, 224])])
+        result = self.est.predict(input_data, batch_size=4)
         assert isinstance(result, np.ndarray)
-        assert result.shape == (22, 1000)
+        assert result.shape == (23, 1000)
         assert self.check_result(result, 22)
+        assert not self.check_result(result[22:], 1)
+
+    def test_openvino_predict_ndarray_multi_input(self):
+        self.load_roberta()
+        input_data = [np.zeros([32, 128])] * 3
+        result = self.est.predict(input_data, batch_size=5)
+        assert isinstance(result, np.ndarray)
+        assert result.shape == (32, 2)
 
     def test_openvino_predict_xshards(self):
-        input_data_list = [np.array([self.input] * 4), np.array([self.input] * 2)]
+        self.load_resnet()
+        input_data_list = [np.array([self.input] * 4),
+                           np.concatenate([np.array([self.input] * 2),
+                                           np.zeros([1, 3, 224, 224])])]
         sc = init_nncontext()
         rdd = sc.parallelize(input_data_list, numSlices=2)
         shards = SparkXShards(rdd)
@@ -91,13 +122,15 @@ class TestEstimatorForOpenVINO(TestCase):
         result_c = result.collect()
         assert isinstance(result, SparkXShards)
         assert result_c[0]["prediction"].shape == (4, 1000)
-        assert result_c[1]["prediction"].shape == (2, 1000)
+        assert result_c[1]["prediction"].shape == (3, 1000)
         assert self.check_result(result_c[0]["prediction"], 4)
         assert self.check_result(result_c[1]["prediction"], 2)
+        assert not self.check_result(result_c[1]["prediction"][2:], 1)
 
     def test_openvino_predict_spark_df(self):
         from pyspark.sql import SparkSession
 
+        self.load_resnet()
         sc = init_nncontext()
         spark = SparkSession(sc)
         input_list = self.input.tolist()
