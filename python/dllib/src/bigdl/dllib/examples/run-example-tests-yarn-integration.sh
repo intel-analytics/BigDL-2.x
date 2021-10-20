4f6da8926211752fac7e51fc37abf5c9eb3dc737
qiuxin2012 <qiuxin2012cs@gmail.com>
2021-10-20 14:25:40 +0800

    add inception to script

--- python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
+++ python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
@@ -2,23 +2,44 @@
 clear_up() {
   echo "Clearing up environment. Uninstalling dllib"
   pip uninstall -y bigdl-dllib
-  pip uninstall -y pyspark
 }
 
-echo "#1 start test for orca tf transfer_learning"
+echo "#1 start test for dllib lenet5"
 
 #timer
 start=$(date "+%s")
 #run the example
-export SPARK_DRIVER_MEMORY=3g
-${HADOOP_HOME}/bin/hadoop fs -get ${HDFS_URI}/mnist /tmp/mnist
+${HADOOP_HOME}/bin/hadoop fs -get ${HDFS_URI}/mnist /tmp/mnist -e 1
 python ${BIGDL_ROOT}/python/dllib/src/bigdl/dllib/models/lenet/lenet5.py --on-yarn
 exit_status=$?
 if [ $exit_status -ne 0 ]; then
   clear_up
-  echo "orca tf transfer_learning failed"
+  echo "dllib lenet5 failed"
   exit $exit_status
 fi
 now=$(date "+%s")
 time1=$((now - start))
+
+echo "#1 start test for dllib inception"
+
+#timer
+start=$(date "+%s")
+#run the example
+python ${BIGDL_ROOT}/python/dllib/src/bigdl/dllib/models/inception/inception.py -f ${HDFS_URI}/imagenet-mini \
+	--batchSize 128 \
+	--learningRate 0.065 \
+	--weightDecay 0.0002 \
+	--executor-memory 20g \
+	--driver-memory 20g \
+	--executor-cores 4 \
+	--num-executors 4 \
+        -i 20
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+  clear_up
+  echo "dllib inception failed"
+  exit $exit_status
+fi
+now=$(date "+%s")
+time2=$((now - start))
 
