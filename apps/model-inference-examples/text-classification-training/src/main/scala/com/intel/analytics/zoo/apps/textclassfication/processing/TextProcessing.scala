26a86ca0a8fc2b3dac328ac9cdda7e2c7e40b350
Le-Zheng <alettazheng@hotmail.com>
2021-12-17 08:50:32 +0800

    update

--- apps/model-inference-examples/text-classification-training/src/main/scala/com/intel/analytics/zoo/apps/textclassfication/processing/TextProcessing.scala
+++ apps/model-inference-examples/text-classification-training/src/main/scala/com/intel/analytics/zoo/apps/textclassfication/processing/TextProcessing.scala
@@ -11,7 +11,7 @@ import scala.io.Source
 
 trait TextProcessing {
 
-  val logger = LogManager.getLogger(getClass)
+  val logger = LoggerFactory.getLogger(getClass)
 
   def doTokenize(text: String, toLowerCase: Boolean = true): List[String] = {
     val replaced = text.replaceAll("[^a-zA-Z]", " ")
