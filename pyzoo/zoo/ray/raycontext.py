1fcfe4d51b5678cd4ebea97cf1c6a2e36f60549c
Shaojun Liu <johnssalyn@outlook.com>
2022-12-20 14:25:56 +0800

    update redis_password to None

--- pyzoo/zoo/ray/raycontext.py
+++ pyzoo/zoo/ray/raycontext.py
@@ -262,7 +262,7 @@ class RayServiceFuncGenerator(object):
 class RayContext(object):
     _active_ray_context = None
 
-    def __init__(self, sc, redis_port=None, password="", object_store_memory=None,
+    def __init__(self, sc, redis_port=None, password=None, object_store_memory=None,
                  verbose=False, env=None, extra_params=None, include_webui=True,
                  num_ray_nodes=None, ray_node_cpu_cores=None):
         """
